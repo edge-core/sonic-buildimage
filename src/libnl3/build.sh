b4f27357858029c583ea95df6840c982216cba17
Shuotian Cheng <stcheng_89@hotmail.com>
2016-09-23 14:11:35 -0700

    src: Add libnl3 build.sh script (#11)

---
+++ src/libnl3/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## This script is to build the libnl3 3.2.27-1
+##
+## USAGE:
+##   ./build.sh
+
+# Obtaining the libnl3
+rm -rf ./libnl3
+git clone https://anonscm.debian.org/git/collab-maint/libnl3.git
+pushd ./libnl3
+git checkout -f b77c0e49cb
+
+# Patch
+export QUILT_PATCHES=debian/patches
+quilt push
+quilt push
+quilt push
+quilt push
+quilt push
+dpkg-buildpackage -rfakeroot -b -us -uc
+
+popd
