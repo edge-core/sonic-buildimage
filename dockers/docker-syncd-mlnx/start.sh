93852194eb9bcda6810ae556b5e757b32631371b
Shuotian Cheng <shuche@microsoft.com>
2016-10-19 14:39:44 -0700

    start.sh: Remove rsyslogd.pid file on mlnx syncd docker (#35)

--- dockers/docker-syncd-mlnx/start.sh
+++ dockers/docker-syncd-mlnx/start.sh
@@ -12,6 +12,9 @@ trap clean_up SIGTERM SIGKILL
 # occures
 . mlnx-fw-upgrade.sh
 
+# FIXME: the script cannot trap SIGTERM signal and it exits without clean_up
+# Remove rsyslogd.pid file manually so that to start the rsyslog instantly
+[ -e /var/run/rsyslogd.pid ] && rm /var/run/rsyslogd.pid
 service rsyslog start
 service syncd start
 
