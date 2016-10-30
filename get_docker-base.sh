076c847bf89bcf7eab7d3fd2ce5c754c89ed8f92
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-10-29 19:38:31 -0700

    Update get_docker-base.sh (#47)

--- get_docker-base.sh
+++ get_docker-base.sh
@@ -10,7 +10,7 @@ set -x -e
 . ./functions.sh
 
 ## [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Read-only link of Azure Blob storage with shared access signature (SAS)")]
-BASE_URL="https://sonicstorage.blob.core.windows.net/packages/docker-base.ea507753d98b0769e2a15be13003331f8ad38d1c15b40a683e05fc53b1463b10.gz?sv=2015-04-05&sr=b&sig=DjbdpLaslHeiaZLwFuauhPA2wR0v7kOH5yPHiAbzgBc%3D&se=2016-10-28T13%3A04%3A23Z&sp=r"
+BASE_URL="https://sonicstorage.blob.core.windows.net/packages/docker-base.ea507753d98b0769e2a15be13003331f8ad38d1c15b40a683e05fc53b1463b10.gz?sv=2015-04-05&sr=b&sig=YNN6eYVMEFndUaiHIRnqcZFdDZwIG%2BaAuVj0IoyDWPw%3D&se=2026-10-27T20%3A46%3A18Z&sp=r"
 
 base_image_name=docker-base
 docker_try_rmi $base_image_name
