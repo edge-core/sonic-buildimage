30438bb9877fb8f374203446fad62dad4630a88d
Pavel Shirshov <pavelsh@microsoft.com>
2016-10-31 12:25:09 -0700

    update port configurations in lldpd as soon as port created

---
+++ dockers/docker-lldp-sv2/reconfigure.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+num_of_interfaces=32
+if_step=4
+last_if_idx=$((num_of_interfaces*if_step - if_step))
+
+function wait_until_if_exists
+{
+  while /bin/true ;
+  do
+    if [ -L /sys/class/net/"$if" ];
+    then
+      break
+    fi
+    sleep 1
+  done
+  echo interface "$if" is up
+}
+
+
+function wait_until_if_not_exists
+{
+  while /bin/true ;
+  do
+    if [ ! -L /sys/class/net/"$if" ];
+    then
+      break
+    fi
+    sleep 1
+  done
+  echo interface "$if" is down
+}
+
+
+while /bin/true ;
+do
+  # wait until all interfaces are up
+  echo Wait until all ifaces are up
+  for i in $(seq 0 $if_step $last_if_idx)
+  do
+    if=Ethernet${i}
+    wait_until_if_exists
+  done
+
+  # apply lldpd configuration
+  echo apply lldpd configuration
+  lldpcli -c /etc/lldpd.conf
+
+  # wait until all interfaces are down
+  echo Wait until all ifaces are down
+  for i in $(seq 0 $if_step $last_if_idx)
+  do
+    if=Ethernet${i}
+    wait_until_if_not_exists
+  done
+done
