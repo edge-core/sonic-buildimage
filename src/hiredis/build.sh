923b13afb4894fc8ec65b465be8e55789aba8f19
Shuotian Cheng <shuche@microsoft.com>
2016-11-30 17:03:09 -0800

    src: Remove libjemalloc-dev from hiredis build.sh (#88)
    libjemalloc-dev is installed in sonic-slave

--- src/hiredis/build.sh
+++ src/hiredis/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 
-sudo apt-get install -y libjemalloc-dev
-
 # Install redis-server
 sudo dpkg -i redis/*.deb
 
--- src/redis/build.sh
+++ src/redis/build.sh
@@ -5,7 +5,6 @@ REDIS_VERION_FULL=$REDIS_VERION-1~bpo8+1
 
 wget -O redis_$REDIS_VERION.orig.tar.gz -N "https://sonicstorage.blob.core.windows.net/packages/redis_$REDIS_VERION.orig.tar.gz?sv=2015-04-05&sr=b&sig=B3qGEoSHe%2FBh5rVwvXHpKijgBtKF7dHeuJWp1p17UnU%3D&se=2026-11-26T22%3A31%3A31Z&sp=r"
 wget -O redis_$REDIS_VERION_FULL.dsc -N "https://sonicstorage.blob.core.windows.net/packages/redis_$REDIS_VERION_FULL.dsc?sv=2015-04-05&sr=b&sig=LoUtjLXa%2BCcoM%2BsPewRLkY7YPRvSJTbsvQoW%2BL%2B3QWM%3D&se=2026-11-26T22%3A32%3A11Z&sp=r"
-
 wget -O redis_$REDIS_VERION_FULL.debian.tar.xz -N "https://sonicstorage.blob.core.windows.net/packages/redis_$REDIS_VERION_FULL.debian.tar.xz?sv=2015-04-05&sr=b&sig=I33UsbDHiffEkQRndpFwY9y3I%2FrKTu0wmG%2FMXB98kys%3D&se=2026-11-26T22%3A32%3A34Z&sp=r"
 
 dpkg-source -x redis_$REDIS_VERION_FULL.dsc
