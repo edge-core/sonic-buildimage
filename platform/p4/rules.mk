44265fcffdf7a7bf76902a9e8c7c721f6e6020c7
Marian Pritsak <marianp@mellanox.com>
2016-12-08 17:07:05 +0200

    Add docker-p4 to all tagets for p4 platform (#107)
    Add dependency on p4 switch for libsairedis

--- platform/p4/rules.mk
+++ platform/p4/rules.mk
@@ -4,3 +4,8 @@ include $(PLATFORM_PATH)/p4-hlir.mk
 include $(PLATFORM_PATH)/p4c-bm.mk
 include $(PLATFORM_PATH)/tenjin.mk
 include $(PLATFORM_PATH)/docker-sonic-p4.mk
+
+SONIC_ALL += $(DOCKER_SONIC_P4)
+
+$(LIBSAIREDIS)_DEPENDS += $(P4_SWITCH)
+$(LIBSAIREDIS)_RDEPENDS += $(P4_SWITCH)
