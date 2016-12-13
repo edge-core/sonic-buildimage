1e3fdc45c4cd4b94493672f2f6b59d67959b238d
Marian Pritsak <marianp@mellanox.com>
2016-12-14 00:42:43 +0200

    Use same URL for mlnx FW as mlnx SDK does (#116)

--- platform/mellanox/fw.mk
+++ platform/mellanox/fw.mk
@@ -1,5 +1,5 @@
 # mellanox firmware
 
 MLNX_FW = fw-SPC.mfa
-$(MLNX_FW)_URL = https://github.com/Mellanox/SAI-Implementation/raw/sonic/sdk/fw-SPC.mfa
+$(MLNX_FW)_URL = $(MLNX_SDK_BASE_URL)/$(MLNX_FW)
 SONIC_ONLINE_FILES += $(MLNX_FW)
