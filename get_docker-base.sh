f20bba87a067844baab5c6e8fb160257af449ef2
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-12-15 17:19:11 -0800

    Refine get_docker-base: store the file in target directory (#129)

--- get_docker-base.sh
+++ get_docker-base.sh
@@ -9,9 +9,12 @@ set -x -e
 
 . ./functions.sh
 
+TARGET_PATH=$(sed -n 's/TARGET_PATH\s*=\s*//p' slave.mk)
+
 ## [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Read-only link of Azure Blob storage with shared access signature (SAS)")]
 BASE_URL="https://sonicstorage.blob.core.windows.net/packages/docker-base.ea507753d98b0769e2a15be13003331f8ad38d1c15b40a683e05fc53b1463b10.gz?sv=2015-04-05&sr=b&sig=YNN6eYVMEFndUaiHIRnqcZFdDZwIG%2BaAuVj0IoyDWPw%3D&se=2026-10-27T20%3A46%3A18Z&sp=r"
 
 base_image_name=docker-base
 docker_try_rmi $base_image_name
-curl "$BASE_URL" | docker load
+wget --no-use-server-timestamps -O $TARGET_PATH/$base_image_name.gz "$BASE_URL"
+docker load < $TARGET_PATH/$base_image_name.gz
