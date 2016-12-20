3c01b398f48274d8cc738fe71d72ab82f620b70d
lguohan <lguohan@gmail.com>
2016-12-20 13:29:00 -0800

    [docker]: add support for Force10-S6100 (#145)

--- dockers/docker-orchagent/start.sh
+++ dockers/docker-orchagent/start.sh
@@ -38,6 +38,9 @@ if [ "$onie_platform" == "x86_64-dell_s6000_s1220-r0" ]; then
     PORTSYNCD_ARGS+="-p /etc/ssw/Force10-S6000/port_config.ini"
     SWSSCONFIG_ARGS+="td2.32ports.qos.1.json td2.32ports.qos.2.json td2.32ports.qos.3.json td2.32ports.qos.4.json td2.32ports.qos.5.json td2.32ports.qos.6.json "
     SWSSCONFIG_ARGS+="td2.32ports.buffers.1.json td2.32ports.buffers.2.json td2.32ports.buffers.3.json "
+elif [ "$onie_platform" == "x86_64-dell_s6100_c2538-r0" ]; then
+    ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
+    PORTSYNCD_ARGS+="-p /etc/ssw/Force10-S6100/port_config.ini"
 elif [ "$aboot_platform" == "x86_64-arista_7050_qx32" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
     PORTSYNCD_ARGS+="-p /etc/ssw/Arista-7050-QX32/port_config.ini"
