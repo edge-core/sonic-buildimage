66e3392e8a75d5afbd1877f7851283a8855cceb0
lguohan <lguohan@gmail.com>
2016-12-29 00:15:31 -0800

    [linux-kernel]: update linux kernel to 3.16.36-1+deb8u2 (#168)

--- rules/linux-kernel.mk
+++ rules/linux-kernel.mk
@@ -4,6 +4,6 @@ KVERSION = 3.16.0-4-amd64
 
 export KVERSION
 
-LINUX_KERNEL = linux-image-3.16.0-4-amd64_3.16.7-ckt11-2+acs8u2_amd64.deb
+LINUX_KERNEL = linux-image-3.16.0-4-amd64_3.16.36-1+deb8u2_amd64.deb
 $(LINUX_KERNEL)_SRC_PATH = $(SRC_PATH)/sonic-linux-kernel
 SONIC_MAKE_DEBS += $(LINUX_KERNEL)
