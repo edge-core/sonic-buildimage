d1399e2380a52978ca77b263d5c394f9e520764b
lguohan <lguohan@gmail.com>
2017-01-24 19:18:42 -0800

    [docker]: Add ptf docker to generic target (#210)

--- platform/generic/rules.mk
+++ platform/generic/rules.mk
@@ -4,4 +4,5 @@ SONIC_ALL += $(DOCKER_DATABASE) \
          $(DOCKER_LLDP_SV2) \
          $(DOCKER_SNMP_SV2) \
          $(DOCKER_PLATFORM_MONITOR) \
-         $(DOCKER_DHCP_RELAY)
+         $(DOCKER_DHCP_RELAY) \
+         $(DOCKER_PTF)
