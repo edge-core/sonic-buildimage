4ce17b100fc4d9cf3beb671fb27c55d4d1756d90
Oleksandr Ivantsiv <oleksandri@mellanox.com>
2017-02-01 10:09:02 +0200

    Install orchagent and syncd containers to Broadcom and Cavium images (#227)

--- platform/broadcom/docker-orchagent-brcm.mk
+++ platform/broadcom/docker-orchagent-brcm.mk
@@ -5,3 +5,13 @@ $(DOCKER_ORCHAGENT_BRCM)_PATH = $(DOCKERS_PATH)/docker-orchagent
 $(DOCKER_ORCHAGENT_BRCM)_DEPENDS += $(SWSS) $(REDIS_TOOLS)
 $(DOCKER_ORCHAGENT_BRCM)_LOAD_DOCKERS += $(DOCKER_BASE)
 SONIC_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_BRCM)
+SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_BRCM)
+
+$(DOCKER_ORCHAGENT_BRCM)_CONTAINER_NAME = swss
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += --net=host --privileged -t
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += --volumes-from database
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/ssw/:/etc/ssw/:ro
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/network/interfaces:/etc/network/interfaces:ro
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/network/interfaces.d/:/etc/network/interfaces.d/:ro
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /host/machine.conf:/host/machine.conf
+$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
--- platform/cavium/docker-orchagent-cavm.mk
+++ platform/cavium/docker-orchagent-cavm.mk
@@ -5,3 +5,13 @@ $(DOCKER_ORCHAGENT_CAVM)_PATH = $(DOCKERS_PATH)/docker-orchagent
 $(DOCKER_ORCHAGENT_CAVM)_DEPENDS += $(SWSS) $(REDIS_TOOLS)
 $(DOCKER_ORCHAGENT_CAVM)_LOAD_DOCKERS += $(DOCKER_BASE)
 SONIC_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_CAVM)
+SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_CAVM)
+
+$(DOCKER_ORCHAGENT_CAVM)_CONTAINER_NAME = swss
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += --net=host --privileged -t
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += --volumes-from database
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += -v /etc/ssw/:/etc/ssw/:ro
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += -v /etc/network/interfaces:/etc/network/interfaces:ro
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += -v /etc/network/interfaces.d/:/etc/network/interfaces.d/:ro
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += -v /host/machine.conf:/host/machine.conf
+$(DOCKER_ORCHAGENT_CAVM)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
