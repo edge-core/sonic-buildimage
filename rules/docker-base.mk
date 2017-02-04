26f4c85825a25f6b3da5ab46ba711e4de7705b3a
antonpatenko <c_antonp@mellanox.com>
2017-02-04 07:38:32 +0200

    Add possibility to install debug packages during build. (#234)
    * Enabled common debug packages installation.
    
    * Update gitignore.
    
    * Make debug-packages installation disabled by default.

--- rules/docker-base.mk
+++ rules/docker-base.mk
@@ -3,4 +3,12 @@
 DOCKER_BASE = docker-base.gz
 $(DOCKER_BASE)_PATH = $(DOCKERS_PATH)/docker-base
 
-SONIC_SIMPLE_DOCKER_IMAGES += $(DOCKER_BASE)
+ifeq ($(SONIC_CONFIG_DEBUG),y)
+GDB = gdb
+VIM = vim
+OPENSSH = openssh-client
+SSHPASS = sshpass
+$(DOCKER_BASE)_DBG_PACKAGES += $(GDB) $(VIM) $(OPENSSH) $(SSHPASS)
+endif
+
+SONIC_DOCKER_IMAGES += $(DOCKER_BASE)
