877291ae91cfa94e1125826c00b86815920b4b79
Shuotian Cheng <shuche@microsoft.com>
2017-02-15 08:54:10 -0800

    [docker-teamd]: Automatically start the processes after host interfaces are created (#278)
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- dockers/docker-teamd/start.sh
+++ dockers/docker-teamd/start.sh
@@ -2,23 +2,35 @@
 
 TEAMD_CONF_PATH=/etc/teamd
 
+function start_app {
+    if [ -d $TEAMD_CONF_PATH ]; then
+        for f in $TEAMD_CONF_PATH/*; do
+            teamd -f $f -d
+        done
+    fi
+    teamsyncd &
+}
+
 function clean_up {
-	pkill -9 teamd
-	pkill -9 teamsyncd
-	service rsyslog stop
-	exit
+    pkill -9 teamd
+    pkill -9 teamsyncd
+    service rsyslog stop
+    exit
 }
 
 trap clean_up SIGTERM SIGKILL
 
 service rsyslog start
 
-if [ -d $TEAMD_CONF_PATH ]; then
-	for f in $TEAMD_CONF_PATH/*; do
-		teamd -f $f -d
-	done
-fi
-
-teamsyncd &
-
-read
+# Before teamd could automatically add newly created host interfaces into the
+# LAG, this workaround will wait until the host interfaces are created and then
+# the processes will be started.
+while true; do
+    # Check if front-panel ports are configured
+    result=`echo -en "SELECT 0\nHGETALL PORT_TABLE:ConfigDone" | redis-cli | sed -n 3p`
+    if [ "$result" != "0" ]; then
+        start_app
+        read
+    fi
+    sleep 1
+done
