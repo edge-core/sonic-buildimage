f57fd0bee8e9f3a40e984caabf96f38ffbb9c16a
Samuel Angebault <angebault.samuel@gmail.com>
2017-02-21 13:37:07 -0800

    [platform] Add support configurations files for DCS-7060CX-32S (#313)

--- dockers/docker-orchagent/start.sh
+++ dockers/docker-orchagent/start.sh
@@ -45,6 +45,8 @@ elif [ "$HWSKU" == "Arista-7050-QX32" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
     SWSSCONFIG_ARGS+="td2.32ports.qos.1.json td2.32ports.qos.2.json td2.32ports.qos.3.json td2.32ports.qos.4.json td2.32ports.qos.5.json td2.32ports.qos.6.json "
     SWSSCONFIG_ARGS+="td2.32ports.buffers.1.json td2.32ports.buffers.2.json td2.32ports.buffers.3.json "
+elif [ "$HWSKU" == "Arista-7060-CX32S" ]; then
+    ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 elif [ "$HWSKU" == "AS7512" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 fi
