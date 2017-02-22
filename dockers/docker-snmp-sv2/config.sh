95ad5c79fd7297205554a444c32d0c0daa9cae5b
Taoyu Li <taoyl@microsoft.com>
2017-02-22 14:28:37 -0800

    Fix the reference in docker-snmp-sv2 to deprecated alias_map.json (#320)

--- dockers/docker-snmp-sv2/config.sh
+++ dockers/docker-snmp-sv2/config.sh
@@ -8,8 +8,7 @@ mkdir -p /etc/snmp
 
 sonic-cfggen -m /etc/sonic/minigraph.xml -y /etc/sonic/snmp.yml -t /usr/share/sonic/templates/snmpd.conf.j2 >/etc/snmp/snmpd.conf
 
-hwsku=`sonic-cfggen -m /etc/sonic/minigraph.xml -v minigraph_hwsku`
-/bin/cp -rf /usr/share/sonic/$hwsku/alias_map.json /etc/snmp/
+sonic-cfggen -m /etc/sonic/minigraph.xml -s >/etc/snmp/alias_map.json
 
 mkdir -p /var/sonic
 echo "# Config files managed by sonic-config-engine" >/var/sonic/config_status
