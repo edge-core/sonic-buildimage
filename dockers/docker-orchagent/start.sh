2a2af6a44ee76387aa2afac725488ef4a6a5c236
Shuotian Cheng <shuche@microsoft.com>
2017-03-01 18:23:28 -0800

    [orchagent]: Update start.sh to combine td2 qos/buffers config (#344)
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- dockers/docker-orchagent/start.sh
+++ dockers/docker-orchagent/start.sh
@@ -37,16 +37,14 @@ SWSSCONFIG_ARGS="00-copp.config.json "
 
 if [ "$HWSKU" == "Force10-S6000" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
-    SWSSCONFIG_ARGS+="td2.32ports.qos.1.json td2.32ports.qos.2.json td2.32ports.qos.3.json td2.32ports.qos.4.json td2.32ports.qos.5.json td2.32ports.qos.6.json "
-    SWSSCONFIG_ARGS+="td2.32ports.buffers.1.json td2.32ports.buffers.2.json td2.32ports.buffers.3.json "
+    SWSSCONFIG_ARGS+="td2.32ports.qos.json td2.32ports.buffers.json "
 elif [ "$HWSKU" == "Force10-S6100" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 elif [ "$HWSKU" == "Force10-Z9100" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 elif [ "$HWSKU" == "Arista-7050-QX32" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
-    SWSSCONFIG_ARGS+="td2.32ports.qos.1.json td2.32ports.qos.2.json td2.32ports.qos.3.json td2.32ports.qos.4.json td2.32ports.qos.5.json td2.32ports.qos.6.json "
-    SWSSCONFIG_ARGS+="td2.32ports.buffers.1.json td2.32ports.buffers.2.json td2.32ports.buffers.3.json "
+    SWSSCONFIG_ARGS+="td2.32ports.qos.json td2.32ports.buffers.json "
 elif [ "$HWSKU" == "Arista-7060-CX32S" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 elif [ "$HWSKU" == "AS7512" ]; then
