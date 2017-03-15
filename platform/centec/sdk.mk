831b10ea26952478f8f8168d03adb9e5ab92cc83
Min Yao <yaom@centecnetworks.com>
2017-03-14 21:03:30 -0700

    [Centec] update centec gg sdk3.5 kernel module debian package (#393)

--- platform/centec/sdk.mk
+++ platform/centec/sdk.mk
@@ -1,4 +1,4 @@
-CENTEC_SDK_KERNEL = centec-gg-sdk3.5-modules-3.16.36-1-amd64.deb
-$(CENTEC_SDK_KERNEL)_URL = "https://github.com/CentecNetworks/goldengate-sai/raw/master/lib/centec-gg-sdk3.5-modules-3.16.36-1-amd64.deb"
+CENTEC_SDK_KERNEL = centec-gg-sdk3.5-modules-3.16.0-4-amd64.deb
+$(CENTEC_SDK_KERNEL)_URL = "https://github.com/CentecNetworks/goldengate-sai/raw/master/lib/centec-gg-sdk3.5-modules-3.16.0-4-amd64.deb"
 
 SONIC_ONLINE_DEBS += $(CENTEC_SDK_KERNEL)
