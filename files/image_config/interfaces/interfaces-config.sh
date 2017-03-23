5150ed55c7f3f0d137c96ab2f38b42de19738304
Shuotian Cheng <shuche@microsoft.com>
2017-03-23 16:30:18 -0700

    [interfaces]: Restart networking service after the file is updated (#430)
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 sonic-cfggen -m /etc/sonic/minigraph.xml -t /usr/share/sonic/templates/interfaces.j2 >/etc/network/interfaces
-ifdown eth0 && ifup eth0
-ifdown lo && ifup lo
-
+service networking restart
