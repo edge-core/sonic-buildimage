27dae9072620b4abbcc832a54c9a126bd8b410f5
Shuotian Cheng <shuche@microsoft.com>
2017-03-28 15:49:48 -0700

    [docker-teamd]: Clean /etc/teamd/ folder before adding new configurations (#451)
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- dockers/docker-teamd/config.sh
+++ dockers/docker-teamd/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p /etc/teamd
+rm -f /etc/teamd/*
 
 for pc in `sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_portchannels.keys() | join(' ')"`; do
   sonic-cfggen -m /etc/sonic/minigraph.xml -a '{"pc":"'$pc'"}' -t /usr/share/sonic/templates/teamd.j2 >/etc/teamd/$pc.conf
