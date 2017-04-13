abf54c22c4cc0cf561015b4b5318e99e1c6adf32
Shuotian Cheng <shuche@microsoft.com>
2017-04-13 15:40:05 -0700

    [Broadcom]: Update debian package versions (#504)
    - OpenNSL 3.2.2.2-2
    
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,7 +1,7 @@
-BRCM_OPENNSL = libopennsl_3.2.2.2-1-20170406004418.26_amd64.deb
-$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.2.2-1-20170406004418.26_amd64.deb?sv=2015-04-05&sr=b&sig=aWbjpqH2A5JewLoVs8o%2BkriOCRvqDwuVTZygwy%2B1XMQ%3D&se=2030-12-14T19%3A41%3A16Z&sp=r"
+BRCM_OPENNSL = libopennsl_3.2.2.2-2-20170413185612.27_amd64.deb
+$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.2.2-2-20170413185612.27_amd64.deb?sv=2015-04-05&sr=b&sig=xEbASK6Jiug8I%2BaJFphRnuH4cOEgxIyAkhj6rGKfsOE%3D&se=2030-12-21T22%3A05%3A31Z&sp=r"
 
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.2.2-1-20170406004418.26_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.2.2-1-20170406004418.26_amd64.deb?sv=2015-04-05&sr=b&sig=fL88BnMJ9uMMozh6xDqYn7oDc%2FwhX53sd3WG1v%2BkqDA%3D&se=2030-12-14T19%3A40%3A23Z&sp=r"
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.2.2-2-20170413185612.27_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.2.2-2-20170413185612.27_amd64.deb?sv=2015-04-05&sr=b&sig=i%2F4NXu0wzMSAQjDcQTg%2FtpULn5%2Fn%2FVgLu3Lg24QMQM0%3D&se=2030-12-21T22%3A06%3A49Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL) $(BRCM_OPENNSL_KERNEL)
