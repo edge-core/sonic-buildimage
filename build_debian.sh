fae53f7ea24651a63b8f27227b3551a5605169bc
lguohan <lguohan@gmail.com>
2017-04-16 01:17:22 -0700

    [build]: add commit id and build number in sonic_version (#508)

--- build_debian.sh
+++ build_debian.sh
@@ -264,7 +264,9 @@ build_version: $(sonic_get_version)
 debian_version: $(cat $FILESYSTEM_ROOT/etc/debian_version)
 kernel_version: $kversion
 asic_type: $sonic_asic_platform
+commit_id: $(git rev-parse --short HEAD)
 build_date: $(date -u)
+build_number: ${BUILD_NUMBER:-0}
 built_by: $USER@$BUILD_HOSTNAME
 EOF
 
