3223996404c33c9e5587106090b423a2e9092a0e
Shuotian Cheng <shuche@microsoft.com>
2017-04-26 06:12:23 -0700

    [Broadcom]: Update debian package versions (#538)
    - libsaibcm 2.1.5.1-3
    
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-2-20170419194756.21_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-2-20170419194756.21_amd64.deb?sv=2015-04-05&sr=b&sig=On9N1tlOSbYj%2Fb0JudmUjYRTbvS5cvqcGJUEIDX8wzk%3D&se=2030-12-27T20%3A56%3A24Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-3-20170426013947.22_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-3-20170426013947.22_amd64.deb?sv=2015-04-05&sr=b&sig=nhMTShwA1OeKoLNWcIY0wMyuiDSdKYWTH%2BJGlr%2BpgU4%3D&se=2031-01-03T01%3A43%3A35Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-2-20170419194756.21_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-3-20170426013947.22_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-2-20170419194756.21_amd64.deb?sv=2015-04-05&sr=b&sig=iVQRbQUTmeKqd01pMMR%2FIwlEUsAeiGIxCrqfr24lQ8k%3D&se=2030-12-27T20%3A57%3A08Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-3-20170426013947.22_amd64.deb?sv=2015-04-05&sr=b&sig=c4%2B6zRDuA%2BTeLr3MPkAqsuvG%2BAg9cd7ffgpp2mhl9bM%3D&se=2031-01-03T01%3A43%3A08Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
