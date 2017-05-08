97b4924476543800a449962486e72be260e368e6
Shuotian Cheng <shuche@microsoft.com>
2017-05-08 11:31:00 -0700

    [Broadcom]: Update broacom SDK to 3.2.2.2-5 and SAI to 2.1.5.1-5 (#570)
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-4~20170427032904.25_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-4~20170427032904.25_amd64.deb?sv=2015-04-05&sr=b&sig=NqmcwFHarhYwoV6WTou8wGbEMCoXZUdPXuegPUDpwU0%3D&se=2154-03-20T05%3A16%3A44Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-5-20170505222214.31_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-5-20170505222214.31_amd64.deb?sv=2015-04-05&sr=b&sig=jxKRlFJ%2BjDmq6Xi2p1SXc35IVdhsfRoR0umcOnM5Cj8%3D&se=2031-01-12T22%3A43%3A54Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-4~20170427032904.25_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-5-20170505222214.31_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-4~20170427032904.25_amd64.deb?sv=2015-04-05&sr=b&sig=8RErjP5TGdXqENcTbVrgQbvVAexMt4b%2BjU2BXFb4%2B0M%3D&se=2154-03-20T05%3A16%3A07Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-5-20170505222214.31_amd64.deb?sv=2015-04-05&sr=b&sig=HZN4itbQuJXfZCPZx1A%2BSxN3hBxZ05%2FmCE8Lp1zTgXM%3D&se=2031-01-12T22%3A44%3A20Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,7 +1,7 @@
-BRCM_OPENNSL = libopennsl_3.2.2.2-4-20170428181434.34_amd64.deb
-$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.2.2-4-20170428181434.34_amd64.deb?sv=2015-04-05&sr=b&sig=Izxx7%2ByNMXB11E3rdBuEQNOohGMoDKMCZIsVqfdCjSI%3D&se=2031-01-08T20%3A49%3A30Z&sp=r"
+BRCM_OPENNSL = libopennsl_3.2.2.2-5-20170505212020.37_amd64.deb
+$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.2.2-5-20170505212020.37_amd64.deb?sv=2015-04-05&sr=b&sig=wTWOEP8zJE1Jba3M5n8we4x%2Fcpq39%2B00ORByVHhBOuQ%3D&se=2031-01-12T22%3A42%3A51Z&sp=r"
 
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.2.2-4-20170428181434.34_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.2.2-4-20170428181434.34_amd64.deb?sv=2015-04-05&sr=b&sig=18MkouZ0zMum2CzLz7ut8nXmjn36nqCNg2go9ISCwKU%3D&se=2031-01-08T20%3A52%3A39Z&sp=r"
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.2.2-5-20170505212020.37_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.2.2-5-20170505212020.37_amd64.deb?sv=2015-04-05&sr=b&sig=QovFF5suqpKxHqZ01r0BiooKw7ik5Kpk6GU0pxf8xmg%3D&se=2031-01-12T22%3A43%3A24Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL) $(BRCM_OPENNSL_KERNEL)
