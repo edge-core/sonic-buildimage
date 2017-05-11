3af7c3a13274aa40348caaf341abb0c6b74ac233
pavel-shirshov <pavelsh@microsoft.com>
2017-05-10 17:39:51 -0700

    [debian]: Disable receiving default routes for ipv6 on mgmt interface (#588)
    * Disable net.ipv6.conf.eth0.accept_ra_defrtr.
    It will remain IPv6 working on mgmt interface, but it will disable receiving default routes

--- build_debian.sh
+++ build_debian.sh
@@ -237,6 +237,8 @@ set /files/etc/sysctl.conf/net.ipv6.conf.eth0.forwarding 0
 
 set /files/etc/sysctl.conf/net.ipv6.conf.default.accept_dad 0
 set /files/etc/sysctl.conf/net.ipv6.conf.all.accept_dad 0
+
+set /files/etc/sysctl.conf/net.ipv6.conf.eth0.accept_ra_defrtr 0
 " -r $FILESYSTEM_ROOT
 
 ## docker-py is needed by Ansible docker module
