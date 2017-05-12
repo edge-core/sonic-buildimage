973ee89b27cefae334c44bf93620e4030de70b3d
Shuotian Cheng <shuche@microsoft.com>
2017-05-12 12:08:09 -0700

    [Broadcom]: Update Broadcom SDK to 3.2.2.2-7 (#595)

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,7 +1,7 @@
-BRCM_OPENNSL = libopennsl_3.2.2.2-6-20170509231001.38_amd64.deb
-$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.2.2-6-20170509231001.38_amd64.deb?sv=2015-04-05&sr=b&sig=xawExeGCWMMToo%2Fsk%2BNFRHIXK1eJiqOir4lZ5FUvIdo%3D&se=2031-01-17T07%3A00%3A10Z&sp=r"
+BRCM_OPENNSL = libopennsl_3.2.2.2-7-20170512003351.39_amd64.deb
+$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.2.2-7-20170512003351.39_amd64.deb?sv=2015-04-05&sr=b&sig=YHETYSIxpB6LyTh2vhQ1TrGXommA2NiUdolk5QBJ0PE%3D&se=2031-01-19T01%3A40%3A27Z&sp=r"
 
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.2.2-6-20170509231001.38_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.2.2-6-20170509231001.38_amd64.deb?sv=2015-04-05&sr=b&sig=PPo0AKc%2FwnaAwNsA2O6e5erAZ4vso6KRXxgrNN%2FlJ3A%3D&se=2031-01-17T07%3A00%3A56Z&sp=r"
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.2.2-7-20170512003351.39_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.2.2-7-20170512003351.39_amd64.deb?sv=2015-04-05&sr=b&sig=33kv%2F2UogAuvHu3gPQ1rB7TVp9I%2Bjq%2FbmObzEZgfKD4%3D&se=2031-01-19T01%3A39%3A56Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL) $(BRCM_OPENNSL_KERNEL)
