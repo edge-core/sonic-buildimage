3e5adf018041e7398f06d4cf0dd681ea6a746c0f
Joe LeVeque <jleveque@users.noreply.github.com>
2017-05-18 20:07:31 -0700

    [sonic-utilities]: Bump version to 1.1 (#612)

--- rules/sonic-utilities.mk
+++ rules/sonic-utilities.mk
@@ -1,5 +1,5 @@
 # sonic utilities package
 
-SONIC_UTILS = python-sonic-utilities_1.0-1_all.deb
+SONIC_UTILS = python-sonic-utilities_1.1-1_all.deb
 $(SONIC_UTILS)_SRC_PATH = $(SRC_PATH)/sonic-utilities
 SONIC_PYTHON_STDEB_DEBS += $(SONIC_UTILS)
