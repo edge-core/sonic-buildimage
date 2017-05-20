faf33d80d33787cb830ee47f23cf905b45a9dbbd
Shuotian Cheng <shuche@microsoft.com>
2017-05-19 23:23:59 -0700

    [Broadcom]: Update Broadcom SAI to 2.1.5.1-7 (#616)
    * 20170519 (0843e2a858c2e730b25e7106c83a472e43cd527d)
      Fixed unknown neighbor processing and neighbor update scenarios.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-6-20170510001051.32_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-6-20170510001051.32_amd64.deb?sv=2015-04-05&sr=b&sig=PS9GQvhA2n%2BLXtu93KYKybvmmLyeIX%2BcrRZspIBvm5o%3D&se=2031-01-17T07%3A02%3A06Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-7-20170519212441.39_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-7-20170519212441.39_amd64.deb?sv=2015-04-05&sr=b&sig=CVc9ldYoy6xdUY0mKg4Rqz4uNDgkTUrPOw1ULrxa8N4%3D&se=2031-01-26T21%3A38%3A42Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-6-20170510001051.32_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-7-20170519212441.39_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-6-20170510001051.32_amd64.deb?sv=2015-04-05&sr=b&sig=d2%2BYxTvzjR92hDXx5Wx7gWDQU52wpLzw4ECxNiAwExM%3D&se=2031-01-17T07%3A01%3A37Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-7-20170519212441.39_amd64.deb?sv=2015-04-05&sr=b&sig=xzCr9xLGF3IaNK8aQGVeU4jY7YkqNEdYAuq5IP2uQM4%3D&se=2031-01-26T21%3A38%3A16Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
