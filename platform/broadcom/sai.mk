a5c0dff2096a03f489bca698486dae506c2e9fda
Shuotian Cheng <shuche@microsoft.com>
2017-05-31 10:57:42 -0700

    [Broadcom]: Update SAI package to 2.1.5.1-8 (#649)
    Add config_checker/permitted_list
    Please check etc/bcm/ folder to check the README.msft file!

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-7-20170519212441.39_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-7-20170519212441.39_amd64.deb?sv=2015-04-05&sr=b&sig=CVc9ldYoy6xdUY0mKg4Rqz4uNDgkTUrPOw1ULrxa8N4%3D&se=2031-01-26T21%3A38%3A42Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-8-20170531021119.40_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-8-20170531021119.40_amd64.deb?sv=2015-04-05&sr=b&sig=rFP9dZ9aXpe3CN5jBVNo6rkgK94%2BJFzMiXd1I2954lU%3D&se=2031-02-07T02%3A13%3A20Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-7-20170519212441.39_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-8-20170531021119.40_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-7-20170519212441.39_amd64.deb?sv=2015-04-05&sr=b&sig=xzCr9xLGF3IaNK8aQGVeU4jY7YkqNEdYAuq5IP2uQM4%3D&se=2031-01-26T21%3A38%3A16Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-8-20170531021119.40_amd64.deb?sv=2015-04-05&sr=b&sig=m5AGCyfLkcYvv8xmY6gPZFiXmygYLQUGfFrEiMO8Jtk%3D&se=2031-02-07T02%3A13%3A55Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
