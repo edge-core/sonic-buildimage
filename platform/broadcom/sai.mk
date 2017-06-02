984e6c4bc9c9b357625183477e5c2bd79eb98ca7
Shuotian Cheng <shuche@microsoft.com>
2017-06-01 17:51:12 -0700

    [Broadcom]: Update SAI package (#659)
    Add Ingrasys config.bcm files

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-8-20170531021119.40_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-8-20170531021119.40_amd64.deb?sv=2015-04-05&sr=b&sig=rFP9dZ9aXpe3CN5jBVNo6rkgK94%2BJFzMiXd1I2954lU%3D&se=2031-02-07T02%3A13%3A20Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-9-20170531184746.41_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-9-20170531184746.41_amd64.deb?sv=2015-04-05&sr=b&sig=VD4QFYR4J5nJPdS6ZHyfKbdroTILV%2Fz6s%2F251NWpyaE%3D&se=2031-02-08T18%3A26%3A35Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-8-20170531021119.40_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-9-20170531184746.41_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-8-20170531021119.40_amd64.deb?sv=2015-04-05&sr=b&sig=m5AGCyfLkcYvv8xmY6gPZFiXmygYLQUGfFrEiMO8Jtk%3D&se=2031-02-07T02%3A13%3A55Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-9-20170531184746.41_amd64.deb?sv=2015-04-05&sr=b&sig=baUDbmm7PzF7Y7Y8Lz%2B759Xl1vhOkcnPBcqd6ZWWwSA%3D&se=2031-02-08T18%3A26%3A58Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
