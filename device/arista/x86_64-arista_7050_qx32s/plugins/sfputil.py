7d33387e7c96bc2e21011f2def25f3968462cbc7
Samuel Angebault <angebault.samuel@gmail.com>
2017-06-02 01:31:53 -0700

    [platform] Complete support for Arista-7050QX-32S (#661)
    * Bump sonic-platform-modules-arista submodule
    
    * Use sonic_sfputil plugin from the arista library
    
    * Fix undefined variable varlog_size
    
    * Prevent minigraph.xml to be removed from the flash
    
    * Update DCS-7050QX-32 sensors config

--- device/arista/x86_64-arista_7050_qx32s/plugins/sfputil.py
+++ device/arista/x86_64-arista_7050_qx32s/plugins/sfputil.py
@@ -1,30 +1,9 @@
 #!/usr/bin/env python
 
 try:
-    from sonic_sfp.sfputilbase import sfputilbase
+    import arista.utils.sonic_sfputil as arista_sfputil
 except ImportError, e:
     raise ImportError (str(e) + "- required module not found")
 
 
-class sfputil(sfputilbase):
-    """Platform specific sfputil class"""
-
-    port_start = 0
-    port_end = 31
-    ports_in_block = 32
-
-    eeprom_offset = 10
-
-    port_to_eeprom_mapping = {}
-
-    _qsfp_ports = range(0, ports_in_block + 1)
-
-    # also has sfp[1-4] ports handled by a mux to choose against qsfp5
-    # i2c-40 to i2c-43
-
-    def __init__(self, port_num):
-        # Override port_to_eeprom_mapping for class initialization
-        eeprom_path = '/sys/class/i2c-adapter/i2c-{0}/{0}-0050/eeprom'
-        for x in range(0, self.port_end + 1):
-            self.port_to_eeprom_mapping[x] = eeprom_path.format(x + self.eeprom_offset)
-        sfputilbase.__init__(self, port_num)
+sfputil = arista_sfputil.getSfpUtil()
--- device/arista/x86_64-arista_7060_cx32s/plugins/sfputil.py
+++ device/arista/x86_64-arista_7060_cx32s/plugins/sfputil.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python
 
 try:
-    from sonic_sfp.sfputilbase import sfputilbase
+    import arista.utils.sonic_sfputil as arista_sfputil
 except ImportError, e:
     raise ImportError (str(e) + "- required module not found")
 
 
-class sfputil(sfputilbase):
-    """Platform specific sfputil class"""
-
-    port_start = 0
-    port_end = 33
-    ports_in_block = 34
-
-    eeprom_offset = 18
-
-    port_to_eeprom_mapping = {}
-
-    _qsfp_ports = range(0, 32 + 1)
-    _sfp_ports = range(33, 34 + 1)
-
-    def __init__(self, port_num):
-        # Override port_to_eeprom_mapping for class initialization
-        eeprom_path = '/sys/class/i2c-adapter/i2c-{0}/{0}-0050/eeprom'
-        for x in range(0, self.port_end + 1):
-            self.port_to_eeprom_mapping[x] = eeprom_path.format(x + self.eeprom_offset)
-        sfputilbase.__init__(self, port_num)
+sfputil = arista_sfputil.getSfpUtil()
