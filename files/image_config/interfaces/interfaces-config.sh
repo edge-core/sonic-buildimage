ea4dfa48c9d9a34e38bcf97bc0ad8e1c4415cedc
lguohan <lguohan@gmail.com>
2017-06-07 10:37:49 -0700

    Revert "[baseos] Avoid disconnecting mgmt when re-cfg interfaces" (#681)
    This reverts commit e74196cd75ed1c8bb2556c2ee0199d03bad98dc2.

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-ifdown -a -X eth0
 sonic-cfggen -m /etc/sonic/minigraph.xml -t /usr/share/sonic/templates/interfaces.j2 >/etc/network/interfaces
-ifup -a -X eth0
-
-NEW_MGMT_IP=`sonic-cfggen -m /etc/sonic/minigraph.xml  -v 'minigraph_mgmt_interface["addr"]'`
-NEW_MGMT_MASK=`sonic-cfggen -m /etc/sonic/minigraph.xml  -v 'minigraph_mgmt_interface["mask"]'`
-
-ifconfig eth0 $NEW_MGMT_IP netmask $NEW_MGMT_MASK
-[ -f /var/run/dhclient.eth0.pid ] && kill `cat /var/run/dhclient.eth0.pid` && rm -f /var/run/dhclient.eth0.pid
-
+service networking restart
+ifdown lo && ifup lo
