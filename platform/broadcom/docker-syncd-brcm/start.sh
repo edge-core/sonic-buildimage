c2c232a1619845108985292ee854bb9e6a954ed5
Joe LeVeque <jleveque@users.noreply.github.com>
2017-06-20 16:37:07 -0700

    Add Broadcom LED microprocessor initialization for Dell S6000 (#726)
    * Add Broadcom LED microprocessor initialization for Dell S6000
    
    * Increase bcmcmd timeout to 60 seconds

--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+PLATFORM_DIR=/usr/share/sonic/platform
+
 rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
 
 supervisorctl start syncd
+
+# If this platform has an initialization file for the Broadcom LED microprocessor, load it
+if [ -r ${PLATFORM_DIR}/led_proc_init.soc ]; then
+    /usr/bin/bcmcmd -t 60 "rcload ${PLATFORM_DIR}/led_proc_init.soc"
+fi
 
