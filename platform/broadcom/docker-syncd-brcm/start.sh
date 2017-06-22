629362cad7b080fe738a4aafab6852a58e50160d
Joe LeVeque <jleveque@users.noreply.github.com>
2017-06-21 18:53:28 -0700

    [docker-syncd-brcm]: Sleep to allow syncd to create socket before calling bcmcmd (#733)
    * [docker-syncd-brcm]: Sleep to allow syncd to create socket before calling bcmcmd
    
    * Instead of fixed sleep interval, continually check for presence of socket

--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -10,6 +10,14 @@ supervisorctl start syncd
 
 # If this platform has an initialization file for the Broadcom LED microprocessor, load it
 if [ -r ${PLATFORM_DIR}/led_proc_init.soc ]; then
+    # Wait until syncd has created the socket for bcmcmd to connect to
+    while true; do
+        if [ -e /var/run/sswsyncd/sswsyncd.socket ]; then
+            break
+        fi
+        sleep 1
+    done
+
     /usr/bin/bcmcmd -t 60 "rcload ${PLATFORM_DIR}/led_proc_init.soc"
 fi
 
