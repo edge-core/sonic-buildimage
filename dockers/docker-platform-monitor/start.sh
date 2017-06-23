22819d9983cdad5d331422c6dd12e151fe2b14ad
Joe LeVeque <jleveque@users.noreply.github.com>
2017-06-23 15:23:00 -0700

    [docker-platform-monitor]: Add fancontrol (#735)

--- dockers/docker-platform-monitor/start.sh
+++ dockers/docker-platform-monitor/start.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-mkdir -p /etc/sensors.d
-if [ -e /usr/share/sonic/platform/sensors.conf ]; then
-    /bin/cp -rf /usr/share/sonic/platform/sensors.conf /etc/sensors.d/
-fi
-
 mkdir -p /var/sonic
 echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
 
 rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
-supervisorctl start lm-sensors
+
+# If this platform has an lm-sensors config file, copy it to it's proper place
+# and start lm-sensors
+if [ -e /usr/share/sonic/platform/sensors.conf ]; then
+    mkdir -p /etc/sensors.d
+    /bin/cp -f /usr/share/sonic/platform/sensors.conf /etc/sensors.d/
+    supervisorctl start lm-sensors
+fi
+
+# If this platform has a fancontrol config file, copy it to it's proper place
+# and start fancontrol
+if [ -e /usr/share/sonic/platform/fancontrol ]; then
+    /bin/cp -f /usr/share/sonic/platform/fancontrol /etc/
+    supervisorctl start fancontrol
+fi
+
 supervisorctl start ledd
 
