017eea8a874cee17b1eb08d9b46e7235b96cfcc1
Joe LeVeque <jleveque@users.noreply.github.com>
2017-06-24 12:05:04 -0700

    [DHCP Relay]: Add support for custom Option 82 circuit_id of the form '<hostname>:<portname>' (#747)
    * Add docker-dhcp-relay/Dockerfile to .gitignore
    
    * Add isc-dhcp-relay .deb package to image build process, along with my Option 82 patch
    
    * Install custom isc-dhcp-relay in dhcp_relay docker
    
    * Install isc-dhcp-relay build dependencies in sonic-slave Docker container
    
    * Copy the built .deb package to the destination directory
    
    * Add dependencies for isc-dhcp-relay
    
    * Change Option 82 string to '<hostname>:<portname>'
    
    * Install dependencies of .deb files implicitly in Dockerfile
    
    * Remove unused line
    
    * Remove unnecessary space

---
+++ rules/isc-dhcp.mk
@@ -0,0 +1,13 @@
+# isc-dhcp packages
+
+ISC_DHCP_VERSION = 4.3.1-6
+
+export ISC_DHCP_VERSION
+
+ISC_DHCP_COMMON = isc-dhcp-common_$(ISC_DHCP_VERSION)_amd64.deb
+$(ISC_DHCP_COMMON)_SRC_PATH = $(SRC_PATH)/isc-dhcp
+SONIC_MAKE_DEBS += $(ISC_DHCP_COMMON)
+
+ISC_DHCP_RELAY = isc-dhcp-relay_$(ISC_DHCP_VERSION)_amd64.deb
+$(eval $(call add_derived_package,$(ISC_DHCP_COMMON),$(ISC_DHCP_RELAY)))
+
