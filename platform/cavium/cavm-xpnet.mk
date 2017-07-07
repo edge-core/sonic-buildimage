18627bcf3695004064c00894d2e558bdc4daff46
Nadiya <Nadiya.Stetskovych@cavium.com>
2017-07-07 20:24:20 +0300

    Update cavium xpnet driver (#792)
    Signed-off-by: Nadiya.Stetskovych <Nadiya.Stetskovych@cavium.com>

--- platform/cavium/cavm-xpnet.mk
+++ platform/cavium/cavm-xpnet.mk
@@ -1,4 +1,4 @@
-CAVM_SAI_URL = https://github.com/XPliant/OpenXPS/raw/18cd5fb556b6262943c0af70375e2631ac3fe08d/SAI
+CAVM_SAI_URL = https://github.com/XPliant/OpenXPS/raw/092461a1cf57a11132fbf8e74fa79bab3ab00f2a/SAI
 
 CAVM_XPNET_DEB = xp80-Pcie-Endpoint.deb
 $(CAVM_XPNET_DEB)_URL = $(CAVM_SAI_URL)/netdev/$(CAVM_XPNET_DEB)
