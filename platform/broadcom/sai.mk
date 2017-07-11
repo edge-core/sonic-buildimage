ecd0b29533c03ff5f8ae6af1076deda1b344e43b
Shuotian Cheng <shuche@microsoft.com>
2017-07-11 13:01:45 -0700

    [Broadcom]: Update SAI to 2.1.5.1-15 (#799)
    Add Arista-7050-Q16S64 config.bcm file

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-14-20170627090913.47_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-14-20170627090913.47_amd64.deb?sv=2015-04-05&sr=b&sig=MqrcsnIaaFfekaAqcjgbi0mDl94BJ9eRsslLJrp23q8%3D&se=2031-03-06T22%3A29%3A19Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-15-20170711181855.48_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-15-20170711181855.48_amd64.deb?sv=2015-04-05&sr=b&sig=shAVuRyM%2ByENjVs9QSEDt%2FALvITRS5GoTvC69MJ4G8M%3D&se=2031-03-20T18%3A21%3A43Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-14-20170627090913.47_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-15-20170711181855.48_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-14-20170627090913.47_amd64.deb?sv=2015-04-05&sr=b&sig=GyAwaEdgSFGXbbFq%2FN2RgSKQ5%2Fc73NgxKMMKq3RVZw0%3D&se=2031-03-06T22%3A29%3A48Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-15-20170711181855.48_amd64.deb?sv=2015-04-05&sr=b&sig=oksLplSCyP58%2BYap4JWi%2FgNz2%2BeJtVDbLui1IUDZSbw%3D&se=2031-03-20T18%3A21%3A16Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
