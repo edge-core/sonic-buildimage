2090cd341ff48f202205d4c12d5c753195e74995
Shuotian Cheng <shuche@microsoft.com>
2017-07-12 18:15:35 -0700

    [Broadcom]: Update SAI to 2.1.5.1-16 (#803)
    Add Inventec-D7032Q28B-C32 config.bcm file

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_2.1.5.1-15-20170711181855.48_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-15-20170711181855.48_amd64.deb?sv=2015-04-05&sr=b&sig=shAVuRyM%2ByENjVs9QSEDt%2FALvITRS5GoTvC69MJ4G8M%3D&se=2031-03-20T18%3A21%3A43Z&sp=r"
+BRCM_SAI = libsaibcm_2.1.5.1-16-20170712202323.49_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.1.5.1-16-20170712202323.49_amd64.deb?sv=2015-04-05&sr=b&sig=jsPXiAoSyKqZ1SmiyeEj73W8tRlri8ysExnWvc%2BWSi4%3D&se=2031-03-21T22%3A49%3A32Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-15-20170711181855.48_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_2.1.5.1-16-20170712202323.49_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-15-20170711181855.48_amd64.deb?sv=2015-04-05&sr=b&sig=oksLplSCyP58%2BYap4JWi%2FgNz2%2BeJtVDbLui1IUDZSbw%3D&se=2031-03-20T18%3A21%3A16Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.1.5.1-16-20170712202323.49_amd64.deb?sv=2015-04-05&sr=b&sig=azYZkCi%2FFGS4eELKhIozOok3qimfH%2FjdXlz%2BS2MRBco%3D&se=2031-03-21T22%3A49%3A57Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
