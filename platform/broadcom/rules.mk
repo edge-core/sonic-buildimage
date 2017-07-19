12e5e22572ab143848422963f6bee255db186806
celestica-abhisit <asang@celestica.com>
2017-07-20 00:29:46 +0700

    [platform/Seastone] Add a new supported platform, Celestica Seastone-DX010 (#816)
    * [platform/Seastone] Add a new supported platform, Celestica Seastone-DX010.
    
    Switch Vendor: Celestica
    Switch SKU : Seastone-DX010
    ASIC Vendor: Broadcom
    Swich ASIC: Tomahawk
    Port Configuration: 32x100G, 64x50G and 96x10G+16x50G.
    SONiC Image: SONiC-ONIE-Broadcom
    
    Signed-off-by: asang@celestica.com
    
    * Help to convert tab to white space for Inventec-D7032Q28B platform.

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -6,6 +6,7 @@ include $(PLATFORM_PATH)/platform-modules-arista.mk
 include $(PLATFORM_PATH)/platform-modules-ingrasys.mk
 include $(PLATFORM_PATH)/platform-modules-accton.mk
 include $(PLATFORM_PATH)/platform-modules-inventec.mk
+include $(PLATFORM_PATH)/platform-modules-cel.mk
 include $(PLATFORM_PATH)/docker-orchagent-brcm.mk
 include $(PLATFORM_PATH)/docker-syncd-brcm.mk
 include $(PLATFORM_PATH)/docker-syncd-brcm-rpc.mk
