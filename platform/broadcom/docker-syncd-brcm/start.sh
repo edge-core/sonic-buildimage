96636e2124c55700f7542d3f3581ab4b28242770
Polly Hsu <polly_hsu@accton.com>
2017-08-09 12:19:50 +0800

    [device/accton]: Update Accton-AS7712-32X (#776)
    * [device/accton]: Update Accton-AS7712-32X
    
    * Add Broadcom LED microprocessor / pre-emphasis initialization
    
    Signed-off-by: polly_hsu@accton.com
    
    * Update start.sh
    
    * Update start.sh
    
    * [device/accton]: Update Accton-AS7712-32X
    
    * Add Broadcom LED microprocessor (Remove pre-emphasis initialization)
    
    Signed-off-by: polly_hsu@accton.com

--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -8,15 +8,18 @@ supervisorctl start rsyslogd
 
 supervisorctl start syncd
 
-# If this platform has an initialization file for the Broadcom LED microprocessor, load it
-if [ -r ${PLATFORM_DIR}/led_proc_init.soc ]; then
-    # Wait until syncd has created the socket for bcmcmd to connect to
+# Function: wait until syncd has created the socket for bcmcmd to connect to
+wait_syncd() {
     while true; do
         if [ -e /var/run/sswsyncd/sswsyncd.socket ]; then
             break
         fi
         sleep 1
     done
+}
 
+# If this platform has an initialization file for the Broadcom LED microprocessor, load it
+if [ -r ${PLATFORM_DIR}/led_proc_init.soc ]; then
+    wait_syncd
     /usr/bin/bcmcmd -t 60 "rcload ${PLATFORM_DIR}/led_proc_init.soc"
 fi
