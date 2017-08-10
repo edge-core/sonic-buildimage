5cf37cbcb39ea1eda5b034f98f20b9247ce9f9c6
Shuotian Cheng <shuche@microsoft.com>
2017-08-10 16:48:45 -0700

    [Broadcom]: Update Broadcom SAI/SDK version (#883)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,11 +1,11 @@
-BRCM_SAI = libsaibcm_3.0.2.2_amd64.deb
+BRCM_SAI = libsaibcm_3.0.2.3_amd64.deb
 # TODO: upload new SAI build to blob
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.2.2_amd64.deb"
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.2.3_amd64.deb"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.2.2_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.2.3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 # TODO: upload new SAI build to blob
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.2.2_amd64.deb"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.2.3_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,9 +1,9 @@
-BRCM_OPENNSL = libopennsl_3.2.3.1_amd64.deb
+BRCM_OPENNSL = libopennsl_3.2.3.3_amd64.deb
 # TODO: upload new SDK build to blob
-$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.3.1+0~20170605184634.7~1.gbp4a0717_amd64.deb"
+$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.3.3_amd64.deb"
 
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.1_amd64.deb
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb
 # TODO: upload new SDK build to blob
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.1+0~20170605184634.7~1.gbp4a0717_amd64.deb"
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL) $(BRCM_OPENNSL_KERNEL)
