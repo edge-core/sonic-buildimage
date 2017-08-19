9c5988fa7ce330dae15122e5e973a76c774e82d7
Vitaly.Vovnoboy <vitalyv@marvell.com>
2017-08-19 11:57:36 +0300

    Add Marvell platform (#765)
    * Adding Marvell platform.

---
+++ platform/marvell/docker-syncd-mrvl.mk
@@ -0,0 +1,15 @@
+# docker image for mrvl syncd
+
+DOCKER_SYNCD_MRVL = docker-syncd-mrvl.gz
+$(DOCKER_SYNCD_MRVL)_PATH = $(PLATFORM_PATH)/docker-syncd-mrvl
+$(DOCKER_SYNCD_MRVL)_DEPENDS += $(SYNCD) $(MRVL_FPA) $(REDIS_TOOLS)
+$(DOCKER_SYNCD_MRVL)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
+SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_MRVL)
+ifneq ($(ENABLE_SYNCD_RPC),y)
+SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_SYNCD_MRVL)
+endif
+
+$(DOCKER_SYNCD_MRVL)_CONTAINER_NAME = syncd
+$(DOCKER_SYNCD_MRVL)_RUN_OPT += --net=host --privileged -t
+$(DOCKER_SYNCD_MRVL)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
+$(DOCKER_SYNCD_MRVL)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
