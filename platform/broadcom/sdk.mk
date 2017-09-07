38cd1d5368f626243d3455c102b07db9289dfd46
Shuotian Cheng <shuche@microsoft.com>
2017-09-06 20:08:25 -0700

    [Broadcom]: Remove BRCM_OPENNSL library and upgrade BRCM_SAI to 3.0.3.2 (#938)
    This library will be archived in to BRCM_SAI library

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,9 +1,5 @@
-BRCM_OPENNSL = libopennsl_3.2.3.3_amd64.deb
-# TODO: upload new SDK build to blob
-$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.3.3_amd64.deb"
-
 BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb
 # TODO: upload new SDK build to blob
 $(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb"
 
-SONIC_ONLINE_DEBS += $(BRCM_OPENNSL) $(BRCM_OPENNSL_KERNEL)
+SONIC_ONLINE_DEBS += $(BRCM_OPENNSL_KERNEL)
