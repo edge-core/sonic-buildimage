e4cae4e8897b7f613365d9242df17f30a742dc78
cytsai0409 <cytsai0409@gmail.com>
2017-09-14 10:18:26 +0800

    [Ingrasys] update port_config.ini and sfputil for ingrasys platforms (#952)
    * [Ingrasys] update port_config.ini and sfputil for ingrasys platforms
    
    - What I did
    
    Update port_config.ini on S8810-32Q/S8900-54XC/S8900-64XC/S9100-C32 platforms
    
    - How I did it
    
    Update alias field and add port field in port_config.ini
    
    - How to verify it
    
    Check with the "sfputil show eeprom" command. If no error occurs, it is passed.
    
    - Description for the changelog
    
    Update port_config.ini on S8810-32Q/S8900-54XC/S8900-64XC/S9100-C32 platforms
    
    * [Ingrasys] remove debug message in sfputil.py on S8810-32Q
    
    - What I did
    
    remove debug message in sfputil.py on S8810-32Q
    
    - How I did it
    
    remove print in get_low_power_mode() and set_low_power_mode()
    
    - How to verify it
    
    There should be no debug message when executing "sfputil show lpmode"
    and "sfputil lpmode on Ethernet0"command.
    
    - Description for the changelog
    
    remove debug message in sfputil.py on S8810-32Q
    
    - A picture of a cute animal (not mandatory but encouraged)

--- device/ingrasys/x86_64-ingrasys_s8810_32q-r0/plugins/sfputil.py
+++ device/ingrasys/x86_64-ingrasys_s8810_32q-r0/plugins/sfputil.py
@@ -1,61 +1,147 @@
-#!/usr/bin/env python
+# sfputil.py
+#
+# Platform-specific SFP transceiver interface for SONiC
+#
 
 try:
-    from sonic_sfp.sfputilbase import sfputilbase
-except ImportError, e:
-    raise ImportError (str(e) + "- required module not found")
+    import time
+    from sonic_sfp.sfputilbase import SfpUtilBase
+except ImportError as e:
+    raise ImportError("%s - required module not found" % str(e))
 
 
-class sfputil(sfputilbase):
-    """Platform specific sfputil class"""
+class SfpUtil(SfpUtilBase):
+    """Platform-specific SfpUtil class"""
 
-    port_start = 0
-    port_end = 31
-    ports_in_block = 32
+    PORT_START = 0
+    PORT_END = 31
+    PORTS_IN_BLOCK = 32
 
-    port_to_eeprom_mapping = {}
-    #FIXME
-    port_to_i2c_mapping = {
-        0: 18,
-        1: 19,
-        2: 20,
-        3: 21,
-        4: 22,
-        5: 23,
-        6: 24,
-        7: 25,
-        8: 26,
-        9: 27,
-        10: 28,
-        11: 29,
-        12: 30,
-        13: 31,
-        14: 32,
-        15: 33,
-        16: 34,
-        17: 35,
-        18: 36,
-        19: 37,
-        20: 38,
-        21: 39,
-        22: 40,
-        23: 41,
-        24: 42,
-        25: 43,
-        26: 44,
-        27: 45,
-        28: 46,
-        29: 47,
-        30: 48,
-        31: 49            
-    }
+    EEPROM_OFFSET = 18
 
-    _qsfp_ports = range(0, ports_in_block + 1)
+    ABS_GPIO_BASE = 224
+    #INT_GPIO_BASE = 192
+    LP_GPIO_BASE = 160
+    RST_GPIO_BASE = 128
+    
+    BASE_DIR_PATH = "/sys/class/gpio/gpio{0}/direction"
+    BASE_VAL_PATH = "/sys/class/gpio/gpio{0}/value"
 
-    def __init__(self, port_num):
+    _port_to_eeprom_mapping = {}
+
+    @property
+    def port_start(self):
+        return self.PORT_START
+
+    @property
+    def port_end(self):
+        return self.PORT_END
+
+    @property
+    def qsfp_ports(self):
+        return range(0, self.PORTS_IN_BLOCK + 1)
+
+    @property
+    def port_to_eeprom_mapping(self):
+        return self._port_to_eeprom_mapping
+
+    def __init__(self):
         # Override port_to_eeprom_mapping for class initialization
-        eeprom_path = '/sys/class/i2c-adapter/i2c-{0}/{0}-0050/eeprom'
+        eeprom_path = "/sys/class/i2c-adapter/i2c-{0}/{0}-0050/eeprom"
+
         for x in range(self.port_start, self.port_end + 1):
-            port_eeprom_path = eeprom_path.format(self.port_to_i2c_mapping[x])
-            self.port_to_eeprom_mapping[x] = port_eeprom_path
-        sfputilbase.__init__(self, port_num)
+            self._port_to_eeprom_mapping[x] = eeprom_path.format(x + self.EEPROM_OFFSET)
+
+        SfpUtilBase.__init__(self)
+
+    def get_presence(self, port_num):
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
+
+        try:
+            abs_device_file = self.BASE_VAL_PATH.format(
+                    port_num + self.ABS_GPIO_BASE)
+            val_file = open(abs_device_file)
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        content = val_file.readline().rstrip()
+
+        # content is a string, either "0" or "1"
+        if content == "1":
+            return True
+
+        return False
+
+    def get_low_power_mode(self, port_num):
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
+
+        try:
+            lpmode_val_device_file = self.BASE_VAL_PATH.format(
+                    port_num + self.LP_GPIO_BASE)
+            val_file = open(lpmode_val_device_file)
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        content = val_file.readline().rstrip()
+        
+        # content is a string, either "0" or "1"
+        if content == "1":
+            return True
+
+        return False
+
+    def set_low_power_mode(self, port_num, lpmode):
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
+
+        try:
+            lpmode_val_device_file = self.BASE_VAL_PATH.format(
+                    port_num + self.LP_GPIO_BASE)
+            val_file = open(lpmode_val_device_file, "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        val_file.write("1" if lpmode is True else "0")
+        val_file.close()
+
+        return True
+
+    def reset(self, port_num):
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
+        
+        try:
+            reset_val_device_file = self.BASE_VAL_PATH.format(
+                    port_num + self.RST_GPIO_BASE)
+            val_file = open(reset_val_device_file, "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        val_file.write("1")
+        val_file.close()
+        
+        # Sleep 1 second to allow it to settle
+        time.sleep(1)
+        
+        try:
+            reset_val_device_file = self.BASE_VAL_PATH.format(
+                    port_num + self.RST_GPIO_BASE)
+            val_file = open(reset_val_device_file, "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        val_file.write("0")
+        val_file.close()
+
+        return True
--- device/ingrasys/x86_64-ingrasys_s8900_54xc-r0/plugins/sfputil.py
+++ device/ingrasys/x86_64-ingrasys_s8900_54xc-r0/plugins/sfputil.py
@@ -1,19 +1,27 @@
-#!/usr/bin/env python
+# sfputil.py
+#
+# Platform-specific SFP transceiver interface for SONiC
+#
 
 try:
-    from sonic_sfp.sfputilbase import sfputilbase
-except ImportError, e:
-    raise ImportError (str(e) + "- required module not found")
+    import time
+    from sonic_sfp.sfputilbase import SfpUtilBase
+except ImportError as e:
+    raise ImportError("%s - required module not found" % str(e))
 
 
-class sfputil(sfputilbase):
-    """Platform specific sfputil class"""
+class SfpUtil(SfpUtilBase):
+    """Platform-specific SfpUtil class"""
 
-    port_start = 0
-    port_end = 53
-    ports_in_block = 54
+    PORT_START = 0
+    PORT_END = 53
+    QSFP_PORT_START = 48
+    PORTS_IN_BLOCK = 54
+    
+    BASE_DIR_PATH = "/sys/class/gpio/gpio{0}/direction"
+    BASE_VAL_PATH = "/sys/class/gpio/gpio{0}/value"
 
-    port_to_eeprom_mapping = {}
+    _port_to_eeprom_mapping = {}
     port_to_i2c_mapping = {
         0: 18,
         1: 19,
@@ -71,12 +79,201 @@ class sfputil(sfputilbase):
         53: 71
     }
 
-    _qsfp_ports = range(0, ports_in_block + 1)
+    abs_to_gpio_mapping = {
+        0: 192,
+        1: 193,
+        2: 194,
+        3: 195,
+        4: 196,
+        5: 197,
+        6: 198,
+        7: 199,
+        8: 200,
+        9: 201,
+        10: 202,
+        11: 203,
+        12: 204,
+        13: 205,
+        14: 206,
+        15: 207,
+        16: 176,
+        17: 177,
+        18: 178,
+        19: 179,
+        20: 180,
+        21: 181,
+        22: 182,
+        23: 183,
+        24: 184,
+        25: 185,
+        26: 186,
+        27: 187,
+        28: 188,
+        29: 189,
+        30: 190,
+        31: 191,
+        32: 160,
+        33: 161,
+        34: 162,
+        35: 163,
+        36: 164,
+        37: 165,
+        38: 166,
+        39: 167,
+        40: 168,
+        41: 169,
+        42: 170,
+        43: 171,
+        44: 172,
+        45: 173,
+        46: 174,
+        47: 175,
+        48: 240,
+        49: 241,
+        50: 242,
+        51: 243,
+        52: 244,
+        53: 245
+    }
 
-    def __init__(self, port_num):
+    lpmode_to_gpio_mapping = {
+        48: 224,
+        49: 225,
+        50: 226,
+        51: 227,
+        52: 228,
+        53: 229
+    }
+
+    reset_to_gpio_mapping = {
+        48: 208,
+        49: 209,
+        50: 210,
+        51: 211,
+        52: 212,
+        53: 213
+    }
+
+    @property
+    def port_start(self):
+        return self.PORT_START
+
+    @property
+    def qsfp_port_start(self):
+        return self.QSFP_PORT_START
+
+    @property
+    def port_end(self):
+        return self.PORT_END
+
+    @property
+    def qsfp_ports(self):
+        return range(0, self.PORTS_IN_BLOCK + 1)
+
+    @property
+    def port_to_eeprom_mapping(self):
+        return self._port_to_eeprom_mapping
+
+    def __init__(self):
         # Override port_to_eeprom_mapping for class initialization
-        eeprom_path = '/sys/class/i2c-adapter/i2c-{0}/{0}-0050/eeprom'
+        eeprom_path = "/sys/class/i2c-adapter/i2c-{0}/{0}-0050/eeprom"
+
         for x in range(self.port_start, self.port_end + 1):
             port_eeprom_path = eeprom_path.format(self.port_to_i2c_mapping[x])
             self.port_to_eeprom_mapping[x] = port_eeprom_path
-        sfputilbase.__init__(self, port_num)
+
+        SfpUtilBase.__init__(self)
+
+    def get_presence(self, port_num):
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
+
+        try:
+            abs_device_file = self.BASE_VAL_PATH.format(
+                    self.abs_to_gpio_mapping[port_num])
+            val_file = open(abs_device_file)
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        content = val_file.readline().rstrip()
+
+        # content is a string, either "0" or "1"
+        if content == "1":
+            return True
+
+        return False
+
+    def get_low_power_mode(self, port_num):
+        # Check for invalid port_num
+        if port_num < self.qsfp_port_start or port_num > self.port_end:
+            return False
+
+        try:
+            lpmode_val_device_file = self.BASE_VAL_PATH.format(
+                    self.lpmode_to_gpio_mapping[port_num])
+            val_file = open(lpmode_val_device_file)
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        content = val_file.readline().rstrip()
+        
+        # content is a string, either "0" or "1"
+        if content == "1":
+            return True
+
+        return False
+
+    def set_low_power_mode(self, port_num, lpmode):
+        # Check for invalid port_num
+        if port_num < self.qsfp_port_start or port_num > self.port_end:
+            return False
+
+        try:
+            lpmode_val_device_file = self.BASE_VAL_PATH.format(
+                    self.lpmode_to_gpio_mapping[port_num])
+            val_file = open(lpmode_val_device_file, "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        val_file.write("1" if lpmode is True else "0")
+        val_file.close()
+
+        return True
+
+    def reset(self, port_num):
+        # Check for invalid port_num
+        if port_num < self.qsfp_port_start or port_num > self.port_end:
+            print "Error: unable to reset non-QSFP module: port %s" % str(port_num)
+            return False
+       
+        try:
+            print "port %s" % str(port_num)
+            reset_val_device_file = self.BASE_VAL_PATH.format(
+                    self.reset_to_gpio_mapping[port_num])
+            val_file = open(reset_val_device_file, "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        val_file.write("1")
+        val_file.close()
+        
+        # Sleep 1 second to allow it to settle
+        time.sleep(1)
+        
+        try:
+            reset_val_device_file = self.BASE_VAL_PATH.format(
+                    self.reset_to_gpio_mapping[port_num])
+            val_file = open(reset_val_device_file, "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+            
+        val_file.write("0")
+        val_file.close()
+
+        return True
