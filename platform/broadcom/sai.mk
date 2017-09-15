034b47df6c9becc615bfbef5ec28bc55044d8cbe
Shuotian Cheng <shuche@microsoft.com>
2017-09-15 11:39:29 -0700

    [Broadcom]: Update OpenNSL/SAI version (#959)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,11 +1,9 @@
 BRCM_SAI = libsaibcm_3.0.3.2_amd64.deb
-# TODO: upload new SAI build to blob
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2_amd64.deb"
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2_amd64.deb?sv=2015-04-05&sr=b&sig=d50c3WTnlQGVgybK4RyNFWqvXPHPpAotNSongSABpWg%3D&se=2031-05-25T18%3A52%3A35Z&sp=r"
 
 BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-# TODO: upload new SAI build to blob
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2_amd64.deb"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2_amd64.deb?sv=2015-04-05&sr=b&sig=psB%2BzhbludRstCuuGncrMOg5oETOY13U26yEXyR2yWc%3D&se=2031-05-25T18%3A50%3A38Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,5 +1,4 @@
 BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb
-# TODO: upload new SDK build to blob
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb"
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.3_amd64.deb?sv=2015-04-05&sr=b&sig=Qjb29DKq2%2FAvRJAju%2FU91Gu5orJC0R5LTJ%2BwYm%2BYFiA%3D&se=2031-05-25T18%3A36%3A33Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL_KERNEL)
