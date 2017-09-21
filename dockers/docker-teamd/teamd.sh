4ff0ee5e867e99ea25b03aed137cc76e50f446f2
Shuotian Cheng <shuche@microsoft.com>
2017-09-21 14:56:16 -0700

    [teamd]: Remove deprecated blocking logic before starting teamd (#976)
    With the fixes in /etc/network/interfaces file, host interfaces
    could be added into the corresponding LAGs automatically. Thus,
    the logic of checking if port initialization is ready is no longer
    needed.
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- dockers/docker-teamd/teamd.sh
+++ dockers/docker-teamd/teamd.sh
@@ -24,15 +24,5 @@ function clean_up {
 
 trap clean_up SIGTERM SIGKILL
 
-# Before teamd could automatically add newly created host interfaces into the
-# LAG, this workaround will wait until the host interfaces are created and then
-# the processes will be started.
-while true; do
-    # Check if front-panel ports are configured
-    result=`echo -en "SELECT 0\nHGETALL PORT_TABLE:ConfigDone" | redis-cli | sed -n 3p`
-    if [ "$result" == "0" ]; then
-        start_app
-        read
-    fi
-    sleep 1
-done
+start_app
+read
