0db870475468f768493485d89ce7e033b90f3bd8
Marian Pritsak <marianp@mellanox.com>
2017-09-26 21:22:00 +0300

    [Mellanox]: Remove FW upgrade procedure in docker (#989)
    FW upgrade is done outside the container.
    This piece of code was left accidentally during the merge.

--- platform/mellanox/docker-syncd-mlnx/start.sh
+++ platform/mellanox/docker-syncd-mlnx/start.sh
@@ -4,8 +4,4 @@ rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
 
-# mlnx-fw-upgrade.sh will exit if firmware was actually upgraded
-# or if some error occurs
-. mlnx-fw-upgrade.sh
-
 supervisorctl start syncd
