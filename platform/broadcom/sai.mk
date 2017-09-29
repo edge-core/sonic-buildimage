6483919852ade151b6c02c8c932b5cbce2260fb8
Shuotian Cheng <shuche@microsoft.com>
2017-09-28 20:29:31 -0700

    [Broadcom]: Update SAI version to 3.0.3.2-5 (#997)
    - Add additional DropCancel and CopyToCpuCancel action rules for every redirect

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.0.3.2-4_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-4_amd64.deb?sv=2015-04-05&sr=b&sig=8LE5JB9YlYHVWFf0QF5vFF7fCUbpo14OXokBi%2BKb9GI%3D&se=2031-06-04T20%3A15%3A13Z&sp=r"
+BRCM_SAI = libsaibcm_3.0.3.2-5_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-5_amd64.deb?sv=2015-04-05&sr=b&sig=MQE6FrxHs%2BIUPjRaSpWagcSjY6bbHLCUYasusxILkEs%3D&se=2031-06-07T21%3A50%3A36Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-4_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-5_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-4_amd64.deb?sv=2015-04-05&sr=b&sig=UGqSnxGtIxFBdO%2FDcFbZdPFgBPN695JQ6hLP5mBOt7g%3D&se=2031-06-04T20%3A15%3A26Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-5_amd64.deb?sv=2015-04-05&sr=b&sig=o8bjWlxxYAM%2F95aSshRFJE57JwKVjRaH4jDU2lDEoMg%3D&se=2031-06-07T21%3A50%3A19Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
