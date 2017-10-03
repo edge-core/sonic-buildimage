72e947621c413b6069b719f6bbad05c8abbdf1df
Shuotian Cheng <shuche@microsoft.com>
2017-10-03 16:23:19 -0700

    [config-engine]: Fix bug multiple ports connecting to same neighbor (#1005)
    The current DEVICE_NEIGHBOR format doesn't support multiple different
    ports connecting with same neighbor. Thus the lldpd.conf file is not
    generated correctly, causing missing information for LAG members.
    
    This fix reverts the data structure in the previous version of
    minigraph parser - using local port as the key and remote port/device
    as the value of the map. Sample format is:
    
    DEVICE_NEIGHBOR['Ethernet124'] = {
        'name': 'ARISTA04T1',
        'port': 'Ethernet1/1'
    }
    
    The corresponding unit test in test_cfggen is updated.
    Add one more unit test for lldpd.conf.j2 verification.
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -91,21 +91,17 @@ def parse_png(png, hname):
                 if enddevice == hname:
                     if port_alias_map.has_key(endport):
                         endport = port_alias_map[endport]
-                    neighbors[startdevice] = {'local_port': endport, 'port': startport}
+                    neighbors[endport] = {'name': startdevice, 'port': startport}
                 else:
                     if port_alias_map.has_key(startport):
                         startport = port_alias_map[startport]
-                    neighbors[enddevice] = {'local_port': startport, 'port': endport}
+                    neighbors[startport] = {'name': enddevice, 'port': endport}
 
         if child.tag == str(QName(ns, "Devices")):
             for device in child.findall(str(QName(ns, "Device"))):
                 (lo_prefix, mgmt_prefix, name, hwsku, d_type) = parse_device(device)
                 device_data = {'lo_addr': lo_prefix, 'type': d_type, 'mgmt_addr': mgmt_prefix, 'hwsku': hwsku } 
-                name = name.replace('"', '')
-                if neighbors.has_key(name):
-                    neighbors[name].update(device_data)
-                else:
-                    devices[name] = device_data
+                devices[name] = device_data
 
         if child.tag == str(QName(ns, "DeviceInterfaceLinks")):
             for if_link in child.findall(str(QName(ns, 'DeviceLinkBase'))):
