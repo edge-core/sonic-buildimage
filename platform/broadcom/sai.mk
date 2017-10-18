834890f192d89874eab02293fc316f91546e393f
Shuotian Cheng <shuche@microsoft.com>
2017-10-18 01:14:15 -0700

    [Broadcom]: Update Broadcom SAI package to 3.0.3.2-7 (#1049)
    Update Accton config.bcm files
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.0.3.2-6_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-6_amd64.deb?sv=2015-04-05&sr=b&sig=78jpXMULiubmup2PBYzM5MCrt7KIHA913FlDOsdgmGY%3D&se=2031-06-25T21%3A52%3A36Z&sp=r"
+BRCM_SAI = libsaibcm_3.0.3.2-7_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-7_amd64.deb?sv=2015-04-05&sr=b&sig=RUUm3WJuGYmtIatkKtR5iAuI%2FG2KM9hMXewAEnR2AKY%3D&se=2031-06-26T21%3A45%3A55Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-6_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-7_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-6_amd64.deb?sv=2015-04-05&sr=b&sig=Vg%2BjJpwyJ3smRBaNVkbWH4xkNGOn%2BOxI0w3VcDgAxV0%3D&se=2031-06-25T21%3A52%3A54Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-7_amd64.deb?sv=2015-04-05&sr=b&sig=hTQZeaqa7oiR9YTIuUGG1DGYNg8bwlWKB2J2r46Mq%2BE%3D&se=2031-06-26T21%3A46%3A16Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
