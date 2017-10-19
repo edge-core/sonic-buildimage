a9566bdb1e708de2a0ec249e54d05bdf42c90618
Shuotian Cheng <shuche@microsoft.com>
2017-10-19 16:52:33 -0700

    [Broadcom]: Update Broadcom SAI package to 3.0.3.2-8 (#1057)
    Update Inventec config.bcm files
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.0.3.2-7_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-7_amd64.deb?sv=2015-04-05&sr=b&sig=RUUm3WJuGYmtIatkKtR5iAuI%2FG2KM9hMXewAEnR2AKY%3D&se=2031-06-26T21%3A45%3A55Z&sp=r"
+BRCM_SAI = libsaibcm_3.0.3.2-8_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-8_amd64.deb?sv=2015-04-05&sr=b&sig=PYRdEo1SB0TfoahBoxwoNMt4g1V5aQpBH5RolBGZ6Lw%3D&se=2031-06-28T20%3A29%3A10Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-7_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-8_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-7_amd64.deb?sv=2015-04-05&sr=b&sig=hTQZeaqa7oiR9YTIuUGG1DGYNg8bwlWKB2J2r46Mq%2BE%3D&se=2031-06-26T21%3A46%3A16Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-8_amd64.deb?sv=2015-04-05&sr=b&sig=HWoCzzgNscATWcW0KXjh24LdoJ0AM4sVgISgyBh60MI%3D&se=2031-06-28T20%3A28%3A29Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
