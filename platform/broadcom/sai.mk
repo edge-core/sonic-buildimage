a4ecade8f6eba814e947204f36927d2a6561d31c
Shuotian Cheng <shuche@microsoft.com>
2017-10-23 15:46:07 -0700

    [Broadcom]: Update Broadcom SAI package to 3.0.3.2-8 (#1070)
    Update Arista config.bcm files
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.0.3.2-8_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-8_amd64.deb?sv=2015-04-05&sr=b&sig=PYRdEo1SB0TfoahBoxwoNMt4g1V5aQpBH5RolBGZ6Lw%3D&se=2031-06-28T20%3A29%3A10Z&sp=r"
+BRCM_SAI = libsaibcm_3.0.3.2-9_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-9_amd64.deb?sv=2015-04-05&sr=b&sig=w4MY%2FMlnCe59xwMXRxODyLkaEDLEljeBlT3bPUnedoU%3D&se=2031-07-02T20%3A17%3A33Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-8_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-9_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-8_amd64.deb?sv=2015-04-05&sr=b&sig=HWoCzzgNscATWcW0KXjh24LdoJ0AM4sVgISgyBh60MI%3D&se=2031-06-28T20%3A28%3A29Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-9_amd64.deb?sv=2015-04-05&sr=b&sig=zfBAMJKd0LUesg88UjDdUor%2Bc4j8omvw08MKymQQFnE%3D&se=2031-07-02T20%3A17%3A10Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
