7f7ddc80a33851f4ea2e9aec942a4172fc9ab4e6
Shuotian Cheng <shuche@microsoft.com>
2017-10-25 17:57:28 -0700

    [swss]: Change the env name from ASIC to platform (#1033)

--- dockers/docker-orchagent/orchagent.sh
+++ dockers/docker-orchagent/orchagent.sh
@@ -2,9 +2,7 @@
 
 # Export platform information. Required to be able to write
 # vendor specific code.
-export platform=`sonic-cfggen -v onie_switch_asic`
-
-export ASIC=`sonic-cfggen -y /etc/sonic/sonic_version.yml -v asic_type`
+export platform=`sonic-cfggen -y /etc/sonic/sonic_version.yml -v asic_type`
 
 MAC_ADDRESS=`ip link show eth0 | grep ether | awk '{print $2}'`
 
@@ -16,9 +14,9 @@ ORCHAGENT_ARGS="-d /var/log/swss "
 ORCHAGENT_ARGS+="-b 8192 "
 
 # Add platform specific arguments if necessary
-if [ "$ASIC" == "broadcom" ]; then
+if [ "$platform" == "broadcom" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
-elif [ "$ASIC" == "cavium" ]; then
+elif [ "$platform" == "cavium" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 fi
 
