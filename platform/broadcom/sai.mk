961a6669f7c896d0521b0a9bfc796330f12608fe
Shuotian Cheng <shuche@microsoft.com>
2017-10-27 13:59:51 -0700

    [Broadcom]: Update Broadcom OpenNSL/SAI packages (#1090)
    Update OpenNSL to 3.2.3.3-2
    Update SAI to 3.0.3.2-10
    
    This commit fixes S6100 CPU high usage issue
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.0.3.2-9_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-9_amd64.deb?sv=2015-04-05&sr=b&sig=w4MY%2FMlnCe59xwMXRxODyLkaEDLEljeBlT3bPUnedoU%3D&se=2031-07-02T20%3A17%3A33Z&sp=r"
+BRCM_SAI = libsaibcm_3.0.3.2-10_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-10_amd64.deb?sv=2015-04-05&sr=b&sig=tByZ7QDBsYlJ4UHbapnzqHYrbA8rD92%2FQXEpupITTmM%3D&se=2031-07-06T19%3A19%3A32Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-9_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-10_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-9_amd64.deb?sv=2015-04-05&sr=b&sig=zfBAMJKd0LUesg88UjDdUor%2Bc4j8omvw08MKymQQFnE%3D&se=2031-07-02T20%3A17%3A10Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-10_amd64.deb?sv=2015-04-05&sr=b&sig=T6U8sF%2BW8B%2FffBzPoUJ9peLcg2O9MunHBBKSu7SZOKo%3D&se=2031-07-06T19%3A19%3A52Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,4 +1,4 @@
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.3-1_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.3-1_amd64.deb?sv=2015-04-05&sr=b&sig=Uepf4z2wOadX%2F6OR%2BCoQzjv2tkwEZ2AspBiuo5sb25s%3D&se=2031-05-30T19%3A37%3A19Z&sp=r"
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.3.3-2_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.3.3-2_amd64.deb?sv=2015-04-05&sr=b&sig=RADtBDA9oZmwHnTzBY76ewajyJ8Af%2BchVCzNbe%2BPsbc%3D&se=2031-07-06T19%3A18%3A56Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL_KERNEL)
