1ce9b85e3cc02b2461778a66fc7d83bed13510be
lguohan <lguohan@gmail.com>
2017-11-06 23:40:27 -0800

    [init]: save the initial switch mac to config db (#1125)
    * [init]: save the initial switch mac to config db
    
    Save the initial switch mac to config db DEVICE_METADATA|localhost entry.
    
    * update sonic-swss submodule

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-SONIC_ASIC_TYPE=$(sonic-cfggen -y /etc/sonic/sonic_version.yml -v asic_type)
-SYSTEM_MAC_ADDRESS=$(ip link show eth0 | grep ether | awk '{print $2}')
-
-# Align last byte of MAC if necessary
-if [ "$SONIC_ASIC_TYPE" == "mellanox" -o "$SONIC_ASIC_TYPE" == "centec" ]; then
-    last_byte=$(python -c "print '$SYSTEM_MAC_ADDRESS'[-2:]")
-    aligned_last_byte=$(python -c "print format(int(int('$last_byte', 16) & 0b11000000), '02x')")  # put mask and take away the 0x prefix
-    SYSTEM_MAC_ADDRESS=$(python -c "print '$SYSTEM_MAC_ADDRESS'[:-2] + '$aligned_last_byte'")      # put aligned byte into the end of MAC
-fi
-
-sonic-cfggen -d -a '{"hwaddr":"'$SYSTEM_MAC_ADDRESS'"}' -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
-
-# Also store the system mac to configDB switch table. User configured switch_mac is not supported for now.
-/usr/bin/docker exec database redis-cli -n 4 hset SWITCH\|SWITCH_ATTR switch_mac $SYSTEM_MAC_ADDRESS
+sonic-cfggen -d -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
 
 [ -f /var/run/dhclient.eth0.pid ] && kill `cat /var/run/dhclient.eth0.pid` && rm -f /var/run/dhclient.eth0.pid
 
