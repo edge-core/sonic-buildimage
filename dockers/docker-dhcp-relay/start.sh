1f9107d044190078dfc884249563d0eb8c28a4d7
Joe LeVeque <jleveque@users.noreply.github.com>
2017-11-20 21:07:28 -0800

    [DHCP relay]: Wait for all interfaces to be assigned IPv4 addresses before starting relay agent(s) (#1173)

--- dockers/docker-dhcp-relay/start.sh
+++ dockers/docker-dhcp-relay/start.sh
@@ -6,7 +6,12 @@ rm -f /var/run/rsyslogd.pid
 # Start rsyslog
 supervisorctl start rsyslogd
 
-# Wait for all interfaces to come up before starting the DHCP relay agent(s)
+# Wait for all interfaces to come up and be assigned IPv4 addresses before
+# starting the DHCP relay agent(s). If an interface the relay should listen
+# on is down, the relay agent will not start. If an interface the relay should
+# listen on is up but does not have an IP address assigned when the relay
+# agent starts, it will not listen or send on that interface for the lifetime
+# of the process.
 /usr/bin/wait_for_intf.sh
 
 # Start the DHCP relay agent(s)
