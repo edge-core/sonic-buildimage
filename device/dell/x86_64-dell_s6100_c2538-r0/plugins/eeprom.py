1c0c3ff7a9ae0d17ebb3cabed9df8952e4f408c3
Joe LeVeque <jleveque@users.noreply.github.com>
2017-12-14 17:34:58 -0800

    [Dell S6100]: EEPROM plugin overrides serial_number_str() to return service tag instead of serial number (#1239)
    * [Dell S6100]: EEPROM plugin overrides serial_number_str() to return service tag instead of serial number
    
    * Refactor to simplify

--- device/dell/x86_64-dell_s6100_c2538-r0/plugins/eeprom.py
+++ device/dell/x86_64-dell_s6100_c2538-r0/plugins/eeprom.py
@@ -21,3 +21,13 @@ class board(eeprom_tlvinfo.TlvInfoDecoder):
         self.eeprom_path = "/sys/class/i2c-adapter/i2c-2/2-0050/eeprom"
         super(board, self).__init__(self.eeprom_path, 0, '', True)
 
+    def serial_number_str(self, e):
+        """Return service tag instead of serial number"""
+
+        (is_valid, results) = self.get_tlv_field(e, self._TLV_CODE_SERVICE_TAG)
+        if is_valid == False:
+            return "Bad service tag"
+
+        # 'results' is a list containing 3 elements, type (int), length (int),
+        # and value (string) of the requested TLV
+        return  results[2]
