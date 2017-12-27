5eb5e2162ba9697384c82712d2bba3f05d012a42
chenyuqiang <chenyq@centecnetworks.com>
2017-12-28 03:25:53 +0800

    [centec]: support sai1.0 (#1268)

--- platform/centec/sai.mk
+++ platform/centec/sai.mk
@@ -1,5 +1,5 @@
 # Centec SAI
 CENTEC_SAI = libsai_1.0.0_amd64.deb
-$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/goldengate-sai/raw/master/lib/SONiC_0.9.4/libsai_1.0.0_amd64.deb
+$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/goldengate-sai/raw/master/lib/SONiC_1.0/libsai_1.0.0_amd64.deb
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
