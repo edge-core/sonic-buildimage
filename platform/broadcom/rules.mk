13c83ac6f94ab24789d4d844564950f7163189b1
lguohan <lguohan@gmail.com>
2018-01-03 00:52:57 -0800

    [platform]: enable platform driver for dell s6000 (#5)

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -1,6 +1,6 @@
 include $(PLATFORM_PATH)/sdk.mk
 include $(PLATFORM_PATH)/sai.mk
-#include $(PLATFORM_PATH)/platform-modules-s6000.mk
+include $(PLATFORM_PATH)/platform-modules-s6000.mk
 #include $(PLATFORM_PATH)/platform-modules-dell.mk
 #include $(PLATFORM_PATH)/platform-modules-arista.mk
 #include $(PLATFORM_PATH)/platform-modules-ingrasys.mk
