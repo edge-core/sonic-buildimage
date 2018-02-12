ed408cde54f67fba6f3584e29033585867b1baac
cpchang <ieder1113@gmail.com>
2018-02-12 11:11:26 +0800

    [Quanta]: Add a new supported device and platform, IX1B-32X (#1372)
    * [Quanta]: Add a new supported device and platform, IX1B-32X
    
    Switch Vendor: Quanta
    Switch SKU:    IX1B-32X
    ASIC Vendor:   Broadcom
    Swich ASIC:    Tomahawk
    Port Config:   32x100G
    
    Signed-off-by: Chih-Pei.Chang@qct.io

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -8,6 +8,7 @@ include $(PLATFORM_PATH)/platform-modules-accton.mk
 include $(PLATFORM_PATH)/platform-modules-inventec.mk
 include $(PLATFORM_PATH)/platform-modules-cel.mk
 include $(PLATFORM_PATH)/platform-modules-delta.mk
+include $(PLATFORM_PATH)/platform-modules-quanta.mk
 include $(PLATFORM_PATH)/docker-orchagent-brcm.mk
 include $(PLATFORM_PATH)/docker-syncd-brcm.mk
 include $(PLATFORM_PATH)/docker-syncd-brcm-rpc.mk
