6ccd1601b801c26e0fe3557395b5c16ca0436fae
Joe LeVeque <jleveque@users.noreply.github.com>
2018-02-12 11:10:01 -0800

    Modify Arista service ACL solution to listen to ACL changes in ConfigDB (#1385)

--- dockers/docker-snmp-sv2/start.sh
+++ dockers/docker-snmp-sv2/start.sh
@@ -13,5 +13,6 @@ echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
 rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
+supervisorctl start snmpd-config-updater
 supervisorctl start snmpd
 supervisorctl start snmp-subagent
