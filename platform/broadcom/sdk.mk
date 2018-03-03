689bbd2b7211367294a7b1e4424eda7e2c03479b
Guohan Lu <gulv@microsoft.com>
2018-03-03 09:10:18 +0000

    [broadcom]: update opennsl module to 3.4.1.11-1

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,4 +1,4 @@
-BRCM_OPENNSL_KERNEL = opennsl-modules-4.9.0-5-amd64_3.2.3.7-1_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-4.9.0-5-amd64_3.2.3.7-1_amd64.deb?sv=2015-04-05&sr=b&sig=nju9vaqIjRlzV7V0wirMP6%2FNCJRsNqC%2FdqdqgxA0wrQ%3D&se=2154-12-03T07%3A37%3A17Z&sp=r"
+BRCM_OPENNSL_KERNEL = opennsl-modules-4.9.0-5-amd64_3.4.1.11-1_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-4.9.0-5-amd64_3.4.1.11-1_amd64.deb?sv=2015-04-05&sr=b&sig=vdIqSXaJhvN7Blk08WyxWhfMxky0XBx37JYAmNozx3k%3D&se=2155-01-24T09%3A08%3A25Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL_KERNEL)
