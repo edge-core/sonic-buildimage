126d08d000c009ab2e3b9aabb07b497e4134faa8
ZivErlich <32637684+ZivErlich@users.noreply.github.com>
2018-03-05 03:58:53 +0200

    [devices]: Updating SFPUtil plugin for Acton AS7212 and Marvell SLM5401 (#1456)

--- device/marvell/x86_64-marvell_slm5401_54x-r0/plugins/sfputil.py
+++ device/marvell/x86_64-marvell_slm5401_54x-r0/plugins/sfputil.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 try:
-    from sonic_sfp.sfputilbase import sfputilbase
+    import time
+    from sonic_sfp.sfputilbase import SfpUtilBase
 except ImportError, e:
     raise ImportError (str(e) + "- required module not found")
 
 
-class sfputil(sfputilbase):
+class sfputil(SfpUtilBase):
     """Platform specific sfputil class"""
 
     port_start = 0
@@ -51,10 +52,76 @@ class sfputil(sfputilbase):
 
     _qsfp_ports = range(0, ports_in_block + 1)
 
-    def __init__(self, port_num):
+    def __init__(self):
         # Override port_to_eeprom_mapping for class initialization
         eeprom_path = '/sys/bus/i2c/devices/{0}-0050/sfp_eeprom'
         for x in range(self.port_start, self.port_end + 1):
             port_eeprom_path = eeprom_path.format(self.port_to_i2c_mapping[x+1])
             self.port_to_eeprom_mapping[x] = port_eeprom_path
-        sfputilbase.__init__(self, port_num)
+        SfpUtilBase.__init__(self)
+
+    def reset(self, port_num):
+        # Check for invalid port_num
+        if port_num < self._port_start or port_num > self._port_end:
+            return False
+
+        path = "/sys/bus/i2c/devices/{0}-0050/sfp_port_reset"
+        port_ps = path.format(self.port_to_i2c_mapping[port_num+1])
+          
+        try:
+            reg_file = open(port_ps, 'w')
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        #toggle reset
+        reg_file.seek(0)
+        reg_file.write('1')
+        time.sleep(1)
+        reg_file.seek(0)
+        reg_file.write('0')
+        reg_file.close()
+        return True
+
+    def set_low_power_mode(self, port_nuM, lpmode):
+        raise NotImplementedErro
+
+    def get_low_power_mode(self, port_num):
+        raise NotImplementedErro
+        
+    def get_presence(self, port_num):
+        # Check for invalid port_num
+        if port_num < self._port_start or port_num > self._port_end:
+            return False
+
+        path = "/sys/bus/i2c/devices/{0}-0050/sfp_is_present"
+        port_ps = path.format(self.port_to_i2c_mapping[port_num+1])
+
+          
+        try:
+            reg_file = open(port_ps)
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        reg_value = reg_file.readline().rstrip()
+        if reg_value == '1':
+            return True
+
+        return False
+
+    @property
+    def port_start(self):
+        return self._port_start
+
+    @property
+    def port_end(self):
+        return self._port_end
+	
+    @property
+    def qsfp_ports(self):
+        return range(0, self.ports_in_block + 1)
+
+    @property 
+    def port_to_eeprom_mapping(self):
+         return self._port_to_eeprom_mapping
