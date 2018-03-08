c161de406a8759723ff1c31175b2d73290c43a51
Joe LeVeque <jleveque@users.noreply.github.com>
2018-03-07 17:08:45 -0800

    [lldpmgrd] Fix potential race condition when interfaces are created (#1469)

--- rules/lldpd.mk
+++ rules/lldpd.mk
@@ -1,12 +1,17 @@
 # lldpd package
 
-LLDPD_VERSION = 0.9.5-0
+LLDPD_VERSION = 0.9.5
 
-LLDPD = lldpd_$(LLDPD_VERSION)_amd64.deb
+LLDPD = lldpd_$(LLDPD_VERSION)-0_amd64.deb
 $(LLDPD)_DEPENDS += $(LIBSNMP_DEV)
 $(LLDPD)_RDEPENDS += $(LIBSNMP)
 $(LLDPD)_SRC_PATH = $(SRC_PATH)/lldpd
-SONIC_DPKG_DEBS += $(LLDPD)
+SONIC_MAKE_DEBS += $(LLDPD)
 
-LIBLLDPCTL = liblldpctl-dev_$(LLDPD_VERSION)_amd64.deb
+LIBLLDPCTL = liblldpctl-dev_$(LLDPD_VERSION)-0_amd64.deb
 $(eval $(call add_derived_package,$(LLDPD),$(LIBLLDPCTL)))
+
+# Export these variables so they can be used in a sub-make
+export LLDPD_VERSION
+export LLDPD
+export LIBLLDPCTL
