9065e09beed612f99d53eedd9e73e92df32b5825
AndriiS <andriis@mellanox.com>
2018-03-14 18:20:47 +0200

    [baseimage] Added net.core.wmem_max value required by Copp test (#1489)

--- build_debian.sh
+++ build_debian.sh
@@ -304,6 +304,7 @@ set /files/etc/sysctl.conf/net.ipv6.conf.all.accept_dad 0
 set /files/etc/sysctl.conf/net.ipv6.conf.eth0.accept_ra_defrtr 0
 
 set /files/etc/sysctl.conf/net.core.rmem_max 2097152
+set /files/etc/sysctl.conf/net.core.wmem_max 2097152
 " -r $FILESYSTEM_ROOT
 
 ## docker-py is needed by Ansible docker module
