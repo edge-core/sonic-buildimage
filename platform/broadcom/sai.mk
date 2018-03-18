3ba84ec1f755ed3258e6e0a8af2cc2e63ee47d07
lguohan <lguohan@gmail.com>
2018-03-17 19:07:28 -0700

    [SAI]: Update sonic to use SAI v1.2.4 (#1509)
    * Update SAI to v1.2.4
    
    Signed-off-by: Andriy Moroz <c_andriym@mellanox.com>
    
    * update SAI to v1.2.4 header

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-5_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.1.3.4-5_amd64.deb?sv=2015-04-05&sr=b&sig=s757Jmeq0blUuQa%2BnWXfe80sChLHWAnuSDf2J84s4OE%3D&se=2031-11-16T21%3A58%3A22Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-7_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.1.3.4-7_amd64.deb?sv=2015-04-05&sr=b&sig=elzOgHCA3G8oKKMfWcbFa%2BvQzAh727mtYJnnVOzVJtY%3D&se=2155-02-07T23%3A37%3A54Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-5_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-7_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.1.3.4-5_amd64.deb?sv=2015-04-05&sr=b&sig=BOLw2xZ53Tp1JfBXZtqsKAltbYGTJuRV89Y9jDSSp50%3D&se=2031-11-16T21%3A56%3A19Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.1.3.4-7_amd64.deb?sv=2015-04-05&sr=b&sig=rysxdbCA%2BaqBgDnxztdRA2ixiME3ypqRvzyEds8hLw4%3D&se=2155-02-07T23%3A38%3A39Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -1,5 +1,5 @@
-MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/05493363f389a5601322d664bdb004434e1a7a39/sdk
-MLNX_SDK_VERSION = 4.2.7004X004
+MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/fb4cd4cec4b8718c54d5bf5c64df9b474738f404/sdk
+MLNX_SDK_VERSION = 4.2.7101
 MLNX_SDK_RDEBS += $(APPLIBS) $(IPROUTE2_MLNX) $(SX_ACL_RM) $(SX_COMPLIB) \
 		  $(SX_EXAMPLES) $(SX_GEN_UTILS) $(SX_SCEW) $(SX_SDN_HAL) \
 		  $(SXD_LIBS) $(TESTX)
