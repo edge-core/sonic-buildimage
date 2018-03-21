87e942343f9c06ffd02aa0f85833965f6544e53d
Harish Venkatraman <vharish30@hotmail.com>
2018-03-21 09:28:23 -0700

    [device/dell] Added dynamic sai.profile generation (#1518)
    * [device/dell] Added dynamic sai.profile generation
    
    This commit addds new code for generating dynamic sai.profile file.
    The sai.profile.j2 will generate the sai.profile dynamically based on
    the topology. It will generate the sai.profile under /etc/sai.d/ directory
    in syncd. Before syncd is started this J2 file will be run from the start.sh
    file from /usr/bin/ directory. Since the sai.profile is dynamically generated
    the old sai.profile file is not required so deleted the file for S6100.
    It also address couple of more changes for port_config.ini support is added
    for speed in the file which can be used later to find the port speed. Also
    the buffer_default_t*.j2 file the ports should be from 0 to 64 changed.
    
    Unit tested the code on S6100 for dynamic generation of sai.profile file for
    both T0 and T1 and the file was created in /etc/sai.d/sai.profile
    Similarly tested the sonic binary on S6000 to make sure that the sai.profile is
    copied from the /usr/share/sonic/hwsku/sai.profile to /etc/sai.d/sai.profile.
    
    Signed-off-by: Harish Venkatraman <Harish_Venkatraman@dell.com>
    
    * [sonic-buildimage] Updating module sai-redis
    
    Updating the sonic-sairedis point.
    
    Signed-off-by: Harish Venkatraman <Harish_Venkatraman@dell.com>

--- device/dell/x86_64-dell_s6100_c2538-r0/Force10-S6100/sai.profile
+++
@@ -1,1 +0,0 @@
-SAI_INIT_CONFIG_FILE=/etc/bcm/th-s6100-64x40G-t0.config.bcm
--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
 PLATFORM_DIR=/usr/share/sonic/platform
+HWSKU_DIR=/usr/share/sonic/hwsku
 
 rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
+
+mkdir -p /etc/sai.d/
+
+# Create/Copy the sai.profile to /etc/sai.d/sai.profile
+if [ -f $HWSKU_DIR/sai.profile.j2 ]; then
+    sonic-cfggen -d -t $HWSKU_DIR/sai.profile.j2 > /etc/sai.d/sai.profile
+else
+    if [ -f $HWSKU_DIR/sai.profile ]; then
+        cp $HWSKU_DIR/sai.profile /etc/sai.d/sai.profile
+    fi
+fi
 
 supervisorctl start syncd
 
