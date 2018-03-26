9b14addbf1c32561620de41c544a98e9314ff296
simonJi2018 <37395146+simonJi2018@users.noreply.github.com>
2018-03-26 13:32:54 -0700

    [platform]: Upgrade Nephos SAI to v1.2.4 (#1530)

--- platform/nephos/sai.mk
+++ platform/nephos/sai.mk
@@ -1,9 +1,9 @@
-NEPHOS_SAI = libsainps_2.0.3_amd64.deb
-$(NEPHOS_SAI)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps_2.0.3_amd64.deb"
+NEPHOS_SAI = libsainps_2.0.4_amd64.deb
+$(NEPHOS_SAI)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps_2.0.4_amd64.deb"
 
-NEPHOS_SAI_DEV = libsainps-dev_2.0.3_sai_1.2.3_amd64.deb
+NEPHOS_SAI_DEV = libsainps-dev_2.0.4_sai_1.2.4_amd64.deb
 $(eval $(call add_derived_package,$(NEPHOS_SAI),$(NEPHOS_SAI_DEV)))
-$(NEPHOS_SAI_DEV)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps-dev_2.0.3_sai_1.2.3_amd64.deb"
+$(NEPHOS_SAI_DEV)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps-dev_2.0.4_sai_1.2.4_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(NEPHOS_SAI) $(NEPHOS_SAI_DEV)
 $(NEPHOS_SAI_DEV)_DEPENDS += $(NEPHOS_SAI)
