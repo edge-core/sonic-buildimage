30466b27c1b096998d2e30c72f49adfb48d662e3
Joe LeVeque <jleveque@users.noreply.github.com>
2018-04-06 19:24:18 -0700

    [router advertiser] Only start radvd process if device role is 'ToRRouter' (#1569)

--- dockers/docker-router-advertiser/start.sh
+++ dockers/docker-router-advertiser/start.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-# Generate /etc/radvd.conf config file
-sonic-cfggen -d -t /usr/share/sonic/templates/radvd.conf.j2 > /etc/radvd.conf
-
 rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
+
+# Router advertiser should only run on ToR (T0) devices
+DEVICE_ROLE=$(sonic-cfggen -d -v "DEVICE_METADATA.localhost.type")
+if [ "$DEVICE_ROLE" != "ToRRouter" ]; then
+    echo "Device role is not ToRRouter. Not starting router advertiser process."
+    exit 0
+fi
+
+# Generate /etc/radvd.conf config file
+sonic-cfggen -d -t /usr/share/sonic/templates/radvd.conf.j2 > /etc/radvd.conf
 
 # Start the router advertiser
 supervisorctl start radvd
