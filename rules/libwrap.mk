2b9f877b8f5c55288ef01bce8e3adc7ab0e95e26
yurypm <yurypm@arista.com>
2018-04-14 01:00:38 +0100

    [baseimage]: Fix tcpdmatch dependency (#1594)
    * Fix tcpdmatch dependency
    
    Issue: sonic_debian_extension.j2 uses tcpdmatch from src folder
           which is result of libwrap build
    Fix: added tcpd.deb to build results and extract required files
         from build results
    
    * Install libwrap0 and tcpd deb packages

--- rules/libwrap.mk
+++ rules/libwrap.mk
@@ -8,3 +8,5 @@ LIBWRAP = libwrap0_$(LIBWRAP_VERSION)_amd64.deb
 $(LIBWRAP)_SRC_PATH = $(SRC_PATH)/libwrap
 SONIC_MAKE_DEBS += $(LIBWRAP)
 
+TCPD = tcpd_$(LIBWRAP_VERSION)_amd64.deb
+$(eval $(call add_derived_package,$(LIBWRAP),$(TCPD)))
