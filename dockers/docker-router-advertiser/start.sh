f46e87391620b56f0acd70b03e93aaf9c4d54315
Joe LeVeque <jleveque@users.noreply.github.com>
2018-04-24 15:13:51 -0700

    [radvd] Ensure at least one interface is specified in radvd.conf before starting radvd (#1636)

--- dockers/docker-router-advertiser/start.sh
+++ dockers/docker-router-advertiser/start.sh
@@ -14,5 +14,12 @@ fi
 # Generate /etc/radvd.conf config file
 sonic-cfggen -d -t /usr/share/sonic/templates/radvd.conf.j2 > /etc/radvd.conf
 
+# Enusre at least one interface is specified in radvd.conf
+NUM_IFACES=$(grep -c "^interface " /etc/radvd.conf)
+if [ $NUM_IFACES -eq 0 ]; then
+    echo "No interfaces specified in radvd.conf. Not starting router advertiser process."
+    exit 0
+fi
+
 # Start the router advertiser
 supervisorctl start radvd
