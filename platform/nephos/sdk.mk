65b3774487ef7599e6deec07356935284d168fdc
simonJi2018 <37395146+simonJi2018@users.noreply.github.com>
2018-04-25 22:20:14 -0700

    [nehpos] fix libsainps and nps-sdk bug for support sai 1.2.4 (#1639)

--- platform/nephos/sdk.mk
+++ platform/nephos/sdk.mk
@@ -1,4 +1,4 @@
-NEPHOS_NPS_KERNEL = nps-modules-3.16.0-5-amd64_2.0.3_amd64.deb
-$(NEPHOS_NPS_KERNEL)_URL = "https://github.com/NephosInc/SONiC/raw/master/sdk/nps-modules-3.16.0-5-amd64_2.0.3_amd64.deb"
+NEPHOS_NPS_KERNEL = nps-modules-3.16.0-5_2.0.3_d15aca_amd64.deb
+$(NEPHOS_NPS_KERNEL)_URL = "https://github.com/NephosInc/SONiC/raw/master/sdk/nps-modules-3.16.0-5_2.0.3_d15aca_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(NEPHOS_NPS_KERNEL)
