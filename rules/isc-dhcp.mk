0c9e8eee65a377a2e7fbc521dc34b7d34aa3dd5a
Joe LeVeque <jleveque@users.noreply.github.com>
2018-05-02 14:17:04 -0700

    Revert "Fix the sonic build issue" (#1671)
    * Revert "[docker-dhcp]: Fix the sonic build issue (#1659)"
    
    This reverts commit 8d3c5fbcc21e72d38827c742f208a9984456be2d.

--- rules/isc-dhcp.mk
+++ rules/isc-dhcp.mk
@@ -8,9 +8,5 @@ ISC_DHCP_COMMON = isc-dhcp-common_$(ISC_DHCP_VERSION)_amd64.deb
 $(ISC_DHCP_COMMON)_SRC_PATH = $(SRC_PATH)/isc-dhcp
 SONIC_MAKE_DEBS += $(ISC_DHCP_COMMON)
 
-ISC_DHCP_CLIENT = isc-dhcp-client_$(ISC_DHCP_VERSION)_amd64.deb
-$(ISC_DHCP_CLIENT)_SRC_PATH = $(SRC_PATH)/isc-dhcp
-SONIC_MAKE_DEBS += $(ISC_DHCP_CLIENT)
-
 ISC_DHCP_RELAY = isc-dhcp-relay_$(ISC_DHCP_VERSION)_amd64.deb
-$(eval $(call add_derived_package,$(ISC_DHCP_COMMON),$(ISC_DHCP_RELAY),$(ISC_DHCP_CLIENT)))
+$(eval $(call add_derived_package,$(ISC_DHCP_COMMON),$(ISC_DHCP_RELAY)))
