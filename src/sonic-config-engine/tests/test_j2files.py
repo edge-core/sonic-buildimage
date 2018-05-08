323cafd7d9af0d13b69dab5c6816d021f0b85bf9
Andriy Moroz <c_andriym@mellanox.com>
2018-05-08 18:37:35 +0300

    Update buffers config for Mellanox 27xx devices (#1649)
    * Update buffers config for Mellanox 27xx devices
    
    Signed-off-by: Andriy Moroz <c_andriym@mellanox.com>
    
    * Remove buffers template test for msn27xx
    
    Signed-off-by: Andriy Moroz <c_andriym@mellanox.com>

--- src/sonic-config-engine/tests/test_j2files.py
+++ src/sonic-config-engine/tests/test_j2files.py
@@ -111,15 +111,6 @@ class TestJ2Files(TestCase):
         sample_output_file = os.path.join(self.test_dir, 'sample_output', 'ipinip.json')
         assert filecmp.cmp(sample_output_file, self.output_file)
 
-    def test_msn27xx_32ports_buffers(self):
-        buffer_file = os.path.join(self.test_dir, '..', '..', '..', 'device', 'mellanox', 'x86_64-mlnx_msn2700-r0', 'ACS-MSN2700', 'buffers.json.j2')
-        argument = '-m ' + self.t1_mlnx_minigraph + ' -p ' + self.mlnx_port_config + ' -t ' + buffer_file + ' > ' + self.output_file
-        self.run_script(argument)
-
-        sample_output_file = os.path.join(self.test_dir, 'sample_output', 'msn27.32ports.json')
-
-        self.assertTrue(filecmp.cmp(sample_output_file, self.output_file))
-
     def test_sku_render_template(self):
         argument = '-k Mellanox-SN2700 -t ' + os.path.join(self.test_dir, '../data/l2switch.j2') + ' -p ' + self.t0_port_config + ' > ' + self.output_file
         self.run_script(argument)
