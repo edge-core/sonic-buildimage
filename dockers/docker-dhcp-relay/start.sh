35bc68d65241dd973be8ff8d44c6b2db0da0b629
Joe LeVeque <jleveque@users.noreply.github.com>
2018-05-16 14:32:42 -0700

    [dhcp_relay] Only attempt to start 'isc-dhcp-relay' group if it is not empty (#1713)

--- dockers/docker-dhcp-relay/start.sh
+++ dockers/docker-dhcp-relay/start.sh
@@ -6,13 +6,16 @@ rm -f /var/run/rsyslogd.pid
 # Start rsyslog
 supervisorctl start rsyslogd
 
-# Wait for all interfaces to come up and be assigned IPv4 addresses before
-# starting the DHCP relay agent(s). If an interface the relay should listen
-# on is down, the relay agent will not start. If an interface the relay should
-# listen on is up but does not have an IP address assigned when the relay
-# agent starts, it will not listen or send on that interface for the lifetime
-# of the process.
-/usr/bin/wait_for_intf.sh
+# If our supervisor config has entries in the "isc-dhcp-relay" group...
+if [ $(supervisorctl status | grep -c "^isc-dhcp-relay:") -gt 0 ]; then
+    # Wait for all interfaces to come up and be assigned IPv4 addresses before
+    # starting the DHCP relay agent(s). If an interface the relay should listen
+    # on is down, the relay agent will not start. If an interface the relay
+    # should listen on is up but does not have an IP address assigned when the
+    # relay agent starts, it will not listen or send on that interface for the
+    # lifetime of the process.
+    /usr/bin/wait_for_intf.sh
 
-# Start the DHCP relay agent(s)
-supervisorctl start isc-dhcp-relay:*
+    # Start all DHCP relay agent(s)
+    supervisorctl start isc-dhcp-relay:*
+fi
