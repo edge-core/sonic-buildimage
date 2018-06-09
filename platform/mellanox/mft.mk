ed06aca40e715fee8d9ecf869848fed52aa160df
stepanblyschak <38952541+stepanblyschak@users.noreply.github.com>
2018-06-09 21:31:34 +0300

    [mellanox] Add cpldupdate utility to SONiC image (#1746)
    Signed-off-by: Stepan Blyschak <stepanb@mellanox.com>

--- platform/mellanox/mft.mk
+++ platform/mellanox/mft.mk
@@ -1,9 +1,17 @@
 # Mellanox SAI
 
-MFT = mft-4.5.0-31.amd64.deb
+MFT_VERSION = 4.5.0
+MFT_REVISION = 31
+
+export MFT_VERSION MFT_REVISION
+
+MFT = mft-$(MFT_VERSION)-$(MFT_REVISION).amd64.deb
 $(MFT)_SRC_PATH = $(PLATFORM_PATH)/mft
 $(MFT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_MAKE_DEBS += $(MFT)
 
-KERNEL_MFT = kernel-mft-dkms_4.5.0-$(KVERSION)_all.deb
+KERNEL_MFT = kernel-mft-dkms_$(MFT_VERSION)-$(KVERSION)_all.deb
 $(eval $(call add_derived_package,$(MFT),$(KERNEL_MFT)))
+
+MFT_OEM = mft-oem-$(MFT_VERSION)-$(MFT_REVISION).amd64.deb
+$(eval $(call add_derived_package,$(MFT),$(MFT_OEM)))
