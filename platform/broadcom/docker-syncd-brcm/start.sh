ff237aaf180981bf9b9d13d6bc180767a84fc6a5
Qi Luo <qiluo-msft@users.noreply.github.com>
2018-06-29 08:37:20 -0700

    [syncd] Treat bcmcmd as a supervisor task so we could collect stdout/stderr (#1825)

--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -18,6 +18,7 @@ else
     fi
 fi
 
+rm -f /var/run/sswsyncd/sswsyncd.socket
 supervisorctl start syncd
 
 # Function: wait until syncd has created the socket for bcmcmd to connect to
@@ -33,5 +34,5 @@ wait_syncd() {
 # If this platform has an initialization file for the Broadcom LED microprocessor, load it
 if [ -r ${PLATFORM_DIR}/led_proc_init.soc ]; then
     wait_syncd
-    /usr/bin/bcmcmd -t 60 "rcload ${PLATFORM_DIR}/led_proc_init.soc"
+    supervisorctl start ledinit
 fi
