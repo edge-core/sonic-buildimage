5e6d36c2a522744a67e5780c05d86993c69fe43f
lguohan <lguohan@gmail.com>
2018-07-13 00:07:30 -0700

    [vs]: set platform=vs for orchagent (#1860)
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- platform/vs/docker-sonic-vs/orchagent.sh
+++ platform/vs/docker-sonic-vs/orchagent.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+export platform=vs
+
 MAC_ADDRESS=`ip link show eth0 | grep ether | awk '{print $2}'`
 
-# Create a folder for SsWW record files
+# Create a folder for SwSS record files
 mkdir -p /var/log/swss
 ORCHAGENT_ARGS="-d /var/log/swss "
 
