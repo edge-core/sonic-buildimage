38beca654c678dafe4399d208addf2b1a51a4c5d
Kebo Liu <kebol@mellanox.com>
2018-08-04 07:33:56 +0800

    [docker-platform-monitor] make file and supervisord conf change for new xcvrd deamon (#1840)
    * [docker-platform-monitor] make file and supervisord conf change for new xcvrd deamon
    
    * make file change for the new daemon
    * supervisord conf change for the new daemon
    
    signed-off-by Liu Kebo kebol@mellanox.com
    
    * make xcvrd start sequence aligned with the supervisord conf
    
    * update submodules to include xcvrd modification

---
+++ rules/sonic-xcvrd.mk
@@ -0,0 +1,5 @@
+# sonic-xcvrd (SONiC Transceiver monitoring daemon) Debian package
+
+SONIC_XCVRD = python-sonic-xcvrd_1.0-1_all.deb
+$(SONIC_XCVRD)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-xcvrd
+SONIC_PYTHON_STDEB_DEBS += $(SONIC_XCVRD)
