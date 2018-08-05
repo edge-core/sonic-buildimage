75361042f53c1cbbdad01a2d12e36598cf1e8626
lguohan <lguohan@gmail.com>
2018-08-04 21:19:29 -0700

    [device]: add support for celestica E1031 (#1528)

---
+++ device/celestica/x86_64-cel_e1031-r0/Celestica-E1031-T48S4/sai.profile
@@ -0,0 +1,1 @@
+SAI_INIT_CONFIG_FILE=/etc/bcm/helix4-e1031-48x1G+4x10G.config.bcm
--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.5-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.1.3.5-3_amd64.deb?sv=2015-04-05&sr=b&sig=lzB9IHpJuMEENr9N9W0LBFamJ7mpvRVWgigfQmpIrPc%3D&se=2155-06-05T09%3A13%3A41Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.5-5_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.5-5_amd64.deb?sv=2015-04-05&sr=b&sig=QPeazVTcPxPiWnLQRX4BqAAoyo0kTXJlfNAAtp2nilk%3D&se=2155-06-28T00%3A35%3A53Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.5-3_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.5-5_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.1.3.5-3_amd64.deb?sv=2015-04-05&sr=b&sig=WoRAz6j8G3Xk%2BT3MOmhp5f%2BvWggw%2BgGgk2JtDJHkKjs%3D&se=2155-06-05T09%3A14%3A46Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.5-5_amd64.deb?sv=2015-04-05&sr=b&sig=6yrT5BVHkTr5955Rf9sADlVVN8o8TzxSjoj3JZg6nHw%3D&se=2155-06-28T00%3A37%3A34Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
