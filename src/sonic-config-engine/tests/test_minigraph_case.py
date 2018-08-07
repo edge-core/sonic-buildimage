355246151a83ecdabdd7332959c8809ee42add4c
Taoyu Li <taoyl@microsoft.com>
2018-08-07 08:43:08 -0700

    [minigraph.py] add support to parse deployment id of neighbor devices (#1890)
    * [minigraph.py] add support to parse deployment id of neighbor devices
    
    * Add unitest
    
    * Fix function name

--- src/sonic-config-engine/tests/test_minigraph_case.py
+++ src/sonic-config-engine/tests/test_minigraph_case.py
@@ -94,6 +94,11 @@ class TestCfgGenCaseInsensitive(TestCase):
         output = self.run_script(argument)
         self.assertEqual(output.strip(), "1")
 
+    def test_minigraph_neighbor_metadata(self):
+        argument = '-m "' + self.sample_graph + '" -p "' + self.port_config + '" -v "DEVICE_NEIGHBOR_METADATA"'
+        output = self.run_script(argument)
+        self.assertEqual(output.strip(), "{'switch-01t1': {'lo_addr': '10.1.0.186/32', 'mgmt_addr': '10.7.0.196/26', 'hwsku': 'Force10-S6000', 'type': 'LeafRouter', 'deployment_id': '2'}}")
+
     def test_metadata_everflow(self):
         argument = '-m "' + self.sample_graph + '" -p "' + self.port_config + '" -v "MIRROR_SESSION"'
         output = self.run_script(argument)
