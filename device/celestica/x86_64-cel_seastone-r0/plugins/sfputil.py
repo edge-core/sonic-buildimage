bef80b3fc3203b4dab7e8d0733ac732adc9a74b8
Pradchaya P <pphuchar@celestica.com>
2018-08-17 00:38:33 +0700

    [device/dx010] configuration port index start from 1 (#1927)
    * update sfputil, port indext start from 1
    
    * DX010 update SKUs port config index
    
    Port index in port_config.ini now start from 1.
    * Seastone-DX010-10-50 port config update
    * Seastone-DX010-50 port config update
    * Seastone-DX010 port config update
    
    * Update dx010 sfputil plugin
    
     * Implement lpmode set function.
     * Implement transceiver reset function.
     * Python code style format.
    
    * Remove new line at the end of port_config.ini
    
     *New line cause parsing error in sfputilbase.
    
    * Add get_transceiver_change_event method
    
     *This needed by new sfputilbase implement.
    
    * Fix unintended line breaks

--- device/celestica/x86_64-cel_seastone-r0/plugins/sfputil.py
+++ device/celestica/x86_64-cel_seastone-r0/plugins/sfputil.py
@@ -13,12 +13,11 @@ except ImportError as e:
 class SfpUtil(SfpUtilBase):
     """Platform-specific SfpUtil class"""
 
-    PORT_START = 0
-    PORT_END = 31
+    PORT_START = 1
+    PORT_END = 32
     PORTS_IN_BLOCK = 32
 
     _port_to_eeprom_mapping = {}
-    qsfp_ports = range(0, PORTS_IN_BLOCK + 1)
 
     @property
     def port_start(self):
@@ -30,7 +29,7 @@ class SfpUtil(SfpUtilBase):
 
     @property
     def qsfp_ports(self):
-        return range(0, self.PORTS_IN_BLOCK + 1)
+        return range(self.PORT_START, self.PORTS_IN_BLOCK + 1)
 
     @property
     def port_to_eeprom_mapping(self):
@@ -41,7 +40,10 @@ class SfpUtil(SfpUtilBase):
         eeprom_path = '/sys/bus/i2c/devices/i2c-{0}/{0}-0050/eeprom'
 
         for x in range(self.PORT_START, self.PORT_END + 1):
-            self.port_to_eeprom_mapping[x] = eeprom_path.format( x + 26 )
+            if self.port_start == 1:
+                self.port_to_eeprom_mapping[x] = eeprom_path.format((x - 1) + 26)
+            else:
+                self.port_to_eeprom_mapping[x] = eeprom_path.format(x + 26)
         SfpUtilBase.__init__(self)
 
     def get_presence(self, port_num):
@@ -60,8 +62,14 @@ class SfpUtil(SfpUtilBase):
         # content is a string containing the hex representation of the register
         reg_value = int(content, 16)
 
+        # Determind if port_num start from 1 or 0
+        if self.port_start == 1:
+            bit_index = port_num - 1
+        else:
+            bit_index = port_num
+
         # Mask off the bit corresponding to our port
-        mask = (1 << port_num)
+        mask = (1 << bit_index)
 
         # ModPrsL is active low
         if reg_value & mask == 0:
@@ -85,8 +93,14 @@ class SfpUtil(SfpUtilBase):
         # content is a string containing the hex representation of the register
         reg_value = int(content, 16)
 
+        # Determind if port_num start from 1 or 0
+        if self.port_start == 1:
+            bit_index = port_num - 1
+        else:
+            bit_index = port_num
+
         # Mask off the bit corresponding to our port
-        mask = (1 << port_num)
+        mask = (1 << bit_index)
 
         # LPMode is active high
         if reg_value & mask == 0:
@@ -95,8 +109,96 @@ class SfpUtil(SfpUtilBase):
         return True
 
     def set_low_power_mode(self, port_num, lpmode):
-        raise NotImplementedError
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
+
+        try:
+            reg_file = open("/sys/devices/platform/dx010_cpld/qsfp_lpmode", "r+")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        content = reg_file.readline().rstrip()
+
+        # content is a string containing the hex representation of the register
+        reg_value = int(content, 16)
+
+        # Determind if port_num start from 1 or 0
+        if self.port_start == 1:
+            bit_index = port_num - 1
+        else:
+            bit_index = port_num
+
+        # Mask off the bit corresponding to our port
+        mask = (1 << bit_index)
+
+        # LPMode is active high; set or clear the bit accordingly
+        if lpmode is True:
+            reg_value = reg_value | mask
+        else:
+            reg_value = reg_value & ~mask
+
+        # Convert our register value back to a hex string and write back
+        content = hex(reg_value).strip('L')
+
+        reg_file.seek(0)
+        reg_file.write(content)
+        reg_file.close()
+
+        return True
 
     def reset(self, port_num):
-        raise NotImplementedError
+        # Check for invalid port_num
+        if port_num < self.port_start or port_num > self.port_end:
+            return False
 
+        try:
+            reg_file = open("/sys/devices/platform/dx010_cpld/qsfp_reset", "r+")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        content = reg_file.readline().rstrip()
+
+        # File content is a string containing the hex representation of the
+        # register
+        reg_value = int(content, 16)
+
+        # Determind if port_num start from 1 or 0
+        if self.port_start == 1:
+            bit_index = port_num - 1
+        else:
+            bit_index = port_num
+
+        # Mask off the bit corresponding to our port
+        mask = (1 << bit_index)
+
+        # ResetL is active low
+        reg_value = reg_value & ~mask
+
+        # Convert our register value back to a hex string and write back
+        reg_file.seek(0)
+        reg_file.write(hex(reg_value).rstrip('L'))
+        reg_file.close()
+
+        # Sleep 1 second to allow it to settle
+        time.sleep(1)
+
+        # Flip the bit back high and write back to the register to take port
+        # out of reset
+        try:
+            reg_file = open("/sys/devices/platform/dx010_cpld/qsfp_reset", "w")
+        except IOError as e:
+            print "Error: unable to open file: %s" % str(e)
+            return False
+
+        reg_value = reg_value | mask
+        reg_file.seek(0)
+        reg_file.write(hex(reg_value).rstrip('L'))
+        reg_file.close()
+
+        return True
+
+    def get_transceiver_change_event(self, timeout=0):
+        raise NotImplementedError
