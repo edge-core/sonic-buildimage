cd8f6c8195f697405ade70190ea0eaf77d7c17de
Guohan Lu <gulv@microsoft.com>
2018-08-19 21:32:56 +0000

    [sonic-swss]: support dynamic configuration of ecn on/off, threshold, prob
    Upgrade to libsai 3.1.3.4-16 to support dynamic configuration
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-15_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.1.3.4-15_amd64.deb?sv=2015-04-05&sr=b&sig=7CIkdlBB%2BY6Xjae0vc1QBnlAVedRkreJRhWM93SyHOI%3D&se=2032-02-29T18%3A31%3A54Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.5-6_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.5-6_amd64.deb?sv=2015-04-05&sr=b&sig=Srm9zk6mYTYpqaifRwrMwQ9%2Bqf2b0HqrkS9okhSA%2Bq0%3D&se=2155-07-11T19%3A40%3A27Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-15_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.5-6_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.1.3.4-15_amd64.deb?sv=2015-04-05&sr=b&sig=rBOT802o1o%2BAcogaXq%2FgqyuTAmSlFKhtPzYM%2BtXlIdM%3D&se=2032-02-29T18%3A34%3A10Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.5-6_amd64.deb?sv=2015-04-05&sr=b&sig=xqMANzXnUJli4hcggZ8wJpepxfCaWedW2%2Fyx3X%2BXDH4%3D&se=2155-07-11T19%3A40%3A50Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
