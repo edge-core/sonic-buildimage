ff91c0511cd4c0de11ad1d942749c90aa58a3bd0
Guohan Lu <gulv@microsoft.com>
2018-08-20 04:43:05 +0000

    [broadcom]: update sai to 3.1.3.4-16
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.5-6_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.5-6_amd64.deb?sv=2015-04-05&sr=b&sig=Srm9zk6mYTYpqaifRwrMwQ9%2Bqf2b0HqrkS9okhSA%2Bq0%3D&se=2155-07-11T19%3A40%3A27Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-16_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-16_amd64.deb?sv=2015-04-05&sr=b&sig=p7e4USdBWxIKYxP22t7yb%2BKtzlWBidZxQF1HZ%2FDs16I%3D&se=2155-07-13T04%3A43%3A02Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.5-6_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-16_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.5-6_amd64.deb?sv=2015-04-05&sr=b&sig=xqMANzXnUJli4hcggZ8wJpepxfCaWedW2%2Fyx3X%2BXDH4%3D&se=2155-07-11T19%3A40%3A50Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-16_amd64.deb?sv=2015-04-05&sr=b&sig=80iUtmr%2BbdWg7p5Zst%2FQrWuukgSx0TME2fRPPIhfLMQ%3D&se=2155-07-12T21%3A32%3A56Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
