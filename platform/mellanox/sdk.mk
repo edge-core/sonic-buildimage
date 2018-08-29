929ef530ce1dfa4050615dd8057e3458b50e441e
Volodymyr Samotiy <volodymyrs@mellanox.com>
2018-08-29 11:12:47 +0300

    [mellanox]: Update SAI, SDK, FW pointers (#1996)
    * SAI v1.12.3 (API headers v1.3.3)
    * SDK v4.2.8300
    * FW v13.1703.0000
    
    Signed-off-by: Volodymyr Samotiy <volodymyrs@mellanox.com>

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -1,5 +1,5 @@
-MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/16e0822d7077d8558e6ff92b3c6650df17a88219/sdk
-MLNX_SDK_VERSION = 4.2.8103
+MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/71ae8c6d386f57a89742fc3261775b338bfc9c25/sdk
+MLNX_SDK_VERSION = 4.2.8300
 MLNX_SDK_RDEBS += $(APPLIBS) $(IPROUTE2_MLNX) $(SX_ACL_RM) $(SX_COMPLIB) \
 		  $(SX_EXAMPLES) $(SX_GEN_UTILS) $(SX_SCEW) $(SX_SDN_HAL) \
 		  $(SXD_LIBS) $(TESTX)
