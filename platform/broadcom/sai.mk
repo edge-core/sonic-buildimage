431a9f67d516253c1b667af670891ec4d1cd8710
Guohan Lu <gulv@microsoft.com>
2018-10-05 19:58:13 +0000

    [broadcom]: update SAI to 3.1.3.4-18
    Merged PR 1138465: Disable port global pause frame rx processing logic [Wenda Ni]
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-17_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-17_amd64.deb?sv=2015-04-05&sr=b&sig=wDTUZwUF05gxdoB%2BHVZeB8uQQWq3qFmvM5VLliLHA8E%3D&se=2155-08-17T07%3A45%3A56Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-18_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-18_amd64.deb?sv=2015-04-05&sr=b&sig=FqiCA8J4cvVyaKG9VrA9HAh2TedtJHHQUmjXQOoxiJY%3D&se=2155-08-28T19%3A38%3A44Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-17_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-18_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-17_amd64.deb?sv=2015-04-05&sr=b&sig=l%2Bvb%2F4%2FCB8IEz70msUGMYsWCpeDPp7slxwZeiKqnNAo%3D&se=2155-08-17T07%3A43%3A58Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-18_amd64.deb?sv=2015-04-05&sr=b&sig=kI7rK1xQ%2BkNePGDlagQgDCYmoBN%2By9Dn2UFnDAT34oA%3D&se=2155-08-28T19%3A42%3A49Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
