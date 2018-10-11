773ed99d0a4360539e6e9d63b46cc3978e9bee39
zhenggen-xu <zxu@linkedin.com>
2018-10-10 23:41:27 -0700

    [security] Upgrade kernel to 3.16.57-2 on 201803 branch (#2116)
    * [security] Upgrade kernel to 3.16.57-2
    
    Fix issues below:
    https://www.debian.org/security/2018/dsa-4120
    https://www.debian.org/security/2018/dsa-4179
    https://www.debian.org/security/2018/dsa-4187
    https://www.debian.org/security/2018/dsa-4188
    https://www.debian.org/security/2018/dsa-4196
    
    and more.
    
    * update opennsl-modules-3.16.0-6-amd64_3.4.1.11-7_amd64.deb package
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>
    
    * [mellanox] update sdk base url (new kernel version)

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,4 +1,6 @@
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-5-amd64_3.4.1.11-2_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-5-amd64_3.4.1.11-2_amd64.deb?sv=2015-04-05&sr=b&sig=xtf8nafmS1pcqx5hhBsfmLNSx2BeqmwN4Dwq5uwM1bo%3D&se=2031-11-16T21%3A54%3A27Z&sp=r"
+# mock link here, need to be replaced by real link from MSFT
+
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-6-amd64_3.4.1.11-7_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/opennsl-modules-3.16.0-6-amd64_3.4.1.11-7_amd64.deb?sv=2015-04-05&sr=b&sig=HGePoJSCcURIMW3bPRh5iXlx6z5SWiElmqD44mqUchI%3D&se=2155-08-28T16%3A31%3A48Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL_KERNEL)
