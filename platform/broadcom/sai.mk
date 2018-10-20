5c65c7730db250117f17ebd34fc9541cf466b6e2
Joe LeVeque <jleveque@users.noreply.github.com>
2018-10-20 14:39:04 -0700

    [build]: Eliminate build warning messages (#2172)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -5,5 +5,5 @@ BRCM_SAI_DEV = libsaibcm-dev_3.1.3.5-11_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.5-11_amd64.deb?sv=2015-04-05&sr=b&sig=YE%2BCpURwLGSzE8qBVandbERncFUkm5CEvCdMB9YrKZM%3D&se=2155-09-02T02%3A43%3A51Z&sp=r"
 
-SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
+SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
