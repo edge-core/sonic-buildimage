c64f105a3e5a9857b7ee3391a54cb49a1216aef2
Jipan Yang <jipan.yang@alibaba-inc.com>
2018-10-29 12:06:57 -0700

    [build]: Add missing timestamp and build_number in docker meta label tag value (#2194)
    * Add missing timestamp in docker meta label tag value
    
    Signed-off-by: Jipan Yang <jipan.yang@alibaba-inc.com>
    
    * add BUILD_NUMBER as well
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- slave.mk
+++ slave.mk
@@ -9,7 +9,7 @@ SHELL = /bin/bash
 USER = $(shell id -un)
 UID = $(shell id -u)
 GUID = $(shell id -g)
-SONIC_GET_VERSION=$(shell . functions.sh && sonic_get_version)
+SONIC_GET_VERSION=$(shell export BUILD_TIMESTAMP=$(BUILD_TIMESTAMP) && export BUILD_NUMBER=$(BUILD_NUMBER) && . functions.sh && sonic_get_version)
 
 .SECONDEXPANSION:
 
