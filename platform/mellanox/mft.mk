ce54ef72569a6263da5f3410640d4b5c21d827c7
Andriy Moroz <c_andriym@mellanox.com>
2018-10-30 10:49:36 +0200

    [mellanox]: Update MFT tools to v4.9.0 (#2207)
    Signed-off-by: Andriy Moroz <c_andriym@mellanox.com>

--- platform/mellanox/mft.mk
+++ platform/mellanox/mft.mk
@@ -1,9 +1,9 @@
 # Mellanox SAI
 
-MFT = mft-4.5.0-31.amd64.deb
+MFT = mft-4.9.0-38.amd64.deb
 $(MFT)_SRC_PATH = $(PLATFORM_PATH)/mft
 $(MFT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_MAKE_DEBS += $(MFT)
 
-KERNEL_MFT = kernel-mft-dkms_4.5.0-$(KVERSION)_all.deb
+KERNEL_MFT = kernel-mft-dkms_4.9.0-$(KVERSION)_all.deb
 $(eval $(call add_derived_package,$(MFT),$(KERNEL_MFT)))
