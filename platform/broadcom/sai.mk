ff7ef2a8fcec38a0ddd842e53ab03b20cc28eec0
Ying Xie <yxieca@users.noreply.github.com>
2018-12-17 00:44:05 -0800

    [bcm sai] upgrace Broadcom SAI to 3.3.3.1-3 (#2389)
    - Include helix4 fix.
    - Include support for TD2 56854.
    - Add dummy support for SAI_PORT_ATTR_ADVERTISED_SPEED.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.3.3.1-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm_3.3.3.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=Kp6Pjrvt9DD8fThhSjzDJNuVRYuW6aLwi2bgegM0hd8%3D&se=2032-08-09T02%3A22%3A31Z&sp=r"
+BRCM_SAI = libsaibcm_3.3.3.1-3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm_3.3.3.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=LcClnc7O9BUMfh6nYVNr76UAjLxVMtGztoH24qcY5rk%3D&se=2032-08-24T21%3A56%3A57Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.3.3.1-1_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.3.3.1-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm-dev_3.3.3.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=fTWUp3gOcNQNT9sS66CSEyP0JkSlPHNRlsvG4L64I0g%3D&se=2032-08-09T02%3A22%3A08Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm-dev_3.3.3.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=OS4wTDTjFVZMFKKFx0hKtZEKUDbOyLO69gqIUaM3I4M%3D&se=2032-08-24T21%3A56%3A25Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
