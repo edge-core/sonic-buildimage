b84dbd807438252d3d14bbb281bf7aedb617cf4c
sunesh <1797605+sunesh@users.noreply.github.com>
2019-01-04 09:33:53 -0800

    [barefoot]: updating deb package for platform and sai (#2413)

--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,5 +1,5 @@
-BFN_SAI = bfnsdk_1.0.0_amd64.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_5/bfnsdk_1.0.0_amd64.deb"
+BFN_SAI = bfnsdk_8.5.x.59217b4.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_5/bfnsdk_8.5.x.59217b4.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_SAI) # $(BFN_SAI_DEV)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
