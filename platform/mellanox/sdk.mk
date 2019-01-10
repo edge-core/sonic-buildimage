ae190725ffd4c2d154621f70226049d70caad2cc
stepanblyschak <38952541+stepanblyschak@users.noreply.github.com>
2019-01-10 07:11:03 +0200

    [mellanox] Update SDK to 4.3.0132 (#2430)
    Signed-off-by: Stepan Blyschak <stepanb@mellanox.com>

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -1,5 +1,5 @@
-MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/7cc737246fce514372cf8fd447360c5f11e7c8b9/sdk
-MLNX_SDK_VERSION = 4.2.9306
+MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/543d9e8b5e8c6c58805b3579a0d9d4796d496812/sdk
+MLNX_SDK_VERSION = 4.3.0132
 MLNX_SDK_RDEBS += $(APPLIBS) $(IPROUTE2_MLNX) $(SX_ACL_RM) $(SX_COMPLIB) \
 		  $(SX_EXAMPLES) $(SX_GEN_UTILS) $(SX_SCEW) $(SX_SDN_HAL) \
 		  $(SXD_LIBS) $(TESTX)
