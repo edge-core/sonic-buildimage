9c2d7240ea9b9ebfce31632f11b8ad7f1e17b161
lguohan <lguohan@gmail.com>
2019-02-01 11:18:02 -0800

    [vs]: Force10-S6000 buffer settings for virtual switch (#2515)
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- rules/scripts.mk
+++ rules/scripts.mk
@@ -8,8 +8,12 @@ $(CONFIGDB_LOAD_SCRIPT)_PATH = files/scripts
 BUFFERS_CONFIG_TEMPLATE = buffers_config.j2
 $(BUFFERS_CONFIG_TEMPLATE)_PATH = files/build_templates
 
+QOS_CONFIG_TEMPLATE = qos_config.j2
+$(QOS_CONFIG_TEMPLATE)_PATH = files/build_templates
+
 SONIC_COPY_FILES += $(CONFIGDB_LOAD_SCRIPT) \
                     $(ARP_UPDATE_SCRIPT) \
-                    $(BUFFERS_CONFIG_TEMPLATE)
+                    $(BUFFERS_CONFIG_TEMPLATE) \
+                    $(QOS_CONFIG_TEMPLATE)
 
 
