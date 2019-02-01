6b42f753c6150667ce10e34c95eff80c7e85a8e6
lguohan <lguohan@gmail.com>
2019-02-01 11:18:02 -0800

    [vs]: Force10-S6000 buffer settings for virtual switch (#2515)
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- platform/vs/docker-sonic-vs/start.sh
+++ platform/vs/docker-sonic-vs/start.sh
@@ -18,8 +18,9 @@ if [ -f /etc/sonic/config_db.json ]; then
 else
     # generate and merge buffers configuration into config file
     sonic-cfggen -t /usr/share/sonic/hwsku/buffers.json.j2 > /tmp/buffers.json
+    sonic-cfggen -t /usr/share/sonic/hwsku/qos.json.j2 > /tmp/qos.json
     sonic-cfggen -p /usr/share/sonic/hwsku/port_config.ini -k $HWSKU --print-data > /tmp/ports.json
-    sonic-cfggen -j /etc/sonic/init_cfg.json -j /tmp/buffers.json -j /tmp/ports.json --print-data > /etc/sonic/config_db.json
+    sonic-cfggen -j /etc/sonic/init_cfg.json -j /tmp/buffers.json -j /tmp/qos.json -j /tmp/ports.json --print-data > /etc/sonic/config_db.json
 fi
 
 mkdir -p /etc/swss/config.d/
