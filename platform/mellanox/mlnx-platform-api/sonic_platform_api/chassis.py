a9d2bf37e165c0cfd8e846d5a013c83541c4e885
Kevin(Shengkai) Wang <kevinw@mellanox.com>
2019-02-13 21:21:07 +0800

    [mellanox] Implement PSU related APIs based on the new platform API (#2460)
    * Implement part of PSU related APIs including get_status(), get_presence()
    
    Signed-off-by: Kevin Wang <kevinw@mellanox.com>

---
+++ platform/mellanox/mlnx-platform-api/sonic_platform_api/chassis.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#############################################################################
+# Mellanox
+#
+# Module contains an implementation of SONiC Platform Base API and
+# provides the Chassis information which are available in the platform
+#
+#############################################################################
+
+import sys
+
+try:
+    from sonic_platform_base.chassis_base import ChassisBase
+    from sonic_platform_api.psu import Psu
+except ImportError as e:
+    raise ImportError (str(e) + "- required module not found")
+
+MLNX_NUM_PSU = 2
+
+class Chassis(ChassisBase):
+    """Platform-specific Chassis class"""
+    def __init__(self):
+        ChassisBase.__init__(self)
+        for index in range(MLNX_NUM_PSU):
+            psu = Psu(index)
+            self._psu_list.append(psu)
+
---
+++ platform/mellanox/mlnx-platform-api/sonic_platform_api/psu.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+#############################################################################
+# Mellanox
+#
+# Module contains an implementation of SONiC Platform Base API and
+# provides the PSUs status which are available in the platform
+#
+#############################################################################
+
+import os.path
+
+try:
+    from sonic_platform_base.psu_base import PsuBase
+except ImportError as e:
+    raise ImportError (str(e) + "- required module not found")
+
+psu_list = []
+
+class Psu(PsuBase):
+    """Platform-specific Psu class"""
+    def __init__(self, psu_index):
+        global psu_list
+        PsuBase.__init__(self)
+        # PSU is 1-based on Mellanox platform
+        self.index = psu_index + 1
+        psu_list.append(psu_index)
+        self.psu_path = "/var/run/hw-management/thermal/"
+        self.psu_oper_status = "psu{}_pwr_status".format(self.index)
+        self.psu_presence = "psu{}_status".format(self.index)
+        if os.path.exists(os.path.join(self.psu_path, self.psu_presence)):
+            self.presence_file_exists = True
+        else:
+            self.presence_file_exists = False
+
+    def get_status(self):
+        """
+        Retrieves the operational status of power supply unit (PSU) defined
+
+        Returns:
+            bool: True if PSU is operating properly, False if not
+        """
+        status = 0
+        try:
+            with open(os.path.join(self.psu_path, self.psu_oper_status), 'r') as power_status:
+                status = int(power_status.read())
+        except (ValueError, IOError):
+            status = 0
+
+        return status == 1
+
+    def get_presence(self):
+        """
+        Retrieves the presence status of power supply unit (PSU) defined
+
+        Returns:
+            bool: True if PSU is present, False if not
+        """
+        status = 0
+        if self.presence_file_exists:
+            try:
+                with open(os.path.join(self.psu_path, self.psu_presence), 'r') as presence_status:
+                    status = int(presence_status.read())
+            except (ValueError, IOError):
+                status = 0
+        else:
+            status = self.index in psu_list
+
+        return status == 1
+
