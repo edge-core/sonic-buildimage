4faa5f2f92e3ff22dffe10f3a2d6c4c3cb89f2f6
Ying Xie <yxieca@users.noreply.github.com>
2019-02-14 12:12:55 -0800

    [warm boot] cherry-pick PR #2538 and advance related sub-modules (#2569)
    PR#2538 cannot merge due to master branch status. It has been tested
    against 201811 branch.
    
    Submodule src/sonic-sairedis 21f4a49..d57222a:
      > Add more specific logic for ingress ACL and buffer profile (#421)
      > Move warm_restart enable/disable config to stateDB WARM_RESTART_ENABLE_TABLE (#418)
      > Add support for vlan tagged frames in virtual switch (#417)
    
    Submodule src/sonic-swss 1590030..584490c:
      > Move warm_restart enable/disable config to stateDB WARM_RESTART_ENABLE_TABLE (#786)
      > [vstest]: Potential fix for timing issue in warm_reboot's routing UT (#788)
    
    Submodule src/sonic-swss-common 594f4e8..286ef34:
      > Move warm_restart enable/disable config to stateDB WARM_RESTART_ENABLE_TABLE (#260)
    
    Submodule src/sonic-utilities c6666e2..b44b462:
      > Move warm_restart enable/disable config to stateDB WARM_RESTART_ENABL… (#458)
      > [aclshow] output only counters per table/rule (#442)
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>
    
    [PR 2538] Move warm_restart enable/disable config to stateDB WARM_RESTART_ENABLE_TABLE
    
    Signed-off-by: Jipan Yang <jipan.yang@alibaba-inc.com>

--- files/scripts/syncd.sh
+++ files/scripts/syncd.sh
@@ -30,8 +30,8 @@ function unlock_service_state_change()
 
 function check_warm_boot()
 {
-    SYSTEM_WARM_START=`/usr/bin/redis-cli -n 4 hget "WARM_RESTART|system" enable`
-    SERVICE_WARM_START=`/usr/bin/redis-cli -n 4 hget "WARM_RESTART|${SERVICE}" enable`
+    SYSTEM_WARM_START=`/usr/bin/redis-cli -n 6 hget "WARM_RESTART_ENABLE_TABLE|system" enable`
+    SERVICE_WARM_START=`/usr/bin/redis-cli -n 6 hget "WARM_RESTART_ENABLE_TABLE|${SERVICE}" enable`
     # SYSTEM_WARM_START could be empty, always make WARM_BOOT meaningful.
     if [[ x"$SYSTEM_WARM_START" == x"true" ]] || [[ x"$SERVICE_WARM_START" == x"true" ]]; then
         WARM_BOOT="true"
