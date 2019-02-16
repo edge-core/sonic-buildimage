3eea909d5e24e46989acdfe9257ad109bc6f0f89
Guohan Lu <gulv@microsoft.com>
2019-02-16 00:01:01 +0000

    [quagga]: collect quagga-dbg debian package
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- rules/quagga.mk
+++ rules/quagga.mk
@@ -1,6 +1,11 @@
 # quagga package
 
-QUAGGA = quagga_0.99.24.1-2.1_amd64.deb
+QUAGGA_VERSION_FULL = 0.99.24.1-2.1
+
+QUAGGA = quagga_$(QUAGGA_VERSION_FULL)_amd64.deb
 $(QUAGGA)_DEPENDS += $(LIBSNMP_DEV)
 $(QUAGGA)_SRC_PATH = $(SRC_PATH)/sonic-quagga
 SONIC_DPKG_DEBS += $(QUAGGA)
+
+QUAGGA_DBG = quagga-dbg_$(QUAGGA_VERSION_FULL)_amd64.deb
+$(eval $(call add_derived_package,$(QUAGGA),$(QUAGGA_DBG)))
