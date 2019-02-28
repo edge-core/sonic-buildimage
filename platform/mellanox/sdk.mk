a06f5901d0032d29d90ccda63e6cfd94082b41dd
Andriy Moroz <c_andriym@mellanox.com>
2019-02-28 20:47:33 +0200

    [Mellanox] Update SAI, SDK/FW (v4.3.0136/13.1910.0920) (#2616)
    Signed-off-by: Andriy Moroz <c_andriym@mellanox.com>

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -1,5 +1,5 @@
-MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/9248a185b7da775b11daa5cb79ea22195c0b99f1/sdk
-MLNX_SDK_VERSION = 4.3.0134
+MLNX_SDK_BASE_URL = https://github.com/Mellanox/SAI-Implementation/raw/f3e389f1ba9535fdd6afb134897683b65d1ad4e7/sdk
+MLNX_SDK_VERSION = 4.3.0136
 MLNX_SDK_RDEBS += $(APPLIBS) $(IPROUTE2_MLNX) $(SX_ACL_RM) $(SX_COMPLIB) \
 		  $(SX_EXAMPLES) $(SX_GEN_UTILS) $(SX_SCEW) $(SX_SDN_HAL) \
 		  $(SXD_LIBS) $(TESTX)
