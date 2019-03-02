8bc45ac708b5dda08589097fb99391c336bcf352
Deepinder Setia <39503446+dsetia-bfn@users.noreply.github.com>
2019-03-02 15:27:59 -0800

    [barefoot]: Updating deb package for platform and sai (#2635)

--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,5 +1,5 @@
-BFN_SAI = bfnsdk_8.5.x.59217b4.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_5/bfnsdk_8.5.x.59217b4.deb"
+BFN_SAI = bfnsdk_master.c7af495.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/vxlan_poc/bfnsdk_master.c7af495.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_SAI) # $(BFN_SAI_DEV)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
