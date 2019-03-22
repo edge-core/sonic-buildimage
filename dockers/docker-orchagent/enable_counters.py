3bc29d8c8036c89591432e3cd51d8f1d5889b650
Wenda Ni <wenni@microsoft.com>
2019-03-22 16:56:46 -0700

    [counters]: Enable watermark stats polling by default (#2603)
    Signed-off-by: Wenda Ni <wenni@microsoft.com>

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -14,6 +14,8 @@ def enable_counters():
     enable_counter_group(db, 'PORT')
     enable_counter_group(db, 'QUEUE')
     enable_counter_group(db, 'PFCWD')
+    enable_counter_group(db, 'PG_WATERMARK')
+    enable_counter_group(db, 'QUEUE_WATERMARK')
 
 def get_uptime():
     with open('/proc/uptime') as fp:
