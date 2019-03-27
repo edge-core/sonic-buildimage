09f413246ea8d7fcab36f00a0dda198060ead702
Shuotian Cheng <shuche@microsoft.com>
2019-03-26 23:48:22 -0700

    [Broadcom]: Update SAI to 3.1.3.4-20 (#2708)
    Address parity error in XOR bank for TH device
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-19_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-19_amd64.deb?sv=2015-04-05&sr=b&sig=tbjRGxCW644D0kAuVsl2WQLj99vlb0dKgizE6TpoYjw%3D&se=2032-10-21T03%3A08%3A33Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-20_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-20_amd64.deb?sv=2015-04-05&sr=b&sig=r5ezPQ1tNe53icqTFio2dpE2it2XKPTk5kv4LcEA4gs%3D&se=2032-12-02T18%3A10%3A05Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-19_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-20_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-19_amd64.deb?sv=2015-04-05&sr=b&sig=AIteLXKhrlbhY661nG1LihraEu31j5G9J%2FLsFETfXvc%3D&se=2032-10-21T03%3A09%3A32Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-20_amd64.deb?sv=2015-04-05&sr=b&sig=VFbXjNqtnZTNk7ffwcmjPjeXakXmJL4hozjlyNhT%2F3A%3D&se=2032-12-02T18%3A10%3A28Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
