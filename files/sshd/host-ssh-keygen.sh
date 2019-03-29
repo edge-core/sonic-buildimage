9c83b5480d3e3b7fd96948c4e3ad49e2b7171e34
Qi Luo <qiluo-msft@users.noreply.github.com>
2019-03-29 15:27:33 -0700

    [security] Do not generate ssh server keys for non RSA protocols (#2718)

--- files/sshd/host-ssh-keygen.sh
+++ files/sshd/host-ssh-keygen.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
+set -e
+
 [ -r /etc/ssh/ssh_host_rsa_key ] || {
     rm -f /etc/ssh/ssh_host_*_key*
     /usr/bin/ssh-keygen -t rsa -N '' -f /etc/ssh/ssh_host_rsa_key
-    /usr/bin/ssh-keygen -t dsa -N '' -f /etc/ssh/ssh_host_dsa_key
-    /usr/bin/ssh-keygen -t rsa1 -N '' -f /etc/ssh/ssh_host_key
-    /usr/bin/ssh-keygen -t ecdsa -N '' -f /etc/ssh/ssh_host_ecdsa_key
-    /usr/bin/ssh-keygen -t ed25519 -N '' -f /etc/ssh/ssh_host_ed25519_key
 }
