1af74d56627f668733947ba3f2a0fa0358414068
Ying Xie <yxieca@users.noreply.github.com>
2019-04-10 19:11:59 -0700

    [Bcm SAI] ugprade Broadcom SAI to version 3.3.5.4m-1 (#2765)
    - Broadcom SAI 3.3 GA release 20190402.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.3.4.3m-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm_3.3.4.3m-3_amd64.deb?sv=2015-04-05&sr=b&sig=QpwawYAGZOdlH6Z7PLOlHSl%2B%2Fcqe0IrDAq2a7hlmFmg%3D&se=2156-01-09T02%3A48%3A41Z&sp=r"
+BRCM_SAI = libsaibcm_3.3.5.4m-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm_3.3.5.4m-1_amd64.deb?sv=2015-04-05&sr=b&sig=5al8VoedFxE3anjfi4BwLLX2YqU%2BByafkZgEY85TnN8%3D&se=2032-12-17T20%3A59%3A24Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.3.4.3m-3_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.3.5.4m-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm-dev_3.3.4.3m-3_amd64.deb?sv=2015-04-05&sr=b&sig=8CYNbomcJhVI%2BFdIUN%2F1rIOM%2BwwMzn33Lb8Llgo8GsY%3D&se=2156-01-09T02%3A49%3A03Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.3/libsaibcm-dev_3.3.5.4m-1_amd64.deb?sv=2015-04-05&sr=b&sig=ROLknrDMqOGeGfOXu8%2BUVl8yKKDyxsq3swiUtR7n63A%3D&se=2032-12-17T20%3A58%3A59Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
