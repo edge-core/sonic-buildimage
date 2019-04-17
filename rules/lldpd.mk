047c7c891593e66e8f6cf7b0d14893a090135957
Sangita Maity <sangitamaity0211@gmail.com>
2019-04-17 10:59:58 -0700

    [dockers]: Upgrade LLDP docker to stretch build (#2633)
    * [dockers]: Upgrade LLDP docker to stretch build

--- rules/lldpd.mk
+++ rules/lldpd.mk
@@ -1,17 +1,20 @@
 # lldpd package
 
 LLDPD_VERSION = 0.9.6
+LLDPD_VERSION_SUFFIX = 1
+LLDPD_VERSION_FULL = $(LLDPD_VERSION)-$(LLDPD_VERSION_SUFFIX)
 
-LLDPD = lldpd_$(LLDPD_VERSION)-0_amd64.deb
+LLDPD = lldpd_$(LLDPD_VERSION_FULL)_amd64.deb
 $(LLDPD)_DEPENDS += $(LIBSNMP_DEV)
 $(LLDPD)_RDEPENDS += $(LIBSNMP)
 $(LLDPD)_SRC_PATH = $(SRC_PATH)/lldpd
 SONIC_MAKE_DEBS += $(LLDPD)
 
-LIBLLDPCTL = liblldpctl-dev_$(LLDPD_VERSION)-0_amd64.deb
+LIBLLDPCTL = liblldpctl-dev_$(LLDPD_VERSION_FULL)_amd64.deb
 $(eval $(call add_derived_package,$(LLDPD),$(LIBLLDPCTL)))
 
 # Export these variables so they can be used in a sub-make
 export LLDPD_VERSION
+export LLDPD_VERSION_FULL
 export LLDPD
 export LIBLLDPCTL
