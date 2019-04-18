04debfa4ec2712f53b75f33727f19f74967f5440
Shuotian Cheng <shuche@microsoft.com>
2019-04-18 02:14:09 -0700

    [Broadcom]: Update SAI to 3.1.3.4-21 (#2797)
    Update hashing coverage for HashIP4TcpUdpPortsEqualField
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-20_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-20_amd64.deb?sv=2015-04-05&sr=b&sig=r5ezPQ1tNe53icqTFio2dpE2it2XKPTk5kv4LcEA4gs%3D&se=2032-12-02T18%3A10%3A05Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-21_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-21_amd64.deb?sv=2015-04-05&sr=b&sig=KH4pkYsvmZ2q99k8JX09Zw94dKSr7HMdKznM0eQtyeM%3D&se=2032-12-24T23%3A36%3A03Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-20_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-21_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-20_amd64.deb?sv=2015-04-05&sr=b&sig=VFbXjNqtnZTNk7ffwcmjPjeXakXmJL4hozjlyNhT%2F3A%3D&se=2032-12-02T18%3A10%3A28Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-21_amd64.deb?sv=2015-04-05&sr=b&sig=dKIUB%2BvmJsgw%2FlYa5wGvEL%2FyhbyXwYd5hXrBSXDux5M%3D&se=2032-12-24T23%3A36%3A19Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
