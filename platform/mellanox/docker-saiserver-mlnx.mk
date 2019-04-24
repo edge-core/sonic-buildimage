3a6d7be27e5d85a855f122c969207abad1bef201
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2019-04-23 17:23:17 -0700

    [build]: syncd-rpc.mk: Fix stretch dockers build failure (#2812)
    The base syncd dockers follow a template, which defines the base docker as DOCKER_SYNCD_BASE instead of DOCKER_SYNCD_. Fix the docker-syncd-<mlnx, bfn>.mk to use the new one.

--- platform/mellanox/docker-saiserver-mlnx.mk
+++ platform/mellanox/docker-saiserver-mlnx.mk
@@ -11,4 +11,4 @@ $(DOCKER_SAISERVER_MLNX)_CONTAINER_NAME = saiserver
 $(DOCKER_SAISERVER_MLNX)_RUN_OPT += --net=host --privileged -t
 $(DOCKER_SAISERVER_MLNX)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
 $(DOCKER_SAISERVER_MLNX)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-$(DOCKER_SYNCD_MLNX)_RUN_OPT += --tmpfs /run/criu
+$(DOCKER_SYNCD_BASE)_RUN_OPT += --tmpfs /run/criu
