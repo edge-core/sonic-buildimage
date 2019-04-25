8811b990f0b2ad5df4cab8ffe7dae59af0789410
Nazarii Hnydyn <nazariig@mellanox.com>
2019-04-25 10:34:34 +0300

    Added missing swssloglevel utility. (#2821)
    Signed-off-by: Nazarii Hnydyn <nazariig@mellanox.com>

--- rules/docker-orchagent.mk
+++ rules/docker-orchagent.mk
@@ -33,5 +33,5 @@ $(DOCKER_ORCHAGENT)_RUN_OPT += -v /host/machine.conf:/host/machine.conf:ro
 $(DOCKER_ORCHAGENT)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_ORCHAGENT)_RUN_OPT += -v /var/log/swss:/var/log/swss:rw
 
-$(DOCKER_ORCHAGENT)_IMAGE_FILES += swssloglevel:/usr/bin/swssloglevel
+$(DOCKER_ORCHAGENT)_BASE_IMAGE_FILES += swssloglevel:/usr/bin/swssloglevel
 $(DOCKER_ORCHAGENT)_FILES += $(ARP_UPDATE_SCRIPT)
