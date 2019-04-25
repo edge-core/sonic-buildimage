6bf97d8a23897459a3037dd5383ec7b472afdb64
Nikos <31227248+nikos-github@users.noreply.github.com>
2019-04-25 13:21:20 -0700

    [docker-frr]: Move docker to stretch and add pythontools (#2819)

--- rules/frr.mk
+++ rules/frr.mk
@@ -3,7 +3,13 @@
 FRR_VERSION = 6.0.2
 export FRR_VERSION
 
-FRR = frr_$(FRR_VERSION)-1~sonic.debian8+1_amd64.deb
+FRR = frr_$(FRR_VERSION)-1~sonic.debian9+1_amd64.deb
 $(FRR)_DEPENDS += $(LIBSNMP_DEV)
 $(FRR)_SRC_PATH = $(SRC_PATH)/sonic-frr
 SONIC_MAKE_DEBS += $(FRR)
+
+# FRRouting pythontools
+FRR_PYTHONTOOLS = frr-pythontools_$(FRR_VERSION)-1~sonic.debian9+1_amd64.deb
+$(FRR_PYTHONTOOLS)_DEPENDS += $(LIBSNMP_DEV)
+$(FRR_PYTHONTOOLS)_SRC_PATH = $(SRC_PATH)/sonic-frr
+SONIC_MAKE_DEBS += $(FRR_PYTHONTOOLS)
