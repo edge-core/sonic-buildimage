6eca27e5647f5af9adc149ba3858955ca679f803
Joe LeVeque <jleveque@users.noreply.github.com>
2019-05-01 08:02:38 -0700

    [services] Restart SwSS service upon unexpected critical process exit (#2845)
    * [service] Restart SwSS Docker container if orchagent exits unexpectedly
    
    * Configure systemd to stop restarting swss if it attempts to restart more than 3 times in 20 minutes
    
    * Move supervisor-proc-exit-listener script
    
    * [docker-dhcp-relay] Enhance wait_for_intf.sh.j2 to utilize STATEDB
    
    * Ensure dependent services stop/start/restart with SwSS
    
    * Change 'StartLimitInterval' to 'StartLimitIntervalSec', as Stretch installs systemd 232 (>= v230)
    
    * Also update journald.conf options
    
    * Remove 'PartOf' option from unit files
    
    * Add '$(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)' to new shared docker-orchagent makefile
    
    * Make supervisor-proc-exit-listener script read from 'critical_processes' file inside container
    
    * Update critical_processes file for swss container

--- rules/scripts.mk
+++ rules/scripts.mk
@@ -11,9 +11,13 @@ $(BUFFERS_CONFIG_TEMPLATE)_PATH = files/build_templates
 QOS_CONFIG_TEMPLATE = qos_config.j2
 $(QOS_CONFIG_TEMPLATE)_PATH = files/build_templates
 
+SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT = supervisor-proc-exit-listener
+$(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)_PATH = files/scripts
+
 SONIC_COPY_FILES += $(CONFIGDB_LOAD_SCRIPT) \
                     $(ARP_UPDATE_SCRIPT) \
                     $(BUFFERS_CONFIG_TEMPLATE) \
-                    $(QOS_CONFIG_TEMPLATE)
+                    $(QOS_CONFIG_TEMPLATE) \
+                    $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
 
 
--- src/sonic-config-engine/tests/sample_output/wait_for_intf.sh
+++ src/sonic-config-engine/tests/sample_output/wait_for_intf.sh
@@ -1,43 +1,41 @@
 #!/usr/bin/env bash
 
+STATE_DB_IDX="6"
+
+PORT_TABLE_PREFIX="PORT_TABLE"
+VLAN_TABLE_PREFIX="VLAN_TABLE"
+LAG_TABLE_PREFIX="LAG_TABLE"
+
 function wait_until_iface_ready
 {
-    IFACE=$1
+    TABLE_PREFIX=$1
+    IFACE=$2
 
-    echo "Waiting until interface $IFACE is up..."
+    echo "Waiting until interface $IFACE is ready..."
 
-    # Wait for the interface to come up (i.e., 'ip link show' returns 0)
-    until ip link show dev $IFACE up > /dev/null 2>&1; do
-        sleep 1
-    done
-
-    echo "Interface $IFACE is up"
-
-    echo "Waiting until interface $IFACE has an IPv4 address..."
-
-    # Wait until the interface gets assigned an IPv4 address
+    # Wait for the interface to come up
+    # (i.e., interface is present in STATE_DB and state is "ok")
     while true; do
-        IP=$(ip -4 addr show dev $IFACE | grep "inet " | awk '{ print $2 }' | cut -d '/' -f1)
-
-        if [ -n "$IP" ]; then
+        RESULT=$(redis-cli -n ${STATE_DB_IDX} HGET "${TABLE_PREFIX}|${IFACE}" "state" 2> /dev/null)
+        if [ x"$RESULT" == x"ok" ]; then
             break
         fi
 
         sleep 1
     done
 
-    echo "Interface $IFACE is configured with IP $IP"
+    echo "Interface ${IFACE} is ready!"
 }
 
 
-# Wait for all interfaces to come up and have IPv4 addresses assigned
-wait_until_iface_ready Vlan1000
-wait_until_iface_ready PortChannel01
-wait_until_iface_ready PortChannel01
-wait_until_iface_ready PortChannel02
-wait_until_iface_ready PortChannel02
-wait_until_iface_ready PortChannel03
-wait_until_iface_ready PortChannel03
-wait_until_iface_ready PortChannel04
-wait_until_iface_ready PortChannel04
+# Wait for all interfaces to be up and ready
+wait_until_iface_ready ${VLAN_TABLE_PREFIX} Vlan1000
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel01
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel01
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel02
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel02
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel03
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel03
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel04
+wait_until_iface_ready ${LAG_TABLE_PREFIX} PortChannel04
 
