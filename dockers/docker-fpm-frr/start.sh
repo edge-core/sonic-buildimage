5fb185cd8330329bcab64b2fb0bc8c4da75127c2
lguohan <gulv@microsoft.com>
2019-05-08 23:00:49 -0700

    [docker-frr]: bring quagga docker features to frr docker (#2870)
    - use superviord to manage process in frr docker
    - intro separated configuration mode for frr
    - bring quagga configuration template to frr.
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- dockers/docker-fpm-frr/start.sh
+++ dockers/docker-fpm-frr/start.sh
@@ -1,6 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+mkdir -p /etc/frr
+
+CONFIG_TYPE=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["docker_routing_config_mode"]'`
+
+if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "separated" ]; then
+    sonic-cfggen -d -y /etc/sonic/deployment_id_asn_map.yml -t /usr/share/sonic/templates/bgpd.conf.j2 > /etc/frr/bgpd.conf
+    sonic-cfggen -d -t /usr/share/sonic/templates/zebra.conf.j2 > /etc/frr/zebra.conf
+    touch /etc/frr/vtysh.conf
+elif [ "$CONFIG_TYPE" == "unified" ]; then
+    sonic-cfggen -d -y /etc/sonic/deployment_id_asn_map.yml -t /usr/share/sonic/templates/frr.conf.j2 >/etc/frr/frr.conf
+    echo "service integrated-vtysh-config" > /etc/frr/vtysh.conf
+fi
+
+sonic-cfggen -d -t /usr/share/sonic/templates/isolate.j2 > /usr/sbin/bgp-isolate
+chown root:root /usr/sbin/bgp-isolate
+chmod 0755 /usr/sbin/bgp-isolate
+
+sonic-cfggen -d -t /usr/share/sonic/templates/unisolate.j2 > /usr/sbin/bgp-unisolate
+chown root:root /usr/sbin/bgp-unisolate
+chmod 0755 /usr/sbin/bgp-unisolate
+
+mkdir -p /var/sonic
+echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
 
 rm -f /var/run/rsyslogd.pid
-service rsyslog start
-service frr start
-fpmsyncd &
+
+supervisorctl start rsyslogd
+
+supervisorctl start bgpcfgd
+
+# Start Quagga processes
+supervisorctl start zebra
+supervisorctl start bgpd
+
+if [ "$CONFIG_TYPE" == "unified" ]; then
+    supervisorctl start vtysh_b
+fi
+
+supervisorctl start fpmsyncd
