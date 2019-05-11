23fb00c21b036c70e45c0552836775917ce6f96a
Andriy Kokhan <43479230+akokhan@users.noreply.github.com>
2019-05-11 11:54:47 -0700

    [bfn] fixed build failure (#2885)
    * fixed barefoot platform build
    * added TNA config
    * updated port_config.ini
    
    Signed-off-by: Andriy Kokhan <akokhan@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,9 +1,9 @@
 ifdef BLDENV
-BFN_PLATFORM = bfnplatform_master.92171a1_deb9.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/sde-master/bfnplatform_master.92171a1_deb9.deb"
+BFN_PLATFORM = bfnplatform_8.9.x.98de3ce_pr_deb9.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnplatform_8.9.x.98de3ce_pr_deb9.deb"
 else
-BFN_PLATFORM = bfnplatform_master.92171a1_deb8.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/sde-master/bfnplatform_master.92171a1_deb8.deb"
+BFN_PLATFORM = bfnplatform_8.9.x.98de3ce_pr_deb8.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnplatform_8.9.x.98de3ce_pr_deb8.deb"
 endif
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM) # $(BFN_SAI_DEV)
--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,9 +1,9 @@
 ifdef BLDENV
-BFN_SAI = bfnsdk_master.92171a1_deb9.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/sde-master/bfnsdk_master.92171a1_deb9.deb"
+BFN_SAI = bfnsdk_8.9.x.98de3ce_pr_deb9.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnsdk_8.9.x.98de3ce_pr_deb9.deb"
 else
-BFN_SAI = bfnsdk_master.92171a1_deb8.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/sde-master/bfnsdk_master.92171a1_deb8.deb"
+BFN_SAI = bfnsdk_8.9.x.98de3ce_pr_deb8.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnsdk_8.9.x.98de3ce_pr_deb8.deb"
 endif
 
 SONIC_ONLINE_DEBS += $(BFN_SAI) # $(BFN_SAI_DEV)
