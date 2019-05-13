a7edb869894100735c7cc6ee940f30abfe0a9e01
Shuotian Cheng <shuche@microsoft.com>
2019-05-13 13:17:23 -0700

    [Broadcom]: Update Broadcom SAI to 3.1.3.4-23 (#2890)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-22_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-22_amd64.deb?sv=2015-04-05&sr=b&sig=PXRtXh3GfOKAOky%2FBmeJYeM2H8GcdVsZFBE9%2FHNOPPQ%3D&se=2033-01-08T01%3A54%3A13Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-23_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-23_amd64.deb?sv=2015-04-05&sr=b&sig=VmgeysdXcAhMAQO2rpMhYPhx%2BI6YDm06%2Bx4FTj2nnsA%3D&se=2033-01-19T18%3A10%3A19Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-22_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-23_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-22_amd64.deb?sv=2015-04-05&sr=b&sig=6EXfCbMQwjMYDoNzI81pBb%2B%2Fz8pcCMQoAQzaiND71Cs%3D&se=2033-01-08T01%3A55%3A36Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-23_amd64.deb?sv=2015-04-05&sr=b&sig=OOgj5GJdWN8Ga0bNvoNIfK8%2Brr0WJxwa2RCuCCbPcDs%3D&se=2033-01-19T18%3A10%3A44Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
