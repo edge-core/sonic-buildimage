83e74d6dbb35fb668afbc670f2b8029e970b561f
simonJi2018 <37395146+simonJi2018@users.noreply.github.com>
2019-05-15 07:50:13 -0700

    [nephos]: upgrade sai.mk to cbb99f for support 1.4.1 (#2901)

--- platform/nephos/sai.mk
+++ platform/nephos/sai.mk
@@ -1,6 +1,6 @@
 SDK_VERSION = 2.0.8
-SAI_VERSION = 1.4.0
-SAI_COMMIT_ID = c818c7
+SAI_VERSION = 1.4.1
+SAI_COMMIT_ID = cbb99f
 NEPHOS_SAI = libsainps_$(SDK_VERSION)_sai_$(SAI_VERSION)_$(SAI_COMMIT_ID)_amd64.deb
 $(NEPHOS_SAI)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps_$(SDK_VERSION)_sai_$(SAI_VERSION)_$(SAI_COMMIT_ID)_amd64.deb"
 
