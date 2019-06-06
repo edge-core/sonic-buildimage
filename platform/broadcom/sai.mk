4b78e5021942b3ea903bf2f5c626d21c113b41a8
Shuotian Cheng <shuche@microsoft.com>
2019-06-06 14:27:03 -0700

    [Broadcom]: Update SAI to 3.1.3.4-24 (#2975)
    Update hash seed algorithm to add entrophy
    
    Signed-off-by: Shu0T1an ChenG <shuche@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-23_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-23_amd64.deb?sv=2015-04-05&sr=b&sig=VmgeysdXcAhMAQO2rpMhYPhx%2BI6YDm06%2Bx4FTj2nnsA%3D&se=2033-01-19T18%3A10%3A19Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-24_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm_3.1.3.4-24_amd64.deb?sv=2015-04-05&sr=b&sig=wVm5RKLoYiUDC9hlc4FazjA5lJiRpfBvoV%2BzjKTAiZs%3D&se=2033-02-11T21%3A45%3A57Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-23_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-24_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-23_amd64.deb?sv=2015-04-05&sr=b&sig=OOgj5GJdWN8Ga0bNvoNIfK8%2Brr0WJxwa2RCuCCbPcDs%3D&se=2033-01-19T18%3A10%3A44Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/libsaibcm-dev_3.1.3.4-24_amd64.deb?sv=2015-04-05&sr=b&sig=lb6aT8JY%2FldniendDehhgBhir1BCVNfS0FTgOIS62oA%3D&se=2033-02-11T21%3A46%3A16Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
