f4d07dc0030aa12c0dd8f45b7857e98345b808f8
Myron Sosyak <49795530+msosyak@users.noreply.github.com>
2019-06-21 13:29:35 -0700

    fixed BFN target build (#3061)
    Signed-off-by: Myron Sosyak <msosyak@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,10 +1,5 @@
-ifdef BLDENV
-BFN_PLATFORM = bfnplatform_8.9.x.98de3ce_pr_deb9.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnplatform_8.9.x.98de3ce_pr_deb9.deb"
-else
-BFN_PLATFORM = bfnplatform_8.9.x.98de3ce_pr_deb8.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnplatform_8.9.x.98de3ce_pr_deb8.deb"
-endif
+BFN_PLATFORM = bfnplatform_8_9_1.x.ab1e16f.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9_1/bfnplatform_8_9_1.x.ab1e16f.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM) # $(BFN_SAI_DEV)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,10 +1,5 @@
-ifdef BLDENV
-BFN_SAI = bfnsdk_8.9.x.98de3ce_pr_deb9.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnsdk_8.9.x.98de3ce_pr_deb9.deb"
-else
-BFN_SAI = bfnsdk_8.9.x.98de3ce_pr_deb8.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9/bfnsdk_8.9.x.98de3ce_pr_deb8.deb"
-endif
+BFN_SAI = bfnsdk_8_9_1.x.ab1e16f.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_8_9_1/bfnsdk_8_9_1.x.ab1e16f.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_SAI) # $(BFN_SAI_DEV)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
