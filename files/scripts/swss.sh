59117d23f0eea14fdfc1bf3c79f602eaf98daa9f
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2019-07-09 03:29:57 +0300

    [swss.sh]: Cleanup LAG entries in STATE DB (#3114)
    Signed-off-by: Stepan Blyschak <stepanb@mellanox.com>

--- files/scripts/swss.sh
+++ files/scripts/swss.sh
@@ -104,7 +104,7 @@ start() {
         /usr/bin/docker exec database redis-cli -n 1 FLUSHDB
         /usr/bin/docker exec database redis-cli -n 2 FLUSHDB
         /usr/bin/docker exec database redis-cli -n 5 FLUSHDB
-        clean_up_tables 6 "'PORT_TABLE*', 'MGMT_PORT_TABLE*', 'VLAN_TABLE*', 'VLAN_MEMBER_TABLE*', 'INTERFACE_TABLE*', 'MIRROR_SESSION*', 'VRF_TABLE*', 'FDB_TABLE*'"
+        clean_up_tables 6 "'PORT_TABLE*', 'MGMT_PORT_TABLE*', 'VLAN_TABLE*', 'VLAN_MEMBER_TABLE*', 'LAG_TABLE*', 'LAG_MEMBER_TABLE*', 'INTERFACE_TABLE*', 'MIRROR_SESSION*', 'VRF_TABLE*', 'FDB_TABLE*'"
     fi
 
     # start service docker
