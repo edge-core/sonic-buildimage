c78cbe72adf5692e85fa7235de138aac4ee071c1
Nazarii Hnydyn <nazariig@mellanox.com>
2019-07-11 19:13:30 +0300

    [Mellanox]: Fixed Mellanox ISSU dependency. (#3141)
    Signed-off-by: Nazarii Hnydyn <nazariig@mellanox.com>

--- platform/mellanox/docker-syncd-mlnx.mk
+++ platform/mellanox/docker-syncd-mlnx.mk
@@ -4,11 +4,5 @@ DOCKER_SYNCD_PLATFORM_CODE = mlnx
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(PYTHON_SDK_API) $(MLNX_SFPD)
-
-$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
-									$(LIBSWSSCOMMON_DBG) \
-									$(LIBSAIMETADATA_DBG) \
-									$(LIBSAIREDIS_DBG)
-
-$(DOCKER_SYNCD_MLNX)_RUN_OPT += -v /host/warmboot:/var/warmboot
-
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) $(LIBSWSSCOMMON_DBG) $(LIBSAIMETADATA_DBG) $(LIBSAIREDIS_DBG)
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
