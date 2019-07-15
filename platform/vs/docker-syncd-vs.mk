edge-core/sonic-buildimage

commit 0bca6b8468daa6268476a70dc2adfe03644bcb8c

1) Build debug docker image for each docker (#3104)
2) Install debug tools in every debug docker image
3) Install available debug symbols in debug docker image
4) Provide additional host/docker mapping for host dirs /src & /debug
    4.1) The one-image will have source code under /src
    4.2) /debug is mapped as rw. User can put his core file there and use this dir to
         collect debug session logs too.
5) Build debug image using debug dockers
6) Source code is archived into /src of debug image
7) The welcome banner is extended to display these additional facilities in debug image.

platform/vs/docker-syncd-vs.mk
@@ -1,14 +1,16 @@
 # docker image for vs syncd
 
-DOCKER_SYNCD_VS = docker-syncd-vs.gz
-$(DOCKER_SYNCD_VS)_PATH = $(PLATFORM_PATH)/docker-syncd-vs
-$(DOCKER_SYNCD_VS)_DEPENDS += $(SYNCD_VS)
-$(DOCKER_SYNCD_VS)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
-SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_VS)
+DOCKER_SYNCD_PLATFORM_CODE = vs
+include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+
+$(DOCKER_SYNCD_SYNCD)_DEPENDS += $(SYNCD_VS)
+
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_VS_DBG) \
+									$(LIBSWSSCOMMON_DBG) \
+									$(LIBSAIMETADATA_DBG) \
+									$(LIBSAIREDIS_DBG) \
+									$(LIBSAIVS_DBG)
+
 SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_SYNCD_VS)
 
-$(DOCKER_SYNCD_VS)_CONTAINER_NAME = syncd
-$(DOCKER_SYNCD_VS)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_SYNCD_VS)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
-$(DOCKER_SYNCD_VS)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_SYNCD_VS)_RUN_OPT += -v /host/warmboot:/var/warmboot

build_debian.sh
@@ -412,6 +412,28 @@ fi
 sudo sed -i 's/EBTABLES_LOAD_ON_START="no"/EBTABLES_LOAD_ON_START="yes"/g' ${FILESYSTEM_ROOT}/etc/default/ebtables
 sudo cp files/image_config/ebtables/ebtables.filter ${FILESYSTEM_ROOT}/etc
 
+## Debug Image specific changes
+## Update motd for debug image
+if [ "$DEBUG_IMG" == "y" ]
+then
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo '**************' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo 'Running DEBUG image' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo '**************' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo '/src has the sources' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo '/src is mounted in each docker' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo '/debug is created for core files or temp files' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo 'Create a subdir under /debug to upload your files' >> /etc/motd"
+    sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo '/debug is mounted in each docker' >> /etc/motd"
+
+    sudo mkdir -p $FILESYSTEM_ROOT/src
+    pushd src
+    ../dbg_files.sh | sudo tar -cvzf ../$FILESYSTEM_ROOT/src/sonic_src.tar.gz -T -
+    popd
+
+    sudo mkdir -p $FILESYSTEM_ROOT/debug
+
+fi
+
 ## Remove gcc and python dev pkgs
 sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y remove gcc libpython2.7-dev
 

build_debug_docker_j2.sh
@@ -1,0 +1,44 @@
+#! /bin/bash
+
+echo "
+FROM $1
+
+ARG docker_container_name
+
+## Make apt-get non-interactive
+ENV DEBIAN_FRONTEND=noninteractive
+
+{% if $2 is defined %}
+{% if $2|length %}
+
+COPY \
+{% for deb in $2.split(' ') -%}
+debs/{{ deb }}{{' '}}
+{%- endfor -%}
+debs/
+
+RUN dpkg -i \
+{% for deb in $2.split(' ') -%}
+debs/{{ deb }}{{' '}}
+{%- endfor %}
+
+{% endif %}
+{% endif %}
+
+{% if $3 is defined %}
+{% if $3|length %}
+
+RUN apt-get install -f -y \
+{% for dbg in $3.split(' ') -%}
+{{ dbg }}{{' '}}
+{%- endfor %}
+
+{% endif %}
+{% endif %}
+
+
+## Clean up
+RUN apt-get clean -y; apt-get autoclean -y; apt-get autoremove -y
+RUN rm -rf /debs
+
+"

dbg_files.sh
@@ -1,0 +1,27 @@
+#!/bin/bash
+
+# Provie file paths to archive for debug image as relative to src subdir
+#
+SRC_DIR_LIST="\
+    libteam \
+    lldpd \
+    libnl3 \
+    radvd \
+    redis \
+    snmpd \
+    sonic-dbsyncd \
+    sonic-frr \
+    sonic-platform-common \
+    sonic-platform-daemons \
+    sonic-py-swsssdk \
+    sonic-sairedis \
+    sonic-snmpagent \
+    sonic-swss \
+    sonic-swss-common \
+    tacacs"
+
+for i in $SRC_DIR_LIST
+do
+    find $i/ -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -type f
+done
+

platform/barefoot/docker-orchagent-bfn.mk
@@ -1,24 +1,5 @@
 # docker image for orchagent
 
-DOCKER_ORCHAGENT_BFN = docker-orchagent-bfn.gz
-$(DOCKER_ORCHAGENT_BFN)_PATH = $(DOCKERS_PATH)/docker-orchagent
-$(DOCKER_ORCHAGENT_BFN)_DEPENDS += $(SWSS) $(REDIS_TOOLS) $(IPROUTE2)
-ifeq ($(INSTALL_DEBUG_TOOLS), y)
-$(DOCKER_ORCHAGENT_BFN)_DEPENDS += $(SWSS_DBG) \
-                                   $(LIBSWSSCOMMON_DBG) \
-                                   $(LIBSAIREDIS_DBG)
-endif
-$(DOCKER_ORCHAGENT_BFN)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
-SONIC_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_BFN)
-SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_BFN)
+DOCKER_ORCHAGENT_PLATFORM_CODE = bfn
+include $(PLATFORM_PATH)/../template/docker-orchagent-base.mk
 
-$(DOCKER_ORCHAGENT_BFN)_CONTAINER_NAME = swss
-$(DOCKER_ORCHAGENT_BFN)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_ORCHAGENT_BFN)_RUN_OPT += -v /etc/network/interfaces:/etc/network/interfaces:ro
-$(DOCKER_ORCHAGENT_BFN)_RUN_OPT += -v /etc/network/interfaces.d/:/etc/network/interfaces.d/:ro
-$(DOCKER_ORCHAGENT_BFN)_RUN_OPT += -v /host/machine.conf:/host/machine.conf:ro
-$(DOCKER_ORCHAGENT_BFN)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-$(DOCKER_ORCHAGENT_BFN)_RUN_OPT += -v /var/log/swss:/var/log/swss:rw
-
-$(DOCKER_ORCHAGENT_BFN)_BASE_IMAGE_FILES += swssloglevel:/usr/bin/swssloglevel
-$(DOCKER_ORCHAGENT_BFN)_FILES += $(ARP_UPDATE_SCRIPT)

platform/barefoot/docker-syncd-bfn.mk
@@ -1,15 +1,13 @@
 # docker image for syncd
 
-DOCKER_SYNCD_BFN = docker-syncd-bfn.gz
-$(DOCKER_SYNCD_BFN)_PATH = $(PLATFORM_PATH)/docker-syncd-bfn
-$(DOCKER_SYNCD_BFN)_DEPENDS += $(SYNCD)
-$(DOCKER_SYNCD_BFN)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
-SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_BFN)
-ifneq ($(ENABLE_SYNCD_RPC),y)
-SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_SYNCD_BFN)
-endif
 
-$(DOCKER_SYNCD_BFN)_CONTAINER_NAME = syncd
-$(DOCKER_SYNCD_BFN)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_SYNCD_BFN)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
-$(DOCKER_SYNCD_BFN)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
+DOCKER_SYNCD_PLATFORM_CODE = bfn
+include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+
+$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
+
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
+									$(LIBSWSSCOMMON_DBG) \
+									$(LIBSAIMETADATA_DBG) \
+									$(LIBSAIREDIS_DBG)
+

platform/broadcom/docker-orchagent-brcm.mk
@@ -1,24 +1,5 @@
 # docker image for orchagent
 
-DOCKER_ORCHAGENT_BRCM = docker-orchagent-brcm.gz
-$(DOCKER_ORCHAGENT_BRCM)_PATH = $(DOCKERS_PATH)/docker-orchagent
-$(DOCKER_ORCHAGENT_BRCM)_DEPENDS += $(SWSS) $(REDIS_TOOLS) $(IPROUTE2)
-ifeq ($(INSTALL_DEBUG_TOOLS), y)
-$(DOCKER_ORCHAGENT_BRCM)_DEPENDS += $(SWSS_DBG) \
-                                    $(LIBSWSSCOMMON_DBG) \
-                                    $(LIBSAIREDIS_DBG)
-endif
-$(DOCKER_ORCHAGENT_BRCM)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
-SONIC_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_BRCM)
-SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_ORCHAGENT_BRCM)
+DOCKER_ORCHAGENT_PLATFORM_CODE = brcm
+include $(PLATFORM_PATH)/../template/docker-orchagent-base.mk
 
-$(DOCKER_ORCHAGENT_BRCM)_CONTAINER_NAME = swss
-$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/network/interfaces:/etc/network/interfaces:ro
-$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/network/interfaces.d/:/etc/network/interfaces.d/:ro
-$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /host/machine.conf:/host/machine.conf:ro
-$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-$(DOCKER_ORCHAGENT_BRCM)_RUN_OPT += -v /var/log/swss:/var/log/swss:rw
-
-$(DOCKER_ORCHAGENT_BRCM)_BASE_IMAGE_FILES += swssloglevel:/usr/bin/swssloglevel
-$(DOCKER_ORCHAGENT_BRCM)_FILES += $(ARP_UPDATE_SCRIPT)

platform/broadcom/docker-syncd-brcm.mk
@@ -1,21 +1,18 @@
 # docker image for brcm syncd
 
-DOCKER_SYNCD_BRCM = docker-syncd-brcm.gz
-$(DOCKER_SYNCD_BRCM)_PATH = $(PLATFORM_PATH)/docker-syncd-brcm
-$(DOCKER_SYNCD_BRCM)_DEPENDS += $(SYNCD)
-$(DOCKER_SYNCD_BRCM)_FILES += $(DSSERVE) $(BCMCMD)
-$(DOCKER_SYNCD_BRCM)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
-SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_BRCM)
-ifneq ($(ENABLE_SYNCD_RPC),y)
-SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_SYNCD_BRCM)
-endif
+DOCKER_SYNCD_PLATFORM_CODE = brcm
+include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
-$(DOCKER_SYNCD_BRCM)_CONTAINER_NAME = syncd
-$(DOCKER_SYNCD_BRCM)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_SYNCD_BRCM)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
-$(DOCKER_SYNCD_BRCM)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
-$(DOCKER_SYNCD_BRCM)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-$(DOCKER_SYNCD_BRCM)_RUN_OPT += -v /host/warmboot:/var/warmboot
+$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
+$(DOCKER_SYNCD_BASE)_FILES += $(DSSERVE) $(BCMCMD)
 
-$(DOCKER_SYNCD_BRCM)_BASE_IMAGE_FILES += bcmcmd:/usr/bin/bcmcmd
-$(DOCKER_SYNCD_BRCM)_BASE_IMAGE_FILES += bcmsh:/usr/bin/bcmsh
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
+									$(LIBSWSSCOMMON_DBG) \
+									$(LIBSAIMETADATA_DBG) \
+									$(LIBSAIREDIS_DBG)
+
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+
+$(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcmcmd:/usr/bin/bcmcmd
+$(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcmsh:/usr/bin/bcmsh

commit 87617233eb1fe0fc3ec0727db7911904cb8232b3

[docker-syncd-vs]: fix syncd target for docker-syncd-vs.
The bug is introduced in commit

0bca6b8468daa6268476a70dc2adfe03644bcb8c

Signed-off-by: Guohan Lu <gulv@microsoft.com>

platform/vs/docker-syncd-vs.mk
@@ -3,7 +3,7 @@
 DOCKER_SYNCD_PLATFORM_CODE = vs
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
-$(DOCKER_SYNCD_SYNCD)_DEPENDS += $(SYNCD_VS)
+$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD_VS)
 
 $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_VS_DBG) \
 									$(LIBSWSSCOMMON_DBG) \