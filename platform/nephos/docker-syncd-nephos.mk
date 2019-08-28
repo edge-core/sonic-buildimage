f78fcb10082f7f3e6bad763540038d1a04138b87
simonJi2018 <37395146+simonJi2018@users.noreply.github.com>
2019-08-28 23:43:06 +0800

    [201811][platform/nephos]fix the compile error about syncd  (#3373)
    Fixed errors in the following files to resolve build failures
    - docker-ptf-nephos.mk\docker-syncd-nephos.mk\libsaithrift-dev.mk\rules.mk
    - Upgrade sai.mk for support sai_1.4.1 and upgrade sdk version to 3.0.0

--- platform/nephos/docker-syncd-nephos.mk
+++ platform/nephos/docker-syncd-nephos.mk
@@ -1,17 +1,17 @@
 # docker image for nephos syncd
 
-DOCKER_SYNCD_PLATFORM_CODE = bfn
+DOCKER_SYNCD_PLATFORM_CODE = nephos
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
-
-$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
-									$(LIBSWSSCOMMON_DBG) \
-									$(LIBSAIMETADATA_DBG) \
-									$(LIBSAIREDIS_DBG)
-
 $(DOCKER_SYNCD_BASE)_FILES += $(DSSERVE) $(NPX_DIAG)
 
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
+				    $(LIBSWSSCOMMON_DBG) \
+				    $(LIBSAIMETADATA_DBG) \
+				    $(LIBSAIREDIS_DBG)
+
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
 
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += npx_diag:/usr/bin/npx_diag
--- platform/nephos/rules.mk
+++ platform/nephos/rules.mk
@@ -8,21 +8,20 @@ include $(PLATFORM_PATH)/docker-syncd-nephos.mk
 include $(PLATFORM_PATH)/docker-syncd-nephos-rpc.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
-include $(PLATFORM_PATH)/python-saithrift.mk
 include $(PLATFORM_PATH)/docker-ptf-nephos.mk
 
 NPX_DIAG = npx_diag
 $(NPX_DIAG)_URL = "https://github.com/NephosInc/SONiC/raw/master/sdk/npx_diag"
 
 DSSERVE = dsserve
-$(DSSERVE)_URL = "https://sonicstorage.blob.core.windows.net/packages/20170518/dsserve?sv=2015-04-05&sr=b&sig=gyNbgSL%2FvpMXDdpboVkIJcTKMRdGgEaOR9OukHhEsu8%3D&se=2030-03-31T23%3A06%3A35Z&sp=r"
+$(DSSERVE)_URL = "https://sonicstorage.blob.core.windows.net/packages/20190307/dsserve?sv=2015-04-05&sr=b&sig=lk7BH3DtW%2F5ehc0Rkqfga%2BUCABI0UzQmDamBsZH9K6w%3D&se=2038-05-06T22%3A34%3A45Z&sp=r"
 
 SONIC_ONLINE_FILES += $(NPX_DIAG) $(DSSERVE)
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) $(DOCKER_FPM)
 
 # Inject nephos sai into sairedis
-$(LIBSAIREDIS)_DEPENDS += $(NEPHOS_SAI) $(NEPHOS_SAI_DEV) $(LIBSAITHRIFT_DEV_NEPHOS)
+$(LIBSAIREDIS)_DEPENDS += $(NEPHOS_SAI) $(NEPHOS_SAI_DEV) $(LIBSAITHRIFT_DEV)
 
 # Runtime dependency on nephos sai is set only for syncd
 $(SYNCD)_RDEPENDS += $(NEPHOS_SAI)
