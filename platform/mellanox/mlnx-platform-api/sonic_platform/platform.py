5c2d71138b85cfe6b489cf6bbdabffcadb7b41c3
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2019-08-29 02:59:37 +0800

    [Mellanox] optimize new platform api (#3289)
    optimize SFP module operations and fix issues.
    
    - split initialization of variant categories of devices and initialize each category of devices only when needed, so that unnecessary dependencies can be avoided.
    - update watchdog logic, only initializing watchdog when referenced.
    - support platform.py and enable to initialize variant devices on a host/docker basis
    - update init so that sonic_platform can be imported as a whole.

---
+++ platform/mellanox/mlnx-platform-api/sonic_platform/platform.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#############################################################################
+# Mellanox
+#
+# implementation of new platform api
+#############################################################################
+
+try:
+    import subprocess
+    from sonic_platform_base.platform_base import PlatformBase
+    from sonic_platform.chassis import Chassis
+except ImportError as e:
+    raise ImportError(str(e) + "- required module not found")
+
+class Platform(PlatformBase):
+    def __init__(self):
+        PlatformBase.__init__(self)
+        if self._is_host():
+            self._chassis = Chassis()
+        else:
+            self._chassis = Chassis()
+            self._chassis.initialize_psu()
+            self._chassis.initialize_fan()
+            self._chassis.initialize_eeprom()
+            self._chassis.initialize_components_list()
+
+    def _is_host(self):
+        """
+        Test whether current process is running on the host or an docker
+        return True for host and False for docker
+        """
+        is_host = False
+        try:
+            proc = subprocess.Popen("docker --version 2>/dev/null", stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
+            stdout = proc.communicate()[0]
+            proc.wait()
+            result = stdout.rstrip('\n')
+            if result != '':
+                is_host = True
+
+        except OSError, e:
+            pass
+
+        return is_host
