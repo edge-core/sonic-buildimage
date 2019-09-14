a2cd26a2e19231a26a82b03437c2a8d69cf73cea
Greg Paussa <50457169+gpaussabrcm@users.noreply.github.com>
2019-09-14 01:09:34 -0400

    [build] Avoid race condition in sonic-platform-common (#3457)
    Both python2 and python3 wheels being built out of the same
    source directory can interfere with each other when
    SONIC_BUILD_JOBS > 1.
    
    Signed-off-by: Greg Paussa <greg.paussa@broadcom.com>

--- rules/sonic-platform-common.mk
+++ rules/sonic-platform-common.mk
@@ -6,8 +6,9 @@ $(SONIC_PLATFORM_COMMON_PY2)_PYTHON_VERSION = 2
 SONIC_PYTHON_WHEELS += $(SONIC_PLATFORM_COMMON_PY2)
 
 # Als build sonic-platform-common into python3 wheel, so we can use PSU code in SNMP docker
-# Note: _DEPENDS macro is not defined
 SONIC_PLATFORM_COMMON_PY3 = sonic_platform_common-1.0-py3-none-any.whl
 $(SONIC_PLATFORM_COMMON_PY3)_SRC_PATH = $(SRC_PATH)/sonic-platform-common
 $(SONIC_PLATFORM_COMMON_PY3)_PYTHON_VERSION = 3
+# Synthetic dependency just to avoid race condition
+$(SONIC_PLATFORM_COMMON_PY3)_DEPENDS = $(SONIC_PLATFORM_COMMON_PY2)
 SONIC_PYTHON_WHEELS += $(SONIC_PLATFORM_COMMON_PY3)
