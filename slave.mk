0493b6274ead8719467d118a0ab4af9d064f6714
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2019-09-19 09:09:25 -0700

    Map /src & /debug for debug docker containers (#3470)
    * Add debug docker for SNMP.
    
    * Removed a redundant install of debug packages.
    Propagate the debug flag to template file to mount /dbg & /src to debug containers.
    
    * Revert the last change to retain the original

--- slave.mk
+++ slave.mk
@@ -581,6 +581,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
 	export swsssdk_py2_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(SWSSSDK_PY2))"
 	export platform_common_py2_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_PLATFORM_COMMON_PY2))"
 	export redis_dump_load_py2_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(REDIS_DUMP_LOAD_PY2))"
+	export install_debug_image="$(INSTALL_DEBUG_TOOLS)"
 
 	$(foreach docker, $($*_DOCKERS),\
 		export docker_image="$(docker)"
