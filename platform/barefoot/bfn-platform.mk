905d429022f20bf2c118d564f8c1c11c7491e340
John Cheung <john.cheung@barefootnetworks.com>
2019-09-24 16:59:25 -0700

    [barefoot]: updated SDK to 9.0.0 released debian package (#3471)

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,5 +1,5 @@
-BFN_PLATFORM = bfnplatform_9.0.0.cc6ccbe_pr_deb9.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_9_0/bfnplatform_9.0.0.cc6ccbe_pr_deb9.deb"
+BFN_PLATFORM = bfnplatform_9.0.0.e658347_deb9.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_9_0/bfnplatform_9.0.0.e658347_deb9.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
