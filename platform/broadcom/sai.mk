e7f1d4f3e273a6e6c506c860660fa0f795f442b0
Wenda Ni <wenni@microsoft.com>
2019-09-25 15:57:07 -0700

    Remove the divide by 4 operation to the under the hood SAI (#1532)
    * Remove the divide by 4 operation to the under the hood SAI
    
    This is to avoid the need and thus the confusion for application program to know
    the mmu internal architecture
    
    This change must have support from SAI change to reach the correct
    config
    
    Signed-off-by: Wenda <wenni@microsoft.com>
    
    * Relegate the divide by 4 operation to the under the hood SAI for egress
    lossless pool
    
    Extend to 7060 and 6100
    
    Signed-off-by: Wenda <wenni@microsoft.com>
    
    * Add more TH/TH2 hwskus
    
    Signed-off-by: Wenda Ni <wenni@microsoft.com>
    
    * Update config test
    
    Signed-off-by: Wenda Ni <wenni@microsoft.com>
    
    * Add TH2 ingress lossy profile
    
    Signed-off-by: Wenda Ni <wenni@microsoft.com>
    
    * Move the divide by 4 operation to SAI internal
    
    Signed-off-by: Wenda Ni <wenni@microsoft.com>
    
    * [bcm SAI] Upgrade Broadcom SAI to version 3.5.3.1-15
    
    - Broadcom SAI 3.5 GA release 20190924.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.5.2.3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/jessie/libsaibcm_3.5.2.3_amd64.deb?sv=2015-04-05&sr=b&sig=OCYEP1DOHEVlfy6qjckfME%2FoD%2F914zIueu4UKgNsyQA%3D&se=2156-05-18T17%3A37%3A54Z&sp=r"
+BRCM_SAI = libsaibcm_3.5.3.1-15_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/jessie/libsaibcm_3.5.3.1-15_amd64.deb?sv=2015-04-05&sr=b&sig=tZ1NxKEojTqKya6uPoAHckAhcUrV2qegAyp2goN1Yms%3D&se=2033-05-22T17%3A22%3A02Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.5.2.3_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.5.3.1-15_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/jessie/libsaibcm-dev_3.5.2.3_amd64.deb?sv=2015-04-05&sr=b&sig=Ohiw%2FaNhnbB%2FKFn%2BTaZgDrG1ziGfwjwrr2l%2BoIKCmA4%3D&se=2156-05-18T17%3A38%3A34Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/jessie/libsaibcm-dev_3.5.3.1-15_amd64.deb?sv=2015-04-05&sr=b&sig=1buFitrRydJ01Y%2BvmeUYveb1R1UAEQOiYMwgcyPZikw%3D&se=2033-05-22T17%3A21%3A38Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
