a117b25446a97477486143c4a9780d0163c207d7
yozhao101 <56170650+yozhao101@users.noreply.github.com>
2019-11-06 11:02:57 -0800

    [Services] Restart LLDP service upon unexpected critical process exit. (#3713)
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- rules/docker-lldp-sv2.mk
+++ rules/docker-lldp-sv2.mk
@@ -30,3 +30,4 @@ $(DOCKER_LLDP_SV2)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 
 $(DOCKER_LLDP_SV2)_BASE_IMAGE_FILES += lldpctl:/usr/bin/lldpctl
 $(DOCKER_LLDP_SV2)_BASE_IMAGE_FILES += lldpcli:/usr/bin/lldpcli
+$(DOCKER_LLDP_SV2)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
