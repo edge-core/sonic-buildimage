1d5005bc8cb624079fc07968e6db2b3fc08fbb07
Dong Zhang <41927498+dzhangalibaba@users.noreply.github.com>
2019-11-20 10:40:19 -0800

    [multiDB] add database_config.json into vs images (#3757)

--- platform/vs/docker-sonic-vs/start.sh
+++ platform/vs/docker-sonic-vs/start.sh
@@ -29,7 +29,8 @@ rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
 
-mkdir -p /var/run/redis
+mkdir -p /var/run/redis/sonic-db
+cp /etc/default/sonic-db/database_config.json /var/run/redis/sonic-db/
 
 supervisorctl start redis-server
 
