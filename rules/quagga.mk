92df547d838bfb68427820d7804a4da8f365d586
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2019-12-06 20:51:46 -0800

    Build debug docker for fpm-quagga. (#3855)

--- rules/quagga.mk
+++ rules/quagga.mk
@@ -9,3 +9,9 @@ SONIC_DPKG_DEBS += $(QUAGGA)
 
 QUAGGA_DBG = quagga-dbg_$(QUAGGA_VERSION_FULL)_amd64.deb
 $(eval $(call add_derived_package,$(QUAGGA),$(QUAGGA_DBG)))
+
+# The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
+# are archived into debug one image to facilitate debugging.
+#
+DBG_SRC_ARCHIVE += sonic-quagga
+
