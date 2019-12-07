d08730641106d405213efdd128a30a4bf3547af6
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2019-12-06 20:51:31 -0800

    Added debug symbol to dhcp-relay. (#3850)
    * Added debug symbol to dhcp-relay.
    Note: Master is different; Hence explicitly for 201811 only.
    
    * Include debug symbols of isc-dhcp in its debug docker.
    Include isc-dhcp src in source archive.

--- rules/isc-dhcp.mk
+++ rules/isc-dhcp.mk
@@ -9,4 +9,13 @@ $(ISC_DHCP_COMMON)_SRC_PATH = $(SRC_PATH)/isc-dhcp
 SONIC_MAKE_DEBS += $(ISC_DHCP_COMMON)
 
 ISC_DHCP_RELAY = isc-dhcp-relay_$(ISC_DHCP_VERSION)_amd64.deb
+
+ISC_DHCP_DBG = isc-dhcp-dbg_$(ISC_DHCP_VERSION)_amd64.deb
+
 $(eval $(call add_derived_package,$(ISC_DHCP_COMMON),$(ISC_DHCP_RELAY)))
+$(eval $(call add_derived_package,$(ISC_DHCP_RELAY),$(ISC_DHCP_DBG)))
+
+# The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
+# are archived into debug one image to facilitate debugging.
+#
+DBG_SRC_ARCHIVE += isc-dhcp
