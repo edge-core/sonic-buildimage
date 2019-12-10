fec80293dd22c358b5ccc68eedc23d9feb15aca5
rajendra-dendukuri <47423477+rajendra-dendukuri@users.noreply.github.com>
2019-12-10 11:16:56 -0500

    ZTP infrastructure changes to support DHCP discovery provisioning data (#3298)
    * ZTP infrastructure changes to support DHCP discovery provisioning data
    
    - Dynamically generate DHCP client configuration based on current ZTP state
    - Added support to request and process hostname when using DHCPv6
    - Do not process graphservice url dhcp option if ZTP is enabled, ZTP service
    will process it
    - Generate /e/n/i file with all active interfaces seeking address assignment
    via DHCP. Only interfaces that are created in Linux will be added to /e/n/i.
    Also DHCP is started only on linked up in-band interfaces.
    
    Signed-off-by: Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -2,10 +2,40 @@
 
 ifdown --force eth0
 
-sonic-cfggen -d -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
+# Check if ZTP DHCP policy has been installed
+if [ -e /etc/network/ifupdown2/policy.d/ztp_dhcp.json ]; then
+    # Obtain port operational state information
+    redis-dump -d 0 -k "PORT_TABLE:Ethernet*"  -y > /tmp/ztp_port_data.json
+
+    if [ $? -ne 0 ] || [ ! -e /tmp/ztp_port_data.json ] || [ "$(cat /tmp/ztp_port_data.json)" = "" ]; then
+        echo "{}" > /tmp/ztp_port_data.json
+    fi
+
+    # Create an input file with ztp input information
+    echo "{ \"PORT_DATA\" : $(cat /tmp/ztp_port_data.json) }" > \
+          /tmp/ztp_input.json
+else
+    echo "{ \"ZTP_DHCP_DISABLED\" : \"true\" }" > /tmp/ztp_input.json
+fi
+
+# Create /e/n/i file for existing and active interfaces
+sonic-cfggen -d -j /tmp/ztp_input.json -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
 
 [ -f /var/run/dhclient.eth0.pid ] && kill `cat /var/run/dhclient.eth0.pid` && rm -f /var/run/dhclient.eth0.pid
+[ -f /var/run/dhclient6.eth0.pid ] && kill `cat /var/run/dhclient6.eth0.pid` && rm -f /var/run/dhclient6.eth0.pid
 
+for intf_pid in $(ls -1 /var/run/dhclient*.Ethernet*.pid 2> /dev/null); do
+    [ -f ${intf_pid} ] && kill `cat ${intf_pid}` && rm -f ${intf_pid}
+done
+
+sonic-cfggen -d -j /tmp/ztp_input.json -t /usr/share/sonic/templates/90-dhcp6-systcl.conf.j2 > /etc/sysctl.d/90-dhcp6-systcl.conf
+# Read sysctl conf files again
+sysctl -p /etc/sysctl.d/90-dhcp6-systcl.conf
+
+sonic-cfggen -d -j /tmp/ztp_input.json -t /usr/share/sonic/templates/dhclient.conf.j2 > /etc/dhcp/dhclient.conf
 systemctl restart networking
+
+# Clean-up created files
+rm -f /tmp/ztp_input.json /tmp/ztp_port_data.json
 
 ifdown lo && ifup lo
