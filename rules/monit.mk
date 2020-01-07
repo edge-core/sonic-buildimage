5ea7372dbe4aa9f5eb6cd0b790535ebb479ae70d
Ying Xie <yxieca@users.noreply.github.com>
2020-01-07 07:41:40 -0800

    [201811][monit] address build issue: hard code ARCH to amd64 (#3982)
    * [201811][monit] address build issue: hard code ARCH to amd64
    
    - also hard code the debian package path as in 201811 branch.
    
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- rules/monit.mk
+++ rules/monit.mk
@@ -4,11 +4,11 @@ MONIT_VERSION = 5.20.0-6
 
 export MONIT_VERSION
 
-MONIT = monit_$(MONIT_VERSION)_$(CONFIGURED_ARCH).deb
+MONIT = monit_$(MONIT_VERSION)_amd64.deb
 $(MONIT)_SRC_PATH = $(SRC_PATH)/monit
 SONIC_MAKE_DEBS += $(MONIT)
 
 SONIC_STRETCH_DEBS += $(MONIT)
 
-MONIT_DBG = monit-dbgsym_$(MONIT_VERSION)_$(CONFIGURED_ARCH).deb
+MONIT_DBG = monit-dbgsym_$(MONIT_VERSION)_amd64.deb
 $(eval $(call add_derived_package,$(MONIT),$(MONIT_DBG)))
