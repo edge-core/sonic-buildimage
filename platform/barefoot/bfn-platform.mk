f3b5b13a7551f974128c62d2b3f51d20cc199525
Andriy Kokhan <43479230+akokhan@users.noreply.github.com>
2020-01-10 12:56:42 -0800

    [barefoot]: Update SDK package (#3950)
    Updated SDK package with X1/X2 profiles
    
    Signed-off-by: Andriy Kokhan <akokhan@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,5 +1,5 @@
-BFN_PLATFORM = bfnplatform_20191115_deb9.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/bfnplatform_20191115_deb9.deb"
+BFN_PLATFORM = bfnplatform_20191227_deb9.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/bfnplatform_20191227_deb9.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,5 +1,5 @@
-BFN_SAI = bfnsdk_20191115_deb9.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/bfnsdk_20191115_deb9.deb"
+BFN_SAI = bfnsdk_20191227_deb9.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/bfnsdk_20191227_deb9.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_SAI)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
