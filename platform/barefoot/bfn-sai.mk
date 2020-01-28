d2362986883502780ebf6665395fbd8d027fb611
Andriy Kokhan <43479230+akokhan@users.noreply.github.com>
2020-01-28 09:23:36 -0800

    [bfn] Added GENL3 dependency for SAI (#4069)
    Signed-off-by: Andriy Kokhan <akokhan@barefootnetworks.com>

--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,5 +1,8 @@
 BFN_SAI = bfnsdk_9.1.0.fddc672_deb9.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_9_1/bfnsdk_9.1.0.fddc672_deb9.deb"
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/rel_9_1/$(BFN_SAI)"
+
+$(BFN_SAI)_DEPENDS += $(LIBNL_GENL3_DEV)
+$(BFN_SAI)_RDEPENDS += $(LIBNL_GENL3)
 
 SONIC_ONLINE_DEBS += $(BFN_SAI)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
