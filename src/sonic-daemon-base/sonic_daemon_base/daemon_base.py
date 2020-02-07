974e6e98c34b4d3adb9e926e0d07992d5903f998
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2020-02-08 04:34:29 +0800

    [daemon_base]fix error: syslog is not a member of DaemonBase (#4039)

--- src/sonic-daemon-base/sonic_daemon_base/daemon_base.py
+++ src/sonic-daemon-base/sonic_daemon_base/daemon_base.py
@@ -102,15 +102,15 @@ class DaemonBase(object):
     # Signal handler
     def signal_handler(self, sig, frame):
         if sig == signal.SIGHUP:
-            self.syslog.syslog(self.syslog.LOG_INFO, "Caught SIGHUP - ignoring...")
+            syslog.syslog(syslog.LOG_INFO, "Caught SIGHUP - ignoring...")
         elif sig == signal.SIGINT:
-            self.syslog.syslog(self.syslog.LOG_INFO, "Caught SIGINT - exiting...")
+            syslog.syslog(syslog.LOG_INFO, "Caught SIGINT - exiting...")
             sys.exit(128 + sig)
         elif sig == signal.SIGTERM:
-            self.syslog.syslog(self.syslog.LOG_INFO, "Caught SIGTERM - exiting...")
+            syslog.syslog(syslog.LOG_INFO, "Caught SIGTERM - exiting...")
             sys.exit(128 + sig)
         else:
-            self.syslog.syslog(self.syslog.LOG_WARNING, "Caught unhandled signal '" + sig + "'")
+            syslog.syslog(syslog.LOG_WARNING, "Caught unhandled signal '" + sig + "'")
 
     # Returns platform and hwsku
     def get_platform_and_hwsku(self):
