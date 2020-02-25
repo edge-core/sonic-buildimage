71592fc86669bfcf54a6459e2a0b4ed7f2df7a75
arheneus@marvell.com <51254330+antony-rheneus@users.noreply.github.com>
2020-02-25 23:14:45 +0530

    [Platform] Marvell Makefile updates/cleanup (#4168)
    Signed-off-by: Antony Rheneus <arheneus@marvell.com>

--- platform/marvell/sai.mk
+++ platform/marvell/sai.mk
@@ -1,6 +1,7 @@
 # Marvell SAI
 
-export MRVL_SAI = mrvllibsai_amd64_1.4.1.deb
+export MRVL_SAI_VERSION = 1.5.1
+export MRVL_SAI = mrvllibsai_amd64_$(MRVL_SAI_VERSION).deb
 
 $(MRVL_SAI)_SRC_PATH = $(PLATFORM_PATH)/sai
 SONIC_MAKE_DEBS += $(MRVL_SAI)
