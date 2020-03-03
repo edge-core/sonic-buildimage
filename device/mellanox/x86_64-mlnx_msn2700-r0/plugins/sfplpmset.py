0050cc1ce7b0c3f4c633572cf2514c72cf320c58
Kebo Liu <kebol@mellanox.com>
2020-03-04 03:19:30 +0800

    filter out CPU ports to avoid any operation on them (#4197)

--- device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfplpmset.py
+++ device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfplpmset.py
@@ -18,13 +18,18 @@ PMAOS_RST = 0
 PMAOS_ENABLE = 1
 PMAOS_DISABLE = 2
 
+PORT_TYPE_CPU = 4
 PORT_TYPE_NVE = 8
 PORT_TYPE_OFFSET = 28
 PORT_TYPE_MASK = 0xF0000000
 NVE_MASK = PORT_TYPE_MASK & (PORT_TYPE_NVE << PORT_TYPE_OFFSET)
+CPU_MASK = PORT_TYPE_MASK & (PORT_TYPE_CPU << PORT_TYPE_OFFSET)
 
 def is_nve(port):
     return (port & NVE_MASK) != 0
+
+def is_cpu(port):
+    return (port & CPU_MASK) != 0
 
 def is_port_admin_status_up(log_port):
     oper_state_p = new_sx_port_oper_state_t_p()
@@ -57,6 +62,7 @@ def get_log_ports(handle, sfp_module):
     for i in range(0, port_cnt):
         port_attributes = sx_port_attributes_t_arr_getitem(port_attributes_list, i)
         if is_nve(int(port_attributes.log_port)) == False \
+           and is_cpu(int(port_attributes.log_port)) == False \
            and port_attributes.port_mapping.module_port == sfp_module \
            and is_port_admin_status_up(port_attributes.log_port):
             log_port_list.append(port_attributes.log_port)
