055d7cd19d7d827037e6ce39dc011fc72eb24f36
Andriy Kokhan <43479230+akokhan@users.noreply.github.com>
2020-03-16 19:14:43 +0200

    [BFN] Updated Barefoot DEBs to 20200205 (#4261)
    Signed-off-by: Andriy Kokhan <akokhan@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,5 +1,5 @@
-BFN_PLATFORM = bfnplatform_20191115_deb9.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/bfnplatform_20191115_deb9.deb"
+BFN_PLATFORM = bfnplatform_20200205_deb9.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,5 +1,8 @@
-BFN_SAI = bfnsdk_20191115_deb9.deb
-$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/bfnsdk_20191115_deb9.deb"
+BFN_SAI = bfnsdk_20200205_deb9.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_SAI)"
+
+$(BFN_SAI)_DEPENDS += $(LIBNL_GENL3_DEV)
+$(BFN_SAI)_RDEPENDS += $(LIBNL_GENL3)
 
 SONIC_ONLINE_DEBS += $(BFN_SAI)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
