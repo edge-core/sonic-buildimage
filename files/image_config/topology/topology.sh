0099305475759c3cf70561495ae3483aa42a2b07
SuvarnaMeenakshi <50386592+SuvarnaMeenakshi@users.noreply.github.com>
2020-03-31 10:06:19 -0700

    Multi-ASIC implementation (#3888)
    Changes made to support multi-asic platform. Added multi-instance support for swss, syncd, database, bgp, teamd and lldp.

--- files/image_config/topology/topology.sh
+++ files/image_config/topology/topology.sh
@@ -6,18 +6,16 @@
 #
 
 start() {
-	DB_FIRST_INSTANCE="/var/run/redis0/redis.sock"
-	TOPOLOGY_SCRIPT="topology.sh"
-	PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform -s $DB_FIRST_INSTANCE`
-	HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' -s $DB_FIRST_INSTANCE`
-        /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT start
+    TOPOLOGY_SCRIPT="topology.sh"
+    PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform`
+    HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]'`
+    /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT start
 }
 stop() {
-        DB_FIRST_INSTANCE="/var/run/redis0/redis.sock"
-        TOPOLOGY_SCRIPT="topology.sh"
-        PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform -s $DB_FIRST_INSTANCE`
-        HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' -s $DB_FIRST_INSTANCE`
-        /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT stop 
+    TOPOLOGY_SCRIPT="topology.sh"
+    PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform`
+    HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]'`
+    usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT stop 
 }
 
 case "$1" in
