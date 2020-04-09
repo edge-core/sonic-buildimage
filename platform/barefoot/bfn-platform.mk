36752b8775e6a2e6990d21368f251cf0092c7038
Andriy Kokhan <43479230+akokhan@users.noreply.github.com>
2020-04-09 08:49:45 +0300

    [BFN] Update Barefoot SDK packages (#4397)
    Signed-off-by: Andriy Kokhan <akokhan@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,4 +1,4 @@
-BFN_PLATFORM = bfnplatform_20200205_deb9.deb
+BFN_PLATFORM = bfnplatform_20200407_deb9.deb
 $(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
