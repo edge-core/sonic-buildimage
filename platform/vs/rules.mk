f3a1156d451ceb6bf28035faf6828f0be1460409
Guohan Lu <lguohan@gmail.com>
2020-04-15 11:04:57 +0000

    [vsraw]: build sonic-vs.raw image

--- platform/vs/rules.mk
+++ platform/vs/rules.mk
@@ -5,5 +5,6 @@ include $(PLATFORM_PATH)/docker-syncd-vs.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/onie.mk
 include $(PLATFORM_PATH)/kvm-image.mk
+include $(PLATFORM_PATH)/raw-image.mk
 
-SONIC_ALL += $(SONIC_ONE_IMAGE) $(SONIC_KVM_IMAGE) $(DOCKER_SONIC_VS)
+SONIC_ALL += $(SONIC_ONE_IMAGE) $(SONIC_KVM_IMAGE) $(DOCKER_SONIC_VS) $(SONIC_RAW_IMAGE)
