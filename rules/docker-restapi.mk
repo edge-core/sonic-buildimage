12400a447c974c42c7782e7c8ba0ea26ee1cb998
Sumukha Tumkur Vani <sumukhatv@outlook.com>
2020-04-15 02:08:45 -0700

    [docker-restapi]: Fix authentication in restapi (#4383)
    - enabling http/https endpoint and mounting the certificates directory
    - updating the sonic-restapi submodule

--- rules/docker-restapi.mk
+++ rules/docker-restapi.mk
@@ -18,5 +18,11 @@ endif
 
 $(DOCKER_RESTAPI)_CONTAINER_NAME = restapi
 $(DOCKER_RESTAPI)_RUN_OPT += --cap-add NET_ADMIN --privileged -t
+$(DOCKER_RESTAPI)_RUN_OPT += --network="host"
 $(DOCKER_RESTAPI)_RUN_OPT += -v /var/run/redis/redis.sock:/var/run/redis/redis.sock
+$(DOCKER_RESTAPI)_RUN_OPT += -v /etc/sonic/certificates:/etc/sonic/certificates:ro
+$(DOCKER_RESTAPI)_RUN_OPT += -p=8081:8081/tcp
 $(DOCKER_RESTAPI)_RUN_OPT += -p=8090:8090/tcp
+
+$(DOCKER_RESTAPI)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
+$(DOCKER_RESTAPI)_BASE_IMAGE_FILES += monit_restapi:/etc/monit/conf.d
