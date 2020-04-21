13bef09889f7331becad385a02a631785313ebfc
Nazarii Hnydyn <nazariig@mellanox.com>
2020-04-21 05:02:46 +0300

    [mellanox]: Add DPKG local caching support. (#4441)
    Signed-off-by: Nazarii Hnydyn <nazariig@mellanox.com>

--- platform/mellanox/mlnx-onie-fw-update.mk
+++ platform/mellanox/mlnx-onie-fw-update.mk
@@ -1,7 +1,7 @@
 # onie update tool
 
 MLNX_ONIE_FW_UPDATE = mlnx-onie-fw-update.sh
-$(MLNX_ONIE_FW_UPDATE)_PATH = platform/mellanox/
+$(MLNX_ONIE_FW_UPDATE)_PATH = $(PLATFORM_PATH)/
 SONIC_COPY_FILES += $(MLNX_ONIE_FW_UPDATE)
 
 MLNX_FILES += $(MLNX_ONIE_FW_UPDATE)
