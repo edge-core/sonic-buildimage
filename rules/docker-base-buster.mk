fc55329b220e21a25dd145197002a356ef271641
joyas-joseph <51463120+joyas-joseph@users.noreply.github.com>
2020-04-28 15:57:13 -0700

    [mgmt-framework]: convert mgmt-framework to use buster docker (#4480)
    This change adds support to build dockers using buster as base.
    - Define docker-base-buster using docker-base-stretch as starting point
    - Define docker-config-engine-buster using docker-config-engine-stretch as starting point.
    - sonic-mgmt-framework docker is updated to build using buster as base
    
    Signed-off-by: Joyas Joseph <joyas_joseph@dell.com>

---
+++ rules/docker-base-buster.mk
@@ -0,0 +1,17 @@
+# Docker base image (based on Debian Buster)
+
+DOCKER_BASE_BUSTER = docker-base-buster.gz
+$(DOCKER_BASE_BUSTER)_PATH = $(DOCKERS_PATH)/docker-base-buster
+
+$(DOCKER_BASE_BUSTER)_DEPENDS += $(SUPERVISOR) $(REDIS_TOOLS)
+$(DOCKER_BASE_BUSTER)_DEPENDS += $(SOCAT)
+
+GDB = gdb
+GDBSERVER = gdbserver
+VIM = vim
+OPENSSH = openssh-client
+SSHPASS = sshpass
+STRACE = strace
+$(DOCKER_BASE_BUSTER)_DBG_IMAGE_PACKAGES += $(GDB) $(GDBSERVER) $(VIM) $(OPENSSH) $(SSHPASS) $(STRACE)
+
+SONIC_DOCKER_IMAGES += $(DOCKER_BASE_BUSTER)
