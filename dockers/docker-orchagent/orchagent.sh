93ade1d96b167af67adf3cedc818d92b252a3b46
judyjoseph <53951155+judyjoseph@users.noreply.github.com>
2020-04-28 20:32:22 -0700

    [Multi-ASIC] To pass the asic instance ID to orchagent, Advance the swss, swss-common submodules. (#4465)
    * Multi-ASIC platforms will have the ID field in the DEVICE_METADATA, which will be pulled and
    will be used when starting the orchagent process with the new option [-i INST_ID]
    This is currently added only for Broadcom ASIC based platforms
    
    * Making the asic instance ID passing global across asics/platforms.
    Also changed the config DB id field to asic_id
    
    * Minor updates
    
    * Advance sonic-swss submodule
    
    * Advance swss_common submodule as well due to dependencies

--- dockers/docker-orchagent/orchagent.sh
+++ dockers/docker-orchagent/orchagent.sh
@@ -17,6 +17,22 @@ ORCHAGENT_ARGS="-d /var/log/swss "
 # Set orchagent pop batch size to 8192
 ORCHAGENT_ARGS+="-b 8192 "
 
+# Check if there is an "asic_id field" in the DEVICE_METADATA in configDB.
+#"DEVICE_METADATA": {
+#    "localhost": {
+#        ....
+#        "asic_id": "0",
+#    }
+#},
+# ID field could be integers just to denote the asic instance like 0,1,2...
+# OR could be PCI device ID's which will be strings like "03:00.0"
+# depending on what the SAI/SDK expects.
+asic_id=`sonic-cfggen -d -v DEVICE_METADATA.localhost.asic_id`
+if [ -n "$asic_id" ]
+then
+    ORCHAGENT_ARGS+="-i $asic_id "
+fi
+
 # Add platform specific arguments if necessary
 if [ "$platform" == "broadcom" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
