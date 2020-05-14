eb41fd5df6a4bd49157e6a594e07ee99261238cb
Guohan Lu <lguohan@gmail.com>
2020-05-14 22:42:23 +0000

    [docker-nat]: use service dependency in supervisord to start servicesx
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- dockers/docker-nat/start.sh
+++ dockers/docker-nat/start.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-rm -f /var/run/rsyslogd.pid
 rm -f /var/run/nat/*
 
 mkdir -p /var/warmboot/nat
-
-supervisorctl start rsyslogd
-
-supervisorctl start natmgrd
-
-supervisorctl start natsyncd
-
-supervisorctl start restore_nat_entries
-
