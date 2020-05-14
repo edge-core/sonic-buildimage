267b0b7aa8ed6c732fafa59e0ee862526110a96c
Guohan Lu <lguohan@gmail.com>
2020-05-14 22:44:41 +0000

    [docker-iccpd]: use service dependency in supervisord to start services
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- dockers/docker-iccpd/start.sh
+++ dockers/docker-iccpd/start.sh
@@ -10,9 +10,3 @@ sonic-cfggen -d -t /usr/share/sonic/templates/iccpd.j2 > $ICCPD_CONF_PATH/iccpd.
 
 mkdir -p /var/sonic
 echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
-
-rm -f /var/run/rsyslogd.pid
-
-supervisorctl start rsyslogd
-
-supervisorctl start iccpd
