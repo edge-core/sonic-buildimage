7158ccd30d81b6284214d4c8d570c22ddcff78a8
Guohan Lu <lguohan@gmail.com>
2020-05-16 01:12:26 +0000

    [docker-teamd]: use service dependency in supervisord to start services

--- dockers/docker-teamd/start.sh
+++ dockers/docker-teamd/start.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-rm -f /var/run/rsyslogd.pid
 rm -f /var/run/teamd/*
 
 mkdir -p /var/warmboot/teamd
-
-supervisorctl start rsyslogd
-
-supervisorctl start teammgrd
-
-supervisorctl start teamsyncd
