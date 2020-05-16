763673993e495a4fdcb228884f0c6dd73887f267
Guohan Lu <lguohan@gmail.com>
2020-05-16 01:00:47 +0000

    [docker-pmon]: use service dependency in supervisord to start services

---
+++ dockers/docker-platform-monitor/start.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+declare -r EXIT_SUCCESS="0"
+
+mkdir -p /var/sonic
+echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
+
+# If this platform has synchronization script, run it
+if [ -e /usr/share/sonic/platform/platform_wait ]; then
+    /usr/share/sonic/platform/platform_wait
+    EXIT_CODE="$?"
+    if [ "${EXIT_CODE}" != "${EXIT_SUCCESS}" ]; then
+        supervisorctl shutdown
+        exit "${EXIT_CODE}"
+    fi
+fi
+
+# If the sonic-platform package is not installed, try to install it
+pip show sonic-platform > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    SONIC_PLATFORM_WHEEL="/usr/share/sonic/platform/sonic_platform-1.0-py2-none-any.whl"
+    echo "sonic-platform package not installed, attempting to install..."
+    if [ -e ${SONIC_PLATFORM_WHEEL} ]; then
+       pip install ${SONIC_PLATFORM_WHEEL}
+       if [ $? -eq 0 ]; then
+          echo "Successfully installed ${SONIC_PLATFORM_WHEEL}"
+       else
+          echo "Error: Failed to install ${SONIC_PLATFORM_WHEEL}"
+       fi
+    else
+       echo "Error: Unable to locate ${SONIC_PLATFORM_WHEEL}"
+    fi
+fi
