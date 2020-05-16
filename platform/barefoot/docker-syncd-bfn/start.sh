0936e9abc54a432133c1f40ddb5600fce63cd7e4
Guohan Lu <lguohan@gmail.com>
2020-05-16 16:06:05 +0000

    [docker-syncd-bfn]: use service dependency in supervisord to start services

--- platform/barefoot/docker-syncd-bfn/start.sh
+++ platform/barefoot/docker-syncd-bfn/start.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-rm -f /var/run/rsyslogd.pid
-
-supervisorctl start rsyslogd
-
 . /opt/bfn/install/bin/dma_setup.sh
 # . /opt/bfn/install/bin/bf_kdrv_mod_load /opt/bfn/install
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/bfn/install/lib supervisorctl start syncd
