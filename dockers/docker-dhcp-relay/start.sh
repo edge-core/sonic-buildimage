b378b4d249011464efa055abea0f6e73dc0eb817
Guohan Lu <lguohan@gmail.com>
2020-05-16 15:27:31 +0000

    [docker-dhcp-relay]: use service dependency in supervisord to start services

--- dockers/docker-dhcp-relay/start.sh
+++ dockers/docker-dhcp-relay/start.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Remove stale rsyslog PID file if it exists
-rm -f /var/run/rsyslogd.pid
-
-# Start rsyslog
-supervisorctl start rsyslogd
-
 # If our supervisor config has entries in the "isc-dhcp-relay" group...
 if [ $(supervisorctl status | grep -c "^isc-dhcp-relay:") -gt 0 ]; then
     # Wait for all interfaces to come up and be assigned IPv4 addresses before
@@ -15,13 +9,4 @@ if [ $(supervisorctl status | grep -c "^isc-dhcp-relay:") -gt 0 ]; then
     # relay agent starts, it will not listen or send on that interface for the
     # lifetime of the process.
     /usr/bin/wait_for_intf.sh
-
-    # Start all DHCP relay agent(s)
-    supervisorctl start isc-dhcp-relay:*
-fi
-
-# If our supervisor config has entries in the "dhcpmon" group...
-if [ $(supervisorctl status | grep -c "^dhcpmon:") -gt 0 ]; then
-    # Start all DHCP Monitor daemon(s)
-    supervisorctl start dhcpmon:*
 fi
