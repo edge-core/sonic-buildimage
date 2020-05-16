4d2f9d12450790c980ae5310af54edf952569e7b
Guohan Lu <lguohan@gmail.com>
2020-05-16 01:08:00 +0000

    [docker-telemetry]: use service dependency in supervisord to start services
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- dockers/docker-sonic-telemetry/start.sh
+++ dockers/docker-sonic-telemetry/start.sh
@@ -2,10 +2,3 @@
 
 mkdir -p /var/sonic
 echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
-
-rm -f /var/run/rsyslogd.pid
-
-supervisorctl start rsyslogd
-
-supervisorctl start telemetry
-supervisorctl start dialout
