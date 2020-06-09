9505bdb91093e5f3b3defbdd68b855dd68af850f
joyas-joseph <51463120+joyas-joseph@users.noreply.github.com>
2020-06-09 09:07:25 -0700

    [docker-syncd-vs]: Convert syncd-vs docker to buster (#4726)
    Signed-off-by: Joyas Joseph <joyas_joseph@dell.com>

--- platform/centec/docker-syncd-centec.mk
+++ platform/centec/docker-syncd-centec.mk
@@ -10,6 +10,8 @@ $(DOCKER_SYNCD_CENTEC)_DEPENDS += $(SYNCD_DBG) \
                                   $(LIBSAIMETADATA_DBG) \
                                   $(LIBSAIREDIS_DBG)
 
+SONIC_STRETCH_DOCKERS += $(DOCKER_SYNCD_BASE)
+SONIC_STRETCH_DBG_DOCKERS += $(DOCKER_SYNCD_BASE_DBG)
 
 $(DOCKER_SYNCD_CENTEC)_RUN_OPT += --privileged -t
 $(DOCKER_SYNCD_CENTEC)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
