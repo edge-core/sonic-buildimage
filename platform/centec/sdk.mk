830c9eabd41a8541683b0512b669984a39fa010e
taochengyi <54564117+taocy001@users.noreply.github.com>
2020-06-10 16:32:08 +0800

    [centec]: update SAI to v1.6.1 for centec x86 platform (#4730)

--- platform/centec/sdk.mk
+++ platform/centec/sdk.mk
@@ -1,5 +1,5 @@
 # Centec SAI
-CENTEC_SAI = libsai_1.5.1_amd64.deb
-$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/libsai_1.5.1-1.0_amd64.deb
+CENTEC_SAI = libsai_1.6.1_amd64.deb
+$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/libsai_1.6.1-1.0_amd64.deb
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
