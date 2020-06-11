5d5d5739c2d80feb08cae2d3baaaeb0b745bc836
Joe LeVeque <jleveque@users.noreply.github.com>
2020-06-11 16:04:23 -0700

    [dockers] Rename 'docker-snmp-sv2' to 'docker-snmp' (#4699)
    The `-sv2` suffix was used to differentiate SNMP Dockers when we transitioned from "SONiCv1" to "SONiCv2", about four years ago. The old Docker materials were removed long ago; there is no need to keep this suffix. Removing it aligns the name with all the other Dockers.
    
    Also edit Monit configuration to detect proper snmp-subagent command line in Buster, and make snmpd command line matching more robust.

--- platform/generic/rules.mk
+++ platform/generic/rules.mk
@@ -2,8 +2,8 @@ include $(PLATFORM_PATH)/aboot-image.mk
 include $(PLATFORM_PATH)/onie-image.mk
 
 SONIC_ALL += $(DOCKER_DATABASE) \
+         $(DOCKER_SNMP) \
          $(DOCKER_LLDP) \
-         $(DOCKER_SNMP_SV2) \
          $(DOCKER_PLATFORM_MONITOR) \
          $(DOCKER_DHCP_RELAY) \
          $(DOCKER_PTF) \
