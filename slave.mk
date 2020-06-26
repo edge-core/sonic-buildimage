94d448e9bd884932582a83f7cc08fc1dfe3bf579
Praveen Chaudhary <pchaudhary@linkedin.com>
2020-06-26 11:32:35 -0700

    [slave.mk]: Adding support to specify debs dependencies for python-debs package. (#4849)
    **- How I did it**
    added below line:
    $$(addsuffix -install,$$(addprefix $(DEBS_PATH)/,$$($$*_DEBS_DEPENDS))) \
    
    **- How to verify it**
    Added below dependencies in sonic-utils
    ```
    SONIC_UTILS = python-sonic-utilities_1.2-1_all.deb
    $(SONIC_UTILS)_SRC_PATH = $(SRC_PATH)/sonic-utilities
    $(SONIC_UTILS)_DEBS_DEPENDS = $(LIBYANG) $(LIBYANG_CPP) $(LIBYANG_PY2) \  <<<<<<<<<<<
                                         $(LIBYANG_PY3)
    $(SONIC_UTILS)_WHEEL_DEPENDS = $(SONIC_CONFIG_ENGINE) $(SONIC_YANG_MGMT_PY)
    SONIC_PYTHON_STDEB_DEBS += $(SONIC_UTILS)
    ```
    Build the PKGs successfully.
    
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com

--- slave.mk
+++ slave.mk
@@ -501,6 +501,7 @@ $(SONIC_INSTALL_TARGETS) : $(DEBS_PATH)/%-install : .platform $$(addsuffix -inst
 #     $(SOME_NEW_DEB)_DEPENDS = $(SOME_OTHER_DEB1) $(SOME_OTHER_DEB2) ...
 #     SONIC_PYTHON_STDEB_DEBS += $(SOME_NEW_DEB)
 $(addprefix $(PYTHON_DEBS_PATH)/, $(SONIC_PYTHON_STDEB_DEBS)) : $(PYTHON_DEBS_PATH)/% : .platform \
+		$$(addsuffix -install,$$(addprefix $(DEBS_PATH)/,$$($$*_DEBS_DEPENDS))) \
 		$$(addsuffix -install,$$(addprefix $(PYTHON_DEBS_PATH)/,$$($$*_DEPENDS))) \
 		$$(addsuffix -install,$$(addprefix $(PYTHON_WHEELS_PATH)/,$$($$*_WHEEL_DEPENDS))) \
 		$(call dpkg_depend,$(PYTHON_DEBS_PATH)/%.dep)
