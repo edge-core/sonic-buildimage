15440b6e439e2a257a625505741f834770ea21fc
abdosi <58047199+abdosi@users.noreply.github.com>
2020-06-29 11:38:46 -0700

    Changes to make default route programming correct in multi-npu platforms (#4774)
    * Changes to make default route programming
    correct in multi-asic platform where frr is not running
    in host namespace. Change is to set correct administrative distance.
    Also make NAMESPACE* enviroment variable available for all dockers
    so that it can be used when needed.
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>
    
    * Fix review comments
    
    * Review comment to check to add default route
    only if default route exist and delete is successful.

--- dockers/docker-fpm-frr/docker_init.sh
+++ dockers/docker-fpm-frr/docker_init.sh
@@ -6,6 +6,30 @@ mkdir -p /etc/supervisor/conf.d
 sonic-cfggen -d -t /usr/share/sonic/templates/supervisord/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 
 CONFIG_TYPE=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["docker_routing_config_mode"]'`
+
+if [[ ! -z "$NAMESPACE_ID" ]]; then
+   # FRR is not running in host namespace so we need to delete
+   # default gw kernel route added by docker network via eth0 and add it back
+   # with higher administrative distance so that default route learnt
+   # by FRR becomes best route if/when available
+   GATEWAY_IP=$(ip route show 0.0.0.0/0 dev eth0 | awk '{print $3}')
+   #Check if docker default route is there
+   if [[ ! -z "$GATEWAY_IP" ]]; then
+      ip route del 0.0.0.0/0 dev eth0
+      #Make sure route is deleted
+      CHECK_GATEWAY_IP=$(ip route show 0.0.0.0/0 dev eth0 | awk '{print $3}')
+      if [[ -z "$CHECK_GATEWAY_IP" ]]; then
+         # Ref: http://docs.frrouting.org/en/latest/zebra.html#zebra-vrf
+         # Zebra does treat Kernel routes as special case for the purposes of Admin Distance. \
+         # Upon learning about a route that is not originated by FRR we read the metric value as a uint32_t.
+         # The top byte of the value is interpreted as the Administrative Distance and
+         # the low three bytes are read in as the metric.
+         # so here we are programming administrative distance of 210 (210 << 24) > 200 (for routes learnt via IBGP)
+         ip route add 0.0.0.0/0 via $GATEWAY_IP dev eth0 metric 3523215360
+      fi
+   fi
+fi
+
 if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "separated" ]; then
     sonic-cfggen -d -t /usr/share/sonic/templates/bgpd/bgpd.conf.j2 -y /etc/sonic/constants.yml > /etc/frr/bgpd.conf
     sonic-cfggen -d -t /usr/share/sonic/templates/zebra/zebra.conf.j2 > /etc/frr/zebra.conf
