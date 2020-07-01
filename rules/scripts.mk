fc6bcff52bc24bf9f7ff5ccc828b1a2d56f92de9
abdosi <58047199+abdosi@users.noreply.github.com>
2020-07-01 15:58:53 -0700

    [sonic-buildimage] Changes to make network specific sysctl common for both host and docker namespace (#4838)
    * [sonic-buildimage] Changes to make network specific sysctl
    common for both host and docker namespace (in multi-npu).
    
    This change is triggered with issue found in multi-npu platforms
    where in docker namespace
    net.ipv6.conf.all.forwarding was 0 (should be 1) because of
    which RS/RA message were triggered and link-local router were learnt.
    
    Beside this there were some other sysctl.net.ipv6* params whose value
    in docker namespace is not same as host namespace.
    
    So to make we are always in sync in host and docker namespace
    created common file that list all sysctl.net.* params and used
    both by host and docker namespace. Any change will get applied
    to both namespace.
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>
    
    * Address Review Comments and made sure to invoke augtool
    only one and do string concatenation of all set commands
    
    * Address Review Comments.

--- rules/scripts.mk
+++ rules/scripts.mk
@@ -14,10 +14,12 @@ $(QOS_CONFIG_TEMPLATE)_PATH = files/build_templates
 SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT = supervisor-proc-exit-listener
 $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)_PATH = files/scripts
 
+SYSCTL_NET_CONFIG = sysctl-net.conf
+$(SYSCTL_NET_CONFIG)_PATH = files/image_config/sysctl
+
 SONIC_COPY_FILES += $(CONFIGDB_LOAD_SCRIPT) \
                     $(ARP_UPDATE_SCRIPT) \
                     $(BUFFERS_CONFIG_TEMPLATE) \
                     $(QOS_CONFIG_TEMPLATE) \
-                    $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
-
-
+                    $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT) \
+                    $(SYSCTL_NET_CONFIG)
