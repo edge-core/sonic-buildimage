3f05e4a15ee0ca6e3b0c653eee2b0a1fecddbef2
gracelicd <39251567+gracelicd@users.noreply.github.com>
2020-07-08 21:55:57 +0800

    [nephos]: upgrade Nephos SAI version to c749df (#4814)
    Verified with Nephos nps8365 based platform Accton AS7116-54x.

--- platform/nephos/sai.mk
+++ platform/nephos/sai.mk
@@ -1,6 +1,6 @@
 SDK_VERSION = 3.0.0
-SAI_VERSION = 1.5.0
-SAI_COMMIT_ID = 06a67d
+SAI_VERSION = 1.5.1
+SAI_COMMIT_ID = c749df
 
 # Place here URL where SAI deb exist
 NEPHOS_SAI_DEB_LOCAL_URL = 
@@ -19,17 +19,10 @@ else
 $(NEPHOS_SAI)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps_$(SDK_VERSION)_sai_$(SAI_VERSION)_$(SAI_COMMIT_ID)_amd64.deb"
 endif
 
-NEPHOS_SAI_DEV = libsainps-dev_$(SDK_VERSION)_sai_$(SAI_VERSION)_$(SAI_COMMIT_ID)_amd64.deb
-$(eval $(call add_derived_package,$(NEPHOS_SAI),$(NEPHOS_SAI_DEV)))
-ifeq ($(SAI_FROM_LOCAL), y)
-$(NEPHOS_SAI_DEV)_PATH = $(NEPHOS_SAI_DEB_LOCAL_URL)
-else
-$(NEPHOS_SAI_DEV)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/libsainps-dev_$(SDK_VERSION)_sai_$(SAI_VERSION)_$(SAI_COMMIT_ID)_amd64.deb"
-endif
 
 ifeq ($(SAI_FROM_LOCAL), y)
-SONIC_COPY_DEBS += $(NEPHOS_SAI) $(NEPHOS_SAI_DEV)
+SONIC_COPY_DEBS += $(NEPHOS_SAI) 
 else
-SONIC_ONLINE_DEBS += $(NEPHOS_SAI) $(NEPHOS_SAI_DEV)
+SONIC_ONLINE_DEBS += $(NEPHOS_SAI) 
 endif
 $(NEPHOS_SAI_DEV)_DEPENDS += $(NEPHOS_SAI)
