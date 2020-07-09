7c699df654c043b8c017dde1f63f968b90533a3d
arlakshm <55814491+arlakshm@users.noreply.github.com>
2020-07-09 11:02:53 -0700

    Add support for bcmsh and bcmcmd utlitites in multi ASIC devices (#4926)
    Signed-off-by: Arvindsrinivasan Lakshmi Narasimhan <arlakshm@microsoft.com>
    This PR has changes to support accessing the bcmsh and bcmcmd utilities on multi ASIC devices
    Changes done
    - move the link of /var/run/sswsyncd from docker-syncd-brcm.mk to docker_image_ctl.j2
    - update the bcmsh and bcmcmd scripts to take -n [ASIC_ID] as an argument on multi ASIC platforms

--- platform/broadcom/docker-syncd-brcm.mk
+++ platform/broadcom/docker-syncd-brcm.mk
@@ -12,8 +12,8 @@ $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
                                 $(LIBSAIREDIS_DBG)
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
-$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
 
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcmcmd:/usr/bin/bcmcmd
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcmsh:/usr/bin/bcmsh
+$(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcm_common:/usr/bin/bcm_common
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += monit_syncd:/etc/monit/conf.d
