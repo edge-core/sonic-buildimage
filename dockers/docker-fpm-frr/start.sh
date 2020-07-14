b725572023de850ea85b4255ecd3fbcd04073d5b
abdosi <58047199+abdosi@users.noreply.github.com>
2020-07-14 08:02:32 -0700

    Fix the below frr start.sh jija2 exception in 201911 image syslog: (#4958)
    File "/usr/local/bin/sonic-cfggen", line 380, in <module>
         main()
       File "/usr/local/bin/sonic-cfggen", line 354, in main
         print(template.render(data))
       File "/usr/local/lib/python2.7/dist-packages/jinja2/environment.py", line 1090, in render
         self.environment.handle_exception()
       File "/usr/local/lib/python2.7/dist-packages/jinja2/environment.py", line 832, in handle_exception
         reraise(*rewrite_traceback_stack(source=source))
       File "<template>", line 1, in top-level template code
       File "/usr/local/lib/python2.7/dist-packages/jinja2/environment.py", line 471, in getattr
         return getattr(obj, attribute)
     jinja2.exceptions.UndefinedError: 'WARM_RESTART' is undefined
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- dockers/docker-fpm-frr/start.sh
+++ dockers/docker-fpm-frr/start.sh
@@ -57,7 +57,7 @@ rm -f /var/run/rsyslogd.pid
 supervisorctl start rsyslogd
 
 # start eoiu pulling, only if configured so
-if [[ $(sonic-cfggen -d -v 'WARM_RESTART.bgp.bgp_eoiu') == 'true' ]]; then
+if [[ $(sonic-cfggen -d -v 'WARM_RESTART.bgp.bgp_eoiu if WARM_RESTART and WARM_RESTART.bgp and WARM_RESTART.bgp.bgp_eoiu') == 'true' ]]; then
     supervisorctl start bgp_eoiu_marker
 fi
 
