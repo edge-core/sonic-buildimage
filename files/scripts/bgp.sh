de51b9e42424f0c2c7bb03fb13409620c8ea17a4
heidinet2007 <43017482+heidinet2007@users.noreply.github.com>
2020-07-15 11:46:23 -0700

    BGP warm reboot script to service (#3992)
    * [sonic-buildimage] Move BGP warm reboot scripts into BGP service /usr/local/bin
    
    * Revert "[sonic-buildimage] Move BGP warm reboot scripts into BGP service /usr/local/bin"
    
    This reverts commit d16d163fc41c34e129cdfa61d3f27ad51984b197.
    
    * [sonic-buildimage] Move BGP warm reboot script to BGP service
    
    * [sonic-buildimage] Move BGP warm reboot script to BGP service
    
    * [sonic-buildimage] Move BGP warm reboot script to BGP service
    - access DB correctly
    
    * Address code review comments, also change file mode of bgp.sh (+x)
    
    * Address code review comments, also change the file mode of bgp.sh (+x)
    
    * BGP warm reboot script to service, also handle fast boot as indicated by flag saved in StateDB
    
    * BGP warm reboot script to service, code review comments on space alignment
    
    * BGP warm reboot script to service: remove uncesseary space
    
    * BGP warm reboot script to service: replace tab with space
    
    * Code review comments: -) use new multi-db api -) add ignore error from zebra in case it's not configured
    
    * Integrate with multi-ASIC changes committed recently
    
    Co-authored-by: heidi.ou@alibaba-inc.com <heidi.ou@alibaba-inc.com>

---
+++ files/scripts/bgp.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+function debug()
+{
+    /usr/bin/logger $1
+    /bin/echo `date` "- $1" >> ${DEBUGLOG}
+}
+
+function check_warm_boot()
+{
+    SYSTEM_WARM_START=`$SONIC_DB_CLI STATE_DB hget "WARM_RESTART_ENABLE_TABLE|system" enable`
+    SERVICE_WARM_START=`$SONIC_DB_CLI STATE_DB hget "WARM_RESTART_ENABLE_TABLE|${SERVICE}" enable`
+    if [[ x"$SYSTEM_WARM_START" == x"true" ]] || [[ x"$SERVICE_WARM_START" == x"true" ]]; then
+        WARM_BOOT="true"
+    else
+        WARM_BOOT="false"
+    fi
+}
+
+function validate_restore_count()
+{
+    if [[ x"$WARM_BOOT" == x"true" ]]; then
+        RESTORE_COUNT=`$SONIC_DB_CLI STATE_DB hget "WARM_RESTART_TABLE|bgp" restore_count`
+        # We have to make sure db data has not been flushed.
+        if [[ -z "$RESTORE_COUNT" ]]; then
+            WARM_BOOT="false"
+        fi
+    fi
+}
+
+function check_fast_boot ()
+{
+    if [[ $($SONIC_DB_CLI STATE_DB GET "FAST_REBOOT|system") == "1" ]]; then
+        FAST_BOOT = "true"
+    else
+        FAST_BOOT = "false"
+    fi
+}
+
+start() {
+    debug "Starting ${SERVICE}$DEV service..."
+
+    check_warm_boot
+    validate_restore_count
+
+    check_fast_boot
+
+    debug "Warm boot flag: ${SERVICE}$DEV ${WARM_BOOT}."
+    debug "Fast boot flag: ${SERVICE}$DEV ${Fast_BOOT}."
+
+    # start service docker
+    /usr/bin/${SERVICE}.sh start $DEV
+    debug "Started ${SERVICE}$DEV service..."
+
+}
+
+wait() {
+    /usr/bin/${SERVICE}.sh wait $DEV
+}
+
+stop() {
+    debug "Stopping ${SERVICE}$DEV service..."
+
+    check_warm_boot
+    check_fast_boot
+    debug "Warm boot flag: ${SERVICE}$DEV ${WARM_BOOT}."
+    debug "Fast boot flag: ${SERVICE}$DEV ${FAST_BOOT}."
+
+    # Kill bgpd to start the bgp graceful restart procedure
+    if [[ x"$WARM_BOOT" == x"true" ]] || [[ x"$FAST_BOOT" == x"true" ]]; then
+        debug "Kill zebra first"
+        /usr/bin/docker exec -i bgp pkill -9 zebra || [ $? == 1 ]
+        /usr/bin/docker exec -i bgp pkill -9 bgpd || [ $? == 1 ]
+    fi
+
+    /usr/bin/${SERVICE}.sh stop $DEV
+    debug "Stopped ${SERVICE}$DEV service..."
+
+}
+
+DEV=$2
+
+SERVICE="bgp"
+DEBUGLOG="/tmp/bgp-debug$DEV.log"
+NAMESPACE_PREFIX="asic"
+if [ "$DEV" ]; then
+    NET_NS="$NAMESPACE_PREFIX$DEV" #name of the network namespace
+    SONIC_DB_CLI="sonic-db-cli -n $NET_NS"
+else
+    NET_NS=""
+    SONIC_DB_CLI="sonic-db-cli"
+fi
+
+case "$1" in
+    start|wait|stop)
+        $1
+        ;;
+    *)
+        echo "Usage: $0 {start|wait|stop}"
+        exit 1
+        ;;
+esac
