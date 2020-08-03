c4728a8261162e70f08f56e99814fdee087a68fd
Nazarii Hnydyn <nazariig@mellanox.com>
2020-08-03 13:52:40 +0300

    [Mellanox] Add MFT DKMS build support. (#5088)
    Signed-off-by: Nazarii Hnydyn <nazariig@mellanox.com>

--- platform/mellanox/mft.mk
+++ platform/mellanox/mft.mk
@@ -10,7 +10,7 @@ $(MFT)_SRC_PATH = $(PLATFORM_PATH)/mft
 $(MFT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_MAKE_DEBS += $(MFT)
 
-KERNEL_MFT = kernel-mft-dkms_$(MFT_VERSION)-$(KVERSION)_all.deb
+KERNEL_MFT = kernel-mft-dkms-modules-$(KVERSION)_$(MFT_VERSION)_amd64.deb
 $(eval $(call add_derived_package,$(MFT),$(KERNEL_MFT)))
 
 MFT_OEM = mft-oem_$(MFT_VERSION)-$(MFT_REVISION)_amd64.deb
