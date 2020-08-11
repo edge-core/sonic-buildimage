ea1b25a210016e9f845e5ea285fce95272470853
abdosi <58047199+abdosi@users.noreply.github.com>
2020-08-11 08:55:43 -0700

    [libsaibcm] updated libsaibcm debian package for (#5138)
    3.7.5.1-3 in 201911
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_3.7.5.1-2_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.5.1-2_amd64.deb?sv=2015-04-05&sr=b&sig=NMXmDm7ME%2BDN9n4kw6wXgIVmIjRifu%2FWV0UbLU9qllw%3D&se=2034-03-17T05%3A53%3A29Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_3.7.5.1-2_amd64.deb
+BRCM_SAI = libsaibcm_3.7.5.1-3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.5.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=jKyO230pW7whAMsTPZeUvcCjfE7sFin5JKzdvKswgKQ%3D&se=2034-04-19T15%3A59%3A16Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_3.7.5.1-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.5.1-2_amd64.deb?sv=2015-04-05&sr=b&sig=3Q8S5fwg7WV%2BCKVwMALrf8dpQWK2cSD4J4zxbVht%2BT8%3D&se=2034-03-17T05%3A54%3A05Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.5.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=eqVrbb2kbr%2Bz4B8OeyJ2mchjOL70Og9W0demES3uCF0%3D&se=2034-04-19T16%3A00%3A02Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
