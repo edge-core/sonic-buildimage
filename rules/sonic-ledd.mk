fc9e97fc3d156688e46746689e1970a66c7e9afb
Joe LeVeque <jleveque@users.noreply.github.com>
2020-08-13 11:26:43 -0700

    [build] Build/install sonic-ledd as a Python wheel package (#5168)
    As part of migrating all Python-based package installers to wheel format rather than Debian packages. Also to allow for easily building a Python 3 version of the package in the near future.
    
    - Also remove some references to sonic-daemon-base which I previously missed and add missing sonic-py-common dependency for sonic-pcied.

--- rules/sonic-ledd.mk
+++ rules/sonic-ledd.mk
@@ -1,6 +1,7 @@
 # sonic-ledd (SONiC Front-panel LED control daemon) Debian package
 
-SONIC_LEDD = python-sonic-ledd_1.1-1_all.deb
+SONIC_LEDD = sonic_ledd-1.1-py2-none-any.whl
 $(SONIC_LEDD)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-ledd
-$(SONIC_LEDD)_WHEEL_DEPENDS = $(SONIC_PY_COMMON_PY2)
-SONIC_PYTHON_STDEB_DEBS += $(SONIC_LEDD)
+$(SONIC_LEDD)_DEPENDS = $(SONIC_PY_COMMON_PY2)
+$(SONIC_LEDD)_PYTHON_VERSION = 2
+SONIC_PYTHON_WHEELS += $(SONIC_LEDD)
