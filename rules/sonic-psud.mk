c3202d898289a5b399234ad91360374c8896f8d4
Joe LeVeque <jleveque@users.noreply.github.com>
2020-08-14 11:11:45 -0700

    [build] Build/install sonic-psud as a Python wheel package (#5182)
    As part of migrating all Python-based package installers to wheel format rather than Debian packages. Also to allow for easily building a Python 3 version of the package in the near future.

--- rules/sonic-psud.mk
+++ rules/sonic-psud.mk
@@ -1,6 +1,7 @@
 # sonic-psud (SONiC PSU daemon) Debian package
 
-SONIC_PSUD = python-sonic-psud_1.0-1_all.deb
+SONIC_PSUD = sonic_psud-1.0-py2-none-any.whl
 $(SONIC_PSUD)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-psud
-$(SONIC_PSUD)_WHEEL_DEPENDS = $(SONIC_PY_COMMON_PY2)
-SONIC_PYTHON_STDEB_DEBS += $(SONIC_PSUD)
+$(SONIC_PSUD)_DEPENDS = $(SONIC_PY_COMMON_PY2)
+$(SONIC_PSUD)_PYTHON_VERSION = 2
+SONIC_PYTHON_WHEELS += $(SONIC_PSUD)
