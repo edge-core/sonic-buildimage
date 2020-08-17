3a10e9c6fa95f277aa4c456227dce26a7811ab64
Tamer Ahmed <tamer.ahmed@microsoft.com>
2020-08-17 15:47:14 -0700

    [dhcp-relay] Reduce Calls to SONiC Cfggen (#5175)
    Calls to sonic-cfggen is CPU expensive. This PR reduces calls to
    sonic-cfggen to one call during startup when starting dhcp-relay
    service.
    
    singed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- dockers/docker-dhcp-relay/docker_init.sh
+++ dockers/docker-dhcp-relay/docker_init.sh
@@ -2,15 +2,21 @@
 
 # Generate supervisord config file
 mkdir -p /etc/supervisor/conf.d/
-sonic-cfggen -d -t /usr/share/sonic/templates/docker-dhcp-relay.supervisord.conf.j2 > /etc/supervisor/conf.d/docker-dhcp-relay.supervisord.conf
 
-# Generate the script that waits for all interfaces to come up and make it executable
-sonic-cfggen -d -t /usr/share/sonic/templates/wait_for_intf.sh.j2 > /usr/bin/wait_for_intf.sh
+# Generate the following files from templates:
+# 1. supervisord configuration 
+# 2. wait_for_intf.sh, which waits for all interfaces to come up
+# 3. port-to-alias name map
+CFGGEN_PARAMS=" \
+    -d \
+    -t /usr/share/sonic/templates/docker-dhcp-relay.supervisord.conf.j2,/etc/supervisor/conf.d/docker-dhcp-relay.supervisord.conf \
+    -t /usr/share/sonic/templates/wait_for_intf.sh.j2,/usr/bin/wait_for_intf.sh \
+    -t /usr/share/sonic/templates/port-name-alias-map.txt.j2,/tmp/port-name-alias-map.txt \
+"
+sonic-cfggen $CFGGEN_PARAMS
+
+# Make the script that waits for all interfaces to come up executable
 chmod +x /usr/bin/wait_for_intf.sh
-
-# Generate port name-alias map for isc-dhcp-relay to parse. Each line contains one
-# name-alias pair of the form "<name> <alias>"
-sonic-cfggen -d --var-json "PORT" | python -c "import sys, json, os; [sys.stdout.write('%s %s\n' % (k, v['alias'] if 'alias' in v else k)) for (k, v) in json.load(sys.stdin).iteritems()]" > /tmp/port-name-alias-map.txt
 
 # The docker container should start this script as PID 1, so now that supervisord is
 # properly configured, we exec supervisord so that it runs as PID 1 for the
