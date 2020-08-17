066a0b3b2b333efcb2be340b30b615fb5126d70d
Tamer Ahmed <tamer.ahmed@microsoft.com>
2020-08-17 15:46:29 -0700

    [snmp]: Reduce Calls to SONiC Cfggen (#5166)
    Calls to sonic-cfggen is CPU expensive. This PR reduces calls to
    sonic-cfggen to once calle during snmp startup
    
    singed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- dockers/docker-snmp-sv2/start.sh
+++ dockers/docker-snmp-sv2/start.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir -p /etc/ssw
-sonic-cfggen -d -y /etc/sonic/sonic_version.yml -t /usr/share/sonic/templates/sysDescription.j2 > /etc/ssw/sysDescription
+mkdir -p /etc/ssw /etc/snmp
 
-mkdir -p /etc/snmp
-sonic-cfggen -d -y /etc/sonic/snmp.yml -t /usr/share/sonic/templates/snmpd.conf.j2 > /etc/snmp/snmpd.conf
+SONIC_CFGGEN_ARGS=" \
+    -d \
+    -y /etc/sonic/sonic_version.yml \
+    -t /usr/share/sonic/templates/sysDescription.j2,/etc/ssw/sysDescription \
+    -y /etc/sonic/snmp.yml \
+    -t /usr/share/sonic/templates/snmpd.conf.j2,/etc/snmp/snmpd.conf \
+"
+
+sonic-cfggen $SONIC_CFGGEN_ARGS
 
 mkdir -p /var/sonic
 echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
