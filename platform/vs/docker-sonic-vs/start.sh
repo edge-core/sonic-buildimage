a273e56bc672609933567676b2788e9af289b2c7
Sangita Maity <sangitamaity0211@gmail.com>
2020-08-20 17:10:58 -0700

    Add support to VS platform for platform.json and DPB CLI Tests (#5192)
    - Reverts commit 457674c
    - Creates "platform.json" for vs docker
    - Adds test case for port breakout CLI
    - Explicitly sets admin status of all the VS interfaces to down to be compatible with SWSS test cases, specifically vnet tests and sflow tests
    
    Signed-off-by: Sangita Maity <sangitamaity0211@gmail.com>

--- platform/vs/docker-sonic-vs/start.sh
+++ platform/vs/docker-sonic-vs/start.sh
@@ -38,9 +38,11 @@ if [ -f /etc/sonic/config_db.json ]; then
     mv /tmp/config_db.json /etc/sonic/config_db.json
 else
     # generate and merge buffers configuration into config file
-    sonic-cfggen -k $HWSKU -p /usr/share/sonic/hwsku/port_config.ini -t /usr/share/sonic/hwsku/buffers.json.j2 > /tmp/buffers.json
+    sonic-cfggen -k $HWSKU -p /usr/share/sonic/device/$PLATFORM/platform.json -t /usr/share/sonic/hwsku/buffers.json.j2 > /tmp/buffers.json
     sonic-cfggen -j /etc/sonic/init_cfg.json -t /usr/share/sonic/hwsku/qos.json.j2 > /tmp/qos.json
-    sonic-cfggen -p /usr/share/sonic/hwsku/port_config.ini -k $HWSKU --print-data > /tmp/ports.json
+    sonic-cfggen -p /usr/share/sonic/device/$PLATFORM/platform.json -k $HWSKU --print-data > /tmp/ports.json
+    # change admin_status from up to down; Test cases dependent
+    sed -i "s/up/down/g" /tmp/ports.json
     sonic-cfggen -j /etc/sonic/init_cfg.json -j /tmp/buffers.json -j /tmp/qos.json -j /tmp/ports.json --print-data > /etc/sonic/config_db.json
 fi
 
