9048d7ae4d1d69c387998571962e7044f221d1fd
Joe LeVeque <jleveque@users.noreply.github.com>
2020-08-22 17:55:27 -0700

    [201911] Remove sonic-daemon-base package (#5181)
    sonic-daemon-base package has been deprecated in favor of the sonic-py-common package. All related functionality has been moved there.
    
    This is a backport of https://github.com/Azure/sonic-buildimage/pull/5131 and parts of https://github.com/Azure/sonic-buildimage/pull/5168 to the 201911 branch

--- rules/sonic-daemon-base.mk
+++
@@ -1,8 +0,0 @@
-# SONIC_DAEMON_BASE_PY2 package
-
-SONIC_DAEMON_BASE_PY2 = sonic_daemon_base-1.0-py2-none-any.whl
-$(SONIC_DAEMON_BASE_PY2)_SRC_PATH = $(SRC_PATH)/sonic-daemon-base
-$(SONIC_DAEMON_BASE_PY2)_PYTHON_VERSION = 2
-SONIC_PYTHON_WHEELS += $(SONIC_DAEMON_BASE_PY2)
-
-export daemon_base_py2_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_DAEMON_BASE_PY2))"
