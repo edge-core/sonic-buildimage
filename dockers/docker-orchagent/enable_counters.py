c243b8a9f5fb4c8b24c739c060d70f12a6878617
Mykola F <37578614+mykolaf@users.noreply.github.com>
2020-08-25 18:20:05 +0300

    [201911] Update SAI-Implementation submodule and enable port in/out dropped pkts stats (#5093)
    - Enable port buffer drops by default
    - Update SAI submodule
    
    Signed-off-by: Mykola Faryma <mykolaf@mellanox.com>

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -16,6 +16,7 @@ def enable_counters():
     enable_counter_group(db, 'PFCWD')
     enable_counter_group(db, 'PG_WATERMARK')
     enable_counter_group(db, 'QUEUE_WATERMARK')
+    enable_counter_group(db, 'PORT_BUFFER_DROP')
 
 def get_uptime():
     with open('/proc/uptime') as fp:
