7db8cb0e035adf0bc7922051d7828602b79a5ed4
abdosi <58047199+abdosi@users.noreply.github.com>
2020-10-06 09:04:51 -0700

    Taken the fix from BRCM SAI 4.2.1.3 (6.5.19 hsdk). (#5550)
    The error message is updated to provide correct information
    of Netdevice link being down.
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- platform/broadcom/saibcm-modules/systems/linux/kernel/modules/bcm-knet/bcm-knet.c
+++ platform/broadcom/saibcm-modules/systems/linux/kernel/modules/bcm-knet/bcm-knet.c
@@ -5279,7 +5279,7 @@ bkn_tx(struct sk_buff *skb, struct net_device *dev)
     }
 
     if (!netif_carrier_ok(dev)) {
-        DBG_WARN(("Tx drop: Invalid RCPU encapsulation\n"));
+        DBG_WARN(("Tx drop: Netif link is down.\n"));
         priv->stats.tx_dropped++;
         sinfo->tx.pkts_d_no_link++;
         dev_kfree_skb_any(skb);
