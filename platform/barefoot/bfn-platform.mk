8e0e316cf8af6398a249050e1e33d86bb1bf3abc
Andriy Kokhan <43479230+akokhan@users.noreply.github.com>
2020-10-11 06:20:41 +0300

    [BFN] Updated SDK packages to 20201009 (#5576)
    Signed-off-by: Andriy Kokhan <akokhan@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,4 +1,4 @@
-BFN_PLATFORM = bfnplatform_20200721_deb9.deb
+BFN_PLATFORM = bfnplatform_20201009_deb9.deb
 $(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,4 +1,4 @@
-BFN_SAI = bfnsdk_20200721_deb9.deb
+BFN_SAI = bfnsdk_20201009_deb9.deb
 $(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_SAI)"
 
 $(BFN_SAI)_DEPENDS += $(LIBNL_GENL3_DEV)
