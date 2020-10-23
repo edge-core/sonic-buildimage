ace7f24cba58ec7af6904a7a48dc5e43f3062817
judyjoseph <53951155+judyjoseph@users.noreply.github.com>
2020-10-23 00:41:16 -0700

    [docker-teamd]: Add teamd as a depedent service to swss (#5628)
    **- Why I did it**
    On teamd docker restart, the swss and syncd needs to be restarted as there are dependent resources present.
    
    **- How I did it**
    Add the teamd as a dependent service for swss
    Updated the docker-wait script to handle service and dependent services separately.
    Handle the case of warm-restart for the dependent service   
    
    **- How to verify it**
    
    Verified the following scenario's with the following testbed 
    VM1 ----------------------------[DUT 6100] -----------------------VM2,  ping traffic continuous between VMs
    
    1. Stop teamd docker alone  
          >  swss, syncd dockers seen going away
          >  The LAG reference count error messages seen for a while till swss docker stops.
          >  Dockers back up.
    
    2. Enable WR mode for teamd. Stop teamd docker alone  
          >  swss, syncd dockers not removed.
          >  The LAG reference count error messages not seen
          >  Repeated stop teamd docker test - same result, no effect on swss/syncd.
    
    3. Stop swss docker. 
          >  swss, teamd, syncd goes off - dockers comes back correctly, interfaces up
    
    4. Enable WR mode for swss . Stop swss docker 
          >  swss goes off not affecting syncd/teamd dockers.
    
    5. Config reload 
          > no reference counter error seen, dockers comes back correctly, with interfaces up
    
    6. Warm reboot, observations below
    	 > swss docker goes off first 
    	 > teamd + syncd goes off to the end of WR process.
     	 > dockers comes back up fine.
    	 > ping traffic between VM's was NOT HIT
    
    7. Fast reboot, observations below
    	 > teamd goes off first ( **confirmed swss don't exit here** )
    	 > swss goes off next 
    	 > syncd goes away at the end of the FR process
    	 > dockers comes back up fine.
    	 > there is a traffic HIT as per fast-reboot
    
    8. Verified in multi-asic platform, the tests above other than WR/FB scenarios

--- src/sonic-py-common/sonic_py_common/device_info.py
+++ src/sonic-py-common/sonic_py_common/device_info.py
@@ -8,7 +8,7 @@ import yaml
 from natsort import natsorted
 
 # TODO: Replace with swsscommon
-from swsssdk import ConfigDBConnector, SonicDBConfig
+from swsssdk import ConfigDBConnector, SonicDBConfig, SonicV2Connector
 
 USR_SHARE_SONIC_PATH = "/usr/share/sonic"
 HOST_DEVICE_PATH = USR_SHARE_SONIC_PATH + "/device"
@@ -442,3 +442,38 @@ def get_system_routing_stack():
         raise OSError("Cannot detect routing stack")
 
     return result
+
+# Check if System warm reboot or Container warm restart is enabled.
+def is_warm_restart_enabled(container_name):
+    state_db = SonicV2Connector(host='127.0.0.1')
+    state_db.connect(state_db.STATE_DB, False)
+
+    TABLE_NAME_SEPARATOR = '|'
+    prefix = 'WARM_RESTART_ENABLE_TABLE' + TABLE_NAME_SEPARATOR
+
+    # Get the system warm reboot enable state
+    _hash = '{}{}'.format(prefix, 'system')
+    wr_system_state = state_db.get(state_db.STATE_DB, _hash, "enable")
+    wr_enable_state = True if wr_system_state == "true" else False
+
+    # Get the container warm reboot enable state
+    _hash = '{}{}'.format(prefix, container_name)
+    wr_container_state = state_db.get(state_db.STATE_DB, _hash, "enable")
+    wr_enable_state |= True if wr_container_state == "true" else False
+
+    state_db.close(state_db.STATE_DB)
+    return wr_enable_state
+
+# Check if System fast reboot is enabled.
+def is_fast_reboot_enabled():
+    fb_system_state = 0
+    cmd = 'sonic-db-cli STATE_DB get "FAST_REBOOT|system"'
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    (stdout, stderr) = proc.communicate()
+
+    if proc.returncode != 0:
+        log.log_error("Error running command '{}'".format(cmd))
+    elif stdout:
+        fb_system_state = stdout.rstrip('\n')
+
+    return fb_system_state
