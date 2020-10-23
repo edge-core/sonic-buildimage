5bfe37ca42d3033087af893de3aacf7536ed25a6
Samuel Angebault <staphylo@arista.com>
2020-10-23 12:28:36 -0700

    [Arista] Update driver submodules (#5686)
     - Enable thermalctld support for our platforms
     - Fix Chassis.get_num_sfp which had an off by one
     - Implement read_eeprom and write_eeprom in SfpBase
     - Refactor of Psus and PsuSlots. Psus they are now detected and metadata reported
     - Improvements to modular support
    
    Co-authored-by: Zhi Yuan Carl Zhao <zyzhao@arista.com>

--- rules/docker-platform-monitor.mk
+++ rules/docker-platform-monitor.mk
@@ -43,6 +43,7 @@ $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += --privileged -t
 $(DOCKER_PLATFORM_MONITOR)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 
 # Mount Arista python library on Aboot images to be used by plugins
+$(DOCKER_PLATFORM_MONITOR)_aboot_RUN_OPT += -v /run/arista:/run/arista:ro
 $(DOCKER_PLATFORM_MONITOR)_aboot_RUN_OPT += -v /usr/lib/python2.7/dist-packages/arista:/usr/lib/python2.7/dist-packages/arista:ro
 $(DOCKER_PLATFORM_MONITOR)_aboot_RUN_OPT += -v /usr/lib/python3/dist-packages/arista:/usr/lib/python3/dist-packages/arista:ro
 $(DOCKER_PLATFORM_MONITOR)_aboot_RUN_OPT += -v /usr/lib/python2.7/dist-packages/sonic_platform:/usr/lib/python2.7/dist-packages/sonic_platform:ro
