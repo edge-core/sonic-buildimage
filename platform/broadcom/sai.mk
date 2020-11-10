33076348ca12aeaa7aee39d0d6301433a60ddb9b
gechiang <62408185+gechiang@users.noreply.github.com>
2020-11-10 15:28:42 -0800

    Moving BRCM SAI 4.2.1.3 to 4.2.1.3-1 to pick up fix for CS00011396506 to fix CRM nexthop resource inuse leak (#5878)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.2.1.3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm_4.2.1.3_amd64.deb?sv=2015-04-05&sr=b&sig=aA0Ltk2jteFuJZdr1ldj%2F5e6o7R0U5S%2FqVWvutPC7k0%3D&se=2021-08-31T04%3A08%3A35Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.2.1.3_amd64.deb
+BRCM_SAI = libsaibcm_4.2.1.3-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm_4.2.1.3-1_amd64.deb?sv=2015-04-05&sr=b&sig=1%2BrMfgxeSc%2BXJUPw6X%2FhNGxe4KUp15Oqq1lVwMJGiW0%3D&se=2034-07-20T03%3A20%3A08Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.2.1.3-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm-dev_4.2.1.3_amd64.deb?sv=2015-04-05&sr=b&sig=r%2FWgs1VEFo07sbfYK%2FDZmk83QKTzwSSe%2F3%2BN3k3uAcY%3D&se=2022-01-30T22%3A55%3A04Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm-dev_4.2.1.3-1_amd64.deb?sv=2015-04-05&sr=b&sig=lTzaCynflt5UO7Ltf2Z%2B6zHJ4R%2BFj421kupIGO4%2FOCI%3D&se=2034-07-20T03%3A19%3A27Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
