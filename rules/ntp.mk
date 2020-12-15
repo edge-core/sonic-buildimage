9f4ca013882f2df10a499a63aaa4e7b766a89d06
Sabareesh-Kumar-Anandan <59681634+Sabareesh-Kumar-Anandan@users.noreply.github.com>
2020-12-15 22:14:46 +0530

    [sonic-config-engine] Adding dependent pkgs needed for arm compilation (#6186)
    libxslt-dev and libz-dev are dependencies for lxml==4.6.1 which is required for pyangbind==0.8.1
    
    lxml-4.6.2-cp37-cp37m-manylinux1_x86_64.whl is directly downloaded in amd64 whereas in arm this is built from lxml-4.6.2.tar.gz
    
    Signed-off-by: Sabareesh Kumar Anandan <sanandan@marvell.com>

--- rules/ntp.mk
+++ rules/ntp.mk
@@ -3,7 +3,7 @@
 NTP_VERSION = 4.2.8p12+dfsg
 export NTP_VERSION
 
-NTP = ntp_$(NTP_VERSION)-4+deb10u2_amd64.deb
+NTP = ntp_$(NTP_VERSION)-4+deb10u2_$(CONFIGURED_ARCH).deb
 $(NTP)_SRC_PATH = $(SRC_PATH)/ntp
 SONIC_MAKE_DEBS += $(NTP)
 SONIC_STRETCH_DEBS += $(NTP)
