f9e96283e49cb18c5323452668e722e17bc97c5d
gechiang <62408185+gechiang@users.noreply.github.com>
2020-12-18 10:08:56 -0800

    fix BRCM SAI warm-reboot Removing ip2me trap entry returns error results in orchagent crash (#6244)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.2.1.5-6_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm_4.2.1.5-6_amd64.deb?sv=2019-12-12&st=2020-12-15T06%3A40%3A06Z&se=2035-12-16T06%3A40%3A00Z&sr=b&sp=r&sig=aux78f4Uhmh2AHJqZh1GMWPYdQDWI3fVLgLmFXrpbFQ%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5-6_amd64.deb
+BRCM_SAI = libsaibcm_4.2.1.5-7_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm_4.2.1.5-7_amd64.deb?sv=2015-04-05&sr=b&sig=ldUfPMl4Lb0KhXtVtwzFZFYU5qrhxFzT90NX3KSnku0%3D&se=2034-08-27T01%3A34%3A16Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5-7_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm-dev_4.2.1.5-6_amd64.deb?sv=2019-12-12&st=2020-12-15T06%3A40%3A46Z&se=2035-12-16T06%3A40%3A00Z&sr=b&sp=r&sig=%2BnflSlIa9cIMPr%2BDmZLYtO2rhXwdDwv7Z%2BqY5MUlIM0%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm-dev_4.2.1.5-7_amd64.deb?sv=2015-04-05&sr=b&sig=TboOe3jFGgl2j7%2FAFiJNQ%2F%2Fa4JaVGijgAH2MSMVPYOI%3D&se=2034-08-27T01%3A32%3A58Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
