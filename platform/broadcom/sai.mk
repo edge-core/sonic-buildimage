618b4bf24384af8118aae11350c4b8f6ea14b081
lguohan <lguohan@gmail.com>
2021-01-03 08:28:05 -0800

    [broadcom]: match the brcm sai filename version to control file version (#6339)
    the control file version is 4.2.1.5-7
    
    sonic$ sudo dpkg -i target/debs/buster/libsaibcm-dev_4.2.1.5-8_amd64.deb
    (Reading database ... 175880 files and directories currently installed.)
    Preparing to unpack .../libsaibcm-dev_4.2.1.5-8_amd64.deb ...
    Unpacking libsaibcm-dev (4.2.1.5-7) over (4.2.1.5-7) ...
    Setting up libsaibcm-dev (4.2.1.5-7) ...
    lgh@491d842369cf:/sonic$ sudo dpkg -i target/debs/buster/libsaibcm_4.2.1.5-8_amd64.deb
    (Reading database ... 175880 files and directories currently installed.)
    Preparing to unpack .../libsaibcm_4.2.1.5-8_amd64.deb ...
    Unpacking libsaibcm (4.2.1.5-7) over (4.2.1.5-7) ...
    Setting up libsaibcm (4.2.1.5-7) ...
    Processing triggers for libc-bin (2.28-10) ...
    
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,6 +1,6 @@
-BRCM_SAI = libsaibcm_4.2.1.5-8_amd64.deb
+BRCM_SAI = libsaibcm_4.2.1.5-7_amd64.deb
 $(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm_4.2.1.5-8_amd64.deb?sv=2019-12-12&st=2020-12-18T18%3A41%3A23Z&se=2030-12-19T18%3A41%3A00Z&sr=b&sp=r&sig=GFNMn0borytUTFQJf%2F5A0J452XsQ%2FqnzBw5GyjFuYHw%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5-8_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5-7_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm-dev_4.2.1.5-8_amd64.deb?sv=2019-12-12&st=2020-12-18T18%3A42%3A03Z&se=2030-12-19T18%3A42%3A00Z&sr=b&sp=r&sig=J%2BbRtZVVcZTLs3uqzZs9zObLKFxRgzvJkFL5iyJ48mA%3D"
 
