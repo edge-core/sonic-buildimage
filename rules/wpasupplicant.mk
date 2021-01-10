c22575218a07dd379654ee9fd54c6f41a2fff731
Ze Gan <ganze718@gmail.com>
2021-01-11 02:39:59 +0800

    [docker-macsec]: MACsec container and wpa_supplicant component (#5700)
    The HLD about MACsec feature is at :
    
    https://github.com/Azure/SONiC/blob/master/doc/macsec/MACsec_hld.md
    
    - How to verify it
    This PR doesn't set MACsec container automatically start, You should manually start the container by docker run docker-macsec
    wpa_supplicant binary can be found at MACsec container.
    This PR depends on the PR, WPA_SUPPLICANT, and The MACsec container will be set as automatically start by later PR.
    
    Signed-off-by: zegan <zegan@microsoft.com>

---
+++ rules/wpasupplicant.mk
@@ -0,0 +1,19 @@
+# wpa package
+
+WPASUPPLICANT_VERSION = 2.9.0-14
+
+export WPASUPPLICANT_VERSION
+
+WPASUPPLICANT = wpasupplicant_$(WPASUPPLICANT_VERSION)_$(CONFIGURED_ARCH).deb
+$(WPASUPPLICANT)_SRC_PATH = $(SRC_PATH)/wpasupplicant
+$(WPASUPPLICANT)_DEPENDS += $(LIBSWSSCOMMON_DEV) $(LIBNL3_DEV) $(LIBNL_GENL3_DEV) $(LIBNL_ROUTE3_DEV)
+$(WPASUPPLICANT)_RDEPENDS += $(LIBSWSSCOMMON) $(LIBNL3) $(LIBNL_GENL3) $(LIBNL_ROUTE3)
+SONIC_MAKE_DEBS += $(WPASUPPLICANT)
+
+WPASUPPLICANT_DBG = wpasupplicant-dbgsym_$(WPASUPPLICANT_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(WPASUPPLICANT),$(WPASUPPLICANT_DBG)))
+
+# The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
+# are archived into debug one image to facilitate debugging.
+#
+DBG_SRC_ARCHIVE += wpasupplicant
