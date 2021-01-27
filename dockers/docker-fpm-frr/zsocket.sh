aab37b7f42fe823199b88924a7bdcfc153577e1f
Shi Su <67605788+shi-su@users.noreply.github.com>
2021-01-27 12:36:02 -0800

    [FRR] Create a separate script to wait zebra to be ready to receive connections (#6519)
    The requirement for zebra to be ready to accept connections is a generic problem that is not 
    specific to bgpd. Making the script to wait for zebra socket a separate script and let bgpd and 
    staticd to wait for zebra socket.

--- dockers/docker-fpm-frr/zsocket.sh
+++ dockers/docker-fpm-frr/zsocket.sh
@@ -5,8 +5,8 @@ port=2601
 
 function help()
 {
-    echo "This script aims to ensure zebra is ready to accept connections before starting bgpd"
-    echo "Usage: $0 [options] [bgpd options]"
+    echo "This script aims to ensure zebra is ready to accept connections"
+    echo "Usage: $0 [options]"
     echo "Options:"
     echo " -a   Zebra address"
     echo " -o   Zebra port"
@@ -23,11 +23,14 @@ while getopts ":a:o:h" opt; do
             ;;
     esac
 done
-shift $((OPTIND-1))
 
+start=$(date +%s.%N)
 timeout 5s bash -c -- "until </dev/tcp/${addr}/${port}; do sleep 0.1;done"
 if [ "$?" != "0" ]; then
     logger -p error "Error: zebra is not ready to accept connections"
+else
+    timespan=$(awk "BEGIN {print $(date +%s.%N)-$start; exit}")
+    logger -p info "It took ${timespan} seconds to wait for zebra to be ready to accept connections"
 fi
 
-exec /usr/lib/frr/bgpd "$@"
+exit 0
