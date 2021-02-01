a3bdbb79c1ca73d7cdc3c2a73fc561588f98fd19
gechiang <62408185+gechiang@users.noreply.github.com>
2021-01-31 16:59:01 -0800

    [broadcom]: broadcom sai update to 4.3.0.10-3 (#6620)
    1. BRCM SAI Debian build need not have any Kernel version dependency - Starting with 4.3 BRCM made changes in SAI so that this dependency has been cleaned up. We can now remove the Kernel Version dependency from Azure Pipeline build script.
    
    2. Bypass PEER_MODE p2mp setting causing SYNCd crash on non-TD3 SKUs - Temporarily patch BRCM SAI code to not cause SYNCd crash when Orchagent program SAI_TUNNEL_ATTR_PEER_MODE: SAI_TUNNEL_PEER_MODE_P2MP on Non-TD3 SKUs. Will remove this when BRCM provide proper fix to address this issue.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.0.10-2_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.0.10-2_amd64.deb?sv=2015-04-05&sr=b&sig=1L2kJwYBuXDc9ObuVBBUS%2F%2FBVIfAA651ig5k6O1ZztE%3D&se=2022-06-10T21%3A25%3A43Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.0.10-2_amd64.deb
+BRCM_SAI = libsaibcm_4.3.0.10-3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.0.10-3_amd64.deb?sv=2015-04-05&sr=b&sig=snXITt%2BRq2cKD7I%2Bqr2WCbj1Ly%2FB2NM8EW3R7wc%2B1ME%3D&se=2034-10-10T06%3A30%3A07Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.0.10-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.0.10-2_amd64.deb?sv=2015-04-05&sr=b&sig=2Vm6o8HtbjI%2BfVoHJUiO5b75USqGra9CLSFXViQm8yM%3D&se=2022-06-10T21%3A26%3A35Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.0.10-3_amd64.deb?sv=2015-04-05&sr=b&sig=%2BfGcnKeMApru1b8aebMHT68zEc%2BCn%2BTcC27izdHNrlA%3D&se=2034-10-10T06%3A30%3A44Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
