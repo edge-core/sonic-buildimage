f00588855dbe0232e3267b17915e1716a67db3f4
gechiang <62408185+gechiang@users.noreply.github.com>
2021-02-03 15:59:56 -0800

    BRCM SAI 4.3.0.10-4 Fix _brcm_sai_indexed_data_get () with unexpected queue causing _brcm_sai_switch_assert () after warm reboot (#6664)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.0.10-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.0.10-3_amd64.deb?sv=2015-04-05&sr=b&sig=snXITt%2BRq2cKD7I%2Bqr2WCbj1Ly%2FB2NM8EW3R7wc%2B1ME%3D&se=2034-10-10T06%3A30%3A07Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.0.10-3_amd64.deb
+BRCM_SAI = libsaibcm_4.3.0.10-4_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.0.10-4_amd64.deb?sv=2015-04-05&sr=b&sig=nfseU56PACVqklQ4MC0HvZ7qt7Ou4loQMBA7jx8CSOY%3D&se=2034-10-13T16%3A31%3A22Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.0.10-4_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.0.10-3_amd64.deb?sv=2015-04-05&sr=b&sig=%2BfGcnKeMApru1b8aebMHT68zEc%2BCn%2BTcC27izdHNrlA%3D&se=2034-10-10T06%3A30%3A44Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.0.10-4_amd64.deb?sv=2015-04-05&sr=b&sig=4tF26GxI6jmrcvRyCezQ7RL6qMjzip7SFf61eqy%2Bvf4%3D&se=2034-10-13T16%3A31%3A53Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
