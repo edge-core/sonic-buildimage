bb8a1452342b9f787f0b524e0fa70f66f53dbc9e
abdosi <58047199+abdosi@users.noreply.github.com>
2021-02-06 10:28:05 -0800

    Remove makefile and dependency for python2 of dbsyncd. (#6702)
    * Remove makefile and 
    As part of #6046 we have moved to python3 of dbsyncd.
    Cleanup the python2 makefile and dependency.
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>
    
    * Revert "Remove makefile and dependency for python2 of dbsyncd."
    
    This reverts commit ceaa4f8dd9c58975953145dcc0c9e045910e73ad.
    
    * Fix
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- rules/dbsyncd-py2.mk
+++
@@ -1,7 +0,0 @@
-# sonic-dbsyncd python2 wheel
-
-DBSYNCD_PY2 = sonic_d-2.0.0-py2-none-any.whl
-$(DBSYNCD_PY2)_SRC_PATH = $(SRC_PATH)/sonic-dbsyncd
-$(DBSYNCD_PY2)_PYTHON_VERSION = 2
-$(DBSYNCD_PY2)_DEPENDS += $(SWSSSDK_PY2)
-SONIC_PYTHON_WHEELS += $(DBSYNCD_PY2)
