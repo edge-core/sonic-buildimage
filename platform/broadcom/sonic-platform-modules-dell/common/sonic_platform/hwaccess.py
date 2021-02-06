78ad83fe2a98e0cfdc4caff4267e10243135471e
Aravind Mani <53524901+aravindmani-1@users.noreply.github.com>
2021-02-07 03:23:35 +0530

    [DellEMC Z9332f] Added support for platform system health daemon (#6642)

--- platform/broadcom/sonic-platform-modules-dell/common/sonic_platform/hwaccess.py
+++ platform/broadcom/sonic-platform-modules-dell/common/sonic_platform/hwaccess.py
@@ -1,5 +1,6 @@
 # Helper functions to access hardware
 
+import os
 import struct
 import mmap
 import subprocess
@@ -22,3 +23,31 @@ def pci_get_value(resource, offset):
 
 def i2c_get(bus, i2caddr, ofs):
     return int(subprocess.check_output(['/usr/sbin/i2cget', '-y', str(bus), str(i2caddr), str(ofs)]), 16)
+
+def io_reg_read(io_resource, offset):
+    fd = os.open(io_resource, os.O_RDONLY)
+    if fd < 0:
+        print('file open failed %s' % io_resource)
+        return -1
+    if os.lseek(fd, offset, os.SEEK_SET) != offset:
+        print('lseek failed on %s' % io_resource)
+        return -1
+    buf = os.read(fd, 1)
+    reg_val1 = ord(buf)
+    os.close(fd)
+    return reg_val1
+
+def io_reg_write(io_resource, offset, val):
+    fd = os.open(io_resource, os.O_RDWR)
+    if fd < 0:
+        print('file open failed %s' % io_resource)
+        return False
+    if os.lseek(fd, offset, os.SEEK_SET) != offset:
+        print('lseek failed on %s' % io_resource)
+        return False
+    ret = os.write(fd, struct.pack('B', val))
+    if ret != 1:
+        print('write failed %d' % ret)
+        return False
+    os.close(fd)
+    return True
