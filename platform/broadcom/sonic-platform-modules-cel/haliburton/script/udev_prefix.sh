27ca6040f111cac8066a484496d0fef5fd6d63e5
Joe LeVeque <jleveque@users.noreply.github.com>
2021-02-14 10:40:24 -0800

    [platform] Update path to udevprefix.conf file (#6779)
    Azure/sonic-utilities#1431 changes the path to the udevprefix.conf file. The file previously inappropriately resided in the <platform>/plugins/ directory. That directory is reserved for now-deprecated Python platform plugins, and will be removed in the near future.

--- platform/broadcom/sonic-platform-modules-cel/haliburton/script/udev_prefix.sh
+++ platform/broadcom/sonic-platform-modules-cel/haliburton/script/udev_prefix.sh
@@ -3,21 +3,21 @@
 PREV_REBOOT_CAUSE="/host/reboot-cause/"
 DEVICE="/usr/share/sonic/device"
 PLATFORM=$(/usr/local/bin/sonic-cfggen -H -v DEVICE_METADATA.localhost.platform)
-FILES=$DEVICE/$PLATFORM/plugins
+PLATFORM_PATH=$DEVICE/$PLATFORM
 FILENAME="udevprefix.conf"
 
 if [ "$1" = "clear" ]
 then
-	if [ -e $FILES/$FILENAME ]; then
-		rm $FILES/$FILENAME
+	if [ -e $PLATFORM_PATH/$FILENAME ]; then
+		rm $PLATFORM_PATH/$FILENAME
 	fi
 else
-	if [ -e $FILES/$FILENAME ]; then
-		: > $FILES/$FILENAME 
-		echo -n "$1" > $FILES/$FILENAME
+	if [ -e $PLATFORM_PATH/$FILENAME ]; then
+		: > $PLATFORM_PATH/$FILENAME
+		echo -n "$1" > $PLATFORM_PATH/$FILENAME
 	else
-		touch $FILES/$FILENMAE
-		echo -n "$1" > $FILES/$FILENAME 
+		touch $PLATFORM_PATH/$FILENMAE
+		echo -n "$1" > $PLATFORM_PATH/$FILENAME
 	fi
 fi
 
