965f4901ec3ea062ae49aa5fcf1f4190c34f7be6
Rajkumar-Marvell <54936542+rajkumar38@users.noreply.github.com>
2021-02-25 22:47:45 +0530

    [Marvell] Updated armhf SAI deb version info. (#6863)
    Modified the MRVL SAI debian version format to include debian revision number. This helps in identifying the SAI deb causing any build/runtime issue.
    
    Signed-off-by: Rajkumar Pennadam Ramamoorthy <rpennadamram@marvell.com>

--- platform/marvell-armhf/sai.mk
+++ platform/marvell-armhf/sai.mk
@@ -1,7 +1,7 @@
 # Marvell SAI
 
-export MRVL_SAI_VERSION = 1.7.1
-export MRVL_SAI = mrvllibsai_m0_MASTER_$(PLATFORM_ARCH)_$(MRVL_SAI_VERSION).deb
+export MRVL_SAI_VERSION = 1.7.1-1
+export MRVL_SAI = mrvllibsai_$(PLATFORM_ARCH)_$(MRVL_SAI_VERSION).deb
 
 $(MRVL_SAI)_SRC_PATH = $(PLATFORM_PATH)/sai
 $(eval $(call add_conflict_package,$(MRVL_SAI),$(LIBSAIVS_DEV)))
