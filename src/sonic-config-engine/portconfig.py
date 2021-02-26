18263c99dd51b8e833dbb16fd230cb1a3bee6678
Sangita Maity <samaity@linkedin.com>
2021-02-26 00:13:33 -0800

    [DPB|master] Update Dynamic Port Breakout Logic for flexible alias support a… (#6831)
    To fix [DPB| wrong aliases for interfaces](https://github.com/Azure/sonic-buildimage/issues/6024) issue, implimented flexible alias support [design doc](https://github.com/Azure/SONiC/pull/749)
    
    > [[dpb|config] Fix the validation logic of breakout mode](https://github.com/Azure/sonic-utilities/pull/1440) depends on this
    
    #### How I did it
    
    1. Removed `"alias_at_lanes"` from port-configuration file(i.e. platfrom.json) 
    2. Added dictionary to "breakout_modes" values. This defines the breakout modes available on the platform for this parent port, and it maps to the alias list. The alias list presents the alias names for individual ports in order under this breakout mode.
    ```
    {
        "interfaces": {
            "Ethernet0": {
                "index": "1,1,1,1",
                "lanes": "0,1,2,3",
                "breakout_modes": {
                    "1x100G[40G]": ["Eth1"],
                    "2x50G": ["Eth1/1", "Eth1/2"],
                    "4x25G[10G]": ["Eth1/1", "Eth1/2", "Eth1/3", "Eth1/4"],
                    "2x25G(2)+1x50G(2)": ["Eth1/1", "Eth1/2", "Eth1/3"],
                    "1x50G(2)+2x25G(2)": ["Eth1/1", "Eth1/2", "Eth1/3"]
                }
            }
    }
    ```
    #### How to verify it
    `config interface breakout`
    
    Signed-off-by: Sangita Maity <samaity@linkedin.com>

--- src/sonic-config-engine/portconfig.py
+++ src/sonic-config-engine/portconfig.py
@@ -26,8 +26,7 @@ CUR_BRKOUT_MODE = "brkout_mode"
 INTF_KEY = "interfaces"
 OPTIONAL_HWSKU_ATTRIBUTES = ["fec", "autoneg"]
 
-BRKOUT_PATTERN = r'(\d{1,3})x(\d{1,3}G)(\[\d{1,3}G\])?(\((\d{1,3})\))?'
-
+BRKOUT_PATTERN = r'(\d{1,3})x(\d{1,3}G)(\[(\d{1,3}G,?)*\])?(\((\d{1,3})\))?'
 
 #
 # Helper Functions
@@ -149,23 +148,24 @@ def parse_port_config_file(port_config_file):
     return (ports, port_alias_map, port_alias_asic_map)
 
 # Generate configs (i.e. alias, lanes, speed, index) for port
-def gen_port_config(ports, parent_intf_id, index, alias_at_lanes, lanes, k,  offset):
+def gen_port_config(ports, parent_intf_id, index, alias_list, lanes, k,  offset):
     if k is not None:
-        num_lane_used, speed, alt_speed, _ , assigned_lane = k[0], k[1], k[2], k[3], k[4]
+        num_lane_used, speed, alt_speed, _, _ , assigned_lane = k[0], k[1], k[2], k[3], k[4], k[5]
 
         # In case of symmetric mode
         if assigned_lane is None:
             assigned_lane = len(lanes.split(","))
 
         parent_intf_id = int(offset)+int(parent_intf_id)
-        alias_start = 0 + offset
+        alias_position = 0 + int(offset)//int(num_lane_used)
+        lanes_start = 0 + int(offset)
 
         step = int(assigned_lane)//int(num_lane_used)
         for i in range(0,int(assigned_lane), step):
             intf_name = PORT_STR + str(parent_intf_id)
             ports[intf_name] = {}
-            ports[intf_name]['alias'] = alias_at_lanes.split(",")[alias_start]
-            ports[intf_name]['lanes'] = ','.join(lanes.split(",")[alias_start:alias_start+step])
+            ports[intf_name]['alias'] = alias_list[alias_position]
+            ports[intf_name]['lanes'] = ','.join(lanes.split(",")[lanes_start:lanes_start+step])
             if speed:
                 speed_pat = re.search("^((\d+)G|\d+)$", speed.upper())
                 if speed_pat is None:
@@ -179,11 +179,12 @@ def gen_port_config(ports, parent_intf_id, index, alias_at_lanes, lanes, k,  off
             else:
                 raise Exception('Regex return for speed is None...')
 
-            ports[intf_name]['index'] = index.split(",")[alias_start]
+            ports[intf_name]['index'] = index.split(",")[alias_position]
             ports[intf_name]['admin_status'] = "up"
 
             parent_intf_id += step
-            alias_start += step
+            alias_position += 1
+            lanes_start += step
 
         offset = int(assigned_lane) + int(offset)
         return offset
@@ -200,7 +201,7 @@ def get_child_ports(interface, breakout_mode, platform_json_file):
     port_dict = readJson(platform_json_file)
 
     index = port_dict[INTF_KEY][interface]['index']
-    alias_at_lanes = port_dict[INTF_KEY][interface]['alias_at_lanes']
+    alias_list = port_dict[INTF_KEY][interface]['breakout_modes'][breakout_mode]
     lanes = port_dict[INTF_KEY][interface]['lanes']
 
     """
@@ -224,7 +225,7 @@ def get_child_ports(interface, breakout_mode, platform_json_file):
     offset = 0
     parent_intf_id = int(re.search("Ethernet(\d+)", interface).group(1))
     for k in match_list:
-        offset = gen_port_config(child_ports, parent_intf_id, index, alias_at_lanes, lanes, k, offset)
+        offset = gen_port_config(child_ports, parent_intf_id, index, alias_list, lanes, k, offset)
     return child_ports
 
 def parse_platform_json_file(hwsku_json_file, platform_json_file):
