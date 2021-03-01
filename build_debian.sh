f240859c6168df4a6ec9d244f9630612ffff7d83
Joe LeVeque <jleveque@users.noreply.github.com>
2021-02-28 23:59:48 -0800

    [build] No longer install scpay Debian package in host (#6919)
    As of the merging of PR #6799, we are now installing a newer version of scapy via pip, therefore there is no longer a need to install the older Debian package.

--- build_debian.sh
+++ build_debian.sh
@@ -300,7 +300,6 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     ethtool                 \
     screen                  \
     hping3                  \
-    python-scapy            \
     tcptraceroute           \
     mtr-tiny                \
     locales                 \
