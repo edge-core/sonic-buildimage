50e4cc1579e4b40f48e251fdaa1b67c9619156e2
shlomibitton <60430976+shlomibitton@users.noreply.github.com>
2021-03-16 18:56:53 +0200

    Backport ethtool to support QSFP-DD (#5725)
    Backport ethtool debian package version 5.9 to support QSFP-DD cable parsing.
    
    Signed-off-by: Shlomi Bitton <shlomibi@nvidia.com>

--- build_debian.sh
+++ build_debian.sh
@@ -297,7 +297,6 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     squashfs-tools          \
     grub2-common            \
     rsyslog                 \
-    ethtool                 \
     screen                  \
     hping3                  \
     tcptraceroute           \
