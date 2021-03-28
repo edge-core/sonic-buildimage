cc36a145cbff85453f29885737a46c2062c20b99
ArthiSivanantham <arthi.sivanantham@gmail.com>
2021-03-28 00:51:11 -0700

    [yang]: SONiC Yang model for PORTCHANNEL_INTERFACE table (#7034)
    * SONiC Yang model for PORTCHANNEL_INTERFACE table
    
    Signed-off-by: Arthi Sivanantham <arthi_sivanantham@dell.com>

--- src/sonic-yang-models/tests/yang_model_tests/test_yang_model.py
+++ src/sonic-yang-models/tests/yang_model_tests/test_yang_model.py
@@ -65,6 +65,14 @@ class Test_yang_models:
                 'desc': 'Configure a member port in PORT_CHANNEL table.',
                 'eStr': self.defaultYANGFailure['None']
             },
+            'PORTCHANNEL_INTERFACE_IP_ADDR_TEST': {
+                'desc': 'Configure IP address on PORTCHANNEL_INTERFACE table.',
+                'eStr': self.defaultYANGFailure['None']
+            },
+            'PORTCHANNEL_INTERFACE_IP_ADDR_ON_NON_EXIST_PO': {
+                'desc': 'Configure IP address on a non existent PortChannel.',
+                'eStr': self.defaultYANGFailure['LeafRef']
+            },
             'VLAN_MEMEBER_WITH_NON_EXIST_VLAN': {
                 'desc': 'Configure vlan-id in VLAN_MEMBER table which does not exist in VLAN  table.',
                 'eStr': self.defaultYANGFailure['LeafRef']
