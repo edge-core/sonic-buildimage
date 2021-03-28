bcff251a714c73ea95a188efa571f2a430760841
vmittal-msft <46945843+vmittal-msft@users.noreply.github.com>
2021-03-27 21:13:28 -0700

    [broadcom]: Updated bcmsai to 4.3.3.3 (#7090)
    To add latest SAI drop REL_4.3.3.3 to SONIC which addresses the following CSP cases:
    
    CS00012058054: [4.3][IPinIP][TTL-PIPE] IPinIP TTL Pipe Mode is NOT working it is behaving UNIFORM mode even programed as PIPE mode
    CS00011227466: [4.3] Warmboot support with tunnel encap

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.1-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=n7KoEZ5wXY%2FobPAy62d9C%2BKyAkKo4PIdIAWwqnDBm3E%3D&se=2034-11-14T03%3A30%3A33Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.1-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.3_amd64.deb?sv=2019-12-12&st=2021-03-18T03%3A28%3A15Z&se=2030-03-19T03%3A28%3A00Z&sr=b&sp=r&sig=YDCGJeY4Om4%2B5tkdZN%2BvWjLGh8JhsDyfMPs%2FfHgW8nA%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=EavLNMXA6OS3s9oD34bKbdtfYHppR4egkh7V7jc4gWM%3D&se=2034-11-14T03%3A31%3A04Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.3_amd64.deb?sv=2019-12-12&st=2021-03-18T03%3A29%3A30Z&se=2030-03-19T03%3A29%3A00Z&sr=b&sp=r&sig=e5hvdlKvupLmWfNK%2B37LYUXfxNBm6Y%2F0%2Bnv2Aq8q51w%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
