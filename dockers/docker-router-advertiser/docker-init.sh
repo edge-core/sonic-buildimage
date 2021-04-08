86ea554d4a7de6bbcf75d2c0e203f0a8a3a3afb6
Tamer Ahmed <tamer.ahmed@microsoft.com>
2021-04-08 09:54:55 -0700

    [radv] Fix Script Name Change (#7254)
    PR https://github.com/Azure/sonic-buildimage/pull/4599 changed startup
    script name from wait_for_intf.sh.j2 to wait_for_link.sh.j2, however
    when PR https://github.com/Azure/sonic-buildimage/pull/5178 was cherry-
    picked, the script name was not changed to wait_for_link.sh.
    
    signed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- dockers/docker-router-advertiser/docker-init.sh
+++ dockers/docker-router-advertiser/docker-init.sh
@@ -8,10 +8,10 @@ CFGGEN_PARAMS=" \
     -d \
     -t /usr/share/sonic/templates/docker-router-advertiser.supervisord.conf.j2,/etc/supervisor/conf.d/supervisord.conf \
     -t /usr/share/sonic/templates/radvd.conf.j2,/etc/radvd.conf \
-    -t /usr/share/sonic/templates/wait_for_intf.sh.j2,/usr/bin/wait_for_intf.sh \
+    -t /usr/share/sonic/templates/wait_for_link.sh.j2,/usr/bin/wait_for_link.sh \
 "
 sonic-cfggen $CFGGEN_PARAMS
 
-chmod +x /usr/bin/wait_for_intf.sh
+chmod +x /usr/bin/wait_for_link.sh
 
 exec /usr/bin/supervisord
