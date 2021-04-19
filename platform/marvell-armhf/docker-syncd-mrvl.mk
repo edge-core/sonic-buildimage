b8daa00ef08ee774dd4fe3a3cd7d44595451ac92
Rajkumar-Marvell <54936542+rajkumar38@users.noreply.github.com>
2021-04-20 03:40:52 +0530

    [marvell] Move armhf syncd build from stretch to buster. (#7366)
    Signed-off-by: Rajkumar Pennadam Ramamoorthy <rpennadamram@marvell.com>

--- platform/marvell-armhf/docker-syncd-mrvl.mk
+++ platform/marvell-armhf/docker-syncd-mrvl.mk
@@ -3,15 +3,12 @@
 DOCKER_SYNCD_PLATFORM_CODE = mrvl
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
-$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(PYTHON_SDK_API)
+$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
 
 $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
                                 $(LIBSWSSCOMMON_DBG) \
                                 $(LIBSAIMETADATA_DBG) \
                                 $(LIBSAIREDIS_DBG)
 
-SONIC_STRETCH_DOCKERS += $(DOCKER_SYNCD_BASE)
-SONIC_STRETCH_DBG_DOCKERS += $(DOCKER_SYNCD_BASE_DBG)
-
-$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+#$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += monit_syncd:/etc/monit/conf.d
