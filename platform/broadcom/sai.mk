0eb6b713c3a4bf61af69e6871a2e580716104306
vmittal-msft <46945843+vmittal-msft@users.noreply.github.com>
2021-04-20 15:04:54 -0700

    libsaibcm TD3 MMU init patch from BRCM (#7341)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.4_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.4_amd64.deb?sv=2015-04-05&sr=b&sig=RaM7VKtUVZgKVvCbcBp3WP7cqrLT%2BrjwHv3kLdj8zzk%3D&se=2034-12-10T05%3A39%3A44Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.4-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.4-1_amd64.deb?sv=2019-12-12&st=2021-04-16T16%3A07%3A16Z&se=2030-04-17T16%3A07%3A00Z&sr=b&sp=r&sig=BiY2sN%2FVvDzZlxPEGWMwMLeB725yCvwrORHULdnWyXU%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4_amd64.deb?sv=2015-04-05&sr=b&sig=im7%2Fqp2dnoQTaPhY3Jv%2BHV0eZ4mPu27BT%2FbcF%2BQxGyQ%3D&se=2034-12-10T05%3A40%3A26Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4-1_amd64.deb?sv=2019-12-12&st=2021-04-16T16%3A07%3A55Z&se=2030-04-17T16%3A07%3A00Z&sr=b&sp=r&sig=XVNsLj8QwhGN3sgvgvPZ2glnLZm9Dhyci%2F61k36Fl0c%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
