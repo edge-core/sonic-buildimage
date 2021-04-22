4c43ecc81b94abfb89aa34a158e4298773cb0305
gechiang <62408185+gechiang@users.noreply.github.com>
2021-04-22 08:34:52 -0700

    [202012] BRCM SAI 4.3.3.5 pick up 2 bug fixes and MMU changes (#7400)
    This is the SAI 4.3.3.5 code drop from BRCM to address 2 CSP case and initial MMU changes
    Note the MMU changes is the same as that of SAI 4.3.3.4-1 (#7341) but with official patch.
    
    - Case CS00012178716 [4.3] Polling SAI_QUEUE_STAT_SHARED_WATERMARK_BYTES fails often on TH3
    - Case CS00012159273 [4.3.3.3] [TD3][IPFWD] subnet broadcast flooding end with extra VLAN Tag if member port of VLAN interface deleted then added back to VLAN
    
    Once we have this PR merged, will validate each one of the above to ensure they are indeed fixed...
    
    Preliminary tests looks fine. BGP neighbors were all up with proper routes programmed
    interfaces are all up
    Manually ran the following test cases on TD3 DUT and all passed:
    
         ipfwd/test_dir_bcast.py
         fib/test_fib.py
         vxlan/test_vxlan_decap.py 
         decap/test_decap.py
         fdb/test_fdb.py

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.4_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.4_amd64.deb?sv=2015-04-05&sr=b&sig=RaM7VKtUVZgKVvCbcBp3WP7cqrLT%2BrjwHv3kLdj8zzk%3D&se=2034-12-10T05%3A39%3A44Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.5_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5_amd64.deb?sv=2015-04-05&sr=b&sig=7XHTTf9%2FWBxEqX0Y8g7KqH8t3AFz1dJIZeY3YEDRypc%3D&se=2034-12-30T07%3A06%3A11Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4_amd64.deb?sv=2015-04-05&sr=b&sig=im7%2Fqp2dnoQTaPhY3Jv%2BHV0eZ4mPu27BT%2FbcF%2BQxGyQ%3D&se=2034-12-10T05%3A40%3A26Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5_amd64.deb?sv=2015-04-05&sr=b&sig=7I70XjzysspEht%2BsOwCM5QbtCe%2Boelh0%2FfwrhHPGh1g%3D&se=2034-12-30T07%3A06%3A51Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
