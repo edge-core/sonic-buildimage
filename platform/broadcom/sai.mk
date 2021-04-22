ee98b17281522fc6aad917663cc2ee2da0ec771d
abdosi <58047199+abdosi@users.noreply.github.com>
2021-04-21 17:41:45 -0700

    [submodule update] sonic-sairedis to move to SAI1.8.1 (#7393)
    What/Why I did:
    
    Updated sonic-sairedis submodule to use SAI1.8.1
    [Submodule update] sonic-sairedis
    d821bc0b137264daa01c347700c7c14677cf3370 (HEAD -> master, origin/master, origin/HEAD) [Mellanox] Add SAI template 
    config support (#803)
    bb341e9ea069e974a41930d434d437f522476f29 [syncd] Bring back TimerWatchdog (#821)
    badf6cea2650015269420932a9186113d1ad5ec6 Update .gitignore (#822)
    1494bc69046ffe7135377844548a11e4168b407c [meta] Mark local function static (#818)
    34e961cf39e9af93f492f66640739e1c7a1694c8 [pyext] Fix pyext/py2 library (#820)
    0d3749d3a93fd7e59ebb83b49fa1d7e2a56d6cf4 Moved SAI Header to git tag v1.8.1 (#816)
    70fff780d529f78b53af4bd104f4932d0c4d8dd6 Added --purge of base docker image packages before installing new ones. (#819)
    
    Updated Broadcom SAI Debian package to 4.3.3.4-2 to use SAI 1.8.1 Header

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.4-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.4-1_amd64.deb?sv=2019-12-12&st=2021-04-16T16%3A07%3A16Z&se=2030-04-17T16%3A07%3A00Z&sr=b&sp=r&sig=BiY2sN%2FVvDzZlxPEGWMwMLeB725yCvwrORHULdnWyXU%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.4-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.4-2_amd64.deb?sv=2015-04-05&sr=b&sig=DuX9sPYncs6MySLYMFOVSIxHvaBUn%2FJaeokJi9nVUb0%3D&se=2024-01-16T14%3A30%3A20Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4-1_amd64.deb?sv=2019-12-12&st=2021-04-16T16%3A07%3A55Z&se=2030-04-17T16%3A07%3A00Z&sr=b&sp=r&sig=XVNsLj8QwhGN3sgvgvPZ2glnLZm9Dhyci%2F61k36Fl0c%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4-2_amd64.deb?sv=2015-04-05&sr=b&sig=CqqazugaYvhUgsW2CkDvgJljTJL8tFT3WcDAlkIxjdY%3D&se=2024-01-16T14%3A31%3A19Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
