f77d719f7c65d4524a4cd4a23c7e0388a282f705
Ze Gan <ganze718@gmail.com>
2021-04-24 01:16:20 +0800

    [docker-fpm-frr]: Add split mode to routing config (#7307)
    For the split mode, the config files, like bgpd.conf, zebra.conf and so on, were provided by outside. But the docker_init.sh will overwrite the outside config files if restart bgp service.
    
    How I did it
    Add a split mode checking in docker_init.sh, if docker_routing_config_mode is split, don't overwrite the existing routing config files.
    
    How to verify it
    Set split mode in config db
    {
        "DEVICE_METADATA": {
            "localhost": {
                "hwsku": "Force10-S6000",
                "platform": "x86_64-kvm_x86_64-r0",
                "docker_routing_config_mode": "split"
                ...
            }
        }
    }
    Replace your bgpd.conf to /etc/sonic/frr/bgpd.conf
    Restart bgp service by sudo service bgp restart
    The /etc/sonic/frr/bgpd.conf your provided shouldn't be overwritten
    
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- dockers/docker-fpm-frr/docker_init.sh
+++ dockers/docker-fpm-frr/docker_init.sh
@@ -8,23 +8,13 @@ CFGGEN_PARAMS=" \
     -y /etc/sonic/constants.yml \
     -t /usr/share/sonic/templates/frr_vars.j2 \
     -t /usr/share/sonic/templates/supervisord/supervisord.conf.j2,/etc/supervisor/conf.d/supervisord.conf \
-    -t /usr/share/sonic/templates/bgpd/gen_bgpd.conf.j2,/etc/frr/bgpd.conf \
     -t /usr/share/sonic/templates/supervisord/critical_processes.j2,/etc/supervisor/critical_processes \
-    -t /usr/share/sonic/templates/zebra/zebra.conf.j2,/etc/frr/zebra.conf \
-    -t /usr/share/sonic/templates/staticd/gen_staticd.conf.j2,/etc/frr/staticd.conf \
-    -t /usr/share/sonic/templates/gen_frr.conf.j2,/etc/frr/frr.conf \
     -t /usr/share/sonic/templates/isolate.j2,/usr/sbin/bgp-isolate \
     -t /usr/share/sonic/templates/unisolate.j2,/usr/sbin/bgp-unisolate \
-    -t /usr/local/sonic/frrcfgd/bfdd.conf.j2,/etc/frr/bfdd.conf \
-    -t /usr/local/sonic/frrcfgd/ospfd.conf.j2,/etc/frr/ospfd.conf \
 "
 
 FRR_VARS=$(sonic-cfggen $CFGGEN_PARAMS)
-MGMT_FRAMEWORK_CONFIG=$(echo $FRR_VARS | jq -r '.frr_mgmt_framework_config')
 CONFIG_TYPE=$(echo $FRR_VARS | jq -r '.docker_routing_config_mode')
-if [ -z "$MGMT_FRAMEWORK_CONFIG" ] || [ "$MGMT_FRAMEWORK_CONFIG" == "false" ]; then
-    rm /etc/frr/bfdd.conf /etc/frr/ospfd.conf
-fi
 
 update_default_gw()
 {
@@ -57,9 +47,35 @@ if [[ ! -z "$NAMESPACE_ID" ]]; then
 fi
 
 if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "separated" ]; then
+    CFGGEN_PARAMS=" \
+        -d \
+        -y /etc/sonic/constants.yml \
+        -t /usr/share/sonic/templates/bgpd/gen_bgpd.conf.j2,/etc/frr/bgpd.conf \
+        -t /usr/share/sonic/templates/zebra/zebra.conf.j2,/etc/frr/zebra.conf \
+        -t /usr/share/sonic/templates/staticd/gen_staticd.conf.j2,/etc/frr/staticd.conf \
+    "
+    MGMT_FRAMEWORK_CONFIG=$(echo $FRR_VARS | jq -r '.frr_mgmt_framework_config')
+    if [ -n "$MGMT_FRAMEWORK_CONFIG" ] && [ "$MGMT_FRAMEWORK_CONFIG" != "false" ]; then
+        CFGGEN_PARAMS="$CFGGEN_PARAMS \
+            -t /usr/local/sonic/frrcfgd/bfdd.conf.j2,/etc/frr/bfdd.conf \
+            -t /usr/local/sonic/frrcfgd/ospfd.conf.j2,/etc/frr/ospfd.conf \
+        "
+    else
+        rm -f /etc/frr/bfdd.conf /etc/frr/ospfd.conf
+    fi
+    sonic-cfggen $CFGGEN_PARAMS
+    echo "no service integrated-vtysh-config" > /etc/frr/vtysh.conf
+    rm -f /etc/frr/frr.conf
+elif [ "$CONFIG_TYPE" == "split" ]; then
     echo "no service integrated-vtysh-config" > /etc/frr/vtysh.conf
     rm -f /etc/frr/frr.conf
 elif [ "$CONFIG_TYPE" == "unified" ]; then
+    CFGGEN_PARAMS=" \
+        -d \
+        -y /etc/sonic/constants.yml \
+        -t /usr/share/sonic/templates/gen_frr.conf.j2,/etc/frr/frr.conf \
+    "
+    sonic-cfggen $CFGGEN_PARAMS
     echo "service integrated-vtysh-config" > /etc/frr/vtysh.conf
     rm -f /etc/frr/bgpd.conf /etc/frr/zebra.conf /etc/frr/staticd.conf \
           /etc/frr/bfdd.conf /etc/frr/ospfd.conf /etc/frr/pimd.conf
