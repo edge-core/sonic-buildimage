c7540a8aba946166fd35ee41a4032a2a71601aac
gechiang <62408185+gechiang@users.noreply.github.com>
2021-05-06 09:20:30 -0700

    [202012] BRCM SAI 4.3.3.5-1 Added Support for 100G special AN/LT mode (#7534)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.5_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5_amd64.deb?sv=2015-04-05&sr=b&sig=7XHTTf9%2FWBxEqX0Y8g7KqH8t3AFz1dJIZeY3YEDRypc%3D&se=2034-12-30T07%3A06%3A11Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.5-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5-1_amd64.deb?sv=2015-04-05&sr=b&sig=WHmBcthD%2FqBv4Pia0GH4GQ%2BdKm8rolG8dJSh%2BYoGEr8%3D&se=2035-01-12T01%3A52%3A20Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5_amd64.deb?sv=2015-04-05&sr=b&sig=7I70XjzysspEht%2BsOwCM5QbtCe%2Boelh0%2FfwrhHPGh1g%3D&se=2034-12-30T07%3A06%3A51Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5-1_amd64.deb?sv=2015-04-05&sr=b&sig=BcGHyOGfS%2FypmiNUtHnX8WCbUwqoXPiRRe9E%2FUl9Sp8%3D&se=2035-01-12T01%3A52%3A57Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
