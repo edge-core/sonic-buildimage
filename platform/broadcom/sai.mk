3eaadec21cae968f0ac4dfa1c614e0cb5372f6d6
gechiang <62408185+gechiang@users.noreply.github.com>
2021-05-12 07:56:46 -0700

    [202012] BRCM SAI 4.3.3.5-2 picked up BRCM hsdk-6.5.21-p1.patch and CS00012097141 fix (#7581)
    This is to pick up BRCM SAI 4.3.3.5-2 which contains 2 main changes:
    1.  hsdk-6.5.21-p1.patch (to address some field problems related to SDK issue.
    2. Fix for CS00012097141 (remove some unnecessary debug setting that was causing non functional impacting problem at boot time)
    
    Preliminary tests looks fine. BGP neighbors were all up with proper routes programmed
    interfaces are all up
    Manually ran the following test cases on S6100 DUT and all passed:
    ```
         ipfwd/test_dir_bcast.py
         fib/test_fib.py
         vxlan/test_vxlan_decap.py
         decap/test_decap.py
         fdb/test_fdb.py
    ```

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.5-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5-1_amd64.deb?sv=2015-04-05&sr=b&sig=WHmBcthD%2FqBv4Pia0GH4GQ%2BdKm8rolG8dJSh%2BYoGEr8%3D&se=2035-01-12T01%3A52%3A20Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.5-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5-2_amd64.deb?sv=2015-04-05&sr=b&sig=pxqZN%2Fi3op3Fwpu%2BAE6Aa%2BLa%2Bs03UkSoucOXuCn9q7o%3D&se=2035-01-18T07%3A16%3A41Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5-1_amd64.deb?sv=2015-04-05&sr=b&sig=BcGHyOGfS%2FypmiNUtHnX8WCbUwqoXPiRRe9E%2FUl9Sp8%3D&se=2035-01-12T01%3A52%3A57Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5-2_amd64.deb?sv=2015-04-05&sr=b&sig=4NbjhnzdvOOJ2IBdgxBQ8S%2BW5OY2EIt2YFEp47pFLDI%3D&se=2035-01-18T07%3A16%3A08Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
