b64a6402d0b09d10276b35ca0ca50996ca34964f
pra-moh <49077256+pra-moh@users.noreply.github.com>
2021-05-11 17:11:54 -0700

    Revert "[Telemetry docker] add memory and memory swap limits (#7062)" (#7582)
    This reverts commit 0c5927816849b0442f49e6fe79187085ba233421.

--- rules/docker-telemetry.mk
+++ rules/docker-telemetry.mk
@@ -29,7 +29,6 @@ endif
 
 $(DOCKER_TELEMETRY)_CONTAINER_NAME = telemetry
 $(DOCKER_TELEMETRY)_RUN_OPT += --privileged -t
-$(DOCKER_TELEMETRY)_RUN_OPT += --memory 500m
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /usr/share/sonic/scripts:/usr/share/sonic/scripts:ro
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /var/run/dbus:/var/run/dbus:rw
