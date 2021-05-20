908e1a75246360e79e5bf273e3b5ef5a973f2d5c
Neetha John <nejo@microsoft.com>
2021-05-20 05:51:05 -0700

    [bgpcfgd] Enable BGP sessions over subinterfaces (#7654)
    Signed-off-by: Neetha John nejo@microsoft.com
    
    Fixes #7531
    
    Why I did it
    To enable bgp sessions to be established over subinterfaces
    
    How I did it
    Listen to VLAN_SUB_INTERFACE table in config db
    
    How to verify it
    Bgp sessions were established successfully over subinterface

--- src/sonic-bgpcfgd/bgpcfgd/main.py
+++ src/sonic-bgpcfgd/bgpcfgd/main.py
@@ -43,6 +43,7 @@ def do_work():
         InterfaceMgr(common_objs, "CONFIG_DB", swsscommon.CFG_LOOPBACK_INTERFACE_TABLE_NAME),
         InterfaceMgr(common_objs, "CONFIG_DB", swsscommon.CFG_VLAN_INTF_TABLE_NAME),
         InterfaceMgr(common_objs, "CONFIG_DB", swsscommon.CFG_LAG_INTF_TABLE_NAME),
+        InterfaceMgr(common_objs, "CONFIG_DB", swsscommon.CFG_VLAN_SUB_INTF_TABLE_NAME),
         # State DB managers
         ZebraSetSrc(common_objs, "STATE_DB", swsscommon.STATE_INTERFACE_TABLE_NAME),
         # Peer Managers
