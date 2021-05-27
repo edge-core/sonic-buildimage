5d29a2c2a66c03dfbf13b09e5dd1edf71fe9d4b2
gechiang <62408185+gechiang@users.noreply.github.com>
2021-05-27 15:11:03 -0700

    [202012] BRCM SAI 4.3.3.5-3 Enable VFP-based subintf on td2 (#7728)
    Why I did it
    This SAI change is to Enable TD2 platforms to also be able to handle VFP-based sub-interfaces.
    This is the feature that is also needed for TD2 platforms.
    
    How to verify it
    Guohan and Team has validated this feature change on TD2 platforms

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.5-2_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5-2_amd64.deb?sv=2015-04-05&sr=b&sig=pxqZN%2Fi3op3Fwpu%2BAE6Aa%2BLa%2Bs03UkSoucOXuCn9q7o%3D&se=2035-01-18T07%3A16%3A41Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5-2_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.5-3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.5-3_amd64.deb?sv=2015-04-05&sr=b&sig=EqEEo7pUSwkzxVidHnbSlENWyJQkqWZZ4JLN%2B1JZsBY%3D&se=2035-02-03T05%3A58%3A49Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5-2_amd64.deb?sv=2015-04-05&sr=b&sig=4NbjhnzdvOOJ2IBdgxBQ8S%2BW5OY2EIt2YFEp47pFLDI%3D&se=2035-01-18T07%3A16%3A08Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.5-3_amd64.deb?sv=2015-04-05&sr=b&sig=JDFMqPxyb8bJlRG0UFQtpQ3opmP%2FQt%2F08PlMn1RXN%2F4%3D&se=2035-02-03T05%3A59%3A23Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
