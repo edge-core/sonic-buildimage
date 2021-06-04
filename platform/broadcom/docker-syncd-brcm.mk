53663339a2503c985117eb691b961dba1054fadd
賓少鈺 <51807939+geans-pin@users.noreply.github.com>
2021-06-04 14:18:02 +0800

    Per-switching silicon Common config for Broadcom Supported Platforms (#7493)
    * Per-switching silicon Common config for Broadcom Supported Platforms
    
    * Per-switching silicon Common config for Broadcom Supported Platforms
    
    * Per-switching silicon Common config for Broadcom Supported Platforms
    
    * Remove property readme
    
    * Add common config readme

--- platform/broadcom/docker-syncd-brcm.mk
+++ platform/broadcom/docker-syncd-brcm.mk
@@ -15,6 +15,7 @@ $(DOCKER_SYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /usr/share/sonic/device/x86_64-broadcom_common:/usr/share/sonic/device/x86_64-broadcom_common:ro
 
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcmcmd:/usr/bin/bcmcmd
 $(DOCKER_SYNCD_BASE)_BASE_IMAGE_FILES += bcmsh:/usr/bin/bcmsh
