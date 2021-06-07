62a4603eef5c64116e6f914b2215f28b0c4e4e7a
Shi Su <67605788+shi-su@users.noreply.github.com>
2021-06-07 16:44:54 -0700

    Remove quagga related code (#7476)
    Why I did it
    Quagga is no longer being used. Remove quagga-related code (e.g., docker-fpm-quagga, sonic-quagga, etc.).
    
    How I did it
    Remove quagga-related code.

--- platform/vs/docker-sonic-vs.mk
+++ platform/vs/docker-sonic-vs.mk
@@ -34,9 +34,7 @@ $(DOCKER_SONIC_VS)_DEPENDS += $(SWSS_DBG) \
                               $(SYNCD_VS_DBG)
 endif
 
-ifeq ($(SONIC_ROUTING_STACK), quagga)
-$(DOCKER_SONIC_VS)_DEPENDS += $(QUAGGA)
-else ifeq ($(SONIC_ROUTING_STACK), frr)
+ifeq ($(SONIC_ROUTING_STACK), frr)
 $(DOCKER_SONIC_VS)_DEPENDS += $(FRR)
 else
 $(DOCKER_SONIC_VS)_DEPENDS += $(GOBGP)
