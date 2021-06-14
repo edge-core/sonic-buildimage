0d7e4006890166d6436a121d4daf6d3b556ee8df
abdosi <58047199+abdosi@users.noreply.github.com>
2021-06-13 17:00:58 -0700

    [brcm sai] BRCM SAI 5.0.0.1 (#7753)
    Added support BRCM SAI 5.0.0.1.  
    
    Major changes here:
    
    CS00012019568	Link Training (all 100G ASICs - TH families and TD3)
    CS00012184310	[attribute_capability| for port SAI_PORT_ATTR_TPID returns CREATE_IMP=false|SET_IMP=true|GET_IMP=true
    CS00012182145	[IPinIP][Tunnel Delete] If IPinIP tunnel delete is performed observed following SYNCd error: ERR syncd#syncd: [none] SAI_API_TUNNEL:brcm_sai_tnl_mp_remove_tunnel_term_table_entry:4026 _brcm_sai_mptnl_sip_tnl_lookup failed with error -7.
    CS00012182148   Rate Limit Parity error message to syncd/sonic.
    CS00012178692   ACL drops counted as interface drops
    CS00012183901   [WARMBOOT] WARMReboot with active traffic causes port flap reported during warm reboot
    CS00012023263   TD3/TH2 : Support 4 lossless queues(2 SW PFCWD and 2 HW PFCWD)
    CS00012019578   Pre FEC bit-error rate (BER) - DNX and XGS (TD and TH 50/100G)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.5-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.3.5-1_amd64.deb?sv=2019-12-12&st=2021-04-26T22%3A16%3A33Z&se=2030-04-27T22%3A16%3A00Z&sr=b&sp=r&sig=ssF1KPWFqoIsOsLOxwkYbzwsDF%2FNaZ8LfByjn%2BqGccU%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.5-1_amd64.deb
+BRCM_SAI = libsaibcm_5.0.0.1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_5.0.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=FXHu5ggw8zfUdvi0UScTHMAP0X3br0vTM4f2U2brQWo%3D&se=2029-08-15T01%3A20%3A19Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_5.0.0.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.5-1_amd64.deb?sv=2019-12-12&st=2021-04-26T22%3A17%3A56Z&se=2030-04-27T22%3A17%3A00Z&sr=b&sp=r&sig=2eWNoczh9P5c3Ir%2B1YL08ZkPol9GUkvNJKFGf%2BacXlI%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dev_5.0.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=C48%2BIViiA5KAq4ubDkXSehylTQgiIc7ZD47eo4roBYI%3D&se=2029-08-15T01%3A21%3A14Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
