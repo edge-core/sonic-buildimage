efb6c1d9cb39ba8660560131f844c067c00417f7
gechiang <62408185+gechiang@users.noreply.github.com>
2021-06-25 08:06:45 -0700

    [202012][BRCMSAI]Fix two crash issues introduced by SAI 4.3.3.8 (#7979)
    Why I did it
    There were two regression issues introduced by BRCM SAI 4.3.3.8:
    
    CS00012196056 [4.3.3.8][WARMBOOT] syncd[2584]: segfault at 5616ad6c3d80 ip 00007f61e0c6bc65 sp 00007fff0c5a7a90 error 4 in libsai.so.1.0[7f61e0a95000+3cd8000]
    CS00012195956 [4.3.3.8] [TD3]Syncd Crash at brcm_sai_tnl_mp_create_tunnel()
    How I did it
    Patch for CS00012195956 from BRCM was validated to have addressed the tunnel creation issue.
    Temporary worked around the issue by commenting out a portion of questionable code in BRCM SAI that seems to be the root cause of CS00012196056 .
    How to verify it
    See the BRCM cases for details.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.8_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.8_amd64.deb?sv=2015-04-05&sr=b&sig=OPgORcuUzFLJdt9VwqnZuRx2TgGUo5tbDIOtpJuoHB4%3D&se=2035-02-25T22%3A17%3A13Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.8_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.8-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.8-1_amd64.deb?sv=2015-04-05&sr=b&sig=gfKgP7HePZcvoESly6xJ%2BIyTovF%2BWZvyvTjfWR%2BqMcs%3D&se=2035-03-04T06%3A55%3A34Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.8-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.8_amd64.deb?sv=2015-04-05&sr=b&sig=2N3yC%2BDAXt%2BN98V5Qc8cxBV7V%2FE%2B4C%2BYTSzs0d3MIOA%3D&se=2035-02-25T22%3A17%3A52Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.8-1_amd64.deb?sv=2015-04-05&sr=b&sig=ST85a0laEY245n2QAy5gtbpd8LZkIuCSiBDEVc10wC8%3D&se=2035-03-04T06%3A57%3A04Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
