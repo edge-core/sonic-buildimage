a1268b34f5d9efc85abf38b759abc41d7e91eacb
arunlk-dell <83708154+arunlk-dell@users.noreply.github.com>
2021-06-28 22:14:49 +0530

    DellEMC: S5248F - Platform API 2.0 implementation (#7930)
    #### Why I did it
    Support API 2.0 for S5248F platform
    
    #### How I did it
    Making changes to S5248F platform specific directory
    
    Co-authored-by: Arun LK <Arun_L_K@dell.com>

--- platform/broadcom/sonic-platform-modules-dell/common/sonic_platform/hwaccess.py
+++ platform/broadcom/sonic-platform-modules-dell/common/sonic_platform/hwaccess.py
@@ -19,6 +19,23 @@ def pci_get_value(resource, offset):
         mm.close()
     return val
 
+def pci_mem_write(memmap, offset, data):
+    """ Write PCI device """
+    memmap.seek(offset)
+    memmap.write(struct.pack('I', data))
+
+def pci_set_value(resource, val, offset):
+    """ Set a value to PCI device """
+    with open(resource, 'w+b') as filed:
+        memmap = None
+        try:
+            memmap = mmap.mmap(filed.fileno(), 0)
+            pci_mem_write(memmap, offset, val)
+        except EnvironmentError:
+            pass
+        if memmap is not None:
+            memmap.close()
+
 # Read I2C device
 
 def i2c_get(bus, i2caddr, ofs):
