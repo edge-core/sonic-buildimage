01f51e01c3cca5a1461fc0080f71186a0be0bfc6
byu343 <byu@arista.com>
2021-07-01 12:35:09 -0700

    [Arista] Update phy-credo service for config load (#8005)
    phy-credo.service will be restarted when running 'config reload'
    
    Signed-off-by: Boyang Yu <byu@arista.com>

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/6d0d1661d92a342acedb6839dba970ae5778b478/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/08cbc09437e942c1e3cd84a7595ca686193d311b/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
