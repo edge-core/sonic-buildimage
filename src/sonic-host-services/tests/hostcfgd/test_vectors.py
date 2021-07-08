776a446d763ef339107b948f9ab9cb75488be440
shlomibitton <60430976+shlomibitton@users.noreply.github.com>
2021-07-08 09:10:46 +0300

    [dhcp_relay] Disable dhcp_relay for ToRRouter switches type by the feature manager (#7789)
    - Why I did it
    Currently dhcp packets are disabled by the COPP manager for non ToRRouter type switches.
    Even if the feature is enabled, DHCP packets wont hook to the CPU since the COPP manager will not trap this packets.
    This change is to disable dhcp_relay by default for non ToRRouter switches from init_cfg.json.
    With this approach, if the user want to enable the feature for non ToRRouter switches, manual enablement is required by the 'feature' configuration.
    This is to keep the current approach for MSFT production issue with dhcp relay for non ToRRouter switched and allow the user to decide if to use it or not.
    
    - How I did it
    Configure dhcp_relay 'disabled' by default on init_cfg.json for non ToRRouter switches.
    Remove the exclusion of dhcp packets on copp_cfg.json
    
    - How to verify it
    Enable dhcp_relay feature on a non ToRRouter switch.
    Unit-tests modified so the default values on mocked CONFIG DB in 'test_vectors.py' for dhcp_relay will be 'disabled'.
    This is by the change for 'init_cfg.json.j2'.
    For ToRRouter the state will change from 'disabled' to 'enabled'.
    Another test case added for a 'ToR' switch type, this is to test the state is 'enabled' if the user configured it to be so.

--- src/sonic-host-services/tests/hostcfgd/test_vectors.py
+++ src/sonic-host-services/tests/hostcfgd/test_vectors.py
@@ -29,7 +29,7 @@ HOSTCFGD_TEST_VECTOR = [
                         "has_timer": "False",
                         "high_mem_alert": "disabled",
                         "set_owner": "kube",
-                        "state": "enabled"
+                        "state": "{% if not (DEVICE_METADATA is defined and DEVICE_METADATA['localhost'] is defined and DEVICE_METADATA['localhost']['type'] is defined and DEVICE_METADATA['localhost']['type'] != 'ToRRouter') %}enabled{% else %}disabled{% endif %}"
                     },
                     "mux": {
                         "auto_restart": "enabled",
@@ -125,7 +125,7 @@ HOSTCFGD_TEST_VECTOR = [
                         "has_timer": "False",
                         "high_mem_alert": "disabled",
                         "set_owner": "kube",
-                        "state": "enabled"
+                        "state": "{% if not (DEVICE_METADATA is defined and DEVICE_METADATA['localhost'] is defined and DEVICE_METADATA['localhost']['type'] is defined and DEVICE_METADATA['localhost']['type'] != 'ToRRouter') %}enabled{% else %}disabled{% endif %}"
                     },
                     "mux": {
                         "auto_restart": "enabled",
@@ -157,7 +157,7 @@ HOSTCFGD_TEST_VECTOR = [
                         "has_timer": "False",
                         "high_mem_alert": "disabled",
                         "set_owner": "kube",
-                        "state": "enabled"
+                        "state": "disabled"
                     },
                     "mux": {
                         "auto_restart": "enabled",
@@ -181,9 +181,6 @@ HOSTCFGD_TEST_VECTOR = [
                 },
             },
             "expected_subprocess_calls": [
-                call("sudo systemctl unmask dhcp_relay.service", shell=True),
-                call("sudo systemctl enable dhcp_relay.service", shell=True),
-                call("sudo systemctl start dhcp_relay.service", shell=True),
                 call("sudo systemctl stop mux.service", shell=True),
                 call("sudo systemctl disable mux.service", shell=True),
                 call("sudo systemctl mask mux.service", shell=True),
@@ -204,6 +201,99 @@ HOSTCFGD_TEST_VECTOR = [
                 "DEVICE_METADATA": {
                     "localhost": {
                         "type": "T1",
+                    }
+                },
+                "KDUMP": {
+                    "config": {
+                        "enabled": "false",
+                        "num_dumps": "3",
+                        "memory": "0M-2G:256M,2G-4G:320M,4G-8G:384M,8G-:448M"
+                        }
+                },
+                "FEATURE": {
+                    "dhcp_relay": {
+                        "auto_restart": "enabled",
+                        "has_global_scope": "True",
+                        "has_per_asic_scope": "False",
+                        "has_timer": "False",
+                        "high_mem_alert": "disabled",
+                        "set_owner": "kube",
+                        "state": "{% if not (DEVICE_METADATA is defined and DEVICE_METADATA['localhost'] is defined and DEVICE_METADATA['localhost']['type'] is defined and DEVICE_METADATA['localhost']['type'] != 'ToRRouter') %}enabled{% else %}disabled{% endif %}"
+                    },
+                    "mux": {
+                        "auto_restart": "enabled",
+                        "has_global_scope": "True",
+                        "has_per_asic_scope": "False",
+                        "has_timer": "False",
+                        "high_mem_alert": "disabled",
+                        "set_owner": "local",
+                        "state": "{% if 'subtype' in DEVICE_METADATA['localhost'] and DEVICE_METADATA['localhost']['subtype'] == 'DualToR' %}enabled{% else %}always_disabled{% endif %}"
+                    },
+                    "telemetry": {
+                        "auto_restart": "enabled",
+                        "has_global_scope": "True",
+                        "has_per_asic_scope": "False",
+                        "has_timer": "True",
+                        "high_mem_alert": "disabled",
+                        "set_owner": "kube",
+                        "state": "enabled",
+                        "status": "enabled"
+                    },
+                },
+            },
+            "expected_config_db": {
+                "FEATURE": {
+                    "dhcp_relay": {
+                        "auto_restart": "enabled",
+                        "has_global_scope": "True",
+                        "has_per_asic_scope": "False",
+                        "has_timer": "False",
+                        "high_mem_alert": "disabled",
+                        "set_owner": "kube",
+                        "state": "disabled"
+                    },
+                    "mux": {
+                        "auto_restart": "enabled",
+                        "has_global_scope": "True",
+                        "has_per_asic_scope": "False",
+                        "has_timer": "False",
+                        "high_mem_alert": "disabled",
+                        "set_owner": "local",
+                        "state": "always_disabled"
+                    },
+                    "telemetry": {
+                        "auto_restart": "enabled",
+                        "has_global_scope": "True",
+                        "has_per_asic_scope": "False",
+                        "has_timer": "True",
+                        "high_mem_alert": "disabled",
+                        "set_owner": "kube",
+                        "state": "enabled",
+                        "status": "enabled"
+                    },
+                },
+            },
+            "expected_subprocess_calls": [
+                call("sudo systemctl stop mux.service", shell=True),
+                call("sudo systemctl disable mux.service", shell=True),
+                call("sudo systemctl mask mux.service", shell=True),
+                call("sudo systemctl unmask telemetry.service", shell=True),
+                call("sudo systemctl unmask telemetry.timer", shell=True),
+                call("sudo systemctl enable telemetry.timer", shell=True),
+                call("sudo systemctl start telemetry.timer", shell=True),
+            ],
+            "popen_attributes": {
+                'communicate.return_value': ('output', 'error')
+            },
+        },
+    ],
+    [
+        "SingleToRCase_DHCP_Relay_Enabled",
+        {
+            "config_db": {
+                "DEVICE_METADATA": {
+                    "localhost": {
+                        "type": "ToR",
                     }
                 },
                 "KDUMP": {
@@ -318,7 +408,7 @@ HOSTCFGD_TEST_VECTOR = [
                         "has_timer": "False",
                         "high_mem_alert": "disabled",
                         "set_owner": "kube",
-                        "state": "enabled"
+                        "state": "{% if not (DEVICE_METADATA is defined and DEVICE_METADATA['localhost'] is defined and DEVICE_METADATA['localhost']['type'] is defined and DEVICE_METADATA['localhost']['type'] != 'ToRRouter') %}enabled{% else %}disabled{% endif %}"
                     },
                     "mux": {
                         "auto_restart": "enabled",
