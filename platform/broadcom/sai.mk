514f7607931de95ccba1b8b23319dd0296f72776
gechiang <62408185+gechiang@users.noreply.github.com>
2021-07-14 10:36:15 -0700

    [202012] BRCM SAI 4.3.3.9 Changes for ISSU support and Dual ToR fixes (#8179)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.8-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.8-1_amd64.deb?sv=2015-04-05&sr=b&sig=gfKgP7HePZcvoESly6xJ%2BIyTovF%2BWZvyvTjfWR%2BqMcs%3D&se=2035-03-04T06%3A55%3A34Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.8-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.3.9_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.9_amd64.deb?sv=2015-04-05&sr=b&sig=UgZ4pYi9Q5mSK6QLlDCkz%2Br3u0WBdbS28K5apa0RmgQ%3D&se=2035-03-23T05%3A31%3A58Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.3.9_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.8-1_amd64.deb?sv=2015-04-05&sr=b&sig=ST85a0laEY245n2QAy5gtbpd8LZkIuCSiBDEVc10wC8%3D&se=2035-03-04T06%3A57%3A04Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.9_amd64.deb?sv=2015-04-05&sr=b&sig=yQJG%2Fy%2BRyxbgHXERIHMeHDGcy%2BSjV%2BFeCTop9wfW5hQ%3D&se=2035-03-23T05%3A33%3A01Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
