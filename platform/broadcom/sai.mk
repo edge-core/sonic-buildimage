fe24a4da2b367272d3bf34fe2d18a6725b36d12e
judyjoseph <53951155+judyjoseph@users.noreply.github.com>
2021-07-15 10:24:57 -0700

    Update XGS and DNX SAI to 5.0.0.4 and additional flags needed in saibcm-modules. (#8160)
    Why I did it
    Update XGS and DNX SAI to 5.0.0.4 and additional flags needed in saibcm-modules
    
    The following CSP's are merged in 5.0.0.4
    
    CS00012182148 [4.3] Rate Limit Parity error message to syncd/sonic.
    CS00012178692 [4.3] ACL drops counted as interface drops
    CS00012183901 [4.3][WARMBOOT] WARMReboot with active traffic causes port flap reported during warm reboot
    CS00012070713 [SAI 4.3 , DNX, 8690] Everflow ACL creation fails - brcm_sai_dnx_create_acl_table API fails, with unknown attribute error.
    CS00012023263 [4.4] TD3/TH2 : Support 4 lossless queues(2 SW PFCWD and 2 HW PFCWD)
    CS00012019578 [4.4] Pre FEC bit-error rate (BER) - DNX and XGS (TD and TH 50/100G)
    
    How I did it
    Changes the various make files to include the new SAI release + update the opennsl-modules.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,12 +1,12 @@
-BRCM_SAI = libsaibcm_5.0.0.1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_5.0.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=FXHu5ggw8zfUdvi0UScTHMAP0X3br0vTM4f2U2brQWo%3D&se=2029-08-15T01%3A20%3A19Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_5.0.0.1_amd64.deb
+BRCM_SAI = libsaibcm_5.0.0.4_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_5.0.0.4_amd64.deb?sv=2015-04-05&sr=b&sig=ZuEkoLskVsCtNoej4I%2BBuhX01hFJXZC4yuhoFg0jzuw%3D&se=2035-03-21T23%3A30%3A55Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_5.0.0.4_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dev_5.0.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=C48%2BIViiA5KAq4ubDkXSehylTQgiIc7ZD47eo4roBYI%3D&se=2029-08-15T01%3A21%3A14Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dev_5.0.0.4_amd64.deb?sv=2015-04-05&sr=b&sig=JYxPob3rO%2BnPv%2FTFn9Q3xbVx5l6ZxwrRq7fpg%2BcUqwQ%3D&se=2035-03-21T23%3A31%3A26Z&sp=r"
 
 # SAI module for DNX Asic family
-BRCM_DNX_SAI = libsaibcm_dnx_5.0.0.1_amd64.deb
-$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_dnx_5.0.0.1_amd64.deb?sv=2015-04-05&sr=b&sig=iUW4ZSz43oeOSe21%2BFaNTG1phTr6qgAfeeEN2mCXBWU%3D&se=2035-01-17T05%3A15%3A51Z&sp=r"
+BRCM_DNX_SAI = libsaibcm_dnx_5.0.0.4_amd64.deb
+$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_dnx_5.0.0.4_amd64.deb?sv=2015-04-05&sr=b&sig=CNtYmID5aZtc0ZFHldWYCQSbzyb9xT18vGxT%2BZj7kHc%3D&se=2035-03-21T23%3A31%3A50Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 SONIC_ONLINE_DEBS += $(BRCM_DNX_SAI)
