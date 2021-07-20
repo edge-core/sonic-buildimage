950c24c5ae8cb5ef538a650fd976d9bebcea2ddc
tomer-israel <76040066+tomer-israel@users.noreply.github.com>
2021-07-20 11:56:04 +0300

    [PMON] [Mellanox] fix syseepromd issue on simx (#8131)
    Avoid initializing sfp/thermal/components/fan/psu/leds on simx and create vpd_info file on hw_management when we use mellanox simulator platform
    
    - Why I did it
    this is a fix for issue in mellanox simulator platforms. the syseepromd failed on the pmon docker. also "decode-syseeprom" failed also
    
    - How I did it
    before initializing thermal/components/fan/psu/leds --> check if we are running on simx
    creating the vpd_info on the hw_management folder.
    
    - How to verify it
    check if syseepromd process was loaded properly on the pmon docker.
    decode-syseeprom is working well without errors/warnings

--- platform/mellanox/mlnx-platform-api/sonic_platform/eeprom.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/eeprom.py
@@ -7,9 +7,10 @@
 #############################################################################
 import os
 import time
+import subprocess
 
 from sonic_py_common.logger import Logger
-
+from sonic_py_common.device_info import get_platform, get_path_to_platform_dir
 try:
     from sonic_platform_base.sonic_eeprom import eeprom_tlvinfo
 except ImportError as e:
@@ -24,6 +25,16 @@ logger = Logger()
 # should this be moved to chass.py or here, which better?
 #
 EEPROM_SYMLINK = "/var/run/hw-management/eeprom/vpd_info"
+
+platform_name = get_platform()
+if 'simx' in platform_name:
+    platform_path = get_path_to_platform_dir()
+
+    if not os.path.exists(EEPROM_SYMLINK):
+        if not os.path.exists(os.path.dirname(EEPROM_SYMLINK)):
+            os.makedirs(os.path.dirname(EEPROM_SYMLINK))
+    
+        subprocess.check_call(['/usr/bin/xxd', '-r', '-p', 'syseeprom.hex', EEPROM_SYMLINK], cwd=platform_path)
 
 class Eeprom(eeprom_tlvinfo.TlvInfoDecoder):
     RETRIES = 3
