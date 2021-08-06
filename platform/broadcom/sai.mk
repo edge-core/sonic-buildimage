0f3f0c2a1a203ccb9c08952bb41727d4598b485d
gechiang <62408185+gechiang@users.noreply.github.com>
2021-08-05 19:03:06 -0700

    [202012] BRCM SAI 4.3.5.1 Fix for TH3 FDB Flush Timeout (#8342)
    This is to pick up BRCM SAI 4.3.5.1 which contains the following fix:
    CS00012201406: [4.3.3.9] SAI_STATUS_FAILURE on FDB flush after all ports flapped
    
    Preliminary tests looks fine. BGP neighbors were all up with proper routes programmed
    interfaces are all up
    Manually ran the following test cases on z9332f (TH3) T0 DUT and all passed:
    ```
         ipfwd/test_dir_bcast.py
         fib/test_fib.py
    ```
    Manually ran the following test cases on S6100 (TH) and all passed:
    ```
         ipfwd/test_dir_bcast.py
         fdb/test_fdb.py
    ```

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.3.9_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.3.9_amd64.deb?sv=2015-04-05&sr=b&sig=UgZ4pYi9Q5mSK6QLlDCkz%2Br3u0WBdbS28K5apa0RmgQ%3D&se=2035-03-23T05%3A31%3A58Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.3.9_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1_amd64.deb?sv=2015-04-05&sr=b&sig=c%2BgR2UeeyjEyo%2BBwCFtcTOxOQbX38EeIiwqdfQSh76o%3D&se=2035-04-13T19%3A30%3A14Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.3.9_amd64.deb?sv=2015-04-05&sr=b&sig=yQJG%2Fy%2BRyxbgHXERIHMeHDGcy%2BSjV%2BFeCTop9wfW5hQ%3D&se=2035-03-23T05%3A33%3A01Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1_amd64.deb?sv=2015-04-05&sr=b&sig=qxAl66xr1T%2B0ibG%2BSSc4xM3nfVpE5w%2Bk1z8jI%2Fihu6c%3D&se=2035-04-13T19%3A33%3A19Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
