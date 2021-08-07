de3d30f36d181047ab04129e4e28e6f0e574adeb
abdosi <58047199+abdosi@users.noreply.github.com>
2021-08-06 17:55:41 -0700

    Updated Broadcom SAI Debian package to 3.7.6.1 (#8365)
    Updated Broadcom SAI Debian package to 3.7.6.1 Following are the major changes here:
    
    - CS00011651922/CS00012192502 SID:Parity error in TDM Calendar memories causes traffic drop after SER correction
    - CS00011222060 soc_mem_alpm_delete: unit 0: ALPM delete operation[L3_DEFIP_ALPM_IPV6_128] encountered parity error
    - Cesto Phy Recovery enhancement.
    - SDK compile with flag -DBCM_MONOTONIC_TIME and -DBCM_MONOTONIC_MUTEXES

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_3.7.5.2-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.5.2-3_amd64.deb?sv=2015-04-05&sr=b&sig=dvVHQluR6fJy1bo%2Bj7l8jazCq6v6fVTwGXvkWa8uoa8%3D&se=2029-09-01T22%3A07%3A34Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_3.7.5.2-3_amd64.deb
+BRCM_SAI = libsaibcm_3.7.6.1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.6.1_amd64.deb?sv=2015-04-05&sr=b&sig=PXZNHvLaofj6qevZqf3eDUX7SGp%2BJnblyEV%2FkMFcwus%3D&se=2035-04-15T19%3A36%3A16Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_3.7.6.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.5.2-3_amd64.deb?sv=2015-04-05&sr=b&sig=WiEoc6PuzBvtInb%2FDAV%2Bv1whOJN4zc8AC59yCXAe9Xo%3D&se=2029-09-01T22%3A06%3A25Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.6.1_amd64.deb?sv=2015-04-05&sr=b&sig=S67%2FNP8J2xshBYhDV6NwP7LBnvyUOO1EZKspL1O6bCY%3D&se=2035-04-15T19%3A37%3A50Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
