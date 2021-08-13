03ef27531408fbcf9060b62f24ae8d968085353f
carl-nokia <63672637+carl-nokia@users.noreply.github.com>
2021-08-13 06:27:06 -0400

    [Nokia ixs7215] sfputil support + component tests (#8445)
    Deliver sfputil support for sfputil show eeprom and sfputil reset along with some component test case fixes
    
    Co-authored-by: Carl Keene <keene@nokia.com>

--- platform/marvell-armhf/sonic-platform-nokia/7215/sonic_platform/component.py
+++ platform/marvell-armhf/sonic-platform-nokia/7215/sonic_platform/component.py
@@ -75,6 +75,55 @@ class Component(ComponentBase):
         """
         return self.name
 
+    def get_model(self):
+        """
+        Retrieves the part number of the component
+        Returns:
+            string: Part number of component
+        """
+        return 'NA'
+
+    def get_serial(self):
+        """
+        Retrieves the serial number of the component
+        Returns:
+            string: Serial number of component
+        """
+        return 'NA'
+
+    def get_presence(self):
+        """
+        Retrieves the presence of the component
+        Returns:
+            bool: True if  present, False if not
+        """
+        return True
+
+    def get_status(self):
+        """
+        Retrieves the operational status of the component
+        Returns:
+            bool: True if component is operating properly, False if not
+        """
+        return True
+
+    def get_position_in_parent(self):
+        """
+        Retrieves 1-based relative physical position in parent device.
+        Returns:
+            integer: The 1-based relative physical position in parent
+            device or -1 if cannot determine the position
+        """
+        return -1
+
+    def is_replaceable(self):
+        """
+        Indicate whether component is replaceable.
+        Returns:
+            bool: True if it is replaceable.
+        """
+        return False
+
     def get_description(self):
         """
         Retrieves the description of the component
