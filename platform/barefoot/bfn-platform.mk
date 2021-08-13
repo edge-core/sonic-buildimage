93eb182d78ed203e28648d196503d8a5753b77c5
roman_savchuk <romanx.savchuk@intel.com>
2021-08-13 09:27:51 +0300

    [barefoot]: Updated SAI/SDK packages to 20210811 (#8427)
    Updated SAI/SDK package to 20210811. Includes fix for pfc_wd.
    
    Signed-off-by: Roman Savchuk <romanx.savchuk@intel.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,4 +1,4 @@
-BFN_PLATFORM = bfnplatform_20210423_sai_1.8.1_deb10.deb
+BFN_PLATFORM = bfnplatform_20210811_sai.1.8.1_deb10.deb
 $(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
