fcdd63835b741b0ff33b256aa759a713a2864d71
gechiang <62408185+gechiang@users.noreply.github.com>
2021-08-26 14:30:22 -0700

    [202012]BRCM SAI 4.3.5.1-1 Fix configurable drop counter out of resource (#8601)
    * [202012]BRCM SAI 4.3.5.1 Fix for configurable drop counter out of resource

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1_amd64.deb?sv=2015-04-05&sr=b&sig=c%2BgR2UeeyjEyo%2BBwCFtcTOxOQbX38EeIiwqdfQSh76o%3D&se=2035-04-13T19%3A30%3A14Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.1-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=lQsSB%2BJnoynyjbrRl30QSWKsl5qb7kVq9L9DB%2BhZeP4%3D&se=2035-05-05T06%3A04%3A02Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1_amd64.deb?sv=2015-04-05&sr=b&sig=qxAl66xr1T%2B0ibG%2BSSc4xM3nfVpE5w%2Bk1z8jI%2Fihu6c%3D&se=2035-04-13T19%3A33%3A19Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=2YjE1nTWN5Nr5403N4kYge1kSvgGtE3t87HY7FsGKeo%3D&se=2035-05-05T06%3A05%3A03Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
