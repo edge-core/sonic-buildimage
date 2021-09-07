84b5659372eeb8562878972aad1ecac33a23cc4c
gechiang <62408185+gechiang@users.noreply.github.com>
2021-09-06 22:12:59 -0700

    [202012] BRCM SAI 4.3.5.1-2 Fix BRCM SAI regression due to ACL Egress Mirroring Action capability (#8682)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.1-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=lQsSB%2BJnoynyjbrRl30QSWKsl5qb7kVq9L9DB%2BhZeP4%3D&se=2035-05-05T06%3A04%3A02Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.1-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-2_amd64.deb?sv=2020-04-08&st=2021-09-03T19%3A11%3A02Z&se=2036-01-01T20%3A11%3A00Z&sr=b&sp=r&sig=N8u96t8srU9bfs8xnVlFZ4%2Fw%2BZg6YYLLSc3j%2FaNPss0%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=2YjE1nTWN5Nr5403N4kYge1kSvgGtE3t87HY7FsGKeo%3D&se=2035-05-05T06%3A05%3A03Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-2_amd64.deb?sv=2015-04-05&sr=b&sig=RvQcM1FHKbezWN3GMHZ6VuslpR0d5TLxpuRjskjJ%2Fbw%3D&se=2035-05-13T19%3A31%3A33Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
