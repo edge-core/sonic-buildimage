5356244e538f6d866285a4d9952515aba86af47a
Dror Prital <76714716+dprital@users.noreply.github.com>
2021-10-17 19:03:02 +0300

    [Mellanox] Add NVIDIA Copyright header to "mellanox" files (#8799)
    - Why I did it
    Add NVIDIA Copyright header to "mellanox" files
    
    - How I did it
    Add NVIDIA Copyright header as a comment for Mellanox files
    
    - How to verify it
    Sanity tests and PR checkers.

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2016-2021 NVIDIA CORPORATION & AFFILIATES.
+# Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 MLNX_SDK_BASE_PATH = $(PLATFORM_PATH)/sdk-src/sx-kernel/Switch-SDK-drivers/bin/
 MLNX_SDK_PKG_BASE_PATH = $(MLNX_SDK_BASE_PATH)/$(BLDENV)/
 MLNX_SDK_VERSION = 4.4.3326
