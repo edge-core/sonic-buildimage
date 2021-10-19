c95178157d8345c8e41fc536239df205b1f70a7e
gechiang <62408185+gechiang@users.noreply.github.com>
2021-10-19 14:08:31 -0700

    [202012]BRCM SAI 4.5.3.1-5 picked up SAI fixes for several CSP cases (#9003)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.1-4_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-4_amd64.deb?sv=2015-04-05&sr=b&sig=LfRmvdkpfDcRBIW0R%2BHvD9zdc%2Br5KP%2BRY9aGK9gk1IE%3D&se=2035-06-22T06%3A50%3A41Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-4_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.1-5_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-5_amd64.deb?sv=2015-04-05&sr=b&sig=TpNMP8d%2BuTseukC%2BbiqS1JAsw0mKNCF1dtJOcSC4ydk%3D&se=2035-06-27T06%3A29%3A36Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-5_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-4_amd64.deb?sv=2015-04-05&sr=b&sig=2WoIZ2gpylyuL6XEKTGka0zqXLNpLr407Qfyp6YFqvI%3D&se=2035-06-22T06%3A51%3A34Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-5_amd64.deb?sv=2015-04-05&sr=b&sig=M%2FAiNWLGFMMzLHGJBPpZ%2B18JTYcOFPgHYtKLhkvO0r4%3D&se=2035-06-27T06%3A30%3A28Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
