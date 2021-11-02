3f8821cc4b2413e6535f2cbf575b9a0b31f00e53
roman_savchuk <romanx.savchuk@intel.com>
2021-11-02 16:37:25 +0200

    [BFN]: updated SDK with SAI 1.9.1 (#9135)
    Updated SDK packages to SAI 1.9.1
    
    Signed-off-by: Roman Savchuk <romanx.savchuk@intel.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,4 +1,4 @@
-BFN_PLATFORM = bfnplatform_20211021_sai.1.9_deb10.deb
+BFN_PLATFORM = bfnplatform_20211101_sai.1.9.1_deb10.deb
 $(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
