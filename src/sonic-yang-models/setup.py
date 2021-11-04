2c801ef442da1e50d3be7cc99dd28997bef2b375
Akhilesh Samineni <47657796+AkhileshSamineni@users.noreply.github.com>
2021-11-04 23:14:04 +0530

    [yang]: SONiC Yang model for DHCP-Relay parameters (#8946)
    SONiC Yang model for DHCP-Relay parameters
    
    What I did
    Added DHCPv6 Servers.
    Tables: VLAN, DHCP_RELAY.
    
    How I did it
    Defined Yang model for Relay based on Guideline doc:
    https://github.com/Azure/SONiC/blob/master/doc/mgmt/SONiC_YANG_Model_Guidelines.md
    
    How to verify it
    Added test cases to verify it.
    
    Signed-off-by: Akhilesh Samineni akhilesh.samineni@broadcom.com

--- src/sonic-yang-models/setup.py
+++ src/sonic-yang-models/setup.py
@@ -51,6 +51,7 @@ setup(
                          './yang-models/sonic-crm.yang',
                          './yang-models/sonic-device_metadata.yang',
                          './yang-models/sonic-device_neighbor.yang',
+                         './yang-models/sonic-dhcpv6-relay.yang',
                          './yang-models/sonic-extension.yang',
                          './yang-models/sonic-flex_counter.yang',
                          './yang-models/sonic-feature.yang',
