a7117b905fe64081afa6abaed2105448ea272d8e
Mykhailo Onipko <mykhailox.onipko@intel.com>
2021-11-13 07:47:56 +0200

    [BFN]: Updated SDK packages to 20211112 (#9244)
    Signed-off-by: Mykhailo Onipko <monipko@barefootnetworks.com>

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,4 +1,4 @@
-BFN_PLATFORM = bfnplatform_20210805_sai.1.7.1_deb10.deb
+BFN_PLATFORM = bfnplatform_20211112_sai.1.7.1_deb10.deb
 $(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
