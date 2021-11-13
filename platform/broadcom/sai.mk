7ac5b40f4b1b6a5b7f1933db59acbccbc35549f7
gechiang <62408185+gechiang@users.noreply.github.com>
2021-11-13 10:45:46 -0800

    [202012]BRCM SAI 4.3.5.1-7 Picked up fixes for CS00012209390, CS00012212995, SONIC-51583, CS00012215744, and SONIC-51638 (#9252)
    This is to pick up BRCM SAI 4.3.5.1-7 fixes which contains the following fixes:
    
    1.  CS00012209390: SONIC-50037, Used SAI_SWITCH_ATTR_QOS_DSCP_TO_TC_MAP as a default decap map for IPinIP tunnels.
    2.  CS00012212995: SONIC-50948 SAI_API_QUEUE:_brcm_sai_cosq_stat_get:1353 egress Min limit get failed with error Invalid parameter 
    3.  SONIC-51583: Fixed acl group member creation failure with priority of -1
    4.  CS00012215744:SONIC-51395 [TH, TH2] WB 3.5 to 4.3 fails at APPLY_VIEW while setting SAI_PORT_ATTR_EGRESS_ACL
    5.  SONIC-51638: SDK-249337 ERROR: AddressSanitizer: heap-buffer-overflow in _tlv_print_array
    
    Preliminary tests look fine. BGP neighbors were all up with proper routes programmed
    interfaces are all up
    Manually ran the following test cases on 7050CX3 (TD3) T0 DUT and all passed:
    ```
         fib/test_fib.py
         vxlan/test_vxlan_decap.py
         fdb/test_fdb.py
         decap/test_decap.py
         ipfwd/test_dip_sip.py 
         ipfwd/test_dir_bcast.py
         acl/test_acl.py
         vlan/test_vlan.py
         platform_tests/test_reboot.py
    ```

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.1-6_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-6_amd64.deb?sv=2015-04-05&sr=b&sig=F%2BWXIA%2B1BCf1lfB6X%2B3WDmfrd57TQbRIf448ry%2F0G2M%3D&se=2035-07-13T03%3A22%3A23Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-6_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.1-7_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-7_amd64.deb?sv=2015-04-05&sr=b&sig=DWwsbdvPdUHTB0x%2B5ZK%2Bp1qjqJ4TDb1FV0az09K5pXo%3D&se=2035-07-23T07%3A51%3A17Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-7_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-6_amd64.deb?sv=2015-04-05&sr=b&sig=sxqBQ99A6FUh5fTgCTss%2FlOYXXymGOTjcMF7uofW%2FyI%3D&se=2035-07-13T03%3A23%3A37Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-7_amd64.deb?sv=2015-04-05&sr=b&sig=jZaZP84GRkdZiu%2BtoS7n7K3RC8vf%2FD1ZwnHq5vNeHGA%3D&se=2035-07-23T07%3A52%3A20Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
