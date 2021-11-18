1d2a11bbb802c0ff1bdba6b532c2b2875b3bb21b
Junhua Zhai <junhua.zhai@outlook.com>
2021-11-18 15:49:49 +0800

    [gearbox] provide common gbsyncd.service.j2 to start for platform specific gbsyncd docker (#9286)
    Why I did it
    Fix #9059. It provides common gbsyncd.service.j2 to start for platform specific gbsyncd docker, which must be named 'gbsyncd'.
    
    How I did it
    All of platform specific gbsyncd dockers use a common name 'gbsyncd'
    Use a unique systemd service template gbsyncd.service.j2 for gbsyncd docker

--- files/scripts/gbsyncd-platform.sh
+++ files/scripts/gbsyncd-platform.sh
@@ -8,16 +8,13 @@ DEVPATH="/usr/share/sonic/device"
 CONFIGFILE="${DEVPATH}/${PLATFORM}/gbsyncd.ini"
 
 if [ ! -f "$CONFIGFILE" ]; then
-    if [ gbsyncd = "$SERVICE" ]; then
-       exit 0
-    fi
     exit 1
 fi
 
 while IFS="=" read -r key value; do
     case "$key" in
         platform)
-            if [ "$value" = "$SERVICE" ]; then
+            if [[ "$value" = "$SERVICE"* ]]; then
                 exit 0
             fi
             ;;
