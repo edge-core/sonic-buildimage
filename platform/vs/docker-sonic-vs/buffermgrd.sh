fa0ae42e695ce297d3b8a5706b2232fb3828ee8e
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2021-11-24 21:00:23 +0800

    [Reclaim buffer] Common infrastructure update for reclaiming buffer (#9133)
    - Why I did it
    This is to update the common sonic-buildimage infra for reclaiming buffer.
    
    - How I did it
    Render zero_profiles.j2 to zero_profiles.json for vendors that support reclaiming buffer
    The zero profiles will be referenced in PR [Reclaim buffer] Reclaim unused buffers by applying zero buffer profiles #8768 on Mellanox platforms and there will be test cases to verify the behavior there.
    Rendering is done here for passing azure pipeline.
    Load zero_profiles.json when the dynamic buffer manager starts
    Generate inactive port list to reclaim buffer
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- platform/vs/docker-sonic-vs/buffermgrd.sh
+++ platform/vs/docker-sonic-vs/buffermgrd.sh
@@ -5,9 +5,13 @@ export ASIC_VENDOR=vs
 
 if [ "$BUFFER_CALCULATION_MODE" == "dynamic" ]; then
     BUFFERMGRD_ARGS="-a /etc/sonic/asic_table.json"
+
+    if [ -f /etc/sonic/zero_profiles.json ]; then
+        BUFFERMGRD_ZERO_PROFILE_ARGS=" -z /etc/sonic/zero_profiles.json"
+    fi
 else
     # Should we use the fallback MAC in case it is not found in Device.Metadata
     BUFFERMGRD_ARGS="-l /usr/share/sonic/hwsku/pg_profile_lookup.ini"
 fi
 
-exec /usr/bin/buffermgrd ${BUFFERMGRD_ARGS}
+exec /usr/bin/buffermgrd ${BUFFERMGRD_ARGS} ${BUFFERMGRD_ZERO_PROFILE_ARGS}
