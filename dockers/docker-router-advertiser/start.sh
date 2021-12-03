c07c0bb59cc45c199eb5cd4cc4c348174fd6a7bd
kellyyeh <42761586+kellyyeh@users.noreply.github.com>
2021-12-02 21:06:26 -0800

    [radv] Run radv on MgmtToRRouter (#9423)
    Radvd is not running on M0

--- dockers/docker-router-advertiser/start.sh
+++ dockers/docker-router-advertiser/start.sh
@@ -6,7 +6,7 @@ supervisorctl start rsyslogd
 
 # Router advertiser should only run on ToR (T0) devices
 DEVICE_ROLE=$(sonic-cfggen -d -v "DEVICE_METADATA.localhost.type")
-if [ "$DEVICE_ROLE" != "ToRRouter" ]; then
+if [ "$DEVICE_ROLE" != "ToRRouter" || "$DEVICE_ROLE" != "MgmtToRRouter" || "$DEVICE_ROLE" != "EPMS"]; then
     echo "Device role is not ToRRouter. Not starting router advertiser process."
     exit 0
 fi
