724037ebc3c93e5b7876cb11a80675227084c54d
vmittal-msft <46945843+vmittal-msft@users.noreply.github.com>
2021-12-14 09:56:21 -0800

    BRCM SAI 4.3.5.1-9 for enabling SAI_SWITCH_ATTR_QOS_DSCP_TO_TC_MAP capability (#9463)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.1-8_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-8_amd64.deb?sv=2020-04-08&st=2021-11-17T23%3A06%3A02Z&se=2035-11-18T23%3A06%3A00Z&sr=b&sp=r&sig=0%2BkErbtVjoER05lW5XrB%2B%2BdZ%2FBifcWgk9651zsYMKNw%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-8_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.1-9_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.1-9_amd64.deb?sv=2020-08-04&st=2021-12-07T19%3A29%3A13Z&se=2030-12-08T19%3A29%3A00Z&sr=b&sp=r&sig=0smiVHMjaLsVkr07u%2BpK9xcPFljhGKlnS92L2FGV0sc%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.1-9_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-8_amd64.deb?sv=2020-04-08&st=2021-11-17T23%3A11%3A49Z&se=2035-11-18T23%3A11%3A00Z&sr=b&sp=r&sig=GrvWbhm9UhQVusyEp7SdSUbauvARe6vh%2BQjXfysODBw%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.1-9_amd64.deb?sv=2020-08-04&st=2021-12-07T19%3A32%3A45Z&se=2030-12-08T19%3A32%3A00Z&sr=b&sp=r&sig=sg2j65hKv%2BJsF%2BjATRlSxW415lT6Fa1cSaOlLIfZ7TM%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
