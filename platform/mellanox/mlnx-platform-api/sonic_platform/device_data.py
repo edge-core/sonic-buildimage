d05afb5dbd030ff8408e55d317e8fa3193242fd3
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2021-12-15 15:08:04 +0800

    [Mellanox] Rename platform x86_64-mlnx_msn4800 to x86_64-nvidia_sn4800 (#9512)
    - Why I did it
    Rename platform x86_64-mlnx_msn4800 to x86_64-nvidia_msn4800
    
    - How I did it
    Rename platform folder as well as all code that reference the platform name
    
    - How to verify it
    Manual test

--- platform/mellanox/mlnx-platform-api/sonic_platform/device_data.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/device_data.py
@@ -144,7 +144,7 @@ DEVICE_DATA = {
             }
         }
     },
-    'x86_64-mlnx_msn4800-r0': {
+    'x86_64-nvidia_sn4800-r0': {
         'thermal': {
             "capability": {
                 "comex_amb": False
