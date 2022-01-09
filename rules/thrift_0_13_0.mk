f0067d5eef840285ade3489c63c4be05d90a27d6
Richard.Yu <richard.yu@microsoft.com>
2022-01-09 15:45:40 +0800

    Correct the pkg name for thrift.0.13.0 (#9688)
    Correct thrift.0.13.0 dependent package name.
    In previous code, the buildout target was named as PYTHON3_THRIFT_0_13_0
    But when add the prackage to LIBTHRIFT_0_13_0, it typo as PYTHON_THRIFT_0_13_0

--- rules/thrift_0_13_0.mk
+++ rules/thrift_0_13_0.mk
@@ -11,7 +11,7 @@ LIBTHRIFT_DEV_0_13_0 = libthrift-dev_$(THRIFT_VERSION_0_13_0_FULL)_$(CONFIGURED_
 $(eval $(call add_derived_package,$(LIBTHRIFT_0_13_0),$(LIBTHRIFT_DEV_0_13_0)))
 
 PYTHON3_THRIFT_0_13_0 = python3-thrift_$(THRIFT_VERSION_0_13_0_FULL)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(LIBTHRIFT_0_13_0),$(PYTHON_THRIFT_0_13_0)))
+$(eval $(call add_derived_package,$(LIBTHRIFT_0_13_0),$(PYTHON3_THRIFT_0_13_0)))
 
 THRIFT_COMPILER_0_13_0 = thrift-compiler_$(THRIFT_VERSION_0_13_0_FULL)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBTHRIFT_0_13_0),$(THRIFT_COMPILER_0_13_0)))
