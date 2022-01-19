6b6b40c0469d081f6b3d9203c8791e4710fedae8
Alexander Allen <arallen@nvidia.com>
2022-01-19 13:53:10 -0500

    [pmon] Move smartctl from pmon to host (#9607)
    Why I did it
    Need to be able to run smartctl when pmon docker is not running.
    
    How I did it
    Removed the pmon dependency for pmon as well as the command wrapper and added it to the debian-extension.
    
    How to verify it
    Stop pmon
    Run smartctl from the host and verify it runs without error

--- slave.mk
+++ slave.mk
@@ -958,6 +958,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
                 $(LIBNSS_TACPLUS) \
                 $(MONIT) \
                 $(OPENSSH_SERVER) \
+		$(SMARTMONTOOLS) \
                 $(PYTHON_SWSSCOMMON) \
                 $(PYTHON3_SWSSCOMMON) \
                 $(SONIC_UTILITIES_DATA) \
