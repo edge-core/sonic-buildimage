05138d10e2cc2b66dc013691fdc6fefb072dcc60
byu343 <byu@arista.com>
2022-01-22 10:32:02 -0800

    [phy-credo] Update package for bullseye (#9829)
    This is to fix the issue of phy-credo package in bullseye.

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/0468f6c23a7219dfee6dd2819013e9f1e940c95d/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/d227160f981f27df020ab50b9e0f16b1bd9166ea/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
