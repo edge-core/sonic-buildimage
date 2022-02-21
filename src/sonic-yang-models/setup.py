edge-core/sonic-buildimage

commit 5ce75acfa5a5a32f7cb5f987f3ba72262f85413f

[nvgre] Added YANG model and tests (#9136)
- Why I did it
NVGRE Tunnel feature extends the Config DB with new tables. These tables require a new YANG model.

- How I did it
Added a new YANG model sonic-nvgre-tunnel.yang

- How to verify it
Added YANG test cases.

Signed-off-by: Vadym Hlushko <vadymh@nvidia.com>

src/sonic-yang-models/setup.py
@@ -108,6 +108,7 @@ setup(
                          './yang-models/sonic-mirror-session.yang',
                          './yang-models/sonic-ntp.yang',
                          './yang-models/sonic-nat.yang',
+                         './yang-models/sonic-nvgre-tunnel.yang',
                          './yang-models/sonic-pbh.yang',
                          './yang-models/sonic-port.yang',
                          './yang-models/sonic-policer.yang',
@@ -165,6 +166,7 @@ setup(
                          './cvlyang-models/sonic-mgmt_vrf.yang',
                          './cvlyang-models/sonic-ntp.yang',
                          './cvlyang-models/sonic-nat.yang',
+                         './cvlyang-models/sonic-nvgre-tunnel.yang',
                          './cvlyang-models/sonic-pbh.yang',
                          './cvlyang-models/sonic-policer.yang',
                          './cvlyang-models/sonic-port.yang',

src/sonic-yang-models/yang-models/sonic-nvgre-tunnel.yang
@@ -1,0 +1,114 @@
+module sonic-nvgre-tunnel {
+
+    yang-version 1.1;
+
+    namespace "http://github.com/Azure/sonic-nvgre-tunnel";
+    prefix nvgre;
+
+    import ietf-inet-types {
+        prefix inet;
+    }
+
+    import sonic-vlan {
+        prefix vlan;
+    }
+
+
+    organization
+        "SONiC";
+
+    contact
+        "SONiC";
+
+    description
+        "NVGRE Tunnel YANG Module for SONiC OS";
+
+    revision 2021-10-31 {
+        description
+            "First Revision";
+    }
+
+    container sonic-nvgre-tunnel {
+
+        container NVGRE_TUNNEL {
+
+            description "NVGRE_TUNNEL part of config_db.json";
+
+            list NVGRE_TUNNEL_LIST {
+
+                key "tunnel_name";
+
+                leaf tunnel_name {
+                    description "NVGRE Tunnel name";
+
+                    type string {
+                        length 1..255;
+                    }
+                }
+
+                leaf src_ip {
+                    description "Source IP address";
+
+                    mandatory true;
+                    type inet:ip-address;
+                }
+
+            }
+            /* end of NVGRE_TUNNEL_LIST */
+
+        }
+        /* end of container NVGRE_TUNNEL */
+
+        container NVGRE_TUNNEL_MAP {
+
+            description "NVGRE_TUNNEL_MAP part of config_db.json";
+
+            list NVGRE_TUNNEL_MAP_LIST {
+
+                key "tunnel_name tunnel_map_name";
+
+                leaf tunnel_name {
+                    description "NVGRE Tunnel name";
+
+                    type leafref {
+                        path /nvgre:sonic-nvgre-tunnel/nvgre:NVGRE_TUNNEL/nvgre:NVGRE_TUNNEL_LIST/nvgre:tunnel_name;
+                    }
+                }
+
+                leaf tunnel_map_name {
+                    description "NVGRE Tunnel map name";
+
+                    type string {
+                        length 1..255;
+                    }
+                }
+
+                leaf vlan_id {
+                    description "VLAN identifier";
+
+                    mandatory true;
+                    type uint16 {
+                        range 1..4094;
+                    }
+                }
+
+                leaf vsid {
+                    description "Virtual Subnet Identifier";
+
+                    mandatory true;
+                    type uint32 {
+                        range 0..16777214;
+                    }
+                }
+
+            }
+            /* end of NVGRE_TUNNEL_MAP_LIST */
+
+        }
+        /* end of container NVGRE_TUNNEL_MAP */
+
+    }
+    /* end of container sonic-nvgre-tunnel */
+
+}
+/* end of module sonic-nvgre-tunnel */

commit f21a45b68aa09e088d4149b63e3836246540dc30

[build] fix build exception and revert #9136 (#10037)
#### Why I did it

1. Fix Build exception [example](https://dev.azure.com/mssonic/build/_build/results?buildId=73911&view=logs&jobId=88ce9a53-729c-5fa9-7b6e-3d98f2488e3f&j=cef3d8a9-152e-5193-620b-567dc18af272&t=ac3bce9f-b126-5a26-3fee-28ce0ec1679d)

```
2022-02-19T01:54:23.4200556Z ImportError: cannot import name 'soft_unicode' from 'markupsafe' (/usr/local/lib/python3.8/dist-packages/markupsafe/__init__.py)
```

This is because Jinja2 uses MarkupSafe without specifying an upper limit to the version, MarkupSafe version that was released today removed 'soft_unicode'. So now Jinja2 is complaining.

Related issues:
https://github.com/pallets/jinja/issues/1591
https://github.com/aws/aws-sam-cli/issues/3661


2. Reverts #9136

Fixing build failures in SONiC utils [example](https://dev.azure.com/mssonic/build/_build/results?buildId=73784&view=logs&jobId=83516c17-6666-5250-abde-63983ce72a49&j=83516c17-6666-5250-abde-63983ce72a49&t=6177235f-d4f1-5f72-835a-90ebb93a1784)

One of the errors:
```
 TestPathAddressing.test_find_ref_paths__ref_is_the_whole_key__returns_ref_paths 

self = <tests.generic_config_updater.gu_common_test.TestPathAddressing testMethod=test_find_ref_paths__ref_is_the_whole_key__returns_ref_paths>

    def test_find_ref_paths__ref_is_the_whole_key__returns_ref_paths(self):
        # Arrange
        path = "/PORT/Ethernet0"
        expected = [
            "/ACL_TABLE/NO-NSW-PACL-V4/ports/0",
            "/VLAN_MEMBER/Vlan1000|Ethernet0",
        ]
    
        # Act
        actual = self.path_addressing.find_ref_paths(path, Files.CROPPED_CONFIG_DB_AS_JSON)
    
        # Assert
>       self.assertEqual(expected, actual)
E       AssertionError: Lists differ: ['/ACL_TABLE/NO-NSW-PACL-V4/ports/0', '/VLAN_MEMBER/Vlan1000|Ethernet0'] != ['/ACL_TABLE/NO-NSW-PACL-V4/ports/0']
E       
E       First list contains 1 additional elements.
E       First extra element 1:
E       '/VLAN_MEMBER/Vlan1000|Ethernet0'
E       
E       - ['/ACL_TABLE/NO-NSW-PACL-V4/ports/0', '/VLAN_MEMBER/Vlan1000|Ethernet0']
E       + ['/ACL_TABLE/NO-NSW-PACL-V4/ports/0']
```

The VLAN_MEMBER backlink (can be called referrer link or ref link) is not found.

Issue introduced by https://github.com/Azure/sonic-buildimage/pull/9136

.azure-pipelines/azure-pipelines-image-template.yml
@@ -43,6 +43,7 @@ jobs:
           [ -n "$SYSTEM_PULLREQUEST_PULLREQUESTID" ] && BRANCH_NAME="$SYSTEM_PULLREQUEST_TARGETBRANCH-$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"
           git checkout -b $BRANCH_NAME
           sudo modprobe overlay
+          pip3 install MarkupSafe==2.0.1 --force-reinstall
           sudo apt-get install -y acl
           sudo bash -c "echo 1 > /proc/sys/vm/compact_memory"
           ENABLE_DOCKER_BASE_PULL=y make PLATFORM=$(PLATFORM_AZP) PLATFORM_ARCH=$(PLATFORM_ARCH) $(BUILD_OPTIONS) configure

src/sonic-yang-models/setup.py
@@ -108,7 +108,6 @@ setup(
                          './yang-models/sonic-mirror-session.yang',
                          './yang-models/sonic-ntp.yang',
                          './yang-models/sonic-nat.yang',
-                         './yang-models/sonic-nvgre-tunnel.yang',
                          './yang-models/sonic-pbh.yang',
                          './yang-models/sonic-port.yang',
                          './yang-models/sonic-policer.yang',
@@ -166,7 +165,6 @@ setup(
                          './cvlyang-models/sonic-mgmt_vrf.yang',
                          './cvlyang-models/sonic-ntp.yang',
                          './cvlyang-models/sonic-nat.yang',
-                         './cvlyang-models/sonic-nvgre-tunnel.yang',
                          './cvlyang-models/sonic-pbh.yang',
                          './cvlyang-models/sonic-policer.yang',
                          './cvlyang-models/sonic-port.yang',

src/sonic-yang-models/yang-models/sonic-nvgre-tunnel.yang
@@ -1,114 +1,0 @@
-module sonic-nvgre-tunnel {
-
-    yang-version 1.1;
-
-    namespace "http://github.com/Azure/sonic-nvgre-tunnel";
-    prefix nvgre;
-
-    import ietf-inet-types {
-        prefix inet;
-    }
-
-    import sonic-vlan {
-        prefix vlan;
-    }
-
-
-    organization
-        "SONiC";
-
-    contact
-        "SONiC";
-
-    description
-        "NVGRE Tunnel YANG Module for SONiC OS";
-
-    revision 2021-10-31 {
-        description
-            "First Revision";
-    }
-
-    container sonic-nvgre-tunnel {
-
-        container NVGRE_TUNNEL {
-
-            description "NVGRE_TUNNEL part of config_db.json";
-
-            list NVGRE_TUNNEL_LIST {
-
-                key "tunnel_name";
-
-                leaf tunnel_name {
-                    description "NVGRE Tunnel name";
-
-                    type string {
-                        length 1..255;
-                    }
-                }
-
-                leaf src_ip {
-                    description "Source IP address";
-
-                    mandatory true;
-                    type inet:ip-address;
-                }
-
-            }
-            /* end of NVGRE_TUNNEL_LIST */
-
-        }
-        /* end of container NVGRE_TUNNEL */
-
-        container NVGRE_TUNNEL_MAP {
-
-            description "NVGRE_TUNNEL_MAP part of config_db.json";
-
-            list NVGRE_TUNNEL_MAP_LIST {
-
-                key "tunnel_name tunnel_map_name";
-
-                leaf tunnel_name {
-                    description "NVGRE Tunnel name";
-
-                    type leafref {
-                        path /nvgre:sonic-nvgre-tunnel/nvgre:NVGRE_TUNNEL/nvgre:NVGRE_TUNNEL_LIST/nvgre:tunnel_name;
-                    }
-                }
-
-                leaf tunnel_map_name {
-                    description "NVGRE Tunnel map name";
-
-                    type string {
-                        length 1..255;
-                    }
-                }
-
-                leaf vlan_id {
-                    description "VLAN identifier";
-
-                    mandatory true;
-                    type uint16 {
-                        range 1..4094;
-                    }
-                }
-
-                leaf vsid {
-                    description "Virtual Subnet Identifier";
-
-                    mandatory true;
-                    type uint32 {
-                        range 0..16777214;
-                    }
-                }
-
-            }
-            /* end of NVGRE_TUNNEL_MAP_LIST */
-
-        }
-        /* end of container NVGRE_TUNNEL_MAP */
-
-    }
-    /* end of container sonic-nvgre-tunnel */
-
-}
-/* end of module sonic-nvgre-tunnel */