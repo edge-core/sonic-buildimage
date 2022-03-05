7fb546dce48bab9a0df94335861048cd28e7e45c
gechiang <62408185+gechiang@users.noreply.github.com>
2022-03-04 16:24:59 -0800

    [202012]BRCM SAI 4.3.5.3-2 Fixes CS00012228504, SONIC-55963:SID, CS00012209080, CS00012220761, and CS00012222414 (#10155)

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.3-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.3-1_amd64.deb?sv=2020-08-04&st=2022-02-06T05%3A08%3A24Z&se=2030-02-07T05%3A08%3A00Z&sr=b&sp=r&sig=XXK7%2F1Qyc1aD%2FM29E6mOwyvyD8ARlZ67dVxhw6K56Ic%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.3-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.3-2_amd64.deb?sv=2015-04-05&sr=b&sig=0okWyK4w%2FgsZ41OXDfxd7Nm7KFKts3W9%2FX3aLBtiVIE%3D&se=2035-11-11T03%3A30%3A32Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.3-1_amd64.deb?sv=2020-08-04&st=2022-02-06T05%3A09%3A35Z&se=2030-02-07T05%3A09%3A00Z&sr=b&sp=r&sig=P0xG3DKNPFu2EY3toCqEvflxVE%2F1Ntbreas2YbH2Rqo%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.3-2_amd64.deb?sv=2015-04-05&sr=b&sig=uTQVVAhHHJ2m4WQYnrsUcyq%2FIoUOcQlD4Kh52R7OSrg%3D&se=2035-11-11T03%3A32%3A33Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
