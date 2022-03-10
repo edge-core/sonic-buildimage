e8edbc21ca0042df5f76ea76f121fda6e2251d83
Yang Wang <yangwang1@microsoft.com>
2022-03-10 14:12:36 +0800

    [SAIServerV2] Build SAI Serverv2 docker (#9509) (#10188)
    Support saiserver v2 with python3 and thrift 0.13.0
    
    add variables to support the saiserverv2
    build different thrift in saithrift depends on saiserver version
    build differernt versions of saiserver
    make the saiserver and saiserver docker with version number
    
    test done:
    build two different versions of sasiserver in local build environment
    
    add saiserver to buster
    
    Co-authored-by: Yang Wang <yangwang1@microsoft.comwq>

--- platform/broadcom/docker-saiserver-brcm.mk
+++ platform/broadcom/docker-saiserver-brcm.mk
@@ -1,13 +1,16 @@
 # docker image for brcm saiserver
 
-DOCKER_SAISERVER_BRCM = docker-saiserver-brcm.gz
+DOCKER_SAISERVER_BRCM = docker-saiserver$(SAITHRIFT_VER)-brcm.gz
 $(DOCKER_SAISERVER_BRCM)_PATH = $(PLATFORM_PATH)/docker-saiserver-brcm
 $(DOCKER_SAISERVER_BRCM)_DEPENDS += $(SAISERVER)
 $(DOCKER_SAISERVER_BRCM)_FILES += $(DSSERVE) $(BCMCMD)
 $(DOCKER_SAISERVER_BRCM)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE_BUSTER)
 SONIC_DOCKER_IMAGES += $(DOCKER_SAISERVER_BRCM)
+SONIC_BUSTER_DOCKERS += $(DOCKER_SAISERVER_BRCM)
 
-$(DOCKER_SAISERVER_BRCM)_CONTAINER_NAME = saiserver
+#Support two versions of saiserver
+$(DOCKER_SAISERVER_BRCM)_CONTAINER_NAME = saiserver$(SAITHRIFT_VER)
+
 $(DOCKER_SAISERVER_BRCM)_RUN_OPT += --privileged -t
 $(DOCKER_SAISERVER_BRCM)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
 $(DOCKER_SAISERVER_BRCM)_RUN_OPT += -v /var/run/docker-saiserver:/var/run/sswsyncd
