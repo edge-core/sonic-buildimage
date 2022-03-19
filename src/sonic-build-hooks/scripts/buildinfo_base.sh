b93080d246b324f49a3b21685d7854e896173c36
xumia <59720581+xumia@users.noreply.github.com>
2022-03-19 21:48:27 +0800

    [Build][Bug]: fix the warning message not printed as expected issue (#10278)
    Fix the warning message not printed as expected issue

--- src/sonic-build-hooks/scripts/buildinfo_base.sh
+++ src/sonic-build-hooks/scripts/buildinfo_base.sh
@@ -207,6 +207,10 @@ check_apt_version()
                 continue
             fi
 
+            if [ "$para" == "install" ]; then
+                continue
+            fi
+
             if [[ "$para" == *=* ]]; then
                 continue
             else
