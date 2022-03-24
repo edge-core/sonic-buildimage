f93266293f8ecc5deaa77ce4479983bd0a78fbc7
pavannaregundi <92989231+pavannaregundi@users.noreply.github.com>
2022-03-24 19:54:13 +0530

    [Marvell-armhf] Fixing issues related to partition label (#10203)
    Why I did it
    Removing incorrect check in plt setup for fw_env config: This check was added before to compare 2 different types of disk. Now the check is redundant and check is not required as transition is complete.
    2)Removing legacy_volume_label in create_partition: legacy_volume_label is not used in armhf install files. With legacy_volume_label initialized to NULL, current code will always return true for check, if demo_part exits.
    
    How I did it
    Change is about removing the redundant/incorrect code explained above.
    
    How to verify it
    uboot fw_printenv and fw_setenv is tested
    onie-nos-install has be verified.
    
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-armhf/sonic-platform-nokia/nokia-7215_plt_setup.sh
+++ platform/marvell-armhf/sonic-platform-nokia/nokia-7215_plt_setup.sh
@@ -10,12 +10,6 @@ fw_uboot_env_cfg()
     if [ "$PLATFORM" = "armhf-nokia_ixs7215_52x-r0" ]; then
 	# Ixs7215 / IPD6448M board Uboot ENV offset
         FW_ENV_DEFAULT='/dev/mtd0 0x00100000 0x10000 0x10000'
-
-        demo_part=$(sgdisk -p /dev/sda | grep -e "SONiC-OS")
-        if [ -z "$demo_part" ]; then
-            # ET6448M Board - For Backward compatibility
-            FW_ENV_DEFAULT='/dev/mtd0 0x00500000 0x80000 0x100000 8'
-        fi
     else
         FW_ENV_DEFAULT='/dev/mtd0 0x00500000 0x80000 0x100000 8'
     fi
