8e616c153b9775d2514c68a5112a583eeb334e6b
Vadym Yashchenko <vadymx.yashchenko@intel.com>
2022-04-05 19:13:08 +0300

    [BFN] Refactoring and adding some functions of Thermal class (set and get thresholds and etc.) (#10205)
    * Revised set_high_thershold and set_low_thershold methobs in the thermal.py
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised set_low_thershold and set_high_thershold
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Added separated files with thermal thresholds, changed platform.json and
    thermal.py
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised on code revieww
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Reverted thermal.py
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised ther python.py
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised due to code review
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Added fucntion for fix the problem of tofino sensor high critical threshold
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised due to code review
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised due to code review
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised due to code review
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised only for cab18-4
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised default thresholds
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised ther def thresholds
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised on code review
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised platform.json and thermal_thresholds.json
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Code review in PR to azure (trigger CI)
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Added handle of exception
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Revised exception handler
    
    * Added psu-1 thermal names to platfrom.json
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Changed platform.json and thermal_thresholds.json in
    x86_64-acton_as9516_32d-r0
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>
    
    * Removed indentation from json file
    
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/thermal.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/thermal.py
@@ -1,8 +1,11 @@
 try:
     import subprocess
-
+    from collections import namedtuple
+    import json
     from bfn_extensions.platform_sensors import platform_sensors_get
     from sonic_platform_base.thermal_base import ThermalBase
+    from sonic_py_common import device_info
+    import logging
 except ImportError as e:
     raise ImportError (str(e) + "- required module not found")
 
@@ -18,6 +21,8 @@ Core 0:
   temp2_input: 37.000
   ...
 '''
+Threshold = namedtuple('Threshold', ['crit', 'max', 'min', 'alarm'])
+
 def _sensors_chip_parsed(data: str):
     def kv(line):
         k, v, *_ = [t.strip(': ') for t in line.split(':') if t] + ['']
@@ -68,27 +73,89 @@ def _value_get(d: dict, key_prefix, key_suffix=''):
 
 # Thermal -> ThermalBase -> DeviceBase
 class Thermal(ThermalBase):
+    _thresholds = dict()
+    _max_temperature = 100.0
+    _min_temperature = 0.0
+    _min_high_threshold_temperature = 35.0
+
     def __init__(self, chip, label, index = 0):
         self.__chip = chip
         self.__label = label
         self.__name = f"{chip}:{label}".lower().replace(' ', '-')
         self.__collect_temp = []
         self.__index = index
+        self.__high_threshold = None
+        self.__low_threshold = None
+        f = None
+        try:
+            path = device_info.get_path_to_platform_dir() + '/' + 'thermal_thresholds.json'
+            f = open(path)
+        except FileNotFoundError:
+            logging.warning('can not open the file')
+
+        if f is not None:
+            self.__get_thresholds(f)
+
+    def __get_thresholds(self, f):
+        def_threshold_json = json.load(f)
+        all_data = def_threshold_json["thermals"]
+        for i in all_data:
+            for key, value in i.items():
+                self._thresholds[key] = Threshold(*value)
+
+    def check_in_range(self, temperature):
+        temp_f = float(temperature)
+        return temp_f > self._min_temperature and temp_f <= self._max_temperature
+    
+    def check_high_threshold(self, temperature, attr_suffix):
+        temp_f = float(temperature)
+        check_range = True
+        if attr_suffix == 'max':
+            if temp_f < self._min_high_threshold_temperature:
+                if self.__name in self._thresholds:
+                    temp = self._thresholds[self.__name].max
+                    self.set_high_threshold(temp)
+                check_range = False
+        return check_range
 
     def __get(self, attr_prefix, attr_suffix):
         sensor_data = _sensors_get().get(self.__chip, {}).get(self.__label, {})
         value = _value_get(sensor_data, attr_prefix, attr_suffix)
-        return value if value is not None else -999.9
+        if value is not None and self.check_in_range(value) and self.check_high_threshold(value, attr_suffix):
+            return value
+        elif self.__name in self._thresholds and attr_prefix == 'temp':
+            if attr_suffix == 'crit':
+                return self._thresholds[self.__name].crit
+            elif attr_suffix == 'max':
+                if self.__high_threshold is None:
+                    return self._thresholds[self.__name].max
+                else:
+                    return self.__high_threshold
+            elif attr_suffix == 'min':
+                if self.__low_threshold is None:
+                    return self._thresholds[self.__name].min
+                else:
+                    return self.__low_threshold
+            elif attr_suffix == 'alarm':
+                return self._thresholds[self.__name].alarm
+            else:
+                return 1.0
+        else:
+            return 0.05
 
     # ThermalBase interface methods:
     def get_temperature(self) -> float:
         temp = self.__get('temp', 'input')
         self.__collect_temp.append(float(temp))
         self.__collect_temp.sort()
+        if len(self.__collect_temp) == 3:
+            del self.__collect_temp[1]
         return float(temp)
 
     def get_high_threshold(self) -> float:
-        return float(self.__get('temp', 'max'))
+        if self.__high_threshold is None:
+            return float(self.__get('temp', 'max'))
+        return float(self.__high_threshold)
 
     def get_high_critical_threshold(self) -> float:
         return float(self.__get('temp', 'crit'))
@@ -113,28 +180,38 @@ class Thermal(ThermalBase):
         return False
 
     def get_low_threshold(self) -> float:
-        return float(self.__get('temp', 'min'))
+        if self.__low_threshold is None:
+            return float(self.__get('temp', 'min'))
+        return float(self.__low_threshold)
 
     def get_serial(self):
         return 'N/A'
 
     def get_minimum_recorded(self) -> float:
-        temp = self.__collect_temp[0] if len(self.__collect_temp) > 0 else 0.1
+        temp = self.__collect_temp[0] if len(self.__collect_temp) > 0 else self.get_temperature()
+        temp = temp if temp <= 100.0 else 100.0
         temp = temp if temp > 0.0 else 0.1
         return float(temp)
 
     def get_maximum_recorded(self) -> float:
-        temp = self.__collect_temp[-1] if len(self.__collect_temp) > 0 else 100.0
+        temp = self.__collect_temp[-1] if len(self.__collect_temp) > 0 else self.get_temperature()
         temp = temp if temp <= 100.0 else 100.0
+        temp = temp if temp > 0.0 else 0.1
         return float(temp)
 
     def get_position_in_parent(self):
         return self.__index
 
     def set_high_threshold(self, temperature):
+        if self.check_in_range(temperature):
+            self.__high_threshold = temperature
+            return True
         return False
 
     def set_low_threshold(self, temperature):
+        if self.check_in_range(temperature):
+            self.__low_threshold = temperature
+            return True
         return False
 
 def thermal_list_get():
