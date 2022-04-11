589234a48cccfaf3a1d2bed840534ba253de1efb
Rajkumar-Marvell <54936542+rajkumar38@users.noreply.github.com>
2022-04-11 11:30:46 +0530

    [Marvell] Marvell armhf SAI debian. (#10526)
    Fixed IPv6 route issue resulting in orchagent crash.
    Signed-off-by: Rajkumar Pennadam Ramamoorthy <rpennadamram@marvell.com>

--- platform/marvell-armhf/sai.mk
+++ platform/marvell-armhf/sai.mk
@@ -1,6 +1,6 @@
 # Marvell SAI
 
-export MRVL_SAI_VERSION = 1.7.1-7
+export MRVL_SAI_VERSION = 1.7.1-8
 export MRVL_SAI = mrvllibsai_$(MRVL_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
 $(MRVL_SAI)_SRC_PATH = $(PLATFORM_PATH)/sai
