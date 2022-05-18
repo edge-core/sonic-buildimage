cd28f30969ececdb86450d3e2c82f5d92bf05a76
abdosi <58047199+abdosi@users.noreply.github.com>
2022-05-17 22:39:10 -0700

    Updated Broadcom SAI version to 3.7.6.1-1 (#10859)
    Updated BRCM SAI Version to 3.7.6.1-1

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_3.7.6.1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.6.1_amd64.deb?sv=2015-04-05&sr=b&sig=PXZNHvLaofj6qevZqf3eDUX7SGp%2BJnblyEV%2FkMFcwus%3D&se=2035-04-15T19%3A36%3A16Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_3.7.6.1_amd64.deb
+BRCM_SAI = libsaibcm_3.7.6.1-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.6.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=NQ1hWKi6zjqigwb%2BDhM239G6AhL0HvktYE7FW79VdmY%3D&se=2030-08-03T23%3A01%3A11Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_3.7.6.1-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.6.1_amd64.deb?sv=2015-04-05&sr=b&sig=S67%2FNP8J2xshBYhDV6NwP7LBnvyUOO1EZKspL1O6bCY%3D&se=2035-04-15T19%3A37%3A50Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.6.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=aUS4ZFCfD%2Bct29T%2B0xJAtFHfmtX1dTTsxKTyNtOw1O4%3D&se=2030-08-03T23%3A02%3A56Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
