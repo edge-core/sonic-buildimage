ca284a1bf2bcdb65dd67d5d739dd6a68021f65f7
Myron Sosyak <myronx.sosyak@intel.com>
2022-06-01 05:29:49 -0700

    Fix syncd-rpc build (#10602)

--- platform/barefoot/libsaithrift-dev.mk
+++ platform/barefoot/libsaithrift-dev.mk
@@ -1,18 +1,19 @@
 # libsaithrift-dev package
-
 SAI_VER = 0.9.4
-
-LIBSAITHRIFT_DEV = libsaithrift-dev_$(SAI_VER)_amd64.deb
+LIBSAITHRIFT_DEV = libsaithrift-dev_$(SAI_VER)_$(CONFIGURED_ARCH).deb
 $(LIBSAITHRIFT_DEV)_SRC_PATH = $(SRC_PATH)/sonic-sairedis/SAI
-$(LIBSAITHRIFT_DEV)_DEPENDS += $(LIBTHRIFT) $(LIBTHRIFT_DEV) $(PYTHON_THRIFT) $(THRIFT_COMPILER) $(BFN_SAI) $(BFN_SAI_DEV)
-$(LIBSAITHRIFT_DEV)_RDEPENDS += $(LIBTHRIFT) $(BFN_SAI)
+$(LIBSAITHRIFT_DEV)_DEPENDS += $(LIBTHRIFT_0_14_1) $(LIBTHRIFT_0_14_1_DEV) \
+                               $(PYTHON3_THRIFT_0_14_1) $(THRIFT_0_14_1_COMPILER)  \
+                               $(BFN_SAI)
+
+$(LIBSAITHRIFT_DEV)_RDEPENDS += $(LIBTHRIFT_0_14_1) $(BFN_SAI)
 SONIC_DPKG_DEBS += $(LIBSAITHRIFT_DEV)
 
 PYTHON_SAITHRIFT = python-saithrift_$(SAI_VER)_amd64.deb
 $(eval $(call add_extra_package,$(LIBSAITHRIFT_DEV),$(PYTHON_SAITHRIFT)))
 
 SAISERVER = saiserver_$(SAI_VER)_amd64.deb
-$(SAISERVER)_RDEPENDS += $(LIBTHRIFT) $(BRCM_SAI)
+$(SAISERVER)_RDEPENDS += $(LIBTHRIFT_0_14_1) $(BFN_SAI)
 $(eval $(call add_extra_package,$(LIBSAITHRIFT_DEV),$(SAISERVER)))
 
 SAISERVER_DBG = saiserver-dbg_$(SAI_VER)_amd64.deb
