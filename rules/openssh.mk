14fdcc815a7d414b8ef5b592de02d809f8b7f448
Jing Kan <jika@microsoft.com>
2022-06-02 16:06:22 +0800

    [202012][openssh] openssh: Upgrade from 7.9 to 8.4, to match version in buster-backports (#10910)
    * Use buster-backports version
    * Use dget dsc file instead source repo
    * Update make files
    * Upgrade openssh-client to 8.4 in base image
    * Remove useless installation
    * Install openssh-server from buster-backports in build_debian
    * Update dev buster package version list
    
    Signed-off-by: Jing Kan jika@microsoft.com

--- rules/openssh.mk
+++ rules/openssh.mk
@@ -1,14 +1,15 @@
 # openssh package
 
-OPENSSH_VERSION = 7.9p1-10+deb10u2
+OPENSSH_VERSION = 8.4p1
+OPENSSH_VERSION_FULL = ${OPENSSH_VERSION}-2~bpo10+1
 
-export OPENSSH_VERSION
+export OPENSSH_VERSION OPENSSH_VERSION_FULL
 
-OPENSSH_SERVER = openssh-server_$(OPENSSH_VERSION)_$(CONFIGURED_ARCH).deb
+OPENSSH_SERVER = openssh-server_$(OPENSSH_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(OPENSSH_SERVER)_SRC_PATH = $(SRC_PATH)/openssh
 SONIC_MAKE_DEBS += $(OPENSSH_SERVER)
 
-# The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
-# are archived into debug one image to facilitate debugging.
-#
-DBG_SRC_ARCHIVE += openssh
+OPENSSH_SERVER_DBG = openssh-server-dbgsym_$(OPENSSH_VERSION_FULL)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(OPENSSH_SERVER),$(OPENSSH_SERVER_DBG)))
+
+export OPENSSH_SERVER OPENSSH_SERVER_DBG
