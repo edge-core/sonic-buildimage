8f3edde302aa03fb4615c8263c64dccd5161a581
Richard.Yu <richard.yu@microsoft.com>
2022-06-06 09:54:00 -0700

    [202012][BRCM SAI 4.3.5.3-5] Update saibcm for pcbb feature (#10998)
    Support Tunnel PFC/pcbb feature on Broadcom platform.
    
    How to verify it
    Tested build target, successful
    
    make target/docker-syncd-brcm.gz
    manual run those tests after installing sai binary within image 20201231.67 on 7050CX3 (TD3) T0 DUT, all passed
    
         fib/test_fib.py
         vxlan/test_vxlan_decap.py
         fdb/test_fdb.py
         decap/test_decap.py
         pfcwd/test_pfcwd_all_port_storm.py
         acl/null_route/test_null_route_helper.py
         acl/test_acl.py
         vlan/test_vlan.py
         platform_tests/test_reboot.py
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.3-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.3-3_amd64.deb?sv=2020-04-08&st=2022-03-16T21%3A31%3A49Z&se=2037-03-17T21%3A31%3A00Z&sr=b&sp=r&sig=6meWjaM%2FMyQRJIigjqs4xc7ZCGotP0S94np26kn9zls%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3-3_amd64.deb
+BRCM_SAI = libsaibcm_4.3.5.3-5_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.3-5_amd64.deb?sv=2020-08-04&st=2022-06-06T04%3A44%3A45Z&se=2037-06-07T04%3A44%3A00Z&sr=b&sp=r&sig=uG51Q6rJrZYNVVuoShw8F4zas1n6zi%2FJ7udpTSZ87M8%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3-5_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.3-3_amd64.deb?sv=2020-04-08&st=2022-03-16T21%3A33%3A39Z&se=2037-03-17T21%3A33%3A00Z&sr=b&sp=r&sig=n5nodstTKbsJLZDjhQYmpFB3rafcYPybsMaici4%2FwfE%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.3-5_amd64.deb?sv=2020-08-04&st=2022-06-06T04%3A45%3A06Z&se=2037-06-07T04%3A45%3A00Z&sr=b&sp=r&sig=SF6YOtYzSIcwSR4BRInnimUjbusNp4mPHFunFjm%2Fors%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
