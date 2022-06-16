1d68efe671a4df281b6b5a41759c67627ba052aa
youshcentec <63637102+youshcentec@users.noreply.github.com>
2022-06-17 06:27:57 +0800

    [centec]: fix docker syncd rpc compile(#11097)
    Change makefile to reference to new SAI dev for docker-syncd-centec-rpc compile
    
    Co-authored-by: yoush <yoush@centec.com>

--- platform/centec/sdk.mk
+++ platform/centec/sdk.mk
@@ -1,6 +1,10 @@
 # Centec SAI
 CENTEC_SAI = libsai_1.10.1-0_amd64.deb
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/sai/$(CENTEC_SAI)
-$(eval $(call add_conflict_package,$(CENTEC_SAI),$(LIBSAIVS_DEV)))
+
+CENTEC_SAI_DEV = libsai-dev_1.10.1-0_amd64.deb
+$(CENTEC_SAI_DEV)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/sai/$(CENTEC_SAI_DEV)
+$(eval $(call add_conflict_package,$(CENTEC_SAI_DEV),$(LIBSAIVS_DEV)))
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
+SONIC_ONLINE_DEBS += $(CENTEC_SAI_DEV)
