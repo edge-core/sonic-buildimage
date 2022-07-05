4fb6cf03e68856620038a1d3577ede15e89f8211
youshcentec <63637102+youshcentec@users.noreply.github.com>
2022-07-05 09:16:51 +0800

    for fixing centec-arm64 compile error (#11186)
    Why I did it
    Fix Centec-Arm64 compile error, Centec SAI Dev package reference is error
    
    How I did it
    Modify sai.mk of arm64 platform for Centec
    
    How to verify it
    Build centec amd64 and arm64 sonic image

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -5,7 +5,7 @@ export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 export CENTEC_SAI_DEV = libsai-dev_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
-$(CENTEC_SAI)_DEV_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI_DEV)
+$(CENTEC_SAI_DEV)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI_DEV)
 $(eval $(call add_conflict_package,$(CENTEC_SAI_DEV),$(LIBSAIVS_DEV)))
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
