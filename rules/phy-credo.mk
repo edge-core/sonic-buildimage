b6d16f41f1fd3993b3e164eedde0a7500171500b
byu343 <byu@arista.com>
2022-07-06 14:40:13 -0700

    [Arista][phy-credo]: update deb for speed change and bug fix (#10990)
    Add support for reacting to speed change between 40G and 100G in CONFIG_DB
    Fix a bug on optical bit setting.
    Avoid the random error in shutdown for issue: aristanetworks/sonic#40
    Avoid to run on SmartsvilleBkMs, which depends on a different driver (credo-sai).
    
    How I did it
    How to verify it
    Verified on the duts that the commands printed in the log are matching the expectation and the interfaces are up.

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/d227160f981f27df020ab50b9e0f16b1bd9166ea/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/e89a1696954fd381e1e95edf208cffc97caf15d4/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
