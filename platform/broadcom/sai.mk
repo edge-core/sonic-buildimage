5b5bd5e818eeb21afe0f745393bc4daecae69b82
zitingguo-ms <zitingguo@microsoft.com>
2022-08-11 06:02:47 +0800

    [202012 BRCM SAI 4.3.7.0] Pick up fixes and make up BRCM SAI version to 4.3.7.0 (#11681)
    Pick upfollowing fixes and update BRCM SAI to 4.3.7.0:
    
    CS00012208537: Add back previous commit 54c5bc4848eb748
    CS00012253061,SONIC-63280: WB from 3.5 to 4.3, followed by WB to 4.3
    CS00012207978: SDK-296517, time spent for SAI operations
    CS00012245601,SONIC-62898: Egress ACL Counted ad Interface TX drops
    Update pcbb with Fixes for CS00012243699
    Upgrade on pcbb with Fixes for KB0025353, CS00012221689, CS00012221688, KB0025391, CS00012230519
    commit of "CS00012221688:PFC frames egressing, PFC storm happens simultaneously on 2 ports" is purposely skipped to be picked up later due to SWSS dependency not ready.
    Why I did it
    How I did it
    How to verify it
    Tested build target, successful
    
    Manually run these tests after installing sai binary within image 20201231.73 on 7050CX3 (TD3) T0 DUT, all passed.
    
    vxlan/test_vxlan_decap.py
    fdb/test_fdb.py
    pfcwd/test_pfcwd_all_port_storm.py
    acl/null_route/test_null_route_helper.py
    acl/test_acl.py
    vlan/test_vlan.py
    platform_tests/test_reboot.py
    
    
    Signed-off-by: zitingguo-ms <zitingguo@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.5.3-5_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.5.3-5_amd64.deb?sv=2020-08-04&st=2022-06-06T04%3A44%3A45Z&se=2037-06-07T04%3A44%3A00Z&sr=b&sp=r&sig=uG51Q6rJrZYNVVuoShw8F4zas1n6zi%2FJ7udpTSZ87M8%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.5.3-5_amd64.deb
+BRCM_SAI = libsaibcm_4.3.7.0_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.7.0_amd64.deb?sv=2020-08-04&st=2022-08-10T09%3A20%3A43Z&se=2037-08-11T09%3A20%3A00Z&sr=b&sp=r&sig=T%2FHU25LsKmG5jzXLN%2BXlVXAJFg%2Bv0bfvL%2FNITpRNDyA%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.7.0_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.5.3-5_amd64.deb?sv=2020-08-04&st=2022-06-06T04%3A45%3A06Z&se=2037-06-07T04%3A45%3A00Z&sr=b&sp=r&sig=SF6YOtYzSIcwSR4BRInnimUjbusNp4mPHFunFjm%2Fors%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.7.0_amd64.deb?sv=2020-08-04&st=2022-08-10T09%3A20%3A59Z&se=2037-08-11T09%3A20%3A00Z&sr=b&sp=r&sig=0gWqYSKVRW1h8If5CS63QjwlMt%2F1sCQcFbmFd6WTxeU%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
