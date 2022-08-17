5846cdddfefd8a8ab6fe479a62a43fe1559103fe
jingwenxie <jingwenxie@microsoft.com>
2022-08-17 14:17:46 +0800

    Add yang_config_validation in device_info (#11715)
    Why I did it
    Put yang_config_validation in common lib and make it reusabe
    
    How I did it
    Move the definition to device_info.py

--- src/sonic-py-common/sonic_py_common/device_info.py
+++ src/sonic-py-common/sonic_py_common/device_info.py
@@ -403,6 +403,10 @@ def get_chassis_info():
 
     return chassis_info_dict
 
+
+def is_yang_config_validation_enabled(config_db):
+    return get_localhost_info('yang_config_validation', config_db) == 'enable'
+
 #
 # Multi-NPU functionality
 #
