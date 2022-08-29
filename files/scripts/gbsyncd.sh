3bf1abb2dcaa03f02a245609127fc316829a89a2
abdosi <58047199+abdosi@users.noreply.github.com>
2022-08-29 08:19:28 -0700

    Address Review Comment to define SONIC_GLOBAL_DB_CLI in gbsyncd.sh (#11857)
    As part of PR #11754
    
        Change was added to use variable SONIC_DB_NS_CLI for
        namespace but that will not work since ./files/scripts/syncd_common.sh
        uses SONIC_DB_CLI. So revert back to use SONIC_DB_CLI and define new
        variable for SONIC_GLOBAL_DB_CLI for global/host db cli access
    
       Also fixed DB_CLI not working for namespace.

--- files/scripts/gbsyncd.sh
+++ files/scripts/gbsyncd.sh
@@ -4,8 +4,8 @@
 
 function startplatform() {
 
-    declare -a DbCliArray=($SONIC_DB_CLI $SONIC_DB_NS_CLI)
-    for DB_CLI in  ${DbCliArray[@]}; do
+    declare -A DbCliArray=([0]=$SONIC_GLOBAL_DB_CLI [1]=$SONIC_DB_CLI)
+    for DB_CLI in "${DbCliArray[@]}"; do
         # Add gbsyncd to FEATURE table, if not in. It did have same config as syncd.
         if [ -z $($DB_CLI CONFIG_DB HGET 'FEATURE|gbsyncd' state) ]; then
             local CMD="local r=redis.call('DUMP', KEYS[1]); redis.call('RESTORE', KEYS[2], 0, r)"
@@ -34,11 +34,11 @@ PEER="swss"
 DEBUGLOG="/tmp/swss-$SERVICE-debug$DEV.log"
 LOCKFILE="/tmp/swss-$SERVICE-lock$DEV"
 NAMESPACE_PREFIX="asic"
+SONIC_GLOBAL_DB_CLI="sonic-db-cli"
 SONIC_DB_CLI="sonic-db-cli"
-SONIC_DB_NS_CLI="sonic-db-cli"
 if [ "$DEV" ]; then
     NET_NS="$NAMESPACE_PREFIX$DEV" #name of the network namespace
-    SONIC_DB_NS_CLI="sonic-db-cli -n $NET_NS"
+    SONIC_DB_CLI="sonic-db-cli -n $NET_NS"
 fi
 
 case "$1" in
