4910298f7e8d1e328d8a32ebe92a2093d556cf60
Vivek <vkarri@nvidia.com>
2022-08-29 08:57:45 -0700

    [202205] [sonic_py_common] Cache Static Information in device_info to speed up CLI response (#11866)
    * [sonic_py_common] Cache Static Information in device_info to speed up CLI response (#11696)
    
    - Why I did it
    Profiled the execution for the following cmd intfutil -c status
    
    - How I did it
    Cached the following information:
    1. get_sonic_version_info()
    2. get_platform_info()
    None of the API exposed to the user libraries (for eg: sonic-utilities) has been modified
    These methods involve reading text files or from redis. Thus, caching helped to improve the execution time
    
    - How to verify it
    Added UT's.
    Verified on the device
    
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>
    
    * Removed UT since libswsscommom dep is missing in <=202205
    
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>
    
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>
    Signed-off-by: Vivek Reddy <vkarri@nvidia.com>

--- src/sonic-py-common/sonic_py_common/device_info.py
+++ src/sonic-py-common/sonic_py_common/device_info.py
@@ -40,6 +40,10 @@ CHASSIS_INFO_SERIAL_FIELD = 'serial'
 CHASSIS_INFO_MODEL_FIELD = 'model'
 CHASSIS_INFO_REV_FIELD = 'revision'
 
+# Cacheable Objects
+sonic_ver_info = {}
+hw_info_dict = {}
+
 def get_localhost_info(field, config_db=None):
     try:
         # TODO: enforce caller to provide config_db explicitly and remove its default value
@@ -334,14 +338,17 @@ def get_sonic_version_info():
     if not os.path.isfile(SONIC_VERSION_YAML_PATH):
         return None
 
-    data = {}
+    global sonic_ver_info
+    if sonic_ver_info:
+        return sonic_ver_info
+
     with open(SONIC_VERSION_YAML_PATH) as stream:
         if yaml.__version__ >= "5.1":
-            data = yaml.full_load(stream)
+            sonic_ver_info = yaml.full_load(stream)
         else:
-            data = yaml.load(stream)
+            sonic_ver_info = yaml.load(stream)
 
-    return data
+    return sonic_ver_info
 
 def get_sonic_version_file():
     if not os.path.isfile(SONIC_VERSION_YAML_PATH):
@@ -355,9 +362,12 @@ def get_platform_info(config_db=None):
     """
     This function is used to get the HW info helper function
     """
-    from .multi_asic import get_num_asics
+    global hw_info_dict
 
-    hw_info_dict = {}
+    if hw_info_dict:
+        return hw_info_dict
+
+    from .multi_asic import get_num_asics
 
     version_info = get_sonic_version_info()
 
