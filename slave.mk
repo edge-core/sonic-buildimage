de54eece467452b0909cdf13a8a085b8303812ca
Saikrishna Arcot <sarcot@microsoft.com>
2022-08-29 11:35:07 -0700

    Fix error handling when failing to install a deb package (#11846)
    The current error handling code for when a deb package fails to be
    installed currently has a chain of commands linked together by && and
    ends with `exit 1`. The assumption is that the commands would succeed,
    and the last `exit 1` would end it with a non-zero return code, thus
    fully failing the target and causing the build to stop because of bash's
    -e flag.
    
    However, if one of the commands prior to `exit 1` returns a non-zero
    return code, then bash won't actually treat it as a terminating error.
    From bash's man page:
    
    ```
    -e      Exit immediately if a pipeline (which may consist of a single simple
    	command), a list, or a compound command (see SHELL GRAMMAR above),
            exits with a non-zero status.  The shell does not exit if the
            command that fails is part of the  command  list  immediately
            following a while or until keyword, part of the test following the
            if or elif reserved words, part of any command executed in a && or
            || list except the command following the final && or ||, any
            command in a pipeline but the last, or if the command's return
            value is being inverted with !.  If a compound command other than a
            subshell returns a non-zero status because a command failed while
            -e was being ignored, the shell does not exit.
    ```
    
    The part `part of any command executed in a && or || list except the
    command following the final && or ||` says that if the failing command
    is not the `exit 1` that we have at the end, then bash doesn't treat it
    as an error and exit immediately. Additionally, since this is a compound
    command, but isn't in a subshell (subshell are marked by `(` and `)`,
    whereas `{` and `}` just tells bash to run the commands in the current
    environment), bash doesn't exist. The result of this is that in the
    deb-install target, if a package installation fails, it may be
    infinitely stuck in that while-loop.
    
    There are two fixes for this: change to using a subshell, or use `;`
    instead of `&&`. Using a subshell would, I think, require exporting any
    shell variables used in the subshell, so I chose to change the `&&` to
    `;`. In addition, at the start of the subshell, `set +e` is added in,
    which removes the exit-on-error handling of bash. This makes sure that
    all commands are run (the output of which may help for debugging) and
    that it still exits with 1, which will then fully fail the target.
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- slave.mk
+++ slave.mk
@@ -736,13 +736,13 @@ $(SONIC_INSTALL_DEBS) : $(DEBS_PATH)/%-install : .platform $$(addsuffix -install
 		# put a lock here because dpkg does not allow installing packages in parallel
 		if mkdir $(DEBS_PATH)/dpkg_lock &> /dev/null; then
 ifneq ($(CROSS_BUILD_ENVIRON),y)
-			{ sudo DEBIAN_FRONTEND=noninteractive dpkg -i $(DEBS_PATH)/$* $(LOG) && rm -d $(DEBS_PATH)/dpkg_lock && break; } || { rm -d $(DEBS_PATH)/dpkg_lock && sudo lsof /var/lib/dpkg/lock-frontend && ps aux && exit 1 ; }
+			{ sudo DEBIAN_FRONTEND=noninteractive dpkg -i $(DEBS_PATH)/$* $(LOG) && rm -d $(DEBS_PATH)/dpkg_lock && break; } || { set +e; rm -d $(DEBS_PATH)/dpkg_lock; sudo lsof /var/lib/dpkg/lock-frontend; ps aux; exit 1 ; }
 else
 			# Relocate debian packages python libraries to the cross python virtual env location
 			{ sudo DEBIAN_FRONTEND=noninteractive dpkg -i $(if $(findstring $(LINUX_HEADERS),$*),--force-depends) $(DEBS_PATH)/$* $(LOG) && \
 			rm -rf tmp && mkdir tmp && dpkg -x $(DEBS_PATH)/$* tmp && (sudo cp -rf tmp/usr/lib/python2*/dist-packages/* $(VIRTENV_LIB_CROSS_PYTHON2)/python2*/site-packages/ 2>/dev/null || true) && \
 			(sudo cp -rf tmp/usr/lib/python3/dist-packages/* $(VIRTENV_LIB_CROSS_PYTHON3)/python3.*/site-packages/ 2>/dev/null || true) && \
-			rm -d $(DEBS_PATH)/dpkg_lock && break; } || { rm -d $(DEBS_PATH)/dpkg_lock && sudo lsof /var/lib/dpkg/lock-frontend && ps aux && exit 1 ; }
+			rm -d $(DEBS_PATH)/dpkg_lock && break; } || { set +e; rm -d $(DEBS_PATH)/dpkg_lock; sudo lsof /var/lib/dpkg/lock-frontend; ps aux; exit 1 ; }
 endif
 		fi
 		sleep 10
