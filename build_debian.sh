46292d71bee9378460ffed833867bf12ae10090b
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2022-09-01 04:09:36 +0800

    Add linux perf tool to sonic image (#11906)

--- build_debian.sh
+++ build_debian.sh
@@ -385,7 +385,8 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     fdisk                   \
     gpg                     \
     jq                      \
-    auditd
+    auditd                  \
+    linux-perf
 
 # default rsyslog version is 8.2110.0 which has a bug on log rate limit,
 # use backport version
