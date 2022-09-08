307164f250aa7424605e21ec6eeb21e7bc7fdf83
Zhaohui Sun <94606222+ZhaohuiS@users.noreply.github.com>
2022-09-08 12:16:14 +0800

    Support Restapi/gnmi control plane acls in 202205 branch (#11962)
    For the Restapi/gnmi use-cases, Sonic has to support a new Table: EXTERNAL_CLIENT of type CTRLPLANE, stage ingress
    
    This shall match on 'src ip prefix' and dst port '8080'. Caclmgrd must parse this from acl.json and install as in the below example:
    
    iptables -A INPUT -s 20.20.20.20/27 -p tcp --dport 8080 -j ACCEPT
    
    or ip6tables if the 'src ip prefix' is IPv6.
    
    This change for master branch is in PR sonic-net/sonic-host-services#9
    
    Signed-off-by: Zhaohui Sun <zhaohuisun@microsoft.com>

---
+++ src/sonic-host-services/tests/caclmgrd/test_external_client_acl_vectors.py
@@ -0,0 +1,167 @@
+from unittest.mock import call
+
+"""
+    caclmgrd test external_client_acl vector
+"""
+EXTERNAL_CLIENT_ACL_TEST_VECTOR = [
+    [
+        "Test single IPv4 dst port + src ip for EXTERNAL_CLIENT_ACL",
+        {
+            "config_db": {
+                "ACL_TABLE": {
+                    "EXTERNAL_CLIENT_ACL": {
+                        "stage": "INGRESS",
+                        "type": "CTRLPLANE",
+                        "services": [
+                            "EXTERNAL_CLIENT"
+                        ]
+                    }
+                },
+                "ACL_RULE": {
+                    "EXTERNAL_CLIENT_ACL|DEFAULT_RULE": {
+                        "ETHER_TYPE": "2048",
+                        "PACKET_ACTION": "DROP",
+                        "PRIORITY": "1"
+                    },
+                    "EXTERNAL_CLIENT_ACL|RULE_1": {
+                        "L4_DST_PORT": "8081",
+                        "PACKET_ACTION": "ACCEPT",
+                        "PRIORITY": "9998",
+                        "SRC_IP": "20.0.0.55/32"
+                    },
+                },
+                "DEVICE_METADATA": {
+                    "localhost": {
+                    }
+                },
+                "FEATURE": {},
+            },
+            "return": [
+                "iptables -A INPUT -p tcp -s 20.0.0.55/32 --dport 8081 -j ACCEPT",
+                "iptables -A INPUT -p tcp --dport 8081 -j DROP"
+            ],
+        }
+    ],
+    [
+        "Test IPv4 dst port range + src ip forEXTERNAL_CLIENT_ACL",
+        {
+            "config_db": {
+                "ACL_TABLE": {
+                    "EXTERNAL_CLIENT_ACL": {
+                        "stage": "INGRESS",
+                        "type": "CTRLPLANE",
+                        "services": [
+                            "EXTERNAL_CLIENT"
+                        ]
+                    }
+                },
+                "ACL_RULE": {
+                    "EXTERNAL_CLIENT_ACL|DEFAULT_RULE": {
+                        "ETHER_TYPE": "2048",
+                        "PACKET_ACTION": "DROP",
+                        "PRIORITY": "1"
+                    },
+                    "EXTERNAL_CLIENT_ACL|RULE_1": {
+                        "L4_DST_PORT_RANGE": "8081-8083",
+                        "PACKET_ACTION": "ACCEPT",
+                        "PRIORITY": "9998",
+                        "SRC_IP": "20.0.0.55/32"
+                    },
+                },
+                "DEVICE_METADATA": {
+                    "localhost": {
+                    }
+                },
+                "FEATURE": {},
+            },
+            "return": [
+                "iptables -A INPUT -p tcp -s 20.0.0.55/32 --dport 8081 -j ACCEPT",
+                "iptables -A INPUT -p tcp -s 20.0.0.55/32 --dport 8082 -j ACCEPT",
+                "iptables -A INPUT -p tcp -s 20.0.0.55/32 --dport 8083 -j ACCEPT",
+                "iptables -A INPUT -p tcp --dport 8081 -j DROP",
+                "iptables -A INPUT -p tcp --dport 8082 -j DROP",
+                "iptables -A INPUT -p tcp --dport 8083 -j DROP",
+            ],
+        }
+    ],
+    [
+        "Test IPv6 single dst port range + src ip forEXTERNAL_CLIENT_ACL",
+        {
+            "config_db": {
+                "ACL_TABLE": {
+                    "EXTERNAL_CLIENT_ACL": {
+                        "stage": "INGRESS",
+                        "type": "CTRLPLANE",
+                        "services": [
+                            "EXTERNAL_CLIENT"
+                        ]
+                    }
+                },
+                "ACL_RULE": {
+                    "EXTERNAL_CLIENT_ACL|DEFAULT_RULE": {
+                        "ETHER_TYPE": "2048",
+                        "PACKET_ACTION": "DROP",
+                        "PRIORITY": "1"
+                    },
+                    "EXTERNAL_CLIENT_ACL|RULE_1": {
+                        "L4_DST_PORT": "8081",
+                        "PACKET_ACTION": "ACCEPT",
+                        "PRIORITY": "9998",
+                        "SRC_IP": "2001::2/128"
+                    },
+                },
+                "DEVICE_METADATA": {
+                    "localhost": {
+                    }
+                },
+                "FEATURE": {},
+            },
+            "return": [
+                "iptables -A INPUT -p tcp -s 2001::2/128 --dport 8081 -j ACCEPT",
+                "iptables -A INPUT -p tcp --dport 8081 -j DROP"
+            ],
+        }
+    ],
+    [
+        "Test IPv6 dst port range + src ip forEXTERNAL_CLIENT_ACL",
+        {
+            "config_db": {
+                "ACL_TABLE": {
+                    "EXTERNAL_CLIENT_ACL": {
+                        "stage": "INGRESS",
+                        "type": "CTRLPLANE",
+                        "services": [
+                            "EXTERNAL_CLIENT"
+                        ]
+                    }
+                },
+                "ACL_RULE": {
+                    "EXTERNAL_CLIENT_ACL|DEFAULT_RULE": {
+                        "ETHER_TYPE": "2048",
+                        "PACKET_ACTION": "DROP",
+                        "PRIORITY": "1"
+                    },
+                    "EXTERNAL_CLIENT_ACL|RULE_1": {
+                        "L4_DST_PORT_RANGE": "8081-8083",
+                        "PACKET_ACTION": "ACCEPT",
+                        "PRIORITY": "9998",
+                        "SRC_IP": "2001::2/128"
+                    },
+                },
+                "DEVICE_METADATA": {
+                    "localhost": {
+                    }
+                },
+                "FEATURE": {},
+            },
+            "return": [
+                "iptables -A INPUT -p tcp -s 2001::2/128 --dport 8081 -j ACCEPT",
+                "iptables -A INPUT -p tcp -s 2001::2/128 --dport 8082 -j ACCEPT",
+                "iptables -A INPUT -p tcp -s 2001::2/128 --dport 8083 -j ACCEPT",
+                "iptables -A INPUT -p tcp --dport 8081 -j DROP",
+                "iptables -A INPUT -p tcp --dport 8082 -j DROP",
+                "iptables -A INPUT -p tcp --dport 8083 -j DROP",
+            ],
+        }
+    ]
+]
--- src/sonic-host-services/tests/common/mock_configdb.py
+++ src/sonic-host-services/tests/common/mock_configdb.py
@@ -43,7 +43,10 @@ class MockConfigDb(object):
         MockConfigDb.CONFIG_DB[key][field] = data
 
     def get_table(self, table_name):
-        return MockConfigDb.CONFIG_DB[table_name]
+        data = {}
+        for k, v in MockConfigDb.CONFIG_DB[table_name].items():
+            data[self.deserialize_key(k)] = v
+        return data
 
     def subscribe(self, table_name, callback):
         self.handlers[table_name] = callback
