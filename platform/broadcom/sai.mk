1e6d63a41264118b69d92b12964084c8cc8f6e5b
gechiang <62408185+gechiang@users.noreply.github.com>
2022-10-06 21:25:34 -0700

    [202012][BRCMSAI] 4.3.7.1-2 to  back out a change that broke 4.3.7.1-1 (#12298)
    This is basically the same as previous PR: (#12275)
    With backing out a change that was breaking the build. Copying the same info from that PR here.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.7.1-1_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.7.1-1_amd64.deb?sv=2020-04-08&st=2022-10-05T01%3A58%3A26Z&se=2037-10-06T01%3A58%3A00Z&sr=b&sp=r&sig=wWW5cJ1RpH2NS%2Fz5LIMkuVKMAnw6nPMfR7Q9%2BqCG8ck%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.7.1-1_amd64.deb
+BRCM_SAI = libsaibcm_4.3.7.1-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.7.1-2_amd64.deb?sv=2020-04-08&st=2022-10-07T02%3A22%3A15Z&se=2037-10-08T02%3A22%3A00Z&sr=b&sp=r&sig=T2JdAgBr%2BZs29mfFQrA6KHWwkhcGgnv7MgIsml3qTNY%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.7.1-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.7.1-1_amd64.deb?sv=2020-04-08&st=2022-10-05T02%3A00%3A59Z&se=2037-10-06T02%3A00%3A00Z&sr=b&sp=r&sig=7AozOTdDhbjjc2ubXURoWG%2B9l6tqnRuoMOL%2FBYilDAc%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.7.1-2_amd64.deb?sv=2020-04-08&st=2022-10-07T02%3A23%3A49Z&se=2037-10-08T02%3A23%3A00Z&sr=b&sp=r&sig=tS%2FDMtSSLNZtUsTK0QiV9mHIae7i5mwtWiqy5ZpmOnY%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
