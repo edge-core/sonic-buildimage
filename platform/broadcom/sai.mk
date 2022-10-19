08d1d60ccb6604a3d9ba51684e0e63e478d96abe
zitingguo-ms <zitingguo@microsoft.com>
2022-10-19 12:18:48 +0800

    Pick up fixes and make up BRCM SAI version to 4.3.7.1-3 (#12439)
    Signed-off-by: zitingguo-ms <zitingguo@microsoft.com>
    
    Signed-off-by: zitingguo-ms <zitingguo@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.3.7.1-2_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.7.1-2_amd64.deb?sv=2020-04-08&st=2022-10-07T02%3A22%3A15Z&se=2037-10-08T02%3A22%3A00Z&sr=b&sp=r&sig=T2JdAgBr%2BZs29mfFQrA6KHWwkhcGgnv7MgIsml3qTNY%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.7.1-2_amd64.deb
+BRCM_SAI = libsaibcm_4.3.7.1-3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.7.1-3_amd64.deb?sv=2020-08-04&st=2022-10-18T12%3A18%3A13Z&se=2037-10-19T12%3A18%3A00Z&sr=b&sp=r&sig=r78DbaCBmrQjGVQO84VUvAf%2BPHFeF6uTk%2Br7b2QwO4M%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.3.7.1-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.7.1-2_amd64.deb?sv=2020-04-08&st=2022-10-07T02%3A23%3A49Z&se=2037-10-08T02%3A23%3A00Z&sr=b&sp=r&sig=tS%2FDMtSSLNZtUsTK0QiV9mHIae7i5mwtWiqy5ZpmOnY%3D"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.7.1-3_amd64.deb?sv=2020-08-04&st=2022-10-18T12%3A18%3A28Z&se=2037-10-19T12%3A18%3A00Z&sr=b&sp=r&sig=DjNdlKfDegvbc%2BLgADtr6Z69F5tYqxnYYoLQvvvN3mU%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
