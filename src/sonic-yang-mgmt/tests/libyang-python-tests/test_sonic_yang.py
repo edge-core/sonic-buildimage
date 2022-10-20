e57cd472fb992e13bedb51b2bae16fa1bb954e07
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2022-10-20 08:13:08 -0700

    Add YANG models for structured events (#12290)
    Add YANG models for structured events and unit tests. YANG events include bgp, common, dhcp-relay, swss, syncd, and host.

--- src/sonic-yang-mgmt/tests/libyang-python-tests/test_sonic_yang.py
+++ src/sonic-yang-mgmt/tests/libyang-python-tests/test_sonic_yang.py
@@ -292,11 +292,13 @@ class Test_SonicYang(object):
         '''
         test_file = sonic_yang_data['test_file']
         syc = sonic_yang_data['syc']
-        # Currently only 3 YANG files are not directly related to config
+        # Currently only 3 YANG files are not directly related to config, along with event YANG models
         # which are: sonic-extension.yang, sonic-types.yang and sonic-bgp-common.yang. Hard coding
         # it right now.
+        # event YANG models do not map directly to config_db and are included to NON_CONFIG_YANG_FILES at run time
         # If any more such helper yang files are added, we need to update here.
-        NON_CONFIG_YANG_FILES = 3
+        EVENT_YANG_FILES = sum(1 for yang_model in syc.yangFiles if 'sonic-events' in yang_model)
+        NON_CONFIG_YANG_FILES = 3 + EVENT_YANG_FILES
         # read config
         jIn = self.readIjsonInput(test_file, 'SAMPLE_CONFIG_DB_JSON')
         jIn = json.loads(jIn)
