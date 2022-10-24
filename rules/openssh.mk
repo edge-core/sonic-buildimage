0b9a11da562d261f9bab7fab3c13d85d7d0b1631
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2022-10-24 13:03:52 +0800

    [openssh] Update openssh make file, add missing dependency to libnl. (#12327)
    Update openssh make file, add missing dependency to libnl.
    
    #### Why I did it
    Openssh indirectly depends on libnl.
    Another PR #12447 need add new patch to openssh, after adding new patch to openssh, PR build failed with libnl missing error.
    
    #### How I did it
    Update openssh make file, add missing dependency to libnl.
    
    #### How to verify it
    Pass all test case
    
    #### Which release branch to backport (provide reason below if selected)
    
    <!--
    - Note we only backport fixes to a release branch, *not* features!
    - Please also provide a reason for the backporting below.
    - e.g.
    - [x] 202006
    -->
    
    - [ ] 201811
    - [ ] 201911
    - [ ] 202006
    - [ ] 202012
    - [ ] 202106
    - [ ] 202111
    - [ ] 202205
    
    #### Description for the changelog
    Update openssh make file, add missing dependency to libnl.
    
    #### Ensure to add label/tag for the feature raised. example - PR#2174 under sonic-utilities repo. where, Generic Config and Update feature has been labelled as GCU.
    
    #### Link to config_db schema for YANG module changes
    <!--
    Provide a link to config_db schema for the table for which YANG model
    is defined
    Link should point to correct section on https://github.com/Azure/sonic-buildimage/blob/master/src/sonic-yang-models/doc/Configuration.md
    -->
    
    #### A picture of a cute animal (not mandatory but encouraged)

--- rules/openssh.mk
+++ rules/openssh.mk
@@ -6,6 +6,7 @@ export OPENSSH_VERSION
 
 OPENSSH_SERVER = openssh-server_$(OPENSSH_VERSION)_$(CONFIGURED_ARCH).deb
 $(OPENSSH_SERVER)_SRC_PATH = $(SRC_PATH)/openssh
+$(OPENSSH_SERVER)_DEPENDS +=  $(LIBNL3_DEV) $(LIBNL_ROUTE3_DEV)
 SONIC_MAKE_DEBS += $(OPENSSH_SERVER)
 
 # The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
