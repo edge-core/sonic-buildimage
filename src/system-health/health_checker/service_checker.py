8f48773fd140e9fc27355427ae79386a5f5b7f9b
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2022-11-07 09:57:57 -0800

    Publish additional events (#12563)
    Add event_publish code or regex for rsyslog plugin for additional events

--- src/system-health/health_checker/service_checker.py
+++ src/system-health/health_checker/service_checker.py
@@ -12,6 +12,8 @@ from . import utils
 SYSLOG_IDENTIFIER = 'service_checker'
 logger = Logger(log_identifier=SYSLOG_IDENTIFIER)
 
+EVENTS_PUBLISHER_SOURCE = "sonic-events-host"
+EVENTS_PUBLISHER_TAG = "process-not-running"
 
 class ServiceChecker(HealthChecker):
     """
@@ -55,6 +57,8 @@ class ServiceChecker(HealthChecker):
 
         self.load_critical_process_cache()
 
+        self.events_handle = swsscommon.events_init_publisher(EVENTS_PUBLISHER_SOURCE)
+ 
     def get_expected_running_containers(self, feature_table):
         """Get a set of containers that are expected to running on SONiC
 
@@ -288,7 +292,7 @@ class ServiceChecker(HealthChecker):
         self.reset()
         self.check_by_monit(config)
         self.check_services(config)
-
+        swsscommon.events_deinit_publisher(self.events_handle)
 
     def _parse_supervisorctl_status(self, process_status):
         """Expected input:
@@ -308,6 +312,13 @@ class ServiceChecker(HealthChecker):
                 continue
             data[items[0].strip()] = items[1].strip()
         return data
+
+    def publish_events(self, container_name, critical_process_list):
+        params = swsscommon.FieldValueMap()
+        params["ctr_name"] = container_name
+        for process_name in critical_process_list:
+            params["process_name"] = process_name
+            swsscommon.event_publish(self.events_handle, EVENTS_PUBLISHER_TAG, params)
 
     def check_process_existence(self, container_name, critical_process_list, config, feature_table):
         """Check whether the process in the specified container is running or not.
@@ -333,6 +344,7 @@ class ServiceChecker(HealthChecker):
                 if process_status is None:
                     for process_name in critical_process_list:
                         self.set_object_not_ok('Process', '{}:{}'.format(container_name, process_name), "'{}' is not running".format(process_name))
+                    self.publish_events(container_name, critical_process_list)
                     return
 
                 process_status = self._parse_supervisorctl_status(process_status.strip().splitlines())
