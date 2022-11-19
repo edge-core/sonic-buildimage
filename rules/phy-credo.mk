1302a31eee5f5284eccb13d0f16ba229cfd528bf
Samuel Angebault <staphylo@arista.com>
2022-11-19 21:46:08 +0100

    [Arista] Update phy-credo package (#12755)
    Why I did it
    A recent migration of SonicV2Connector from swsssdk to swsscommon.swsscommon broke phy-credo.
    
    How I did it
    Change the import path while keeping a fallback on the previous one for 202205
    
    How to verify it
    phy-credo.service no longer fails due to an import error

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/e89a1696954fd381e1e95edf208cffc97caf15d4/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/446f30ccd8626f904d89d5798da7294948e090a6/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
