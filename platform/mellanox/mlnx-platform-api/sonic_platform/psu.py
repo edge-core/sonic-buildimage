91e12d7b493deea7e9a7eb789193ed47ff38dd8b
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2022-11-22 06:47:43 +0800

    [Mellanox] Support PSU power threshold checking (#11863)
    * Support power threshold
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>
    
    * get_psu_power_warning_threshold => get_psu_power_warning_suppress_threshold
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>
    
    * Fix comments
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- platform/mellanox/mlnx-platform-api/sonic_platform/psu.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/psu.py
@@ -216,6 +216,11 @@ class Psu(FixedPsu):
     PSU_VPD = "eeprom/psu{}_vpd"
     PSU_CURRENT_IN = "power/psu{}_curr_in"
     PSU_VOLT_IN = "power/psu{}_volt_in"
+    PORT_AMBIENT_TEMP = os.path.join(PSU_PATH, "thermal/port_amb")
+    FAN_AMBIENT_TEMP = os.path.join(PSU_PATH, "thermal/fan_amb")
+    AMBIENT_TEMP_CRITICAL_THRESHOLD = os.path.join(PSU_PATH, "config/amb_tmp_crit_limit")
+    AMBIENT_TEMP_WARNING_THRESHOLD = os.path.join(PSU_PATH, "config/amb_tmp_warn_limit")
+    PSU_POWER_SLOPE = os.path.join(PSU_PATH, "config/psu_power_slope")
 
     shared_led = None
 
@@ -234,6 +239,8 @@ class Psu(FixedPsu):
         self.psu_power = os.path.join(PSU_PATH, self.PSU_POWER.format(self.index))
         self.psu_power_max = self.psu_power + "_max"
         self.psu_presence = os.path.join(PSU_PATH, "thermal/psu{}_status".format(self.index))
+
+        self.psu_power_max_capacity = os.path.join(PSU_PATH, "config/psu{}_power_capacity".format(self.index))
 
         self.psu_temp = os.path.join(PSU_PATH, 'thermal/psu{}_temp'.format(self.index))
         self.psu_temp_threshold = os.path.join(PSU_PATH, 'thermal/psu{}_temp_max'.format(self.index))
@@ -504,6 +511,56 @@ class Psu(FixedPsu):
             amperes = utils.read_int_from_file(self.psu_current_in, log_func=logger.log_info)
             return float(amperes) / 1000
         return None
+
+    def _get_psu_power_threshold(self, temp_threshold_path):
+        """
+        Calculate power threshold for a PSU according to the maximum power capacity and ambient temperature
+            amb_temp = min(port_amb, fan_amb)
+            If amb_temp < ambient_temp_threshold
+                threshold = max capacity
+            else
+                threshold = max capacity - slope*(amb_temp - ambient_temp_threshold)
+        """
+        if self.get_powergood_status():
+            if os.path.exists(self.psu_power_max_capacity):
+                power_max_capacity = utils.read_int_from_file(self.psu_power_max_capacity)
+                temp_threshold = utils.read_int_from_file(temp_threshold_path)
+                fan_ambient_temp = utils.read_int_from_file(Psu.FAN_AMBIENT_TEMP)
+                port_ambient_temp = utils.read_int_from_file(Psu.PORT_AMBIENT_TEMP)
+                ambient_temp = min(fan_ambient_temp, port_ambient_temp)
+                if ambient_temp < temp_threshold:
+                    power_threshold = power_max_capacity
+                else:
+                    slope = utils.read_int_from_file(Psu.PSU_POWER_SLOPE)
+                    power_threshold = power_max_capacity - (ambient_temp - temp_threshold) * slope
+                if power_threshold <= 0:
+                    logger.log_warning('Got negative PSU power threshold {} for {}'.format(power_threshold, self.get_name()))
+                    power_threshold = 0
+                return float(power_threshold) / 1000000
+
+        return None
+
+    def get_psu_power_warning_suppress_threshold(self):
+        """
+        Retrieve the warning suppress threshold of the power on this PSU
+        The value can be volatile, so the caller should call the API each time it is used.
+        On Mellanox platform, it is translated from the `warning threshold`
+
+        Returns:
+            A float number, the warning suppress threshold of the PSU in watts.
+        """
+        return self._get_psu_power_threshold(Psu.AMBIENT_TEMP_WARNING_THRESHOLD)
+
+    def get_psu_power_critical_threshold(self):
+        """
+        Retrieve the critical threshold of the power on this PSU
+        The value can be volatile, so the caller should call the API each time it is used.
+
+        Returns:
+            A float number, the critical threshold of the PSU in watts.
+        """
+        return self._get_psu_power_threshold(Psu.AMBIENT_TEMP_CRITICAL_THRESHOLD)
+
 
 class InvalidPsuVolWA:
     """This class is created as a workaround for a known hardware issue that the PSU voltage threshold could be a 
