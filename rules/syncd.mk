0eb82ab217efdafe4e63736a6321225489f9384f
Richard.Yu <richard.yu@microsoft.com>
2022-11-21 12:50:39 +0800

    [SAI PTF]Support sai ptf v2 Syncd-rpc (#12761)
    Make syncd rpc docker which supports sai-ptf v2
    
    local bulild the target
    
    NOSTRETCH=y NOJESSIE=y make configure PLATFORM=vs
    NOSTRETCH=y NOJESSIE=y NOBULLSEYE=y SAITHRIFT_V2=y make target/docker-ptf-sai.gz
    
    NOSTRETCH=y NOJESSIE=y make configure PLATFORM=vs
    NOSTRETCH=y NOJESSIE=y NOBULLSEYE=y make target/docker-ptf.gz
    
    NOSTRETCH=y NOJESSIE=y make configure PLATFORM=broadcom
    NOSTRETCH=y NOJESSIE=y ENABLE_SYNCD_RPC=y SAITHRIFT_V2=y make target/docker-syncd-brcm-rpcv2.gz
    NOSTRETCH=y NOJESSIE=y ENABLE_SYNCD_RPC=y SAITHRIFT_V2=y make target/docker-saiserverv2-brcm.gz
    
    Test done:
    #12619
    NOSTRETCH=y NOJESSIE=y make configure PLATFORM=broadcom
    NOSTRETCH=y NOJESSIE=y ENABLE_SYNCD_RPC=y make target/docker-syncd-brcm-rpc.gz
    NOSTRETCH=y NOJESSIE=y ENABLE_SYNCD_RPC=y make target/docker-saiserver-brcm.gz
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

--- rules/syncd.mk
+++ rules/syncd.mk
@@ -17,7 +17,13 @@ $(SYNCD_RPC)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA)
 $(eval $(call add_derived_package,$(SYNCD),$(SYNCD_RPC)))
 
 # Inject libthrift build dependency for RPC build
-$(SYNCD)_DEPENDS += $(LIBSWSSCOMMON_DEV) $(LIBTHRIFT_DEV)
+# Support two different versions of thrift
+ifeq ($(SAITHRIFT_V2),y)
+$(SYNCD)_DEPENDS += $(LIBTHRIFT_0_14_1_DEV)
+else
+$(SYNCD)_DEPENDS += $(LIBTHRIFT_DEV)
+endif
+$(SYNCD)_DEPENDS += $(LIBSWSSCOMMON_DEV)
 $(SYNCD)_DPKG_TARGET = binary-syncd-rpc
 endif
 
