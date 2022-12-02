7152e84277e5a3dd9a8a19855b0ecf1c39d62700
mssonicbld <79238446+mssonicbld@users.noreply.github.com>
2022-12-02 13:13:26 +0800

    Make client indentity by AME cert (#11946) (#12908)

--- src/sonic-ctrmgrd/tests/kube_commands_test.py
+++ src/sonic-ctrmgrd/tests/kube_commands_test.py
@@ -15,6 +15,8 @@ import kube_commands
 KUBE_ADMIN_CONF = "/tmp/kube_admin.conf"
 FLANNEL_CONF_FILE = "/tmp/flannel.conf"
 CNI_DIR = "/tmp/cni/net.d"
+AME_CRT = "/tmp/restapiserver.crt"
+AME_KEY = "/tmp/restapiserver.key"
 
 # kube_commands test cases
 # NOTE: Ensure state-db entry is complete in PRE as we need to
@@ -25,8 +27,7 @@ read_labels_test_data = {
         common_test.DESCR: "read labels",
         common_test.RETVAL: 0,
         common_test.PROC_CMD: ["\
-kubectl --kubeconfig {} get nodes --show-labels |\
- grep none | tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)],
+kubectl --kubeconfig {} get nodes none --show-labels |tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)],
         common_test.PROC_OUT: ["foo=bar,hello=world"],
         common_test.POST: {
             "foo": "bar",
@@ -39,8 +40,7 @@ kubectl --kubeconfig {} get nodes --show-labels |\
         common_test.TRIGGER_THROW: True,
         common_test.RETVAL: -1,
         common_test.PROC_CMD: ["\
-kubectl --kubeconfig {} get nodes --show-labels |\
- grep none | tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)],
+kubectl --kubeconfig {} get nodes none --show-labels |tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)],
         common_test.POST: {
         },
         common_test.PROC_KILLED: 1
@@ -49,8 +49,7 @@ kubectl --kubeconfig {} get nodes --show-labels |\
         common_test.DESCR: "read labels fail",
         common_test.RETVAL: -1,
         common_test.PROC_CMD: ["\
-kubectl --kubeconfig {} get nodes --show-labels |\
- grep none | tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)],
+kubectl --kubeconfig {} get nodes none --show-labels |tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)],
         common_test.PROC_OUT: [""],
         common_test.PROC_ERR: ["command failed"],
         common_test.POST: {
@@ -65,8 +64,7 @@ write_labels_test_data = {
         common_test.RETVAL: 0,
         common_test.ARGS: { "foo": "bar", "hello": "World!", "test": "ok" },
         common_test.PROC_CMD: [
-"kubectl --kubeconfig {} get nodes --show-labels |\
- grep none | tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF),
+"kubectl --kubeconfig {} get nodes none --show-labels |tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF),
 "kubectl --kubeconfig {} label --overwrite nodes none hello-".format(
     KUBE_ADMIN_CONF),
 "kubectl --kubeconfig {} label --overwrite nodes none hello=World! test=ok".format(
@@ -79,8 +77,7 @@ write_labels_test_data = {
         common_test.RETVAL: 0,
         common_test.ARGS: { "foo": "bar", "hello": "world" },
         common_test.PROC_CMD: [
-"kubectl --kubeconfig {} get nodes --show-labels |\
- grep none | tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)
+"kubectl --kubeconfig {} get nodes none --show-labels |tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)
  ],
         common_test.PROC_OUT: ["foo=bar,hello=world"]
     },
@@ -90,8 +87,7 @@ write_labels_test_data = {
         common_test.ARGS: { "any": "thing" },
         common_test.RETVAL: -1,
         common_test.PROC_CMD: [
-"kubectl --kubeconfig {} get nodes --show-labels |\
- grep none | tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)
+"kubectl --kubeconfig {} get nodes none --show-labels |tr -s ' ' | cut -f6 -d' '".format(KUBE_ADMIN_CONF)
 ],
         common_test.PROC_ERR: ["read failed"]
     }
@@ -114,10 +110,22 @@ none".format(KUBE_ADMIN_CONF),
             "mkdir -p {}".format(CNI_DIR),
             "cp {} {}".format(FLANNEL_CONF_FILE, CNI_DIR),
             "systemctl start kubelet",
-            "kubeadm join --discovery-file {} --node-name none".format(
+            "kubeadm join --discovery-file {} --node-name none --apiserver-advertise-address FC00:2::32".format(
                 KUBE_ADMIN_CONF)
         ],
-        common_test.PROC_RUN: [True, True]
+        common_test.PROC_RUN: [True, True],
+        common_test.PRE: {
+            common_test.CONFIG_DB_NO: {
+                common_test.MGMT_INTERFACE_TABLE: {
+                    "eth0|FC00:2::32/64": {
+                        "gwaddr": "fc00:2::1"
+                    }
+                }
+            }
+        },
+        common_test.REQ: {
+            "data": {"ca.crt": "test"}
+        }
     },
     1: {
         common_test.DESCR: "Regular secure join",
@@ -135,10 +143,22 @@ none".format(KUBE_ADMIN_CONF),
             "mkdir -p {}".format(CNI_DIR),
             "cp {} {}".format(FLANNEL_CONF_FILE, CNI_DIR),
             "systemctl start kubelet",
-            "kubeadm join --discovery-file {} --node-name none".format(
+            "kubeadm join --discovery-file {} --node-name none --apiserver-advertise-address FC00:2::32".format(
                 KUBE_ADMIN_CONF)
         ],
-        common_test.PROC_RUN: [True, True]
+        common_test.PROC_RUN: [True, True],
+        common_test.PRE: {
+            common_test.CONFIG_DB_NO: {
+                common_test.MGMT_INTERFACE_TABLE: {
+                    "eth0|FC00:2::32/64": {
+                        "gwaddr": "fc00:2::1"
+                    }
+                }
+            }
+        },
+        common_test.REQ: {
+            "data": {"ca.crt": "test"}
+        }
     },
     2: {
         common_test.DESCR: "Skip join as already connected",
@@ -228,11 +248,17 @@ clusters:\n\
             s.close()
         with open(FLANNEL_CONF_FILE, "w") as s:
             s.close()
+        with open(AME_CRT, "w") as s:
+            s.close()
+        with open(AME_KEY, "w") as s:
+            s.close()
         kube_commands.KUBELET_YAML = kubelet_yaml
         kube_commands.CNI_DIR = CNI_DIR
         kube_commands.FLANNEL_CONF_FILE = FLANNEL_CONF_FILE
         kube_commands.SERVER_ADMIN_URL = "file://{}".format(self.admin_conf_file)
         kube_commands.KUBE_ADMIN_CONF = KUBE_ADMIN_CONF
+        kube_commands.AME_CRT = AME_CRT
+        kube_commands.AME_KEY = AME_KEY
 
 
     @patch("kube_commands.subprocess.Popen")
@@ -295,11 +321,13 @@ clusters:\n\
                     json.dumps(labels, indent=4)))
                 assert False
 
-
+    @patch("kube_commands.requests.get")
+    @patch("kube_commands.swsscommon.DBConnector")
+    @patch("kube_commands.swsscommon.Table")
     @patch("kube_commands.subprocess.Popen")
-    def test_join(self, mock_subproc):
+    def test_join(self, mock_subproc, mock_table, mock_conn, mock_reqget):
         self.init()
-        common_test.set_kube_mock(mock_subproc)
+        common_test.set_kube_mock(mock_subproc, mock_table, mock_conn, mock_reqget)
 
         for (i, ct_data) in join_test_data.items():
             lock_file = ""
