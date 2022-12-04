acd24d9804aaa566ee2539264fceb0f14fe459c4
Richard.Yu <richard.yu@microsoft.com>
2022-12-04 14:37:18 +0800

    [submodule]Update SAI SDK URL from package storage to public (#12835)
    In order to make the sai update easier, change the URL pattern to a more unified format, which can be update automated latter.
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,12 @@
-BRCM_SAI = libsaibcm_4.3.7.1-6_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm_4.3.7.1-6_amd64.deb?sv=2021-04-10&st=2022-10-25T06%3A49%3A16Z&se=2037-10-26T06%3A49%3A00Z&sr=b&sp=r&sig=NceGVsZ%2BG9DwI7t82bdQRsXSI5ewZdvx6rtEx8KTB74%3D"
-BRCM_SAI_DEV = libsaibcm-dev_4.3.7.1-6_amd64.deb
+LIBSAIBCM_XGS_VERSION = 4.3.7.1-6
+LIBSAIBCM_BRANCH_NAME = REL_4.3.5.2
+LIBSAIBCM_XGS_URL_PREFIX = "https://sonicstorage.blob.core.windows.net/public/sai/bcmsai/$(LIBSAIBCM_BRANCH_NAME)/$(LIBSAIBCM_XGS_VERSION)"
+
+BRCM_SAI = libsaibcm_$(LIBSAIBCM_XGS_VERSION)_amd64.deb
+$(BRCM_SAI)_URL = "$(LIBSAIBCM_XGS_URL_PREFIX)/$(BRCM_SAI)"
+BRCM_SAI_DEV = libsaibcm-dev_$(LIBSAIBCM_XGS_VERSION)_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/202012/libsaibcm-dev_4.3.7.1-6_amd64.deb?sv=2021-04-10&st=2022-10-25T06%3A50%3A50Z&se=2037-10-26T06%3A50%3A00Z&sr=b&sp=r&sig=bW8ORoQqQl0SGhWSge8C2KH1FcnELM5wxEZ1lMWgH6w%3D"
+$(BRCM_SAI_DEV)_URL = "$(LIBSAIBCM_XGS_URL_PREFIX)/$(BRCM_SAI_DEV)"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
