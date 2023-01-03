d59b43566f3ed5dac2e1031685227fde6d440681
Yoush <63637102+youshcentec@users.noreply.github.com>
2023-01-04 01:53:46 +0800

    [centec]: reference to v1.11.0-1 sai debian package for master (#13206)

--- platform/centec/sdk.mk
+++ platform/centec/sdk.mk
@@ -1,8 +1,8 @@
 # Centec SAI
-CENTEC_SAI = libsai_1.10.1-0_amd64.deb
+CENTEC_SAI = libsai_1.11.0-1_amd64.deb
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/sai/$(CENTEC_SAI)
 
-CENTEC_SAI_DEV = libsai-dev_1.10.1-0_amd64.deb
+CENTEC_SAI_DEV = libsai-dev_1.11.0-1_amd64.deb
 $(CENTEC_SAI_DEV)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/sai/$(CENTEC_SAI_DEV)
 $(eval $(call add_conflict_package,$(CENTEC_SAI_DEV),$(LIBSAIVS_DEV)))
 
