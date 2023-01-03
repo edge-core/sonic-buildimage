f1e84f9cd8a1ce8c3b5ec81daae6cae75854fb0a
Rida Hanif <61490193+ridahanif96@users.noreply.github.com>
2023-01-03 23:02:10 +0500

    [gns3]: modify SONiC VS image name (#13136)
    Modified sonic.gns3.sh file by changing name of VS image from "image" to "img"

--- platform/vs/sonic-gns3a.sh
+++ platform/vs/sonic-gns3a.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # This script creates a .gns3a SONiC appliance file
-IMGFILE="sonic-vs.image"
+IMGFILE="sonic-vs.img"
 RELEASE="latest"
 
 usage() {
-    echo "`basename $0` [ -r <ReleaseNumber> ] -b <SONiC VS image: sonic-vs.image>"
-    echo "e.g.: `basename $0` -r 1.1 -b <store_path>/sonic-vs.image"
+    echo "`basename $0` [ -r <ReleaseNumber> ] -b <SONiC VS image: sonic-vs.img>"
+    echo "e.g.: `basename $0` -r 1.1 -b <store_path>/sonic-vs.img"
     exit 0
 }
 
