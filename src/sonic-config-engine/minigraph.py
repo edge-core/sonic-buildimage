ffa70dfc4ec466832dea0944652fac9ff052239d
mssonicbld <79238446+mssonicbld@users.noreply.github.com>
2023-01-21 05:53:57 +0800

    Support both port name and alias in ACL table `AttachTo` attribute (#13444) (#13451)

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -636,6 +636,40 @@ def parse_dpg(dpg, hname):
             is_mirror = False
             is_mirror_v6 = False
             is_mirror_dscp = False
+            use_port_alias = True
+
+            # Walk through all interface names/alias to determine whether the input string is
+            # port name or alias.We need this logic because there can be duplicaitons in port alias
+            # and port names
+            # The input port name/alias can be either port_name or port_alias. A mix of name and alias is not accepted
+            port_name_count = 0
+            port_alias_count = 0
+            total_count = 0
+            for member in aclattach:
+                member = member.strip()
+                if member in pcs or \
+                    member in vlans or \
+                    member.lower().startswith('erspan') or \
+                    member.lower().startswith('egress_erspan') or \
+                    member.lower().startswith('erspan_dscp'):
+                    continue
+                total_count += 1
+                if member in port_alias_map:
+                    port_alias_count += 1
+                if member in port_names_map:
+                    port_name_count += 1
+            # All inputs are port alias
+            if port_alias_count == total_count:
+                use_port_alias = True
+            # All inputs are port name
+            elif port_name_count == total_count:
+                use_port_alias = False
+            # There are both port alias and port name, then port alias is preferred to keep the behavior not changed
+            else:
+                use_port_alias = True
+                # For CTRLPLANE ACL, both counters are 0
+                if (port_alias_count != 0) and (port_name_count != 0):
+                    print("Warning: The given port name for ACL " + aclname + " is inconsistent. It must be either port name or alias ", file=sys.stderr)
 
             # TODO: Ensure that acl_intfs will only ever contain front-panel interfaces (e.g.,
             # maybe we should explicity ignore management and loopback interfaces?) because we
@@ -653,15 +687,15 @@ def parse_dpg(dpg, hname):
                         acl_intfs.extend(vlan_member_list[member])
                     else:
                         acl_intfs.append(member)
-                elif (member in port_alias_map) or (member in port_names_map):
-                    if member in port_alias_map:
-                        acl_intf = port_alias_map[member]
-                    else:
-                        acl_intf = member
-                    acl_intfs.append(acl_intf)
+                elif use_port_alias and (member in port_alias_map):
+                    acl_intfs.append(port_alias_map[member])
                     # Give a warning if trying to attach ACL to a LAG member interface, correct way is to attach ACL to the LAG interface
-                    if acl_intf in intfs_inpc:
+                    if port_alias_map[member] in intfs_inpc:
                         print("Warning: ACL " + aclname + " is attached to a LAG member interface " + port_alias_map[member] + ", instead of LAG interface", file=sys.stderr)
+                elif (not use_port_alias) and (member in port_names_map):
+                    acl_intfs.append(member)
+                    if member in intfs_inpc:
+                        print("Warning: ACL " + aclname + " is attached to a LAG member interface " + member + ", instead of LAG interface", file=sys.stderr)
                 elif member.lower().startswith('erspan') or member.lower().startswith('egress_erspan') or member.lower().startswith('erspan_dscp'):
                     if 'dscp' in member.lower():
                         is_mirror_dscp = True
--- src/sonic-config-engine/tests/test_minigraph_case.py
+++ src/sonic-config-engine/tests/test_minigraph_case.py
@@ -471,9 +471,24 @@ class TestCfgGenCaseInsensitive(TestCase):
         """
         The test case is to verify ACL table can be bound to both port names and alias
         """
-        result = minigraph.parse_xml(self.sample_graph, port_config_file=self.port_config)
-        expected_dataacl_ports = ['PortChannel01','fortyGigE0/8','Ethernet12']
-        self.assertEqual(result['ACL_TABLE']['DATAACL']['ports'].sort(), expected_dataacl_ports.sort())
+        sample_graph = os.path.join(self.test_dir,'simple-sample-graph-case-acl-test.xml')
+        port_config_duplicated_name_alias = os.path.join(self.test_dir, 't0-sample-port-config-duplicated-name-alias.ini')
+        result = minigraph.parse_xml(sample_graph, port_config_file=port_config_duplicated_name_alias)
+        # TC1: All ports are portchannels or port names
+        expected_dataacl_ports = ['PortChannel01','Ethernet20','Ethernet24']
+        self.assertEqual(sorted(result['ACL_TABLE']['DATAACL_PORT_NAME']['ports']), sorted(expected_dataacl_ports))
+        # TC2: All ports are portchanels or port alias
+        expected_dataacl_ports = ['PortChannel01','Ethernet4','Ethernet8']
+        self.assertEqual(sorted(result['ACL_TABLE']['DATAACL_PORT_ALIAS']['ports']), sorted(expected_dataacl_ports))
+        # TC3: Duplicated values in port names and alias, but all fall in port names
+        expected_dataacl_ports = ['PortChannel01','Ethernet0','Ethernet1','Ethernet2','Ethernet3']
+        self.assertEqual(sorted(result['ACL_TABLE']['DATAACL_MIXED_NAME_ALIAS_1']['ports']), sorted(expected_dataacl_ports))
+        # TC4: Duplicated values in port names and alias, but all fall in port alias
+        expected_dataacl_ports = ['PortChannel01','Ethernet0','Ethernet1','Ethernet4','Ethernet8']
+        self.assertEqual(sorted(result['ACL_TABLE']['DATAACL_MIXED_NAME_ALIAS_2']['ports']), sorted(expected_dataacl_ports))
+        # TC5: Same count in port names and alias, port alias is preferred
+        expected_dataacl_ports = ['Ethernet0']
+        self.assertEqual(sorted(result['ACL_TABLE']['DATAACL_MIXED_NAME_ALIAS_3']['ports']), sorted(expected_dataacl_ports))
 
     def test_parse_device_desc_xml_mgmt_interface(self):
         # Regular device_desc.xml with both IPv4 and IPv6 mgmt address
