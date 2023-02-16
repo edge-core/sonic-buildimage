cf5ca9d27ce1cc24136dc9e932b187aaa37bbe8c
Richard.Yu <richard.yu@microsoft.com>
2023-02-17 07:42:12 +0800

    [SAI-PTF][mlnx]Enable saiserver test container on mlnx platforms (#13311)
    * Why I did it
    Enable Test sai api on bfn container with a lightweight container(saiserver).
    [SAI-PTF][mlnx]Enable saiserver test container on mlnx container
    
    How I did it
    enable saiserver container on mlnx platform.
    
    add docker-saiserver-mlnx.mk for building saiserver container
    in platform/barefoot/docker-saiserver-mlnx, add necessary files that needs in saiserver container
    How to verify it
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

---
+++ platform/mellanox/docker-saiserver-mlnx/old_start.sh
@@ -0,0 +1,13 @@
+start_mlnx()
+{
+    [ -e /dev/sxdevs/sxcdev ] || ( mkdir -p /dev/sxdevs && mknod /dev/sxdevs/sxcdev c 231 193 )
+}
+
+
+rm -f /var/run/rsyslogd.pid
+
+supervisorctl start rsyslogd
+
+start_mlnx
+
+supervisorctl start saiserver
--- platform/mellanox/docker-saiserver-mlnx/start.sh
+++ platform/mellanox/docker-saiserver-mlnx/start.sh
@@ -16,16 +16,6 @@
 # limitations under the License.
 #
 
-start_mlnx()
-{
-    [ -e /dev/sxdevs/sxcdev ] || ( mkdir -p /dev/sxdevs && mknod /dev/sxdevs/sxcdev c 231 193 )
-}
-
-
 rm -f /var/run/rsyslogd.pid
-
 supervisorctl start rsyslogd
-
-start_mlnx
-
 supervisorctl start saiserver
