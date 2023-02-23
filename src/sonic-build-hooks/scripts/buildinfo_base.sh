d15f5201e9262c59d0fddeb5fadeaea2c2368b21
Nazarii Hnydyn <nazariig@nvidia.com>
2023-02-23 08:18:01 +0200

    [buildsystem]: Fix error 'chown: missing operand after'. (#13569)
    Fixes: #13395
    
    This fix resolves ownership configuration for vcache:
    
    Step 24/40 : RUN pip3 install j2cli
     ---> Running in fcc39df62a98
    chown: missing operand after '/sonic/target/vcache/docker-base-bullseye'
    Try 'chown --help' for more information.
    Originally the issue was introduced here: #13287
    
    - Why I did it
    To fix ownership configuration
    
    - How I did it
    Removed redundant stuff
    
    Signed-off-by: Nazarii Hnydyn <nazariig@nvidia.com>

--- src/sonic-build-hooks/scripts/buildinfo_base.sh
+++ src/sonic-build-hooks/scripts/buildinfo_base.sh
@@ -28,8 +28,7 @@ else
 	PKG_CACHE_PATH=/sonic/target/vcache/${IMAGENAME}
 fi
 PKG_CACHE_FILE_NAME=${PKG_CACHE_PATH}/cache.tgz
-$SUDO mkdir -p ${PKG_CACHE_PATH}
-$SUDO chown $USER $PKG_CACHE_PATH
+mkdir -p ${PKG_CACHE_PATH}
 
 . ${BUILDINFO_PATH}/scripts/utils.sh
 
