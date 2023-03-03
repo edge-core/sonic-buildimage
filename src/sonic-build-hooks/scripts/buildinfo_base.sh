280939b5c9aabf2ff4207a687c4290a724947d08
xumia <59720581+xumia@users.noreply.github.com>
2023-03-03 15:21:10 +0800

    [Build] Support to use loosen version when failed to install python packages (#14013)
    Why I did it
    [Build] Support to use loosen version when failed to install python packages
    It is to fix the issue #14012
    
    How I did it
    Try to use the installation command without constraint
    
    How to verify it

--- src/sonic-build-hooks/scripts/buildinfo_base.sh
+++ src/sonic-build-hooks/scripts/buildinfo_base.sh
@@ -183,6 +183,11 @@ run_pip_command()
 
     $REAL_COMMAND "${parameters[@]}"
     local result=$?
+    if [ "$result" != 0 ]; then
+        echo "Failed to run the command with constraint, try to install with the original command" 1>&2
+        $REAL_COMMAND "$@"
+        result=$?
+    fi
     rm $tmp_version_file
     return $result
 }
