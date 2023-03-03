fb0751bc84257032294d2379c3b3be6b1ae07518
Prince Sunny <prince.sunny@microsoft.com>
2023-03-03 10:23:08 -0800

    [201911] Fix a typo (#14050)
    *Fix a typo introduced as part of #13403

--- src/sonic-config-engine/tests/test_cfggen.py
+++ src/sonic-config-engine/tests/test_cfggen.py
@@ -403,7 +403,7 @@ class TestCfgGen(TestCase):
         )
 
     def test_minigraph_default_vxlan(self):
-        argument = '-m "' + self.sample_graph_t0 + '" -p "' + self.port_config + '" -v "VXLAN_TUNNEL"'
+        argument = '-m "' + self.sample_graph_deployment_id + '" -p "' + self.port_config + '" -v "VXLAN_TUNNEL"'
         output = self.run_script(argument)
         self.assertEqual(
             utils.to_dict(output.strip()),
@@ -411,7 +411,7 @@ class TestCfgGen(TestCase):
         )
 
     def test_minigraph_default_vnet(self):
-        argument = '-m "' + self.sample_graph_t0 + '" -p "' + self.port_config + '" -v "VNET"'
+        argument = '-m "' + self.sample_graph_deployment_id  + '" -p "' + self.port_config + '" -v "VNET"'
         output = self.run_script(argument)
         self.assertEqual(
             utils.to_dict(output.strip()),
