6fc357604e24d4931a911755becab1522324a9f1
Ying Xie <yxieca@users.noreply.github.com>
2023-04-24 14:10:38 -0700

    Revert "[Ragile]Import defusedxml packet (#14815)" (#14830)
    This reverts commit c422cab5eb810fc2dc137e9f24a7844ce399fe99.

--- build_debian.sh
+++ build_debian.sh
@@ -538,9 +538,6 @@ sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'docke
 # Install scapy
 sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'scapy==2.4.4'
 
-# Install defusedxml
-sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install defusedxml
-
 ## Note: keep pip installed for maintainance purpose
 
 # Install GCC, needed for building/installing some Python packages
