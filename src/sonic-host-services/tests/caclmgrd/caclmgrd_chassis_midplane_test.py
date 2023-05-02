9ca6b9cb6cb88421bee11fcf44a4c4d541ab0d1d
abdosi <58047199+abdosi@users.noreply.github.com>
2023-05-02 10:55:17 -0700

    Added changes for chassis: (#14816)
    What/Why I did:
    
    Allow traffic with source and destination as chassis eth1-midplane ip. Needed for Supervisor Redis-db connection (Redis packet has source and destination ip as eth1-midpane) after we load acl.json that has catch-all drop rule. Changes are generic and not specific to supervisor and applies on LC also.
    
    Made multi_asic_ns_to_host_fwd as False for ACL service for External Client. This flag is needed for service SSH and SNMP where traffic can come in namespace over front-panel ports and we need to send the traffic in host where corresponding docker/service are running. There is no use-case of External client service for multi-asic as of now. Having flag as True creates failure when we try to load acl.json.

--- src/sonic-host-services/tests/caclmgrd/caclmgrd_chassis_midplane_test.py
+++ src/sonic-host-services/tests/caclmgrd/caclmgrd_chassis_midplane_test.py
@@ -36,7 +36,9 @@ class TestCaclmgrdChassisMidplane(TestCase):
 
         self.caclmgrd.ControlPlaneAclManager.get_namespace_mgmt_ip = mock.MagicMock()
         self.caclmgrd.ControlPlaneAclManager.get_namespace_mgmt_ipv6 = mock.MagicMock()
-        self.caclmgrd.ControlPlaneAclManager.check_chassis_midplane_interface_exist = mock.MagicMock(return_value=True)
+        self.caclmgrd.ControlPlaneAclManager.get_chassis_midplane_interface_ip = mock.MagicMock(return_value="1.0.0.33")
         caclmgrd_daemon = self.caclmgrd.ControlPlaneAclManager("caclmgrd")
         ret = caclmgrd_daemon.generate_allow_internal_chasis_midplane_traffic('')
         self.assertListEqual(test_data["return"], ret)
+        ret = caclmgrd_daemon.generate_allow_internal_chasis_midplane_traffic('asic0')
+        self.assertListEqual([], ret)
--- src/sonic-host-services/tests/caclmgrd/test_chassis_midplane_vectors.py
+++ src/sonic-host-services/tests/caclmgrd/test_chassis_midplane_vectors.py
@@ -8,6 +8,7 @@ CACLMGRD_CHASSIS_MIDPLANE_TEST_VECTOR = [
         "Allow chassis midlane traffic",
         {
             "return": [
+                "iptables -A INPUT -s 1.0.0.33 -d 1.0.0.33 -j ACCEPT",
                 "iptables -A INPUT -i eth1-midplane -j ACCEPT"
             ]
         }
