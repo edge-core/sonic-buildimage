2f8994999bbe054e2f60fbc1217d6a49480ca4f9
Ze Gan <ganze718@gmail.com>
2023-07-06 00:59:35 +0800

    [dash-api]: Add dash-api and related protobuf library (#14515)
    Why I did it
    For the DASH scenario, the APP_DB will be optimized by protobuf message for less memory consumption.
    
    How I did it
    Download the Debian package of protobuf 3.21.12 and create a corresponding rule for building it.
    Add a submodule of sonic-dash-api and generated its Debian package which includes C++ library and Python library
    
    How to verify it
    Check artifacts of Azp that the protobuf-related and dash-api deb packages should be generated.
    
    Signed-off-by: Ze Gan <ganze718@gmail.com>

---
+++ rules/sonic-dash-api.mk
@@ -0,0 +1,20 @@
+# libdashsai package
+
+LIB_SONIC_DASH_API_VERSION = 1.0.0
+
+LIB_SONIC_DASH_API = libdashapi_$(LIB_SONIC_DASH_API_VERSION)_$(CONFIGURED_ARCH).deb
+$(LIB_SONIC_DASH_API)_SRC_PATH = $(SRC_PATH)/sonic-dash-api
+
+$(LIB_SONIC_DASH_API)_DEPENDS += $(PROTOBUF) $(PROTOBUF_LITE) $(PROTOBUF_DEV) $(PROTOBUF_COMPILER)
+$(LIB_SONIC_DASH_API)_RDEPENDS += $(LIBSWSSCOMMON) $(LIBNL3) $(LIBNL_GENL3) $(LIBNL_ROUTE3) $(PROTOBUF) $(PROTOBUF_LITE) 
+
+SONIC_DPKG_DEBS += $(LIB_SONIC_DASH_API)
+
+LIB_SONIC_DASH_API_DBG = libdashapi-dbgsym_$(LIB_SONIC_DASH_API_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(LIB_SONIC_DASH_API),$(LIB_SONIC_DASH_API_DBG)))
+
+# The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
+# are archived into debug one image to facilitate debugging.
+#
+DBG_SRC_ARCHIVE += sonic-dash-api
+
