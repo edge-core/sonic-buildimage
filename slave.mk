a24845997d5cf299c975af6e09473e1a510b7d24
Ze Gan <ganze718@gmail.com>
2023-07-15 02:23:25 +0800

    Add protobuf and dashapi to sonic-mgmt (#15743)
    #### Why I did it
    The testcases in sonic-mgmt need the packages of protobuf and dashapi
    
    ##### Work item tracking
    - Microsoft ADO **(number only)**:
    
    #### How I did it
    Because the docker of sonic-mgmt is based on ubuntu20.04, it cannot directly install the packages compiled by slave due to dependency issues. Download related packaged directly from Azp.
    
    #### How to verify it
    Check azp stats.

--- slave.mk
+++ slave.mk
@@ -1560,4 +1560,4 @@ jessie : $$(addprefix $(TARGET_PATH)/,$$(JESSIE_DOCKER_IMAGES)) \
 
 ## To build some commonly used libs. Some submodules depend on these libs.
 ## It is used in component pipelines. For example: swss needs libnl, libyang
-lib-packages: $(addprefix $(DEBS_PATH)/,$(LIBNL3) $(LIBYANG))
+lib-packages: $(addprefix $(DEBS_PATH)/,$(LIBNL3) $(LIBYANG) $(PROTOBUF) $(LIB_SONIC_DASH_API))
