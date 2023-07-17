c59f55f6a3352676c4d359da678cf3b90c988051
lixiaoyuner <35456895+lixiaoyuner@users.noreply.github.com>
2023-07-17 23:21:01 +0800

    Move k8s script to docker-config-engine (#14788) (#15768)
    Why I did it
    To reduce the container's dependency from host system
    
    Work item tracking
    Microsoft ADO (number only):
    17713469
    How I did it
    Move the k8s container startup script to config engine container, other than mount it from host.
    
    How to verify it
    Check file path(/usr/share/sonic/scripts/container_startup.py) inside config engine container.
    
    Signed-off-by: Yun Li <yunli1@microsoft.com>
    Co-authored-by: Qi Luo <qiluo-msft@users.noreply.github.com>

--- rules/docker-telemetry.mk
+++ rules/docker-telemetry.mk
@@ -30,7 +30,6 @@ endif
 $(DOCKER_TELEMETRY)_CONTAINER_NAME = telemetry
 $(DOCKER_TELEMETRY)_RUN_OPT += --privileged -t
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-$(DOCKER_TELEMETRY)_RUN_OPT += -v /usr/share/sonic/scripts:/usr/share/sonic/scripts:ro
 ifneq ($(INCLUDE_SYSTEM_GNMI), y)
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /var/run/dbus:/var/run/dbus:rw
 endif
