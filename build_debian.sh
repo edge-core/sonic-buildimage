bdef73ea963064cdc9fbebb63ac381440c0fafd6
xumia <59720581+xumia@users.noreply.github.com>
2023-07-19 06:33:49 +0800

    [Build] Fix the PyYang python package installation issue (#15890)
    Why I did it
    Fix the armhf build failure.
    How to reproduce the issue:
    
    docker run -it debain:bullseye bash
    apt-get update && apt-get install -y python3-pip
    pip3 install PyYAML==5.4.1
    Error message:
    
    Collecting PyYAML==5.4.1
      Installing build dependencies ... done
      Getting requirements to build wheel ... error
      ERROR: Command errored out with exit status 1:
       command: /usr/bin/python3 /tmp/tmp6xabslgb_in_process.py get_requires_for_build_wheel /tmp/tmp_er01ztl
    ....
          raise AttributeError(attr)
      AttributeError: cython_sources
      ----------------------------------------
    WARNING: Discarding https://files.pythonhosted.org/packages/a0/a4/d63f2d7597e1a4b55aa3b4d6c5b029991d3b824b5bd331af8d4ab1ed687d/PyYAML-5.4.1.tar.gz#sha256=607774cbba28732bfa802b54baa7484215f530991055bb562efbed5b2f20a45e (from https://pypi.org/simple/pyyaml/) (requires-python:>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*). Command errored out with exit status 1: /usr/bin/python3 /tmp/tmp6xabslgb_in_process.py get_requires_for_build_wheel /tmp/tmp_er01ztl Check the logs for full command output.
    ERROR: Could not find a version that satisfies the requirement PyYAML==5.4.1
    ERROR: No matching distribution found for PyYAML==5.4.1
    root@fa2fa92edcfd:/# 
    But if adding the option --no-build-isolation, then it is good, see fix.
    
    install "PyYAML==5.4.1" --no-build-isolation
    The same error can be found in the multiple builds.
    
    Work item tracking
    Microsoft ADO (number only): 24567457
    
    How I did it
    Add a build option --no-build-isolation.

--- build_debian.sh
+++ build_debian.sh
@@ -546,6 +546,9 @@ sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'docke
 # Install scapy
 sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'scapy==2.4.4'
 
+# The option --no-build-isolation can be removed when upgrading PyYAML to 6.0.1
+sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'PyYAML==5.4.1' --no-build-isolation
+
 ## Note: keep pip installed for maintainance purpose
 
 # Install GCC, needed for building/installing some Python packages
