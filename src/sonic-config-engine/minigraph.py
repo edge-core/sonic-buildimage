f41aad922620b4a5fc81b8d2e8903a77084a4fcc
vdahiya12 <67608553+vdahiya12@users.noreply.github.com>
2023-08-04 14:53:49 -0700

    [minigraph] remove number of lanes check for changing speed from 400G to 100G and set speed setting before lane reconfiguration   (#15721)
    8111 800G interface, split to 2x400G (each has 4 lanes) fails to change interface speed from 400G to 100G during deploy mg. In minigraph.xml, the interface speed configuration is good, but fails to generate the right value to config_db.json.
    
    In order to support this SKU the speed transitioning should support both 4 lanes and 8 lanes in the port_config.ini.
    
    Why I did it
    
    before this change for a 400G to 100G transition, in all cases except when lanes are 8, we would continue and the line
    ports.setdefault(port_name, {})['speed'] = port_speed_png[port_name]
    would not be executed, hence the default speed will never be set for a case and config_db will not be updated,
    where speed is transitioning from 400G to 100G or 40G, but lanes are not equal to 8.
    
    In order for those cases to pass where lanes are not specifically 8, we need the change
    
    Work item tracking
    24242657
    
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -1778,6 +1778,9 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
         port_default_speed =  port_speeds_default.get(port_name, None)
         port_png_speed = port_speed_png[port_name]
 
+        # set Port Speed before lane update
+        ports.setdefault(port_name, {})['speed'] = port_png_speed
+
         # when the port speed is changes from 400g to 100g/40g
         # update the port lanes, use the first 4 lanes of the 400G port to support 100G/40G port
         if port_default_speed == '400000' and (port_png_speed == '100000' or port_png_speed == '40000'):
@@ -1788,7 +1791,6 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
             updated_lanes = ",".join(port_lanes[:4])
             ports[port_name]['lanes'] = updated_lanes
 
-        ports.setdefault(port_name, {})['speed'] = port_speed_png[port_name]
 
     for port_name, port in list(ports.items()):
         # get port alias from port_config.ini
