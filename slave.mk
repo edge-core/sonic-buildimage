04ffd67fdaf6ee0faba199e18cf848a63511d608
Sachin Holla <51310506+sachinholla@users.noreply.github.com>
2023-08-10 00:10:00 +0530

    Ensure sonic yangs wheel is built before sonic-mgmt-common (#15226)
    * Enhanced slave.mk to accept python wheels as dependency for a deb
      target. Dependent wheel names should be specified through the new
      {deb_name}_WHEEL_DEPENDS variable in the deb's make rules. The wheel
      will be built and installed in the slave docker before starting the
      deb build.
    
    * Added sonic_yang_models-1.0-py3-none-any.whl as dependency for
      sonic-mgmt-common.deb. This is required for using the sonic yangs in
      UMF
    
    Signed-off-by: Sachin Holla <sachin.holla@broadcom.com>

--- slave.mk
+++ slave.mk
@@ -679,6 +679,7 @@ SONIC_TARGET_LIST += $(addprefix $(FILES_PATH)/, $(SONIC_MAKE_FILES))
 #     $(SOME_NEW_DEB)_DEPENDS = $(SOME_OTHER_DEB1) $(SOME_OTHER_DEB2) ...
 #     SONIC_MAKE_DEBS += $(SOME_NEW_DEB)
 $(addprefix $(DEBS_PATH)/, $(SONIC_MAKE_DEBS)) : $(DEBS_PATH)/% : .platform $$(addsuffix -install,$$(addprefix $(DEBS_PATH)/,$$($$*_DEPENDS))) \
+			$$(addsuffix -install,$$(addprefix $(PYTHON_WHEELS_PATH)/,$$($$*_WHEEL_DEPENDS))) \
 			$$(addprefix $(DEBS_PATH)/,$$($$*_AFTER)) \
 			$(call dpkg_depend,$(DEBS_PATH)/%.dep)
 	$(HEADER)
