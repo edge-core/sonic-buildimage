519a1e4a9154884c4e3fb0aa79cd6c4ccb6bde53
Saikrishna Arcot <sarcot@microsoft.com>
2023-08-11 09:00:46 -0700

    Update sairedis submodule (#16072)
    * Update sairedis submodule
    
    This submodule update needs to be manually done due to build changes
    done in the sairedis submodule. Specifically, Debian build profiles are
    now being used instead of dpkg build targets, and dbgsym packages are
    being used instead of dbg packages. Because of this, there needs to be
    changes on the sonic-buildimage side for this.
    
    This is a reland of #15720, which was reverted in #15995 due to the RPC
    package build failing. That failure has since been fixed, and the
    PR pipeline has been updated to build the RPC package so that this is
    checked at the PR stage.
    
    This submodule update brings in the following changes:
    
    ```
    4dbdb21 Fix RPC package build failure due to shell syntax issue (#1268)
    588d596 Make sure new binaries replace existing binaries in docker-sonic-vs (#1269)
    ce8f642 [vs] Use boost join to concatenate switch types in config (#1266)
    d6055a2 [vslib]: Temporaily map DPU switch type to NVDA_MBF2H536C (#1259)
    e1cdb4d [CodeQL]: Use dependencies with relevant versions in azp template. (#1262)
    c08f9a2 [CI]: Fix collect log error in azp template. (#1260)
    eed856c [CodeQL]: Fix syncd compilation in azp template. (#1261)
    a3f1f1a Reland 'Make changes to building and packaging sairedis (#1116)' (#1194)
    ```
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>
    
    * Update sairedis submodule with the fix for the RPC package build
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>
    
    ---------
    
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- platform/vs/syncd-vs.mk
+++ platform/vs/syncd-vs.mk
@@ -1,10 +1,10 @@
-$(LIBSAIREDIS)_DPKG_TARGET = binary-syncd-vs
+$(LIBSAIREDIS)_DEB_BUILD_PROFILES += syncd vs
 
 SYNCD_VS = syncd-vs_1.0.0_amd64.deb
 $(SYNCD_VS)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA) $(LIBSAIVS)
 $(eval $(call add_derived_package,$(LIBSAIREDIS),$(SYNCD_VS)))
 
-SYNCD_VS_DBG = syncd-vs-dbg_1.0.0_amd64.deb
-$(SYNCD_VS_DBG)_DEPENDS += $(SYNCD_VS)
-$(SYNCD_VS_DBG)_RDEPENDS += $(SYNCD_VS)
-$(eval $(call add_derived_package,$(LIBSAIREDIS),$(SYNCD_VS_DBG)))
+SYNCD_VS_DBGSYM = syncd-vs-dbgsym_1.0.0_amd64.deb
+$(SYNCD_VS_DBGSYM)_DEPENDS += $(SYNCD_VS)
+$(SYNCD_VS_DBGSYM)_RDEPENDS += $(SYNCD_VS)
+$(eval $(call add_derived_package,$(LIBSAIREDIS),$(SYNCD_VS_DBGSYM)))
--- rules/syncd.mk
+++ rules/syncd.mk
@@ -4,7 +4,7 @@ ifneq ($(CONFIGURED_PLATFORM),vs)
 
 SYNCD = syncd_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA)
-$(SYNCD)_DPKG_TARGET = binary-syncd
+$(SYNCD)_DEB_BUILD_PROFILES += syncd
 $(SYNCD)_SRC_PATH = $(SRC_PATH)/sonic-sairedis
 $(SYNCD)_DEPENDS += $(LIBSWSSCOMMON_DEV) $(LIBSAIREDIS)
 $(SYNCD)_RDEPENDS += $(LIBSWSSCOMMON)
@@ -18,19 +18,19 @@ $(eval $(call add_derived_package,$(SYNCD),$(SYNCD_RPC)))
 
 # Inject libthrift build dependency for RPC build
 $(SYNCD)_DEPENDS += $(LIBSWSSCOMMON_DEV) $(LIBTHRIFT_DEV)
-$(SYNCD)_DPKG_TARGET = binary-syncd-rpc
+$(SYNCD)_DEB_BUILD_PROFILES += rpc
 endif
 
-SYNCD_DBG = syncd-dbg_1.0.0_$(CONFIGURED_ARCH).deb
-$(SYNCD_DBG)_DEPENDS += $(SYNCD)
-$(SYNCD_DBG)_RDEPENDS += $(SYNCD)
-$(eval $(call add_derived_package,$(SYNCD),$(SYNCD_DBG)))
+SYNCD_DBGSYM = syncd-dbgsym_1.0.0_$(CONFIGURED_ARCH).deb
+$(SYNCD_DBGSYM)_DEPENDS += $(SYNCD)
+$(SYNCD_DBGSYM)_RDEPENDS += $(SYNCD)
+$(eval $(call add_derived_package,$(SYNCD),$(SYNCD_DBGSYM)))
 
 ifeq ($(ENABLE_SYNCD_RPC),y)
-SYNCD_RPC_DBG = syncd-rpc-dbg_1.0.0_$(CONFIGURED_ARCH).deb
-$(SYNCD_RPC_DBG)_DEPENDS += $(SYNCD_RPC)
-$(SYNCD_RPC_DBG)_RDEPENDS += $(SYNCD_RPC)
-$(eval $(call add_derived_package,$(SYNCD),$(SYNCD_RPC_DBG)))
+SYNCD_RPC_DBGSYM = syncd-rpc-dbgsym_1.0.0_$(CONFIGURED_ARCH).deb
+$(SYNCD_RPC_DBGSYM)_DEPENDS += $(SYNCD_RPC)
+$(SYNCD_RPC_DBGSYM)_RDEPENDS += $(SYNCD_RPC)
+$(eval $(call add_derived_package,$(SYNCD),$(SYNCD_RPC_DBGSYM)))
 endif
 
 ifeq ($(ENABLE_PY2_MODULES), n)
