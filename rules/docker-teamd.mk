6c96b294846a279972525c605b983c1f6d263c4b
Mai Bui <maibui@microsoft.com>
2023-08-17 12:48:57 -0400

    [docker-teamd] limit privileged flag for teamd container (#15829)
    Signed-off-by: Mai Bui <maibui@microsoft.com>

--- rules/docker-teamd.mk
+++ rules/docker-teamd.mk
@@ -33,7 +33,7 @@ SONIC_INSTALL_DOCKER_DBG_IMAGES += $(DOCKER_TEAMD_DBG)
 endif
 
 $(DOCKER_TEAMD)_CONTAINER_NAME = teamd
-$(DOCKER_TEAMD)_RUN_OPT += --privileged -t
+$(DOCKER_TEAMD)_RUN_OPT += -t --cap-add=NET_ADMIN
 $(DOCKER_TEAMD)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_TEAMD)_RUN_OPT += -v /etc/timezone:/etc/timezone:ro 
 $(DOCKER_TEAMD)_RUN_OPT += -v /host/warmboot:/var/warmboot
