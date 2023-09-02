84cfc3bc69837d88036364bcabe67751f8f19ee9
Zain Budhwani <99770260+zbud-msft@users.noreply.github.com>
2023-09-01 23:01:46 -0700

    [eventd]: Remove unnecessary log (#16166)
    Work item tracking
    Microsoft ADO (number only): 16789053

--- src/sonic-eventd/src/main.cpp
+++ src/sonic-eventd/src/main.cpp
@@ -6,8 +6,7 @@ void run_eventd_service();
 int main()
 {
     swss::Logger::setMinPrio(swss::Logger::SWSS_DEBUG);
-    SWSS_LOG_INFO("The eventd service started");
-    SWSS_LOG_ERROR("ERR:The eventd service started");
+    SWSS_LOG_INFO("The eventd service started"); 
 
     run_eventd_service();
 
