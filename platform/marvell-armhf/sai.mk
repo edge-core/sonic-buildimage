782a92213dba41214648eec0bb2d70f8e850ce03
Rajkumar-Marvell <54936542+rajkumar38@users.noreply.github.com>
2023-09-05 22:50:27 +0530

    [Marvell] Update armhf sai debian to add SAI 1.12 support (#16299)
    - SAI 1.12 support
    
    Signed-off-by: rajkumar38 <rpennadamram@marvell.com>

--- platform/marvell-armhf/sai.mk
+++ platform/marvell-armhf/sai.mk
@@ -1,6 +1,6 @@
 # Marvell SAI
 
-export MRVL_SAI_VERSION = 1.11.0-1
+export MRVL_SAI_VERSION = 1.12.0-1
 export MRVL_SAI = mrvllibsai_$(MRVL_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
 $(MRVL_SAI)_SRC_PATH = $(PLATFORM_PATH)/sai
