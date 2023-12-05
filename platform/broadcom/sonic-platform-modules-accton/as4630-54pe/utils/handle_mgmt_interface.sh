506c77766579956ba65e4b7722b19c3abc84199a
michael_shih <michael_shih@edge-core.com>
2023-12-05 17:17:25 +0800

    [AS4630-54PE] Apply community PR

--- platform/broadcom/sonic-platform-modules-accton/as4630-54pe/utils/handle_mgmt_interface.sh
+++ platform/broadcom/sonic-platform-modules-accton/as4630-54pe/utils/handle_mgmt_interface.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#Due to the hardware design, as4630-54pe use "eth2" instead of "eth0" as management interface.
-#Rename netdev "eth0" and "eth2" to swap original "eth2" to "eth0".
- 
-ifconfig eth0 down
-ip link set eth0 name eth3
-ip link set eth2 name eth0
-ifconfig eth0 up
+# Re-install the igb and ixgbe again to make the NIC sequence follow the udev rule
+modprobe -r igb
+modprobe -r ixgbe
+modprobe igb
+modprobe ixgbe
