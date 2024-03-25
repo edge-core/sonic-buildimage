bf08432231b4e29314833db306fc8889a688cc21
ChrisKe1643 <chris_ke@accton.com>
2024-03-25 15:33:32 +0800

    [Sync PR #14361] [Edgecore][AS4630-54PE/PDDF] Enhance sonic_platform feature
    https://github.com/sonic-net/sonic-buildimage/pull/14361
    
    Why I did it
    Base on Adding PDDF common framework enhancements. #12863 to modify pddf-device.json and platform API.
    Add code to pass pytest.
    
    How I did it
    Add needed code to sonic_platform and driver.
    
    How to verify it
    sonic-cli cmd:
    show platform {fan, temperature, firmware status, syseeprom, psustatus}
    sfputil cmd:
    show {presence, eeprom, lpmode}
    Verify function by sonic-mgmt tools:
    Test file path: /tests/platform_tests/api/
     Test items: test_chassis.py, test_component.py, test_fan_drawer.py, test_psu.py, test_thermal.py, test_sfp.py

--- platform/broadcom/sonic-platform-modules-accton/as4630-54pe/sonic_platform/fan.py
+++ platform/broadcom/sonic-platform-modules-accton/as4630-54pe/sonic_platform/fan.py
@@ -6,6 +6,7 @@ try:
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
+FAN_NAME_LIST = ["FAN-1", "FAN-2", "FAN-3"]
 
 class Fan(PddfFan):
     """PDDF Platform-Specific Fan class"""
@@ -24,25 +25,53 @@ class Fan(PddfFan):
             A string, either FAN_DIRECTION_INTAKE or FAN_DIRECTION_EXHAUST
             depending on fan direction
         """
+        direction = 'N/A'
         if self.is_psu_fan:
             direction = self.FAN_DIRECTION_EXHAUST
-
         else:
-            idx = (self.fantray_index-1)*self.platform['num_fans_pertray'] + self.fan_index
-            attr = "fan" + str(idx) + "_direction"
-            output = self.pddf_obj.get_attr_name_output("FAN-CTRL", attr)
-            if not output:
-                return False
-
-            mode = output['mode']
-            val = output['status']
-
-            val = val.rstrip()
-            vmap = self.plugin_data['FAN']['direction'][mode]['valmap']
-            if val in vmap:
-                direction = vmap[val]
-            else:
-                direction = val
+            direction = super().get_direction()
+            if direction is not None and len(direction) > 0:
+                return direction
 
         return direction
 
+    def get_name(self):
+        """
+        Retrieves the name of the device
+            Returns:
+            string: The name of the device
+        """
+        fan_name = FAN_NAME_LIST[self.fantray_index - 1] \
+            if not self.is_psu_fan \
+            else "PSU-{} FAN-{}".format(self.fans_psu_index, self.fan_index)
+
+        return fan_name
+
+    def get_model(self):
+        """
+        Retrieves the model number (or part number) of the device
+        Returns:
+            string: Model/part number of device
+        """
+        return 'N/A'
+
+    def get_serial(self):
+        """
+        Retrieves the serial number of the device
+        Returns:
+            string: Serial number of device
+        """
+        return 'N/A'
+
+    def get_target_speed(self):
+        """
+        Retrieves the target (expected) speed of the fan
+
+        Returns:
+            An integer, the percentage of full fan speed, in the range 0 (off)
+                 to 100 (full speed)
+        """
+        if self.is_psu_fan:
+            return super().get_speed()
+        else:
+            return super().get_target_speed()
