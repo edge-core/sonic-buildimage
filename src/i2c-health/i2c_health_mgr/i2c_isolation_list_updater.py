63d41ee8fbc883390c2a399c7310ecbd9e42a76b
dennis_chiu <dennis_chiu@edge-core.com>
2024-10-17 08:58:12 +0800

    JIRA-SONIC-10149: Implement Add/Delete isolation i2c devices into/from the isolation list in STATE_DB
    Change-Id: I809cb8f0527e6060d28744160e0ed0c1631b1478

---
+++ src/i2c-health/i2c_health_mgr/i2c_isolation_list_updater.py
@@ -0,0 +1,31 @@
+from swsscommon.swsscommon import SonicV2Connector
+from i2c_health_mgr import i2c_device_entity
+
+I2C_ISOLATION_LIST_TABLE = 'I2C_ISOLATION_LIST|{}|{}'
+
+class I2CIsolationListUpdater():
+    def __init__(self):
+        self.state_db = SonicV2Connector(use_unix_socket_path=True, host='127.0.0.1')
+        self.state_db.connect(self.state_db.STATE_DB)
+
+    def add_device_into_isolation_list(self, region_id, i2c_device_entity_list):
+        """
+        This function is used to add the i2c device into i2c isolation list in STATE DB
+        """
+        for entity in i2c_device_entity_list:
+            try:
+                table_key = I2C_ISOLATION_LIST_TABLE.format(region_id, entity.get_i2c_address_path())
+                self.state_db.set(self.state_db.STATE_DB, table_key, 'device_name', entity.get_name())
+            except:
+                pass
+
+    def remove_device_from_isolation_list(self, region_id, i2c_device_entity_list):
+        """
+        This function is used to remove the i2c device from i2c isolation list in STATE DB
+        """
+        for entity in i2c_device_entity_list:
+            try:
+                table_key = I2C_ISOLATION_LIST_TABLE.format(region_id, entity.get_i2c_address_path())
+                self.state_db.delete(self.state_db.STATE_DB, table_key)
+            except:
+                pass
