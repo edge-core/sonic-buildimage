cdc8d08c63780d88b682b7001707ba9df5f25d96
brandon_cheng <brandon_cheng@edge-core.com>
2024-10-18 05:55:14 +0000

    JIRA-SONIC-10145: Modify the unit test of I2C bus detection.
    Change-Id: Ia13bd7174b9a78730b292cf98fd8b6ca88ad044f

--- src/i2c-health/tests/test_i2c_bus_checker.py
+++ src/i2c-health/tests/test_i2c_bus_checker.py
@@ -19,56 +19,48 @@ sys.path.insert(0, util_path)
 
 from i2c_bus_checker import I2CBusChecker
 
-
 @pytest.fixture
 def mock_logger():
     return MagicMock()
 
+@pytest.fixture
+def mock_platform_api():
+    return MagicMock()
+
 @patch('i2c_bus_checker.subprocess.run')
-@patch('i2c_bus_checker.I2CPlatformAPI')
-def test_is_bus_lock_no(mock_platform_api, mock_subprocess_run, mock_logger):
-    mock_platform_api.return_value.get_i2c_representative_dev_list.return_value = [
+def test_is_bus_lock_no(mock_subprocess_run, mock_platform_api, mock_logger):
+    mock_platform_api.get_i2c_representative_dev_list.return_value = [
         {"name": "Device1", "bus": "12", "device_addr": "0x48", "register_addr": "0x00"},
         {"name": "Device2", "bus": "14", "device_addr": "0x1F", "register_addr": "0x00"}
     ]
-
     mock_subprocess_run.return_value.returncode = 0
-    i2c_checker = I2CBusChecker(mock_logger)
-    result = i2c_checker.is_bus_lock()
+    i2c_checker = I2CBusChecker(mock_logger, mock_platform_api)
 
+    result = i2c_checker.is_bus_lock()
     assert result is False
-    mock_logger.info.assert_called()
 
 @patch('i2c_bus_checker.subprocess.run')
-@patch('i2c_bus_checker.I2CPlatformAPI')
-def test_is_bus_lock_yes(mock_platform_api, mock_subprocess_run, mock_logger):
-    mock_platform_api.return_value.get_i2c_representative_dev_list.return_value = [
+def test_is_bus_lock_yes(mock_subprocess_run, mock_platform_api, mock_logger):
+    mock_platform_api.get_i2c_representative_dev_list.return_value = [
         {"name": "Device1", "bus": "12", "device_addr": "0x48", "register_addr": "0x00"},
         {"name": "Device2", "bus": "14", "device_addr": "0x1F", "register_addr": "0x00"}
     ]
 
     mock_subprocess_run.return_value.returncode = 1
-    i2c_checker = I2CBusChecker(mock_logger)
+    i2c_checker = I2CBusChecker(mock_logger, mock_platform_api)
     result = i2c_checker.is_bus_lock()
 
     assert result is True
-    mock_logger.warning.assert_called_with('I2C bus lock!')
 
-@patch('i2c_bus_checker.I2CPlatformAPI')
 def test_i2c_representative_list_get(mock_platform_api, mock_logger):
-    mock_platform_api.return_value.get_i2c_representative_dev_list.return_value = [
+    original_dev_list=[
         {"name": "Device1", "bus": "12", "device_addr": "0x48", "register_addr": "0x00"},
         {"name": "Device2", "bus": "14", "device_addr": "0x1F", "register_addr": "0x00"}
     ]
+    mock_platform_api.get_i2c_representative_dev_list.return_value = original_dev_list
 
-    i2c_checker = I2CBusChecker(mock_logger)
-
+    i2c_checker = I2CBusChecker(mock_logger, mock_platform_api)
     result = i2c_checker.i2c_representative_list_get()
 
-    expected_result = [
-        {"name": "Device1", "bus": "12", "device_addr": "0x48", "register_addr": "0x00"},
-        {"name": "Device2", "bus": "14", "device_addr": "0x1F", "register_addr": "0x00"}
-    ]
-
-    assert result == expected_result
+    assert result == original_dev_list
 
