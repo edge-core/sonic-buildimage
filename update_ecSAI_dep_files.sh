50de5dfc63c1e71652888be811db3f441485cfce
rayx_huang <rayx_huang@edge-core.com>
2024-12-12 14:45:54 +0800

    [Platform][AS9817-32D/O] Add EC config.yml and community config.yml
    Note:
    EC config.yaml is port config from diag team and features from
    AS9817-64D/O

--- update_ecSAI_dep_files.sh
+++ update_ecSAI_dep_files.sh
@@ -70,7 +70,7 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/med
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/media_settings_ec.json \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/media_settings.json
 
-## update files for as9817
+## update files for as9817-64
 
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/Accton-AS9817-64D-100G/th5-as9817-64d-64x100G_ec.config.yml \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/Accton-AS9817-64D-100G/th5-as9817-64d-64x100G.config.yml
@@ -91,6 +91,11 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/Acc
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/media_settings_ec.json \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/media_settings.json
 
+## update files for as9817-32
+cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D/th5-as9817-32d-32x800G_ec.config.yml \
+        $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D/th5-as9817-32d-32x800G.config.yml
+cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/Accton-AS9817-32O/th5-as9817-32o-32x800G_ec.config.yml \
+        $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/Accton-AS9817-32O/th5-as9817-32o-32x800G.config.yml
 
 ## rebuild sonic-device-data
 rm -f $SONIC_BASE/sonic-buildimage/target/debs/bullseye/sonic-device-data_1.0-1_all.deb
