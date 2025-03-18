97589fc993b5d1e0099c7c7119c4d53f038771fc
tiger_fu <tiger_fu@accton.com>
2025-03-18 13:37:03 +0800

    [Platform][AS9817-32D/O] Update the driver to support Linux kernel 6.1
    References:
    [sonic-net]
    1. PR#16982: [Edgecore][sonic-platform-modules-accton]Support kernel 6.1 and bookworm
    2. PR#18538: address the issue of the PDDF driver failing to allocate sysfs in kernel 6.1
    
    [edge-core]
    1. PR#310: [Platform][AS4625-54P] Update driver for kernel 6.1

--- platform/broadcom/sonic-platform-modules-accton/as9817-32o/modules/accton_as9817_32_fan.c
+++ platform/broadcom/sonic-platform-modules-accton/as9817-32o/modules/accton_as9817_32_fan.c
@@ -585,8 +585,8 @@ static int as9817_32_fan_probe(struct platform_device *pdev)
     int status = 0;
     struct device *hwmon_dev;
 
-    hwmon_dev = hwmon_device_register_with_info(&pdev->dev, DRVNAME,
-                    NULL, NULL, as9817_32_fan_groups);
+    hwmon_dev = hwmon_device_register_with_groups(&pdev->dev, DRVNAME,
+                    NULL, as9817_32_fan_groups);
     if (IS_ERR(data->hwmon_dev)) {
         status = PTR_ERR(data->hwmon_dev);
         return status;
--- platform/broadcom/sonic-platform-modules-accton/as9817-32o/modules/accton_as9817_32_thermal.c
+++ platform/broadcom/sonic-platform-modules-accton/as9817-32o/modules/accton_as9817_32_thermal.c
@@ -266,8 +266,8 @@ static int as9817_32_thermal_probe(struct platform_device *pdev)
     int status = 0;
     struct device *hwmon_dev;
 
-    hwmon_dev = hwmon_device_register_with_info(&pdev->dev, DRVNAME, 
-                    NULL, NULL, as9817_32_thermal_groups);
+    hwmon_dev = hwmon_device_register_with_groups(&pdev->dev, DRVNAME,
+                    NULL, as9817_32_thermal_groups);
     if (IS_ERR(data->hwmon_dev)) {
         status = PTR_ERR(data->hwmon_dev);
         return status;
