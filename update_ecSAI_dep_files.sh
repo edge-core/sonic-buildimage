89f28828ad9de7193ad669d9010bf4444270569d
vic_lin <vic_lin@edge-core.com>
2025-04-16 15:41:25 +0800

    [Platform][media_settings.json] 202411.0 only support default setting

--- update_ecSAI_dep_files.sh
+++ update_ecSAI_dep_files.sh
@@ -25,8 +25,6 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/Ac
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/Accton-AS9737-32DB-100G/th4-as9737-32x100G.config.yml
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/Accton-AS9737-32DB-100G/hwsku_ec.json \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/Accton-AS9737-32DB-100G/hwsku.json
-cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/media_settings_ec.json \
-         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9737_32db-r0/media_settings.json
         
 
 ## update files for as9736
@@ -45,8 +43,6 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/Acc
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/Accton-AS9736-64D-100G/hwsku.json
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/Accton-AS9736-64D-100G/port_config_ec.ini \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/Accton-AS9736-64D-100G/port_config.ini
-cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/media_settings_ec.json \
-         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9736_64d-r0/media_settings.json
 
 
 ## update files for as9817-64
@@ -57,8 +53,6 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/Acc
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/Accton-AS9817-64D-400G/th5-as9817-64d-64x400G.config.yml
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/Accton-AS9817-64D/th5-as9817-64d-64x800G_ec.config.yml \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/Accton-AS9817-64D/th5-as9817-64d-64x800G.config.yml
-cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/media_settings_ec.json \
-         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64d-r0/media_settings.json
 
 
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/Accton-AS9817-64O-100G/th5-as9817-64o-64x100G_ec.config.yml \
@@ -67,12 +61,8 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/Acc
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/Accton-AS9817-64O-2x400G/th5-as9817-64o-128x400G.config.yml
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/Accton-AS9817-64O/th5-as9817-64o-64x800G_ec.config.yml \
          $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/Accton-AS9817-64O/th5-as9817-64o-64x800G.config.yml
-cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/media_settings_ec.json \
-         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_64o-r0/media_settings.json
 
 ## update files for as9817-32
-cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/media_settings_ec.json \
-         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/media_settings.json
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D/th5-as9817-32d-32x800G_ec.config.yml \
         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D/th5-as9817-32d-32x800G.config.yml
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D-400G/th5-as9817-32d-32x400G_ec.config.yml \
@@ -80,8 +70,6 @@ cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Acc
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D-100G/th5-as9817-32d-32x100G_ec.config.yml \
         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32d-r0/Accton-AS9817-32D-100G/th5-as9817-32d-32x100G.config.yml
 
-cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/media_settings_ec.json \
-         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/media_settings.json
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/Accton-AS9817-32O/th5-as9817-32o-32x800G_ec.config.yml \
         $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/Accton-AS9817-32O/th5-as9817-32o-32x800G.config.yml
 cp -f $SONIC_BASE/sonic-buildimage/device/accton/x86_64-accton_as9817_32o-r0/Accton-AS9817-32O-2x400G/th5-as9817-32o-64x400G_ec.config.yml \
