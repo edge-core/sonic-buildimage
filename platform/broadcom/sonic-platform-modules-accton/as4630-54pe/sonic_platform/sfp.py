7e19d018bcb74424abab3ba3cc0c7870baa6bef7
jostar-yang <jostar_yang@accton.com>
2025-05-23 16:05:57 +0800

    [PDDF] Add get_lpmode() to set lpmode default is disable
    Signed-off-by: jostar-yang <jostar_yang@accton.com>

--- platform/broadcom/sonic-platform-modules-accton/as4630-54pe/sonic_platform/sfp.py
+++ platform/broadcom/sonic-platform-modules-accton/as4630-54pe/sonic_platform/sfp.py
@@ -208,3 +208,38 @@ class Sfp(PddfSfp):
             pass
         return self.__get_error_description()
 
+    def get_lpmode(self):
+
+        lpmode = False
+
+        if self.get_presence()==False:
+            return False
+
+        device = 'PORT{}'.format(self.port_index)
+        output = self.pddf_obj.get_attr_name_output(device, 'xcvr_lpmode')
+
+        if output:
+            status = int(output['status'].rstrip())
+
+            if status == 1:
+                lpmode = True
+            else:
+                lpmode = False
+        else:
+            xcvr_id = self._xcvr_api_factory._get_id()
+
+            if xcvr_id is not None:
+                if xcvr_id == 0x18 or xcvr_id == 0x19 or xcvr_id == 0x1e:
+                    # QSFP-DD or OSFP
+                    # Use common SfpOptoeBase implementation for get_lpmode
+                    lpmode = super().get_lpmode()
+                elif xcvr_id == 0x11 or xcvr_id == 0x0d or xcvr_id == 0x0c:
+                    # QSFP28, QSFP+, QSFP
+                    # get_power_set() is not defined in the optoe_base class
+                    api = self.get_xcvr_api()
+                    power_set = api.get_power_set()
+                    power_override = self.get_power_override()
+                    
+                    return power_set if power_override else False
+
+        return lpmode
