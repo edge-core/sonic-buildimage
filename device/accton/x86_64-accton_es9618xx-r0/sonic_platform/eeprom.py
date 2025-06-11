db84c5958a9d8f8b10fc1fc616fc9885bf97cf72
vadyms <vadyms@sw-vm-vadyms.xsight.ent>
2025-06-11 12:44:58 +0300

    SON-2156: es9618xx: switch to main board EEPROM device
    syseeprom switched from CPU EEPROM to the Main Board EEPROM.
    
    The X2 box has two physical EEPROM chips storing TLV data.
    One EEPROM chip is in the CPU Comex module, and another -
    on the motherboard. The EEPROMs have the same TLV data,
    except the “Manufacture Date“which has the same MM/DD/YY
    but a different time (hour:min:sec).
    
    The EEPROM in the CPU COMe module has an I2C address of 0x50,
    located on the I2C Bus 0. The Mainboard EEPROM has an I2C
    address of 0x55, located on the I2C Bus 1.
    
    For correct operation, syseeprom should use Main Board EEPROM.
    
    Signed-off-by: Vadym Sodolevsky <vadyms@xsightlabs.com>

--- device/accton/x86_64-accton_es9618xx-r0/sonic_platform/eeprom.py
+++ device/accton/x86_64-accton_es9618xx-r0/sonic_platform/eeprom.py
@@ -20,7 +20,7 @@ class Tlv(eeprom_tlvinfo.TlvInfoDecoder):
     EEPROM_DECODE_HEADLINES = 6
 
     def __init__(self):
-        self._eeprom_path = "/sys/bus/i2c/devices/0-0050/eeprom"
+        self._eeprom_path = "/sys/bus/i2c/devices/16-0055/eeprom"
         super(Tlv, self).__init__(self._eeprom_path, 0, '', True)
         self._eeprom = self._load_eeprom()
 
