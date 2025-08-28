2a55787f3bdd1b12bd4969d195157188bc7248ca
Anton Parkhomenko <aparkhomenko@larch-networks.com>
2025-08-28 10:45:39 +0000

    upgrade XSAI version to 1.13.3-1
    Signed-off-by: Anton Parkhomenko <aparkhomenko@larch-networks.com>

--- platform/xsight/xsight-sai.mk
+++ platform/xsight/xsight-sai.mk
@@ -1,11 +1,11 @@
 # XSight SAI
 
-XSIGHT_LIBSAI_VERSION = 0.1
-XSIGHT_LIBSAI_URL_PREFIX = "https://github.com/Xsight-Labs/SONiC/raw/refs/heads/202311-ec/amd64/sai-plugin/202311"
+XSIGHT_LIBSAI_VERSION = 1.13.3-1
+XSIGHT_LIBSAI_URL_PREFIX = "https://github.com/Xsight-Labs/SONiC/raw/refs/heads/main/amd64/sai-plugin/202311"
 
-XSIGHT_LIBSAI = xsai-main_$(XSIGHT_LIBSAI_VERSION)_all.deb
-XSIGHT_LIBSAI_DEV = xsai-main-dev_$(XSIGHT_LIBSAI_VERSION)_all.deb
-XSIGHT_XBM = xbm_$(XSIGHT_LIBSAI_VERSION)_all.deb
+XSIGHT_LIBSAI = xsai-main_$(XSIGHT_LIBSAI_VERSION)_amd64.deb
+XSIGHT_LIBSAI_DEV = xsai-main-dev_$(XSIGHT_LIBSAI_VERSION)_amd64.deb
+XSIGHT_XBM = xbm_$(XSIGHT_LIBSAI_VERSION)_amd64.deb
 
 $(XSIGHT_LIBSAI)_URL = "$(XSIGHT_LIBSAI_URL_PREFIX)/$(XSIGHT_LIBSAI)"
 $(eval $(call add_derived_package,$(XSIGHT_LIBSAI),$(XSIGHT_LIBSAI_DEV)))
