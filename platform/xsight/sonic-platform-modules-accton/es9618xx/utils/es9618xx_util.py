e638c080b685cc868fe6846029d4cdf41dfa6dcd
Yuri Danilov <yurid@xsightlabs.com>
2025-08-31 12:03:45 +0000

    SON-2227: es9618xx: fix error transceiver write_timeout
    - Fixed: write_timeout value enlarged up to maximum
    250ms. Lower timeout values caused to following
    error: optoe XX-0050: Restore page register to 0 failed:-110!

--- platform/xsight/sonic-platform-modules-accton/es9618xx/utils/es9618xx_util.py
+++ platform/xsight/sonic-platform-modules-accton/es9618xx/utils/es9618xx_util.py
@@ -358,11 +358,17 @@ def device_install():
         if ret and FORCE == 0:
             return ret
 
+        path = f"/sys/bus/i2c/devices/{bus}-0050/write_timeout"
+        ret, msg = echo_to_file("250", path)
+        if ret and FORCE == 0:
+            return ret
+
         path = f"/sys/bus/i2c/devices/13-0061/module_lpmode_{count}"
         ret, msg = echo_to_file("0", path)
         if ret and FORCE == 0:
             return ret
     update_vddh_for_r0b()
+
 
 def device_uninstall():
     global FORCE
