ace6f76b5db380ae14b2b066a53eddfa705425ad
PJHsieh <49477291+PJHsieh@users.noreply.github.com>
2025-09-09 00:15:44 +0000

    [AIS800-64]Support BIOS version v51.01.11.01
    AIS800-64D Test Log:
    root@ais800-64d-1:/home/admin# ./accton_as9817_64d_util.py install
    Checking system....
    No driver, installing....
    IPMI dev interface is ready.
    Done driver_install
    No device, installing....
    Done device_install
    Using pcie.yaml.ais800_v51.01.11.01 for BIOS v51.01.11.01
    Apply AIS800 Configuration
    Successfully installed sonic_platform-1.0-py3-none-any.whl package
    root@ais800-64d-1:/home/admin#
    
    AIS800-64O Test Log:
    root@ais800-64o-1:/home/admin# ./accton_as9817_64o_util.py install
    Checking system....
    No driver, installing....
    IPMI dev interface is ready.
    Done driver_install
    No device, installing....
    Done device_install
    Using pcie.yaml.ais800_v51.01.11.01 for BIOS v51.01.11.01
    Apply AIS800 Configuration
    Successfully installed sonic_platform-1.0-py3-none-any.whl package
    root@ais800-64o-1:/home/admin#

--- platform/broadcom/sonic-platform-modules-accton/as9817-64d/utils/accton_as9817_64d_util.py
+++ platform/broadcom/sonic-platform-modules-accton/as9817-64d/utils/accton_as9817_64d_util.py
@@ -52,6 +52,8 @@ FORCE = 0
 #logging.basicConfig(filename= PROJECT_NAME+'.log', filemode='w',level=logging.DEBUG)
 #logging.basicConfig(level=logging.INFO)
 
+BIOS_VERSION_PATH = "/sys/class/dmi/id/bios_version"
+PCIE_YAML_AIS800_64D = "pcie.yaml.ais800"
 
 if DEBUG == True:
     print(sys.argv[0])
@@ -438,24 +440,6 @@ def do_sonic_platform_clean():
 
     return
 
-device_path = f"{PLATFORM_ROOT_PATH}/x86_64-accton_{PROJECT_NAME}-r0"
-apply_ais800_cmds = [
-    f"cp -f {device_path}/platform.json.ais800 {device_path}/platform.json",
-    f"cp -f {device_path}/platform_components.json.ais800 {device_path}/platform_components.json",
-    f"cp -f {device_path}/pcie.yaml.ais800 {device_path}/pcie.yaml",
-]
-apply_as9817_cmds = [
-    f"cp -f {device_path}/platform.json.as9817 {device_path}/platform.json",
-    f"cp -f {device_path}/platform_components.json.as9817 {device_path}/platform_components.json",
-    f"cp -f {device_path}/pcie.yaml.as9817 {device_path}/pcie.yaml",
-]
-apply_cmd_sets = {
-    0x00 : apply_as9817_cmds, # OSFP
-    0x01 : apply_as9817_cmds, # QDD
-    0x02 : apply_ais800_cmds, # OSFP_ROT  ==> AIS800-64O
-    0x03 : apply_ais800_cmds  # QDD_ROT   ==> AIS800-64D
-}
-
 def get_pcb_id():
     id = None
     status, output = getstatusoutput_noshell("i2cget -f -y 0 0x60 0x00".split())
@@ -469,6 +453,33 @@ def get_pcb_id():
     return id
 
 def apply_product_conf():
+    pcie_yaml_ais800 = PCIE_YAML_AIS800_64D
+    cmd = ["cat", BIOS_VERSION_PATH]
+    status, bios_version = getstatusoutput_noshell(cmd)
+    if bios_version == "v51.01.11.01":
+        pcie_yaml_ais800 = pcie_yaml_ais800+"_"+bios_version
+        print(f"Using {pcie_yaml_ais800} for BIOS {bios_version}")
+    else:
+        print(f"Using default {pcie_yaml_ais800}")
+
+    device_path = f"{PLATFORM_ROOT_PATH}/x86_64-accton_{PROJECT_NAME}-r0"
+    apply_ais800_cmds = [
+        f"sed -i 's/\"name\":\ \"AS9817-64D\"/\"name\":\ \"AIS800-64D\"/g' {device_path}/platform.json",
+        f"cp -f {device_path}/platform_components.json.ais800 {device_path}/platform_components.json",
+        f"cp -f {device_path}/{pcie_yaml_ais800} {device_path}/pcie.yaml",
+    ]
+    apply_as9817_cmds = [
+        f"sed -i 's/\"name\":\ \"AIS800-64D\"/\"name\":\ \"AS9817-64D\"/g' {device_path}/platform.json",
+        f"cp -f {device_path}/platform_components.json.as9817 {device_path}/platform_components.json",
+        f"cp -f {device_path}/pcie.yaml.as9817 {device_path}/pcie.yaml",
+    ]
+    apply_cmd_sets = {
+        0x00 : apply_as9817_cmds, # OSFP
+        0x01 : apply_as9817_cmds, # QDD
+        0x02 : apply_ais800_cmds, # OSFP_ROT  ==> AIS800-64O
+        0x03 : apply_ais800_cmds  # QDD_ROT   ==> AIS800-64D
+    }
+
     pcb_id = get_pcb_id()
     if pcb_id is None:
         print("Invalid PCB ID.")
@@ -607,7 +618,7 @@ def restricted_float(x):
         raise argparse.ArgumentTypeError("%r not a floating-point literal" % (x,))
 
     if x < THRESHOLD_RANGE_LOW or x > THRESHOLD_RANGE_HIGH:
-        raise argparse.ArgumentTypeError("%r not in range [%.1f ~ %.1f]" % 
+        raise argparse.ArgumentTypeError("%r not in range [%.1f ~ %.1f]" %
                                          (x, THRESHOLD_RANGE_LOW, THRESHOLD_RANGE_HIGH))
 
     return x
