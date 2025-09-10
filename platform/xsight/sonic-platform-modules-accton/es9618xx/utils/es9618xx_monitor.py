5ca9cf10f255e94cd48fed6f538098a1486bd296
Michael Zaidman <michaelz@xsightlabs.com>
2025-09-10 12:57:42 +0000

    SON-2090: es9618xx: add thermal control shutdown
    - Take temperature thresholds from `thermal_device_data.py` instead
      of `sysfs` for LM sensors, and a hardcoded value for ASIC sensors
    - Copy thermal control power shutdown implementation from:
      device/celestica/x86_64-cel_e1031-r0/thermal_policy.json
    - Remove copied `thermal.over.high_threshold` thermal policy condition
    - Remove unused thermal shutdown logic implemented in both `ChangeMinCoolingLevelAction`
      thermal policy action  and es9618xx_monitor.py
    - Update `switch.power_cycling` thermal policy action to do the following operations:
      - Indicate thermal overheating by setting the `diag` LED to amber
      - Stop `SWSS` Docker container, followed by a reset of the X2 chip
      - Set all transceivers to LPM (Low Power Mode) mode
    - Ignore thermal samples for 'ASIC average' and 'ASIC maximum' when determining
      overheating detection
    - Fix potential bug where the temperature reading from `get_temperature` returns `None`
    - Move ES9618XX's thermal policies from being hardcoded in `thermal_manager.py`
      to being configured via `thermal_policy.json`
    - Add configuration variable for the minimum cooling level to `thermal_policy.json`
    - Fix thermal policy name from "all fan and psu presence" to "all fan presence and good"
    - Removed two unused thermal policies that were copied from
      `platform/mellanox/mlnx-platform-api/sonic_platform/thermal_manager.py` as stated
      in commit `SON-310: Added temperature management to be used in thermalctld`,
      since they are not relevant to the ES9618XX platform.
    - Remove following unused thermal policy conditions:
      - 'fan.all.absence'
      - 'psu.any.absence'
      - 'psu.all.absence'
      - 'psu.all.presence'
    - Remove following unused thermal policy actions:
      - 'fan.all.check_and_set_speed'
      - 'thermal.recover'
    - Remove unused thermal control pause logic implemented in the platform-specific
      function 'pause_thermal_algorithm', as the same functionality can be achieved
      using the 'sonic-mgmt' community test repository command:
      'docker exec pmon supervisorctl stop thermalctld'.
    - Fix bug related to saving the previous temperatures array, originally added
      at line 103 in commit `SON-536: Updating of thermal policy implementation`.
      This bug caused the thermal policy condition `thermal.any.change` to be applied
      in each thermal manager cycle, even when the temperature remained unchanged.
    - Refactor `DEVICE_DATA` dictionary in `thermal_device_data.py`
      - Remove X1 platforms entries
      - Remove platform name hierarchy level
      - Remove 'fans' and 'psus' entries as they are not used
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>
    Signed-off-by: Michael Zaidman <michaelz@xsightlabs.com>

--- platform/xsight/sonic-platform-modules-accton/es9618xx/utils/es9618xx_monitor.py
+++ platform/xsight/sonic-platform-modules-accton/es9618xx/utils/es9618xx_monitor.py
@@ -6,24 +6,41 @@
 
 try:
     import os
-    import os.path
     import sys
     import logging
     import time
     import signal
+    import subprocess
+    import shlex
 except ImportError as e:
     raise ImportError('%s - required module not found' % str(e))
 
 FUNCTION_NAME = 'es9618xx_monitor'
 SHUTDOWN_FILE = '/usr/share/sonic/firmware/pmon_system_shutdown'
-SYSTEM_HALT_ON_OVERHEAT = 1
-SYSTEM_WARN_ON_OVERHEAT = 2
+SHUTDOWN_OVERHEAT_CODE = "overheat"
 
 global logger
 
 def handler(signum, frame):
     logging.info('Cause signal %d.', signum)
     sys.exit(0)
+
+def run_command(cmd):
+    status = True
+    result = ""
+    try:
+        args = shlex.split(cmd)
+        p = subprocess.Popen(
+            args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        raw_data, err = p.communicate()
+        if err == '':
+            result = raw_data.strip()
+        else:
+            status = False
+            result = err.strip()
+    except Exception:
+        status = False
+    return status, result
 
 def main(argv):
     logger = logging.getLogger(FUNCTION_NAME)
@@ -37,20 +54,26 @@ def main(argv):
         if os.path.exists(SHUTDOWN_FILE) and os.path.isfile(SHUTDOWN_FILE):
             try:
                 file = open(SHUTDOWN_FILE)
-                file_content = file.readline().split('-')
-                ext_string = ""
-                if 1 < len(file_content):
-                    ext_string = file_content[1]
-                if SYSTEM_HALT_ON_OVERHEAT == int(file_content[0]):
-                    os.remove(SHUTDOWN_FILE)
-                    os.system("/opt/xplt/utils/es9618x_reset_x2.sh")
-                    logger.warning("System HALT due to high temperature of: {} !".format(ext_string))
-                    os.system("/usr/sbin/shutdown -H now")
-                if SYSTEM_WARN_ON_OVERHEAT == int(file_content[0]):
-                    logger.warning("System warning due to overheat of: {} !".format(ext_string))
-                    os.remove(SHUTDOWN_FILE)
+                file_content = file.readline().strip()
+                if SHUTDOWN_OVERHEAT_CODE == file_content:
+                    sts, res = run_command("systemctl is-active swss")
+                    if sts == True and res == 'active':
+                        # Stop `SWSS` Docker container
+                        cmd = "systemctl stop swss"
+                        logger.info("Stop `SWSS` Docker container")
+                        sts, res = run_command(cmd)
+                        if sts == False:
+                            logger.error("Failed to run command: {}".format(cmd))
+
+                        # Reset X2 chip
+                        cmd = "/opt/xplt/utils/es9618x_reset_x2.sh"
+                        logger.info("Reset X2 chip")
+                        sts, res = run_command(cmd)
+                        if sts == False:
+                            logger.error("Failed to run command: {}".format(cmd))
             except:
                 time.sleep(1)
+            os.remove(SHUTDOWN_FILE)
         else:
             time.sleep(2)
 
