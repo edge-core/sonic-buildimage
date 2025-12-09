0fed1f1bc24f8a80d0815b8aee46ff20fa8cdfc5
richard_kuo <richard_kuo1@accton.com>
2025-12-09 18:36:55 +0800

    JIRA-ONSBUSWI-3479:[AS9736-64d][Fix] Dynamically adjust tolerance to avoid fan fault misdetection on large target speed changes
    1. Add a flag to control the tolerance when thermal policy changes the speed
    2. Change global variable platform_chassis to local variable
    3. Add the Fan function for activate the tolerance

--- platform/broadcom/sonic-platform-modules-accton/as9736-64d/sonic_platform/helper.py
+++ platform/broadcom/sonic-platform-modules-accton/as9736-64d/sonic_platform/helper.py
@@ -11,6 +11,7 @@ from sonic_py_common.general import getstatusoutput_noshell
 from sonic_py_common.general import getstatusoutput_noshell_pipe
 
 HOST_CHK_CMD = ["docker"]
+MACHINE_CONF_FILE = "/host/machine.conf"
 EMPTY_STRING = ""
 
 
@@ -105,6 +106,17 @@ class APIHelper():
         except Exception:
             status = False
         return status, result
+
+    def get_platform(self):
+        platform = None
+
+        with open(MACHINE_CONF_FILE, 'r') as file:
+            for line in file:
+                if 'onie_platform=' in line:
+                    platform = line.strip().split('=')[1]
+                    break
+
+        return platform
 
 
 class FileLock:
