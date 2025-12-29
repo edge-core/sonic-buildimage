1a9581e4b35e7a5c0d06c1ed4d846ae6523ab8c6
aparkhomenko-xsight <112386115+aparkhomenko-xsight@users.noreply.github.com>
2025-12-29 02:44:00 +0200

    upgrade XSAI version to 1.13.3-7 (#667)
    Release notes
    Added support of mirroring configuration with an attached policer
    
    Signed-off-by: Anton Parkhomenko <aparkhomenko@larch-networks.com>

--- platform/xsight/xsight-sai.mk
+++ platform/xsight/xsight-sai.mk
@@ -1,6 +1,6 @@
 # XSight SAI
 
-XSIGHT_LIBSAI_VERSION = 1.13.3-6
+XSIGHT_LIBSAI_VERSION = 1.13.3-7
 XSIGHT_LIBSAI_URL_PREFIX = "https://github.com/Xsight-Labs/SONiC/raw/refs/heads/main/amd64/sai-plugin/202311"
 
 XSIGHT_LIBSAI = xsai-main_$(XSIGHT_LIBSAI_VERSION)_amd64.deb
