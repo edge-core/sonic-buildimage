1b882240386583a8b00230236a20756b5ea6d9f6
junying_yeh <junying_yeh@edge-core.com>
2026-02-06 11:54:08 +0800

    copp/copp_cfg.j2: Change the Broadcom ASIC devices to use dhcp_l2 and dhcpv6_l2 traps.

--- files/image_config/copp/copp-config.sh
+++ files/image_config/copp/copp-config.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sonic-cfggen -d -t /usr/share/sonic/templates/copp_cfg.j2 > /etc/sonic/copp_cfg.json
+asic_type=$(sonic-cfggen -d -y /etc/sonic/sonic_version.yml --var asic_type)
+
+sonic-cfggen -d -a "{\"asic_type\": \"$asic_type\"}" \
+  -t /usr/share/sonic/templates/copp_cfg.j2 > /etc/sonic/copp_cfg.json
