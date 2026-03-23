cd020301029cf4be7c316d4d7ad1d80aa5387655
Asher Oved <ashero@xsightlabs.com>
2026-03-23 15:18:20 +0200

    XSW-10893: as9647-32d: copy xcvr thermal temperature reading support
    Copy support for reading XCVR temperatures via `SfpThermal` class from the following paths:
    - `platform/barefoot/sonic-platform-modules-arista/arista/utils/sonic_platform/thermal.py`
    - `platform/barefoot/sonic-platform-modules-arista/arista/utils/sonic_platform/sfp.py`
    
    This support abstracts XCVR thermals for automatic reading in `thermalctld`.
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>

--- device/accton/x86_64-accton_as9647_32d-r0/sonic_platform/thermal.py
+++ device/accton/x86_64-accton_as9647_32d-r0/sonic_platform/thermal.py
@@ -532,3 +532,98 @@ class Thermal(ThermalBase):
         """
         return (self.index + 1)
 
+
+class SfpThermal(ThermalBase):
+    THRESH_DELAY = 1 # 1 second
+
+    def __init__(self, sfp):
+        ThermalBase.__init__(self)
+        self._sfp = sfp
+        self._minimum = None
+        self._maximum = None
+        self._cachedThreshInfo = None
+        self._cachedThreshInfoTime = 0
+
+    def get_name(self):
+        return "%s %s temp sensor" % (self._sfp.sfp_type, self._sfp.get_id())
+
+    def get_presence(self):
+        return self._sfp.get_presence()
+
+    def get_model(self):
+        return "N/A"
+
+    def get_serial(self):
+        return "N/A"
+
+    def get_status(self):
+        return self.get_temperature() is not None
+
+    def get_position_in_parent(self):
+        return 1
+
+    def is_replaceable(self):
+        return False
+
+    def get_temperature(self):
+        value = self._sfp.get_temperature()
+        if value == 'N/A':
+            raise NotImplementedError
+        if self._minimum is None or self._minimum > value:
+            self._minimum = value
+        if self._maximum is None or self._maximum < value:
+            self._maximum = value
+        return value
+
+    def _get_threshold_info(self):
+        currTime = monotonicRaw()
+        if currTime - self._cachedThreshInfoTime > self.THRESH_DELAY:
+            self._cachedThreshInfoTime = currTime
+            self._cachedThreshInfo = self._sfp.get_transceiver_threshold_info()
+        return self._cachedThreshInfo
+
+    def get_low_threshold(self):
+        threshInfo = self._get_threshold_info()
+        if threshInfo:
+            lowThreshold = threshInfo.get("templowwarning")
+        if not threshInfo or lowThreshold == "N/A":
+            raise NotImplementedError
+        return lowThreshold
+
+    def set_low_threshold(self, temperature):
+        return False
+
+    def get_low_critical_threshold(self):
+        threshInfo = self._get_threshold_info()
+        if threshInfo:
+            lowCritThreshold = threshInfo.get("templowalarm")
+        if not threshInfo or lowCritThreshold == "N/A":
+            raise NotImplementedError
+        return lowCritThreshold
+
+    def get_high_threshold(self):
+        threshInfo = self._get_threshold_info()
+        if threshInfo:
+            highThreshold = threshInfo.get("temphighwarning")
+        if not threshInfo or highThreshold == "N/A":
+            raise NotImplementedError
+        return highThreshold
+
+    def set_high_threshold(self, temperature):
+        return False
+
+    def get_high_critical_threshold(self):
+        threshInfo = self._get_threshold_info()
+        if threshInfo:
+            highCritThreshold = threshInfo.get("temphighalarm")
+        if not threshInfo or highCritThreshold == "N/A":
+            raise NotImplementedError
+        return highCritThreshold
+
+    def get_minimum_recorded(self):
+        self.get_temperature()
+        return self._minimum
+
+    def get_maximum_recorded(self):
+        self.get_temperature()
+        return self._maximum
