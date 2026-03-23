b889fc9f4bb736ca3db35208281a9f80e27c70b1
Asher Oved <ashero@xsightlabs.com>
2026-03-23 17:47:20 +0200

    XSW-10850: as9647-32d: adapt and enable fan watchdog
    Adapt watchdog module to the fan CPLD watchdog.
    Enable fan watchdog to protect the thermal control main loop.
    Remove fan watchdog disable when manually setting fan speed via sysfs.
    Reduce thermal control interval to 30 to improve watchdog
    responsiveness. This change has also been implemented in
    Arista’s Barefoot and Broadcom platforms for interval of 20 seconds.
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>

--- device/accton/x86_64-accton_as9647_32d-r0/sonic_platform/fan_watchdog.py
+++ device/accton/x86_64-accton_as9647_32d-r0/sonic_platform/fan_watchdog.py
@@ -1,54 +1,79 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
+#############################################################################
+#
+# Module contains an implementation of SONiC Platform Base API and
+# provides access to fan CPLD watchdog
+#
+#############################################################################
 
 try:
     from sonic_platform_base.watchdog_base import WatchdogBase
+    from .helper import APIHelper
+    from .thermal import logger
 except ImportError as e:
     raise ImportError("%s - required module not found" % e)
 
+WD_ERROR = -1
+WD_ENABLE = 1
+WD_DISABLE = 0
+WD_MAX_PWM = 15 # Represent 100% PWM
+WD_MAX_TIMEOUT_SEC = 255
+
 class Watchdog(WatchdogBase):
     """
-    Platform-specific watchdog class for interfacing with a hardware watchdog module
+    Platform-specific watchdog class for interfacing with the fan CPLD watchdog
     """
 
-    def __init__(self, watchdog):
-        self._watchdog = watchdog
+    def __init__(self):
+        self._api_helper = APIHelper()
 
-    def get_name(self):
-        return "watchdog"
-
-    def get_model(self):
-        return "N/A"
-
-    def get_presence(self):
+    def _run_i2cset_cmd(self, bus, addr, reg, data):
+        cmd = "i2cset -f -y {} {} {} {}".format(bus, addr, reg, data)
+        sts, res = self._api_helper.run_command(cmd)
+        if not (sts and res == ''):
+            logger.log_error("Failed to run i2cset command: {}".format(cmd))
+            return False
         return True
 
-    def get_serial(self):
-        return "N/A"
-
-    def get_status(self):
-        return True
-
-    def get_position_in_parent(self):
-        return -1
-
-    def is_replaceable(self):
-        return False
+    def _run_i2cget_cmd(self, bus, addr, reg):
+        cmd = "i2cget -f -y {} {} {}".format(bus, addr, reg)
+        sts, res = self._api_helper.run_command(cmd)
+        if not sts:
+            logger.log_error("Failed to run i2cget command: {}".format(cmd))
+            return None
+        return res
 
     def arm(self, seconds):
-        if not self._watchdog.arm(seconds * 100):
-            return -1
-        return seconds
+        ret_sts = WD_ERROR
+        if seconds <= 0:
+            return ret_sts
+        if seconds > WD_MAX_TIMEOUT_SEC:
+            logger.log_error("Invalid timeout ({}), must be ≤{}. Timeout forced to {}.".format(
+                seconds, WD_MAX_TIMEOUT_SEC, WD_MAX_TIMEOUT_SEC))
+            seconds = WD_MAX_TIMEOUT_SEC
+        sts1 = self._run_i2cset_cmd('10', '0x66', '0x32', f'0x{WD_MAX_PWM:02X}') # Set max PWM
+        sts2 = self._run_i2cset_cmd('10', '0x66', '0x33', f'0x{WD_ENABLE:02X}') # Enable WD
+        sts3 = self._run_i2cset_cmd('10', '0x66', '0x31', f'0x{seconds:02X}') # Set timeout
+        if all([sts1, sts2, sts3]):
+            ret_sts = seconds
+        return ret_sts
 
     def disarm(self):
-        return self._watchdog.stop()
-
-    def get_remaining_time(self):
-        remainingTime = self._watchdog.status()['remainingTime']
-        if remainingTime != -1:
-            remainingTime //= 100
-        return remainingTime
+        self._run_i2cset_cmd('10', '0x66', '0x33', f'0x{WD_DISABLE:02X}') # Disable WD
+        return True
 
     def is_armed(self):
-        return self._watchdog.status()['enabled']
+        armed = False
+        try:
+            armed = int(self._run_i2cget_cmd('10', '0x66', '0x33'), 16) == WD_ENABLE
+        except (ValueError, TypeError) as e:
+            logger.log_error("Failed to check if watchdog is armed: {}".format(e))
+        return armed
+
+    def get_remaining_time(self):
+        remaining_time = WD_ERROR
+        if self.is_armed():
+            try:
+                remaining_time = int(self._run_i2cget_cmd('10', '0x66', '0x31'), 16)
+            except (ValueError, TypeError) as e:
+                logger.log_error("Failed to get remaining time: {}".format(e))
+        return remaining_time
--- device/accton/x86_64-accton_as9647_32d-r0/sonic_platform/thermal_manager.py
+++ device/accton/x86_64-accton_as9647_32d-r0/sonic_platform/thermal_manager.py
@@ -1,5 +1,6 @@
 from sonic_platform_base.sonic_thermal_control.thermal_manager_base import ThermalManagerBase
 from .helper import APIHelper
+from sonic_platform.thermal import logger
 from .thermal_actions import *
 from .thermal_conditions import *
 from .thermal_infos import *
@@ -7,7 +8,14 @@ from .thermal_infos import *
 CPLD_I2C_PATH = "/sys/bus/i2c/devices/10-0066/fan"
 FAN_LED_MODE = CPLD_I2C_PATH + "_led_mode"
 
+TC_INTERVAL_SEC = 30
+WD_TIMEOUT = TC_INTERVAL_SEC * 2
+
 class ThermalManager(ThermalManagerBase):
+
+    # Override default thermal control interval
+    _interval = TC_INTERVAL_SEC
+
     @classmethod
     def initialize(cls):
         """
@@ -16,13 +24,14 @@ class ThermalManager(ThermalManagerBase):
         :return:
         """
         cls._api_helper = APIHelper()
-        cls._api_helper.write_txt_file(FAN_LED_MODE, 0) # Disable LED debug mode
 
         # Prevent the "Policy {} already exists" exception when reloading the JSON configuration
         # file via thermalctld's call to `load` in `thermal_manager.py`` by deleting all existing policies.
         # This was done to reload all thermal control configurations during instantiation
         # of `ThermalControlDaemon` in each thermal control test run.
         cls._policy_dict = {}
+
+        cls._api_helper.write_txt_file(FAN_LED_MODE, 0) # Disable LED debug mode
 
     @classmethod
     def init_thermal_algorithm(cls, chassis):
@@ -32,6 +41,13 @@ class ThermalManager(ThermalManagerBase):
         :return:
         """
         cls._chassis = chassis
+
+        # Enable fan watchdog and set its initial timeout to twice the thermal control interval
+        cls.fan_watchdog = chassis.get_fan_watchdog()
+        if cls.fan_watchdog:
+            cls.fan_watchdog.arm(WD_TIMEOUT)
+        else:
+            logger.log_error("Failed to get fan watchdog")
 
         # Align 'sensors' command thresholds with thermal control thresholds
         from .thermal_device_data import DEVICE_DATA
@@ -51,6 +67,19 @@ class ThermalManager(ThermalManagerBase):
         super().init_thermal_algorithm(chassis)
 
     @classmethod
+    def run_policy(cls, chassis):
+        """
+        Collect thermal information, run each policy, if one policy matches, execute the policy's action.
+        :param chassis: The chassis object.
+        :return:
+        """
+        if cls.fan_watchdog:
+            cls.fan_watchdog.arm(WD_TIMEOUT)
+
+        # Call the base implementation
+        super().run_policy(chassis)
+
+    @classmethod
     def deinitialize(cls):
         """
         Destroy thermal manager, including any vendor specific cleanup. The default behavior of this function
@@ -60,5 +89,8 @@ class ThermalManager(ThermalManagerBase):
         if cls._chassis:
             for fan in cls._chassis.get_all_fans():
                 fan.set_speed(100)
+
+        if cls.fan_watchdog:
+            cls.fan_watchdog.disarm()
 
         cls._api_helper.write_txt_file(FAN_LED_MODE, 1) # Enable LED debug mode
