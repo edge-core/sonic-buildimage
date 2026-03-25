be933db5cce5c80b9dd432a99a575f4d1c63be70
Asher Oved <ashero@xsightlabs.com>
2026-03-25 14:24:56 +0200

    XSW-10893: as9647-32d: fix newline error in led_control
    Fix newline errors in led_control.py.
    The issue caused the following error log:
    "The sonic ERR pmon#ledd[36]: Failed to load ledutil: invalid non-printable character U+200B (led_control.py, line 8)"
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>

--- device/accton/x86_64-accton_as9647_32d-r0/plugins/led_control.py
+++ device/accton/x86_64-accton_as9647_32d-r0/plugins/led_control.py
@@ -1,21 +1,20 @@
-
 #!/usr/bin/env python
 #
 # led_control.py
 #
 # Platform-specific LED control functionality for SONiC
 #
-​
+
 try:
     from sonic_led.led_control_base import LedControlBase
 except ImportError as e:
     raise ImportError(str(e) + " - required module not found")
-​
+
 class LedControl(LedControlBase):
     """Platform specific LED control class"""
-​
+
     def __init__(self):
         pass
-​
+
     def port_link_state_change(self, portname, state):
         pass
