d77bd6b83db12a622361222a342cbb14c4b91b11
PJHsieh <49477291+PJHsieh@users.noreply.github.com>
2026-04-27 14:19:16 +0800

    i2c-health: Remove trailing space in message index 025

--- src/i2c-health/i2c_health_mgr/i2c_health_msgs.py
+++ src/i2c-health/i2c_health_mgr/i2c_health_msgs.py
@@ -29,7 +29,7 @@ i2c_health_msg_list = [
     "Failed to execute cmd to start i2c daemon: '{}'.", # 022(ERROR): i2c daemon service. Example "Failed to execute cmd to start i2c daemon: 'sudo systemctl start as9716-32d-platform-monitor-fan.service'."
     "Failed to add {}({}:{}) into the isolation list.", # 023(ERROR): device name, region id, device addr. Example: "Failed to add Ethernet1(77-2-72-1:25-0050) into the isolation list."
     "Failed to remove {}({}:{}) into the isolation list.", # 024(ERROR): device name, region id, device addr. Example: "Failed to remove Ethernet1(77-2-72-1:25-0050) into the isolation list."
-    "Abnormal condition in handling cached_data during the STATE DB restore operation. ", # 025(WARNING)
+    "Abnormal condition in handling cached_data during the STATE DB restore operation.", # 025(WARNING)
     "These devices are unfinished during the restoration: {}.", # 026(NOTICE): Uncleared cached device names, Example: "These devices are unfinished during the restoration: ['Ethernet24', 'Ethernet144', 'Ethernet64, ...]."
     "After reset mux, the I2C bus lock is still detected", # 027(ERROR)
     "After reset I2C master, the I2C bus lock is still detected", # 028(ERROR)
