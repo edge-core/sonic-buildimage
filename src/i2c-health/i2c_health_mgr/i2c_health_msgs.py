5c50d6db6aae8312d274af554e286c4b5880cd27
ecsonic <ecsonic@edge-core.com>
2026-04-27 05:56:41 +0000

    JIRA-SONIC-14159: Correct i2c-health message indexes

--- src/i2c-health/i2c_health_mgr/i2c_health_msgs.py
+++ src/i2c-health/i2c_health_mgr/i2c_health_msgs.py
@@ -30,9 +30,9 @@ i2c_health_msg_list = [
     "Failed to add {}({}:{}) into the isolation list.", # 023(ERROR): device name, region id, device addr. Example: "Failed to add Ethernet1(77-2-72-1:25-0050) into the isolation list."
     "Failed to remove {}({}:{}) into the isolation list.", # 024(ERROR): device name, region id, device addr. Example: "Failed to remove Ethernet1(77-2-72-1:25-0050) into the isolation list."
     "Abnormal condition in handling cached_data during the STATE DB restore operation. ", # 025(WARNING)
-    "These devices are unfinished during the restoration: {}.", # 025(NOTICE): Uncleared cached device names, Example: "These devices are unfinished during the restoration: ['Ethernet24', 'Ethernet144', 'Ethernet64, ...]."
-    "After reset mux, the I2C bus lock is still detected", # 026(ERROR)
-    "After reset I2C master, the I2C bus lock is still detected", # 027(ERROR)
+    "These devices are unfinished during the restoration: {}.", # 026(NOTICE): Uncleared cached device names, Example: "These devices are unfinished during the restoration: ['Ethernet24', 'Ethernet144', 'Ethernet64, ...]."
+    "After reset mux, the I2C bus lock is still detected", # 027(ERROR)
+    "After reset I2C master, the I2C bus lock is still detected", # 028(ERROR)
 ]
 
 def get_i2c_health_msg(msg_idx):
