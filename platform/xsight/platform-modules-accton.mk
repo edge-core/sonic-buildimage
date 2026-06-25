xsightlabs/xsight-sonic-buildimage

SON-2266: as9647-32d: Add platform support
- Added initial support for the AS9647-32D platform.
- The platform is based on the X2 ASIC.
- Supports SFP112, SFP56, and SFP28 transceiver modules.

Signed-off-by: Michael Zaidman <michaelz@xsightlabs.com>

platform/xsight/platform-modules-accton.mk
@@ -2,10 +2,12 @@
 ACCTON_ES9632XX_PLATFORM_MODULE_VERSION = 1.1
 ACCTON_ES9632XQ_PLATFORM_MODULE_VERSION = 1.1
 ACCTON_ES9618XX_PLATFORM_MODULE_VERSION = 1.1
+ACCTON_AS9647_32D_PLATFORM_MODULE_VERSION = 1.1
 
 export ACCTON_ES9632XX_PLATFORM_MODULE_VERSION
 export ACCTON_ES9632XQ_PLATFORM_MODULE_VERSION
 export ACCTON_ES9618XX_PLATFORM_MODULE_VERSION
+export ACCTON_AS9647_32D_PLATFORM_MODULE_VERSION
 
 ACCTON_ES9632XX_PLATFORM_MODULE = sonic-platform-accton-es9632xx_$(ACCTON_ES9632XX_PLATFORM_MODULE_VERSION)_amd64.deb
 $(ACCTON_ES9632XX_PLATFORM_MODULE)_SRC_PATH = $(PLATFORM_PATH)/sonic-platform-modules-accton
@@ -20,4 +22,8 @@ $(eval $(call add_extra_package,$(ACCTON_ES9632XX_PLATFORM_MODULE),$(ACCTON_ES96
 ACCTON_ES9618XX_PLATFORM_MODULE = sonic-platform-accton-es9618xx_$(ACCTON_ES9618XX_PLATFORM_MODULE_VERSION)_amd64.deb
 $(ACCTON_ES9618XX_PLATFORM_MODULE)_PLATFORM = x86_64-accton_es9618xx-r0
 $(eval $(call add_extra_package,$(ACCTON_ES9632XX_PLATFORM_MODULE),$(ACCTON_ES9618XX_PLATFORM_MODULE)))
+
+ACCTON_AS9647_32D_PLATFORM_MODULE = sonic-platform-accton-as9647-32d_$(ACCTON_AS9647_32D_PLATFORM_MODULE_VERSION)_amd64.deb
+$(ACCTON_AS9647_32D_PLATFORM_MODULE)_PLATFORM = x86_64-accton_as9647_32d-r0
+$(eval $(call add_extra_package,$(ACCTON_ES9632XX_PLATFORM_MODULE),$(ACCTON_AS9647_32D_PLATFORM_MODULE)))
 

edge-core/sonic-buildimage

platform/xsight/platform-modules-accton.mk
@@ -1,7 +1,9 @@
 # Accton Platform modules
 ACCTON_ES9618XX_PLATFORM_MODULE_VERSION = 1.1
+ACCTON_AS9647_32D_PLATFORM_MODULE_VERSION = 1.1
 
 export ACCTON_ES9618XX_PLATFORM_MODULE_VERSION
+export ACCTON_AS9647_32D_PLATFORM_MODULE_VERSION
 
 ACCTON_ES9618XX_PLATFORM_MODULE = sonic-platform-accton-es9618xx_$(ACCTON_ES9618XX_PLATFORM_MODULE_VERSION)_amd64.deb
 $(ACCTON_ES9618XX_PLATFORM_MODULE)_SRC_PATH = $(PLATFORM_PATH)/sonic-platform-modules-accton
@@ -9,4 +11,8 @@ $(ACCTON_ES9618XX_PLATFORM_MODULE)_PLATFORM = x86_64-accton_es9618xx-r0
 $(ACCTON_ES9618XX_PLATFORM_MODULE)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_DPKG_DEBS += $(ACCTON_ES9618XX_PLATFORM_MODULE) 
 
+
+ACCTON_AS9647_32D_PLATFORM_MODULE = sonic-platform-accton-as9647-32d_$(ACCTON_AS9647_32D_PLATFORM_MODULE_VERSION)_amd64.deb
+$(ACCTON_AS9647_32D_PLATFORM_MODULE)_PLATFORM = x86_64-accton_as9647_32d-r0
+$(eval $(call add_extra_package,$(ACCTON_ES9618XX_PLATFORM_MODULE),$(ACCTON_AS9647_32D_PLATFORM_MODULE)))
 